5469764dc23de560e6fdec8564f74cb5f29c2bb1
wisdompeak <guan.huifeng@gmail.com>
2017-03-19 00:51:35 -0700

    Update leetcode-407.cpp

--- 407-Trapping-Rain-Water-II/leetcode-407.cpp
+++ 407-Trapping-Rain-Water-II/leetcode-407.cpp
@@ -1,71 +1,81 @@
 class Solution {
-private:
-    int M;
-    int N;
 public:
+    struct cmp
+    {
+        bool operator()(pair<int,int>a,pair<int,int>b)
+        {
+            return (a.first>b.first);
+        }
+    };
+
     int trapRainWater(vector<vector<int>>& heightMap) 
     {
-        M=heightMap.size();
+        int M=heightMap.size();
         if (M==0) return 0;
-        N=heightMap[0].size();
+        int N=heightMap[0].size();
         
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>q;
+        auto dir=vector<pair<int,int>>{{-1,0},{1,0},{0,1},{0,-1}};
         auto visited=vector<vector<int>>(M,vector<int>(N,0));
-        for (int i=0; i<M; i++)
-          for (int j=0; j<N; j++)
-          {
-              if (i==0 || i==M-1 || j==0 || j==N-1)
-              {
-                 q.push({heightMap[i][j],i*N+j});
-                 visited[i][j]=1;
-              }
-          }
-        vector<pair<int,int>>dir{{-1,0},{1,0},{0,-1},{0,1}};
         
-        int result=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>q;
+        for (int i=0; i<M; i++)
+        {
+            q.push({heightMap[i][0],i*N+0});
+            q.push({heightMap[i][N-1],i*N+N-1});
+            visited[i][0]=1;
+            visited[i][N-1]=1;
+        }
+        for (int j=1; j<N-1; j++)
+        {
+            q.push({heightMap[0][j],0*N+j});
+            q.push({heightMap[M-1][j],(M-1)*N+j});
+            visited[0][j]=1;
+            visited[M-1][j]=1;
+        }
+        
+        int sum=0;
+        
         while (!q.empty())
         {
-            int i=q.top().second/N;
-            int j=q.top().second%N;
-            int seaLevel=q.top().first;
+            int level = q.top().first;
+            int i=q.top().second /N;
+            int j=q.top().second %N;
             q.pop();
             
-            queue<pair<int,int>>p;
-            p.push({i,j});
-
-            while (!p.empty())
+            queue<pair<int,int>>flood;
+            flood.push({i,j});
+            
+            while (!flood.empty())
             {
-                int m=p.front().first;
-                int n=p.front().second;
-                p.pop();
+                int m=flood.front().first;
+                int n=flood.front().second;
+                flood.pop();
                 
                 for (int k=0; k<4; k++)
                 {
-                    int x=m+dir[k].first;
-                    int y=n+dir[k].second;
+                    int mm = m+dir[k].first;
+                    int nn = n+dir[k].second;
                     
-                    if (x<0 || x>M-1 || y<0 || y>N-1) continue;
+                    if (mm<0 || mm==M || nn<0 || nn==N) continue;
+                    if (visited[mm][nn]==1) continue;
                     
-                    if (visited[x][y]==1) continue;
-                    
-                    if (heightMap[x][y]<seaLevel)
+                    if (heightMap[mm][nn]>=level)
                     {
-                        result+=seaLevel-heightMap[x][y];
-                        p.push({x,y});
+                        q.push({heightMap[mm][nn],mm*N+nn});
+                        visited[mm][nn]=1;
                     }
                     else
                     {
-                        q.push({heightMap[x][y],x*N+y});
+                        flood.push({mm,nn});
+                        sum+=level-heightMap[mm][nn];
+                        visited[mm][nn]=1;
                     }
-                    
-                    visited[x][y]=1;
-                    
                 }
             }
             
         }
         
-        return result;
+        return sum;
         
     }
 };
