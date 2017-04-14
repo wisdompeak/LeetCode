603c99b9a6275f932b91963d156da879cb4a2526
wisdompeak <guan.huifeng@gmail.com>
2017-04-14 16:55:10 -0500

    Update 275.H-Index II.cpp

--- 275.H-Index-II/275.H-Index II.cpp
+++ 275.H-Index-II/275.H-Index II.cpp
@@ -2,30 +2,27 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) 
     {
-        if (citations.size()==0) return 0;
-        
         int left=0;
-        int right=citations.size()-1;
+        int N=citations.size();
+        int right=N-1;
         int mid;
-        int N=citations.size()-1;
+        
+        if (N==0) return 0;
         
         while (left<right)
         {
-            mid = left+(right-left)/2;
-            if (citations[mid]==N-mid+1)
+            int mid= left+(right-left)/2;
+            
+            if (N-mid==citations[mid])
                 return citations[mid];
-            else if (citations[mid]>N-mid+1)
+            else if (N-mid<citations[mid])
                 right = mid;
-            else if (citations[mid]<N-mid+1)
-                left = mid+1;
+            else
+                left= mid+1;
         }
         
-        cout<<left<<endl;
+        return min(N-left,citations[left]);
         
-        if (citations[left] >= N-left+1)
-            return N-left+1;
-        if (citations[left] < N-left+1)
-            return citations[left];
-           
+        
     }
 };
