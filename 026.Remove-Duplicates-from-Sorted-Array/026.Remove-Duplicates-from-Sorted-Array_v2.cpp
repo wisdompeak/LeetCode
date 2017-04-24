877e4a95a143cd524d3e636b6e3cbaac13c5ffd2
wisdompeak <guan.huifeng@gmail.com>
2017-04-24 15:45:44 -0500

    Create 026.Remove-Duplicates-from-Sorted-Array_v2.cpp

---
+++ 026.Remove-Duplicates-from-Sorted-Array/026.Remove-Duplicates-from-Sorted-Array_v2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+        if (nums.size()==0) return 0;
+        
+        int i=0;
+        for (int j=0; j<nums.size(); j++)
+        {
+            if (j==0)
+                continue;
+            else if (nums[j]==nums[j-1])
+                continue;
+            else
+            {
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        
+        return i+1;
+        
+    }
+};
