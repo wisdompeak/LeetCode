113a24b34d449625202086ff9271b750d0c8a0bd
wisdompeak <guan.huifeng@gmail.com>
2017-05-02 15:49:36 -0500

    Update 483.Smallest Good Base.cpp

--- 483.Smallest-Good-Base/483.Smallest Good Base.cpp
+++ 483.Smallest-Good-Base/483.Smallest Good Base.cpp
@@ -2,41 +2,41 @@ class Solution {
 public:
     string smallestGoodBase(string n) 
     {
-        long long N = 0;
+        long long N=0;
         for (int i=0; i<n.size(); i++)
-            N = N*10 + n[i]-'0';
-
-        if (N==1) return "1";
+            N=N*10+n[i]-'0';
         
-        long long M = log(N+1)/log(2)+1;
+        if (N==1) return "2";
         
-        for (long long m =M; m>=2; m--)
+        long long m_max = log(N+1)/log(2)+1;
+        
+        for (long long m=m_max; m>=2; m--)
         {
-            long long K = pow(N,1.0/(m-1))+1;
-            
             long long left = 2;
-            long long right = K;
+            long long right = pow(N,1.0/(m-1));
             long long mid;
-            long long k;
             
-            while (left<right)
-            {
-
-                mid = left+(right-left)/2;
-                k = mid;
-
-                long long sum = 1;
-                for (int i=1; i<=m-1; i++)
-                {
-                    sum = sum*k+1;
-                }
+            // cout<<"m="<<m<<" "<<left<<" "<<right<<" "<<endl;
                 
-                if (sum == N) return to_string(k);
-                if (sum < N) 
+            while (left<=right)  // 两个边界都需要探查
+            {
+                
+                mid = left+(right-left)/2;
+                long long k=mid;
+                long long sum=1;
+                for (int i=1; i<m; i++)
+                {
+                    sum=sum*k+1;
+                }
+                if (sum==N)
+                    return to_string(k);
+                else if (sum<N)
                     left = mid+1;
                 else
-                    right = mid;
-                
+                    right = mid-1;  // 注意，因为mid已经排除，所以right的更新值不应该包括mid本身，否则死循环。
+                    
+                //cout<<sum<<" "<<k<<" "<<m<<" "<<endl;
+                    
             }
         }
         
