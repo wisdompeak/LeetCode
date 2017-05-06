a111a1720ed0efe2f3810075ea44dd2953e83363
wisdompeak <guan.huifeng@gmail.com>
2017-05-06 17:16:54 -0500

    Create 99.Recover Binary Search Tree.cpp

---
+++ 099.Recover-Binary-Search-Tree/99.Recover Binary Search Tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    TreeNode* CurMax = new TreeNode(INT_MIN);
+    TreeNode* first = NULL;
+    TreeNode* second = NULL;
+
+public:
+    void recoverTree(TreeNode* root) 
+    {
+       DFS(root);
+       
+       int temp = first->val;
+       first->val = second->val;
+       second->val = temp;
+    }
+    
+    void DFS(TreeNode* node)
+    {
+        if (node==NULL) return;
+        
+        DFS(node->left);
+        
+        if (node->val>CurMax->val)
+            CurMax = node;
+        else
+        {
+            if (first==NULL) 
+            {
+                first=CurMax;
+                second=node;
+                CurMax=node;
+            }
+            else
+                second=node;
+        }
+        
+        DFS(node->right);
+            
+    }
+    
+};
