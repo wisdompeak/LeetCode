4fdbbbc77808ee36b3c09f0e4dd99bcdd22b44ee
wisdompeak <guan.huifeng@gmail.com>
2017-05-09 21:09:47 -0500

    Update 133.Clone Graph_DFS.cpp

--- 133.Clone-Graph/133.Clone Graph_DFS.cpp
+++ 133.Clone-Graph/133.Clone Graph_DFS.cpp
@@ -7,32 +7,26 @@
  * };
  */
 class Solution {
-private:
     unordered_map<UndirectedGraphNode*,UndirectedGraphNode*>Map;
-    
 public:
     UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) 
     {
         if (node==NULL) return NULL;
         
-        UndirectedGraphNode *root;
-        
-        if (Map.find(node)!=Map.end())
-            return Map[node];
-        else
+        UndirectedGraphNode* root=new UndirectedGraphNode(node->label);
+        Map[node]=root;
+        for (int i=0; i<node->neighbors.size(); i++)
         {
-            root = new UndirectedGraphNode(node->label);
-            Map[node] = root;
-            
-            for (int i=0; i<node->neighbors.size(); i++)
+            if (Map.find(node->neighbors[i])==Map.end())
             {
                 root->neighbors.push_back(cloneGraph(node->neighbors[i]));
             }
-            
-            return root;
+            else
+            {
+                root->neighbors.push_back(Map[node->neighbors[i]]);
+            }
         }
         
-            
+        return root;
     }
-    
 };
