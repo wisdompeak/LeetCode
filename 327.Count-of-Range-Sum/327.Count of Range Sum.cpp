0731a00d5f9e2ac355253b694f34a355b282a4fb
wisdompeak <guan.huifeng@gmail.com>
2017-05-19 00:09:11 -0500

    Create 327.Count of Range Sum.cpp

---
+++ 327.Count-of-Range-Sum/327.Count of Range Sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    int count=0;
+    int Lower;
+    int Upper;
+    static bool cmp1(long a, long b)
+    {
+        return a<b;
+    }
+    static bool cmp2(long a, long b)
+    {
+        return a<=b;
+    }    
+public:
+    long countRangeSum(vector<int>& nums, int lower, int upper) 
+    {
+        Lower=lower;
+        Upper=upper;
+        nums.insert(nums.begin(),0);
+        vector<long>sums(nums.size(),0);
+        for (int i=1; i<sums.size(); i++)
+            sums[i] = sums[i-1]+nums[i];
+        
+        DivideConque(sums,0,sums.size()-1);
+        return count;
+    }
+    
+    void DivideConque(vector<long>&sums, int start, int end)
+    {
+        if (start==end) return;
+        
+        int mid=start+(end-start)/2;
+        DivideConque(sums,start,mid);
+        DivideConque(sums,mid+1,end);
+        
+        for (int i=start; i<=mid; i++)
+        {
+            auto pos1 = lower_bound(sums.begin()+mid+1,sums.begin()+end+1,sums[i]+Lower,cmp1);
+            auto pos2 = lower_bound(sums.begin()+mid+1,sums.begin()+end+1,sums[i]+Upper,cmp2);   
+            count+=pos2-pos1;
+            
+            if (start==0 && end==3 && i==1)
+             cout<<pos1-(sums.begin()+mid+1)<<" "<<pos2-(sums.begin()+mid+1)<<endl;
+        }
+        
+        sort(sums.begin()+start,sums.begin()+end+1);
+    }
+};
