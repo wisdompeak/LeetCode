f5e47f1776d47d2615b9be286c9fbb89466731b1
wisdompeak <guan.huifeng@gmail.com>
2017-06-17 17:04:04 -0700

    Create 532.K-diff-Pairs-in-an-Array_s2.cpp

---
+++ A.Two_Pointers/532.K-diff-Pairs-in-an-Array/532.K-diff-Pairs-in-an-Array_s2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) 
+    {
+        sort(nums.begin(),nums.end());
+        
+        int i=0;
+        int j=0;
+        int count=0;
+        
+        while (j<nums.size())
+        {
+            if (nums[i]+k==nums[j] && i<j)
+            {
+                count++;
+                while (nums[i]==nums[i+1])
+                    i++;
+                i++;    
+            }
+            
+            if (nums[i]+k>=nums[j])
+                j++;
+            else
+                i++;
+        }
+
+        return count;
+        
+    }
+};
