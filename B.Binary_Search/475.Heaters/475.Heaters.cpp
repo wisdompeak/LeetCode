215073f8218862471ab49399d8cbdb736ee125c5
wisdompeak <guan.huifeng@gmail.com>
2017-06-18 17:31:42 -0700

    Update 475.Heaters.cpp

--- B.Binary_Search/475.Heaters/475.Heaters.cpp
+++ B.Binary_Search/475.Heaters/475.Heaters.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
-    static bool cmp1(int a, int b)
-    {
-        return (a<b);
-    }
-
     int findRadius(vector<int>& houses, vector<int>& heaters) 
     {
-        if (houses.size()==0) return 0;
-        if (heaters.size()==0) return INT_MAX;
-        
-        sort(houses.begin(),houses.end());
         sort(heaters.begin(),heaters.end());
-        int result = INT_MIN;
+        int result=0;
         
         for (int i=0; i<houses.size(); i++)
         {
-            int pos = houses[i];
-            auto it1 = lower_bound(heaters.begin(),heaters.end(),pos,cmp1);
-            auto it2 = it1-1;
-            int temp;
+            int radius;
             
-            if (it1 == heaters.end())
-                temp = houses[i]-heaters.back();
-            else if (it1 == heaters.begin())
-                temp = heaters.front()-houses[i];
+            auto pos= lower_bound(heaters.begin(),heaters.end(),houses[i]);
+            
+            if (pos==heaters.begin())
+                radius = heaters[0]-houses[i];
+            else if (pos==heaters.end())
+                radius = houses[i]-heaters.back();
             else
-                temp = min (abs(houses[i]-*it1),abs(houses[i]-*it2));
+                radius = min(*pos-houses[i], houses[i]-*(pos-1));
                 
-            //if (i==1) cout<<abs(houses[i]-*it2)<<endl;
-            
-            result = max(result,temp);
-            
+            result = max(result,radius);
         }
         
         return result;
-
     }
 };
