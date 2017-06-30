400abd58637609141f2703dc969bbd9a59907306
wisdompeak <guan.huifeng@gmail.com>
2017-06-30 00:56:32 -0500

    Update 331.Verify Preorder Serialization of a Binary Tree.cpp

--- Tree/331.Verify-Preorder-Serialization-of-a-Binary-Tree/331.Verify Preorder Serialization of a Binary Tree.cpp
+++ Tree/331.Verify-Preorder-Serialization-of-a-Binary-Tree/331.Verify Preorder Serialization of a Binary Tree.cpp
@@ -4,25 +4,24 @@ public:
     {
 
         int count=0;
-        for (int i=0; i<preorder.size()-1; i++)
+        for (int i=0; i<preorder.size(); i++)
         {
             if (preorder[i]!=',' && preorder[i]!='#')
             {
                 while (i<preorder.size() && preorder[i]!=',')
                     i++;
-                count++;
+                count--;
             }
             else if (preorder[i]=='#')
             {
-                count--;
+                count++;
                 i++;
             }
             
-            if (count<0) return false;
+            if (i<preorder.size()-1 && count>0) return false;
         }
         
-        if (count!=0) return false;
-        if (preorder.back()!='#') return false;
+        if (count!=1) return false;
         return true;
     }
 };
