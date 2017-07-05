f42aebdcf7e11af8477d7ac67aebbd47c766ef64
wisdompeak <guan.huifeng@gmail.com>
2017-07-04 19:02:49 -0500

    Update 159. Longest Substring with At Most Two Distinct Characters.cpp

--- Two_Pointers/159.Longest-Substring-with-At-Most-Two-Distinct-Characters/159. Longest Substring with At Most Two Distinct Characters.cpp
+++ Two_Pointers/159.Longest-Substring-with-At-Most-Two-Distinct-Characters/159. Longest Substring with At Most Two Distinct Characters.cpp
@@ -2,48 +2,39 @@ class Solution {
 public:
     int lengthOfLongestSubstringTwoDistinct(string s) 
     {
-        vector<int>Map(128,0);
+        vector<int>Map(256,0);
         int count=0;
-        
-        int left=0;
-        int right=0;
         int result=0;
+        int i=0;
+        int j=0;
         
-        while (right<s.size())
+        for (int j=0; j<s.size(); j++)
         {
-            
-            if (Map[s[right]]==0)
+            if (Map[s[j]]>0)
             {
-                Map[s[right]]=1;
+                Map[s[j]]++;
+            }                
+            else if (Map[s[j]]==0 && count<2)
+            {
+                Map[s[j]]++;
                 count++;
             }
-            else
+            else if (Map[s[j]]==0 && count==2)
             {
-                Map[s[right]]++;
-            }
-            
-            //cout<<left<<" "<<right<<" "<<count<<endl;
-            
-            if (count<=2)
-            {
-                result = max(result,right-left+1);
-                right++;
-            }
-            else
-            {
-                while (left<right && count>2)
+                while (count==2)
                 {
-                    Map[s[left]]--;
-                    if (Map[s[left]]==0)
+                    Map[s[i]]--;
+                    if (Map[s[i]]==0)
                         count--;
-                    left++;
-                }
-                
-                right++;
+                    i++;
+                }                    
+                Map[s[j]]++;
+                count++;
             }
+            
+            result=max(result,j-i+1);            
         }
         
         return result;
-        
     }
 };
