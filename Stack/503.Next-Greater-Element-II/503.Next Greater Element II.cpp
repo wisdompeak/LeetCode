8d0d23b85e2e06cba66b72c83e8a83bc7b8ec623
wisdompeak <guan.huifeng@gmail.com>
2017-07-05 20:27:46 -0500

    Create 503.Next Greater Element II.cpp

---
+++ Stack/503.Next-Greater-Element-II/503.Next Greater Element II.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) 
+    {
+        int N=nums.size();
+        stack<pair<int,int>>Stack;
+        vector<int>results(N,-1);
+        
+        for (int j=0; j<N*2; j++)
+        {
+            int i=j;
+            if (i>=N) i=i-N;
+            
+            if (Stack.empty() || Stack.top().first>nums[i])
+                Stack.push({nums[i],i});
+            else
+            {
+                while (!Stack.empty() && Stack.top().first<nums[i])
+                {
+                    if (results[Stack.top().second]==-1)
+                        results[Stack.top().second]=nums[i];
+                    Stack.pop();
+                }
+                Stack.push({nums[i],i});
+            }
+        }
+        
+        return results;
+    }
+};
