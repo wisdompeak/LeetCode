4c9486455d3b0b5aec6a17b2adfd92f4d78aff7f
wisdompeak <guan.huifeng@gmail.com>
2017-08-14 21:06:06 -0500

    Update 644.Maximum-Average-Subarray-II.cpp

--- Dynamic_Programming/644.Maximum-Average-Subarray-II/644.Maximum-Average-Subarray-II.cpp
+++ Dynamic_Programming/644.Maximum-Average-Subarray-II/644.Maximum-Average-Subarray-II.cpp
@@ -2,35 +2,35 @@ class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) 
     {
-        vector<int>CumSum(nums.size(),0);
-        CumSum[0]=nums[0];
-        for (int i=1; i<nums.size(); i++)
-            CumSum[i]=CumSum[i-1]+nums[i];
-                
-        vector<int>dp(nums.size(),0);  // record the length of the optimal subarray that ends in nums[i]
-        dp[k-1]=k;
-        double maxAvg=CumSum[k-1]*1.0/k;;
-    
-        for (int i=k; i<nums.size(); i++)
+        
+        vector<int>cumSum(nums.size()+1);
+        cumSum[0]=0;
+        for (int i=1; i<=nums.size(); i++)
+            cumSum[i]=cumSum[i-1]+nums[i-1];
+
+        vector<int>dp(nums.size()+1);
+        dp[k]=1;               
+        double result=(cumSum[k]-cumSum[0])*1.0/k;
+        
+        for (int i=k+1; i<=nums.size(); i++)
         {
-            double avg = -INT_MIN;            
-            for (int j=i-dp[i-1]; j<=i-k+1; j++)
-            {
-                double tempSum=CumSum[i]-CumSum[j-1];
-                int div = i-j+1;
-                double tempAvg = tempSum/div;
-                
-                if (tempAvg>avg)
-                {
-                    avg=tempAvg;
-                    dp[i]=i-j+1;
-                }
-            }
+            double maxAvg=INT_MIN;
             
-            maxAvg = max(maxAvg, (CumSum[i]-CumSum[i-dp[i]])*1.0/dp[i]);
+            for (int j=dp[i-1]; j<=i-k+1; j++)
+            {
+                double avg= (cumSum[i]-cumSum[j-1])*1.0/(i-j+1);
+                if (avg>maxAvg)
+                {
+                    maxAvg=avg;
+                    dp[i]=j;
+                }
+                if (avg>result)
+                    result=avg;
+                
+                // cout<<avg<<endl;
+            }
         }
         
-        return maxAvg;
-        
+        return result;
     }
 };
