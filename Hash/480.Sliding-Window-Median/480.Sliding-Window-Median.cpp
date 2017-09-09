17bc4a2c9956a476a7701f3e2bd27c0711512ae7
wisdompeak <guan.huifeng@gmail.com>
2017-09-09 15:58:58 -0500

    Update 480.Sliding-Window-Median.cpp

--- Hash/480.Sliding-Window-Median/480.Sliding-Window-Median.cpp
+++ Hash/480.Sliding-Window-Median/480.Sliding-Window-Median.cpp
@@ -2,29 +2,23 @@ class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) 
     {
-        multiset<int>Set(nums.begin(),nums.begin()+k-1);                
-        multiset<int>::iterator mid;
-        multiset<int>::iterator mid2;
+        multiset<double>Set(nums.begin(),nums.begin()+k);
+        multiset<double>::iterator mid=next(Set.begin(),k/2);
         
         vector<double>results;
             
-        for (int i=k-1; i<nums.size(); i++)
-        {
+        for (int i=k; i<=nums.size(); i++)
+        {            
+            results.push_back((*mid+*prev(mid,1-k%2))/2.0);
+            if (i==nums.size()) return results;
+                
             Set.insert(nums[i]);
-            
-            if (k%2==1)
-            {
-                mid=next(Set.begin(),k/2);
-                results.push_back(*mid);
-            }
-            else
-            {
-                mid=next(Set.begin(),k/2);
-                mid2=prev(mid,1);
-                results.push_back((*mid*1.0+*mid2*1.0)/2.0);
-            }                
-            
-            Set.erase(Set.lower_bound(nums[i-k+1]));
+                
+            if (nums[i]<*mid) mid--;                                
+                
+            if (nums[i-k]<=*mid) mid++;
+                
+            Set.erase(Set.lower_bound(nums[i-k]));            
         }
         
         return results;
