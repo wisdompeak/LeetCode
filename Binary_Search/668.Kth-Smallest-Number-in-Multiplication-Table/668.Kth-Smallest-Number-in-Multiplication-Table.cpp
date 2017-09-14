aae1b289b1198a360e05bb32e6eb1c1ec8ad01f7
wisdompeak <guan.huifeng@gmail.com>
2017-09-14 00:16:59 -0500

    Create 668.Kth-Smallest-Number-in-Multiplication-Table.cpp

---
+++ Binary_Search/668.Kth-Smallest-Number-in-Multiplication-Table/668.Kth-Smallest-Number-in-Multiplication-Table.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findKthNumber(int m, int n, int k) 
+    {
+        int left=1;
+        int right=m*n;
+        int mid;
+        
+        while (left<right)
+        {
+            mid=left+(right-left)/2;
+            
+            int count=0;
+            for (int i=1; i<=m; i++)
+                count+= min(mid/i,n);
+            if (count<k)
+                left=mid+1;
+            else
+                right=mid;
+        }
+        
+        return left;
+    }
+};
