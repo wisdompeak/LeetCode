76f3439a8074e3302c97e67796c5e3444f1312e6
wisdompeak <guan.huifeng@gmail.com>
2017-09-26 22:52:33 -0500

    Update 450.Delete-Node-in-a-BST_s2.cpp

--- Tree/450.Delete-Node-in-a-BST/450.Delete-Node-in-a-BST_s2.cpp
+++ Tree/450.Delete-Node-in-a-BST/450.Delete-Node-in-a-BST_s2.cpp
@@ -14,28 +14,28 @@ public:
         if (root==NULL) return NULL;
         
         if (root->val<key)
-		{
-			root->right = deleteNode(root->right,key);
-			return root;
-		}
-		else if (root->val>key)
-		{
-			root->left = deleteNode(root->left,key);
-			return root;
-		}
-		else if (root->val==key)
-		{
-            if (root->right==NULL)
-                return root->left;				
-			else
-            {
-                TreeNode* node=root->right;
-                while (node->left!=NULL)
-                    node = node->left;
-                root->val=node->val;
-                root->right = deleteNode(root->right,node->val);
-                return root;
-            }
-		}
+	{
+		root->right = deleteNode(root->right,key);
+		return root;
+	}
+	else if (root->val>key)
+	{
+		root->left = deleteNode(root->left,key);
+		return root;
+	}
+	else if (root->val==key)
+	{
+           	if (root->right==NULL)
+                	return root->left;				
+		else
+            	{
+                	TreeNode* node=root->right;
+                	while (node->left!=NULL)
+                    		node = node->left;
+                	root->val=node->val;
+                	root->right = deleteNode(root->right,node->val);
+                	return root;
+            	}
+	}
     }
 };
