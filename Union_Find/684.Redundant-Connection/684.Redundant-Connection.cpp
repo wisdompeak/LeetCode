ee42a30fb666bd0eabb6089c1b2040c7c2b108da
wisdompeak <guan.huifeng@gmail.com>
2017-10-21 01:22:41 -0700

    Update 684.Redundant-Connection.cpp

--- Union_Find/684.Redundant-Connection/684.Redundant-Connection.cpp
+++ Union_Find/684.Redundant-Connection/684.Redundant-Connection.cpp
@@ -1,52 +1,40 @@
 class Solution {
     unordered_map<int,int>Father;
 public:
-    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) 
-    {
-        vector<int>candA,candB;
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) 
+    {        
+        for (int i=0; i<edges.size(); i++)
+        {
+            Father[edges[i][0]]=edges[i][0];
+            Father[edges[i][1]]=edges[i][1];
+        }
         
         for (int i=0; i<edges.size(); i++)
         {
-            int u=edges[i][0];
-            int v=edges[i][1];
-            if (Father.find(v)==Father.end())
-                Father[v]=u;
+            if (FindSet(edges[i][0])==FindSet(edges[i][1]))
+                return edges[i];
             else
-            {
-                candA={Father[v],v};
-                candB=edges[i];
-                break;
-            }
+                Union(edges[i][0],edges[i][1]);
         }
-        
-        for (int i=1; i<=edges.size(); i++)        
-            Father[i]=i;    
-        
-        for (int i=0; i<edges.size(); i++)
-        {
-            if (edges[i]==candB) continue;
-            
-            int u=edges[i][0];
-            int v=edges[i][1];
-            
-            if (v==findSet(u))
-            {
-                if (candA.size()==0)
-                    return edges[i];
-                else
-                    return candA;
-            }
-            else
-                Father[v]=u;                
-        }
-        
-        return candB;        
+        return edges[0];
     }
     
-	int	findSet(int x)
-	{
-		if (x!=Father[x])
-			Father[x]=findSet(Father[x]);
-		return Father[x];
-	}    
+    int FindSet(int x)
+    {
+        if (x!=Father[x])
+        {
+            Father[x]=FindSet(Father[x]);
+        }
+        return Father[x];
+    }
+    
+    void Union(int x, int y)
+    {
+        x=FindSet(x);
+        y=FindSet(y);
+        if (x<=y)
+            Father[y]=x;
+        else
+            Father[x]=y;
+    }
 };
