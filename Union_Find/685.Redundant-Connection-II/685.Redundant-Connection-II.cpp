08487c71515079e584310b096c87dbc9f8868bf7
wisdompeak <guan.huifeng@gmail.com>
2017-10-21 01:21:16 -0700

    Update 685.Redundant-Connection-II.cpp

--- Union_Find/685.Redundant-Connection-II/685.Redundant-Connection-II.cpp
+++ Union_Find/685.Redundant-Connection-II/685.Redundant-Connection-II.cpp
@@ -1,55 +1,52 @@
 class Solution {
-	unordered_map<int,int>Parent;
+    unordered_map<int,int>Father;
 public:
     vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) 
-	{
+    {
         vector<int>candA,candB;
-		
-		for (int i=0; i<edges.size(); i++)
-		{
-			int u=edges[i][0];
-			int v=edges[i][1];
-			
-			if (Parent.find(v)!=Parent.end())
-			{
-				candA={Parent[v],v};
-				candB=edges[i];
-				edges[i][1]=0;
+        
+        for (int i=0; i<edges.size(); i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            if (Father.find(v)==Father.end())
+                Father[v]=u;
+            else
+            {
+                candA={Father[v],v};
+                candB=edges[i];
                 break;
-			}
-			else
-				Parent[v]=u;
-		}
-
-		int n=edges.size();
-		for (int i=1; i<=n; i++)
-			Parent[i]=i;
-
-		for (int i=0; i<edges.size(); i++)
-		{
-			if (edges[i][1]==0) continue;
-
-			int u=edges[i][0];
-			int v=edges[i][1];
-			int pu=findRoot(u);
-			if (pu==v)
-			{
-				if (candA.size()==0)
-					return edges[i];
-				else
-					return candA;
-			}
-			Parent[v]=u;
-		}
-		return candB;	
-
+            }
+        }
+        
+        for (int i=1; i<=edges.size(); i++)        
+            Father[i]=i;    
+        
+        for (int i=0; i<edges.size(); i++)
+        {
+            if (edges[i]==candB) continue;
+            
+            int u=edges[i][0];
+            int v=edges[i][1];
+            
+            if (v==findSet(u))
+            {
+                if (candA.size()==0)
+                    return edges[i];
+                else
+                    return candA;
+            }
+            else
+                Father[v]=u;                
+        }
+        
+        return candB;        
     }
-
-	int	findRoot(int x)
+    
+	int	findSet(int x)
 	{
-		if (x!=Parent[x])
-			Parent[x]=findRoot(Parent[x]);
-		return Parent[x];
-	}
-
+		if (x!=Father[x])
+			Father[x]=findSet(Father[x]);
+		return Father[x];
+	}    
 };
