32a67cabe216e5ff00cef13653381ce7b1fc67bb
wisdompeak <guan.huifeng@gmail.com>
2017-10-24 11:27:56 -0500

    Update 218.The-Skyline-Problem.cpp

--- Hash/218.The-Skyline-Problem/218.The-Skyline-Problem.cpp
+++ Hash/218.The-Skyline-Problem/218.The-Skyline-Problem.cpp
@@ -1,11 +1,9 @@
 class Solution {
-
 public:
     vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) 
     {
-        vector<pair<int,int>>edges;
-        
-        for (int i=0; i<buildings.size(); i++)
+        vector<vector<int>>edges;
+        for (int i=0;i<buildings.size(); i++)
         {
             edges.push_back({buildings[i][0],-buildings[i][2]});
             edges.push_back({buildings[i][1],buildings[i][2]});
@@ -13,21 +11,21 @@ public:
         
         sort(edges.begin(),edges.end());
         
+        multiset<int>Set={0};        
         vector<pair<int, int>>results;
-        multiset<int>Set;
-        Set.insert(0);
-        int cur = 0;
-            
+        int cur=0;
+        
         for (int i=0; i<edges.size(); i++)
         {
-            if (edges[i].second<0)            
-                Set.insert(-edges[i].second);
+            if (edges[i][1]<0) 
+                Set.insert(-edges[i][1]);
             else
-                Set.erase(Set.find(edges[i].second));
-                                                
-            if (cur != *prev(Set.end(),1))                           
-                results.push_back({edges[i].first,*Set.rbegin()});                
-            cur = *Set.rbegin();           
+                Set.erase(Set.lower_bound(edges[i][1]));
+            
+            int H=*Set.rbegin();
+            if (cur!=H)
+                results.push_back({edges[i][0],H});
+            cur=H;
         }
         
         return results;
