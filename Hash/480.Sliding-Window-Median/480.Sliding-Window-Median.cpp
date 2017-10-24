708ada8612bb09cbcc7c5f4615734b6446e7ef1f
wisdompeak <guan.huifeng@gmail.com>
2017-10-23 21:45:19 -0700

    Update 480.Sliding-Window-Median.cpp

--- Hash/480.Sliding-Window-Median/480.Sliding-Window-Median.cpp
+++ Hash/480.Sliding-Window-Median/480.Sliding-Window-Median.cpp
@@ -2,25 +2,60 @@ class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) 
     {
-        multiset<double>Set(nums.begin(),nums.begin()+k);
-        multiset<double>::iterator mid=next(Set.begin(),k/2);
+        multiset<int>Set;
+        for (int i=0; i<k; i++)        
+            Set.insert(nums[i]);
+        
+        auto pos=Set.begin();
+        for (int i=0; i<k/2; i++)
+            pos=next(pos,1);
         
         vector<double>results;
-            
+        
         for (int i=k; i<=nums.size(); i++)
-        {            
-            results.push_back((*mid+*prev(mid,1-k%2))/2.0);
-            if (i==nums.size()) return results;
-                
+        {
+            if (k%2==1)
+                results.push_back(*pos);
+            else
+                results.push_back(*pos/2.0+*prev(pos,1)/2.0);
+            
+            if (i==nums.size()) break;
+            
             Set.insert(nums[i]);
+            
+            if (k%2==1)   // OXO
+            {
+                if (nums[i]>=*pos)
+                    pos++;
+                                
+                // OOXO
+                if (nums[i-k]>*pos)
+                    pos--;
+                else if (nums[i-k]<*pos)
+                    ;
+                else if (nums[i-k]==*pos && *prev(pos,1)!=*pos)  
+                    // 如果X左边是个重复元素，则无需改动；如果X左边不是重复元素，则X会被删除，指针需左移
+                    pos--;                
+            }
+            else    // OOXO
+            {
+                if (nums[i]<*pos)
+                    pos--;
                 
-            if (nums[i]<*mid) mid--;                                
-                
-            if (nums[i-k]<=*mid) mid++;
-                
-            Set.erase(Set.lower_bound(nums[i-k]));            
+                // OOXOO
+                if (nums[i-k]<*pos)
+                    pos++;
+                else if (nums[i-k]>*pos)
+                    ;
+                else if (nums[i-k]==*pos)
+                    // 如果X左边是个重复元素，则会删除那个O；如果X左边不是重复元素，则会删除X。两种情况下指针都需右移
+                    pos++;
+            }
+            
+            Set.erase(Set.lower_bound(nums[i-k]));     
         }
         
         return results;
+        
     }
 };
