22f06169852724a8dd12963f0e850cb7db8f7e93
wisdompeak <guan.huifeng@gmail.com>
2017-10-27 02:05:29 -0500

    Create 699.Falling-Squares-v2.cpp

---
+++ Hash/699.Falling-Squares/699.Falling-Squares-v2.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> fallingSquares(vector<pair<int, int>>& positions) 
+    {
+        map<int,int>Map;
+        Map[0]=0;
+        Map[INT_MAX]=0;
+        int cur=0;
+        vector<int>results;
+        
+        for (int i=0; i<positions.size(); i++)
+        {
+            int left=positions[i].first;            
+            int len=positions[i].second;
+            int right=left+len-1;
+            
+            auto pos1 = Map.lower_bound(left);
+            
+            int Hmax=0;
+            auto pos=pos1;
+            if (pos->first!=left) pos=prev(pos,1);
+            while (pos->first <= right)            
+            {
+                Hmax = max(Hmax, pos->second);
+                pos = next(pos,1);
+            }
+            int rightHeight = prev(pos,1)->second;
+            
+            Map.erase(pos1,pos);
+            Map[left]=Hmax+len;
+            Map[right+1]=rightHeight;                
+            
+            cur = max(cur, Hmax+len);
+            results.push_back(cur);
+        }
+        
+        return results;
+    }
+};
