dbebea4c58317a9a728cbd2d40ed4422671c4164
wisdompeak <guan.huifeng@gmail.com>
2017-11-12 23:09:15 -0800

    Update 723.Candy-Crush.cpp

--- Others/723.Candy-Crush/723.Candy-Crush.cpp
+++ Others/723.Candy-Crush/723.Candy-Crush.cpp
@@ -14,28 +14,38 @@ public:
         for (int i=0; i<M; i++)
          for (int j=0; j<N; j++)
          {
-             if (!canStart(board,visited,i,j)) continue;             
+             if (board[i][j]==0 || visited[i][j]==1) continue;
              
-             for (int k=0; k<4; k++)
+             int x1=i;
+             int x2=i;
+             while (x1-1>=0 && board[x1-1][j]==board[i][j])
+                 x1--;
+             while (x2+1<M && board[x2+1][j]==board[i][j])
+                 x2++;
+             if (x2-x1+1>=3)
              {
-                 vector<pair<int,int>>path;                                     
-                 int x=i;
-                 int y=j;                 
-                 while (x>=0 && x<M && y>=0 && y<N && board[x][y]==board[i][j])
-                 {                     
-                     path.push_back({x,y});                     
-                     x+=dir[k].first;
-                     y+=dir[k].second;                     
-                 }
-                 if (path.size()>=3)
+                 flag=1;
+                 for (int ii=x1; ii<=x2; ii++)
                  {
-                     flag=1;
-                     for (auto a:path)
-                     {
-                         newboard[a.first][a.second]=0;
-                         visited[a.first][a.second]=1;
-                     }                         
-                 }                   
+                     visited[ii][j]=1;
+                     newboard[ii][j]=0;
+                 }
+             }
+             
+             int y1=j;
+             int y2=j;
+             while (y1-1>=0 && board[i][y1-1]==board[i][j])
+                 y1--;
+             while (y2+1<N && board[i][y2+1]==board[i][j])
+                 y2++;
+             if (y2-y1+1>=3)
+             {
+                 flag=1;
+                 for (int jj=y1; jj<=y2; jj++)
+                 {
+                     visited[i][jj]=1;
+                     newboard[i][jj]=0;
+                 }
              }             
          }
                         
@@ -70,25 +80,6 @@ public:
         if (flag==0)
             return board;
         else
-            return candyCrush(newboard);
-        
-    }
-    
-    bool canStart(vector<vector<int>>& board,vector<vector<int>>& visited, int i, int j)
-    {
-        if (board[i][j]==0) return false;
-        if (visited[i][j]==0) return true;
-        vector<pair<int,int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
-        
-        for (int k=0; k<4; k++)
-        {
-            int x=i+dir[k].first;
-            int y=j+dir[k].second;                        
-            
-            if (x>=0 && x<M && y>=0 && y<N && board[x][y]==board[i][j] && visited[x][y]==0)
-                return true;        
-        }
-        return false;
-    }
-    
+            return candyCrush(newboard);        
+    }        
 };
