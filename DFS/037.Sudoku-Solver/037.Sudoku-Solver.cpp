82b10fe90a8e766606eaa45f8bf6b042f8db2526
wisdompeak <guan.huifeng@gmail.com>
2017-11-16 06:36:36 -0800

    Create 037.Sudoku-Solver.cpp

---
+++ DFS/037.Sudoku-Solver/037.Sudoku-Solver.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) 
+    {
+       if (board.size()!=9 || board[0].size()!=9) return;
+       bool temp = DFS(board, 0, 0);
+    }
+    
+    bool DFS(vector<vector<char>>& board, int i, int j)
+    {
+        if (i==9) return true;
+        if (j==9) return DFS(board, i+1, 0);
+        if (board[i][j]!='.') return DFS(board, i, j+1);
+        
+        for (int k='1'; k<='9'; k++)
+        {
+            board[i][j]=k;
+            if (isValid(board,i,j) && DFS(board, i, j+1))
+                return true;            
+        }
+        board[i][j]='.';
+        return false;
+    }
+    
+    bool isValid(vector<vector<char>>& board, int i, int j)
+    {
+        for (int row=0; row<9; row++)
+        {
+            if (row!=i && board[row][j]==board[i][j])
+                return false;
+        }
+        for (int col=0; col<9; col++)
+        {
+            if (col!=j && board[i][col]==board[i][j])
+                return false;
+        }
+        int m=i/3*3;
+        int n=j/3*3;
+        for (int p=m; p<m+3; p++)
+         for (int q=n; q<n+3; q++)
+         {
+             if ((p!=i||q!=j)&&board[p][q]==board[i][j])
+                 return false;
+         }
+        return true;
+    }
+};
