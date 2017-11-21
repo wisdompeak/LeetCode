18e0b7b05d7276cb9d9cb72f3b65caba72cce10d
wisdompeak <guan.huifeng@gmail.com>
2017-11-21 11:26:16 -0800

    Update 698.Partition-to-K-Equal-Sum-Subsets.cpp

--- DFS/698.Partition-to-K-Equal-Sum-Subsets/698.Partition-to-K-Equal-Sum-Subsets.cpp
+++ DFS/698.Partition-to-K-Equal-Sum-Subsets/698.Partition-to-K-Equal-Sum-Subsets.cpp
@@ -1,50 +1,41 @@
 class Solution {
 public:
-    bool canPartitionKSubsets(vector<int>& nums, int k) 
+    bool canPartitionKSubsets(vector<int>& nums, int k)
     {
+        if (k==0) return false;
         if (k==1) return true;
-        if (k>nums.size()) return false;
-        
-        sort(nums.begin(),nums.end());
-        
         int SUM=0;
         for (int i=0; i<nums.size(); i++)
             SUM+=nums[i];
-        
         if (SUM%k!=0) return false;
-        
-        int sum = SUM/k;
-        int N = nums.size();
-        
-        vector<int>subsetSum(k,0);
-        vector<int>visited(N,0);
-        
-        return DFS(nums, subsetSum, visited, sum, k, 0, N-1);
+        int sum=SUM/k;
+
+        vector<int>sets(k,0);
+        sort(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.end());
+        vector<int>visited(nums.size(),0);
+        return DFS(nums,sets,0,visited,sum,0);
     }
-    
-    bool DFS(vector<int>& nums, vector<int>& subsetSum, vector<int>& visited, int sum, int k, int curSet, int lastNum)
+
+    bool DFS(vector<int>&nums, vector<int>&sets, int curSet, vector<int>&visited, int sum, int curNum)
     {
-        if (curSet==k) return true;
-        
-        if (subsetSum[curSet] == sum)
-            return DFS(nums,subsetSum, visited, sum, k, curSet+1, nums.size()-1);
-                
-        for (int i=lastNum; i>=0; i--)
+        if (curSet==sets.size()) return true;
+        if (sets[curSet]==sum) return DFS(nums,sets,curSet+1,visited,sum,0);
+
+        for (int i=curNum; i<nums.size(); i++)
         {
-            if (visited[i]==1) continue;
-            
-            if (subsetSum[curSet]+nums[i]<=sum)
-            {
-                subsetSum[curSet]+=nums[i];
-                visited[i]=1;
-                
-                if (DFS(nums,subsetSum,visited,sum,k,curSet, i-1))
-                    return true;
-                
-                subsetSum[curSet]-=nums[i];
-                visited[i]=0;
-            }
-        }        
+          if (visited[i]==1) continue;
+
+          if (sets[curSet]+nums[i]<=sum)
+          {
+             visited[i]=1;
+             sets[curSet]+=nums[i];
+             if (DFS(nums,sets,curSet,visited,sum,i+1)) return true;
+             sets[curSet]-=nums[i];
+             visited[i]=0;
+          }
+        }
+
         return false;
     }
 };
