c3b1db2f44d0dba949d512594f3e82d140b26319
wisdompeak <guan.huifeng@gmail.com>
2017-11-25 03:51:46 -0800

    Update 491.Increasing Subsequences.cpp

--- DFS/491.Increasing-Subsequences/491.Increasing Subsequences.cpp
+++ DFS/491.Increasing-Subsequences/491.Increasing Subsequences.cpp
@@ -1,30 +1,28 @@
 class Solution {
-    vector<vector<int>>results;
-    
 public:
     vector<vector<int>> findSubsequences(vector<int>& nums) 
     {
+        vector<vector<int>>results;
         vector<int>curRes;
-        DFS(nums,0,curRes);
+        DFS(nums,0,curRes,results);
         return results;
     }
     
-    void DFS(vector<int>& nums, int start, vector<int>curRes)
+    void DFS(vector<int>& nums, int idx, vector<int>&curRes, vector<vector<int>>&results)
     {
-        if (curRes.size()>1) results.push_back(curRes);
-        
-        if (start>=nums.size()) return;
+        if (curRes.size()>1) results.push_back(curRes);                
+        if (idx==nums.size()) return;                
 
-        unordered_set<int>visited;        
-        for (int i=start; i<nums.size(); i++)
-        {
-            if (start>0 && nums[i]<curRes.back()) continue;
-            if (visited.find(nums[i])!=visited.end()) continue;
-            visited.insert(nums[i]);
+        unordered_set<int>Set;
+        for (int i=idx; i<nums.size(); i++)
+        {            
+            if (curRes.size()!=0 && nums[i]<curRes.back() ) continue;
+            if (Set.find(nums[i])!=Set.end()) continue;            
+            Set.insert(nums[i]);
             
             curRes.push_back(nums[i]);
-            DFS(nums,i+1,curRes);
-            curRes.pop_back();
+            DFS(nums,i+1,curRes,results);
+            curRes.pop_back();                        
         }
     }
 };
