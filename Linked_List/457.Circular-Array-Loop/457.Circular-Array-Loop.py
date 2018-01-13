0303afff6348a86b726f5e4c7024de28085e0db5
wisdompeak <guan.huifeng@gmail.com>
2018-01-13 08:24:26 +0000

    Update 457.Circular-Array-Loop.py

--- Linked_List/457.Circular-Array-Loop/457.Circular-Array-Loop.py
+++ Linked_List/457.Circular-Array-Loop/457.Circular-Array-Loop.py
@@ -4,34 +4,33 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        def getNext(pos,nums,dir):
-            if (dir*nums[pos]<0): return -1
-            x = pos+nums[pos]
-            if (x>=len(nums)): x-=len(nums)
-            elif (x<0): x+=len(nums)
-            if (x==pos): return -1
-            else: return x
-                
-        def getNextTwo(pos,nums,dir):
-            x = getNext(pos,nums,dir)                  
-            if (x==-1 or dir*nums[x]<0): return -1
-            x = getNext(x,nums,dir)
-            return x
+        def getNext(i,nums,direction):
+            if ((nums[i]>0)*2-1)!=direction: return -1                            
+            j=i+nums[i];
+            j=(j+len(nums))%len(nums)
+            if (j==i): return -1
+            return i
+        
+        def getNext2(i,nums,direction):            
+            j=getNext(i,nums,direction)
+            if (j==-1): return -1;
+            k=j+nums[j];
+            k=(k+len(nums))%len(nums)
+            if (k==i): return -1
+            return k
         
         for i in range(len(nums)):
             if (nums[i]==0): continue;
-            fast, slow = i,i
-            if (nums[i]>0): dir = 1
-            else: dir = -1;
-            while (getNextTwo(fast,nums,dir)!=-1 and getNext(slow,nums,dir)!=-1):
-                fast,slow = getNextTwo(fast,nums,dir),getNext(slow,nums,dir)
-                if (fast==slow): break
-            if (getNext(slow,nums,dir)!=-1 and getNextTwo(slow,nums,dir)!=-1):
-                return True
-            slow = i
-            while (getNext(slow,nums,dir)!=-1): 
+            fast,slow = i,i
+            direction = (nums[i]>0)*2-1;
+            while(getNext(slow,nums,direction)!=-1 and getNext2(fast,nums,direction)!=-1):
+                slow,fast = getNext(slow,nums,direction),getNext2(fast,nums,direction)
+                if (fast==slow): break;
+            if (getNext(slow,nums,direction)!=-1 and getNext2(fast,nums,direction)!=-1):
+                return True            
+            slow = i;
+            while (getNext(slow,nums,direction)!=-1):
                 nums[slow]=0
-                slow = getNext(slow,nums,dir)                 
+                slow=getNext(slow,nums,direction)                
         return False
-            
                 
