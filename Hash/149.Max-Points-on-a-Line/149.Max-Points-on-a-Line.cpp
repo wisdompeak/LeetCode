35e05c8c621ad1515df3e7874a230854979b8779
wisdompeak <guan.huifeng@gmail.com>
2018-02-02 00:02:42 +0000

    Update 149.Max-Points-on-a-Line.cpp

--- Hash/149.Max-Points-on-a-Line/149.Max-Points-on-a-Line.cpp
+++ Hash/149.Max-Points-on-a-Line/149.Max-Points-on-a-Line.cpp
@@ -11,60 +11,46 @@ class Solution {
 public:
     int maxPoints(vector<Point>& points) 
     {
-        int result = 0;
+        int result=0;
         
         for (int i=0; i<points.size(); i++)
         {
-            map <pair<int,int>,int> Map;
-
-            int countSame = 0;
-            int countVertical = 0;
+            unordered_map <int,int> Map;
+            int countSame=0;
             
             for (int j=0; j<points.size(); j++)
             {
                 if (j==i) continue;
                 
-                if (points[j].x==points[i].x && points[j].y==points[i].y )
+                if (points[i].x==points[j].x && points[i].y==points[j].y )
                 {
                     countSame++;
                     continue;
                 }
                 
-                if (points[j].x==points[i].x)
-                {
-                    countVertical++;
-                    continue;
-                }                
-                
-                int flag = 1;
-                
+                int flag=0;
                 for (auto a:Map)
                 {
-                    Point p(a.first.first,a.first.second);
-                    
+                    Point p=points[a.first];
                     if ( (long long)(p.y-points[i].y)*(points[j].x-points[i].x) == (long long)(points[j].y-points[i].y)*(p.x-points[i].x) )
                     {
-                        Map[{p.x,p.y}]++;
-                        flag = 0;
+                        flag=1;
+                        Map[a.first]++;
                         break;
                     }
                 }
-                if (flag == 1)
-                    Map[{points[j].x,points[j].y}]=1;
+                
+                if (flag==0)                
+                    Map[j]=1;                               
             }
             
             int temp=0;
             for (auto a:Map)
-            {
-                temp = max(temp, a.second);
-            }
-
-            temp = max(temp,countVertical);
-
-            result = max(result,temp+countSame+1);
-            
+                temp = max(temp,a.second);
+            temp+=(countSame+1);
+            result=max(result,temp);
         }
         
-        return result;    
+        return result;
     }
 };
