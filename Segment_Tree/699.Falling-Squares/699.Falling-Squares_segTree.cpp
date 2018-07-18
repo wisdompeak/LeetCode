3f22f1f3dd29c67bc1cc9466577877fc517c3b00
wisdompeak <guan.huifeng@gmail.com>
2018-07-18 13:50:37 -0700

    Update 699.Falling-Squares_segTree.cpp

--- Segment_Tree/699.Falling-Squares/699.Falling-Squares_segTree.cpp
+++ Segment_Tree/699.Falling-Squares/699.Falling-Squares_segTree.cpp
@@ -2,15 +2,14 @@ class Solution {
     class SegTree
     {
         public:
-        int begin, end;
+        int begin, end, status;
         SegTree* left;
         SegTree* right;
-        int tracked;
         SegTree(int a, int b, int T)
         {
             begin = a;
             end = b;            
-            tracked = T;
+            status = T;
             left = NULL;
             right = NULL;
         }                
@@ -24,47 +23,39 @@ class Solution {
             node = NULL;
         }
         
-        int setTracking(int a, int b, int tracking)
+        int setStatus(int a, int b, int s)
         {
-            if (a<=begin && end<=b)
+            if (begin>=b || end<=a)            // 1. [a,b]与这个区间不相交，返回原先的状态
+                return status;                        
+            if (a<=begin && end<=b)            // 2. [a,b]包括了整个区间，将该区间抹平
             {
                 remove(left);
                 remove(right);
-                return tracked = tracking;                
-            }
-            int mid = (end-begin)/2+begin;
-            if (!left)
+                return status = s;
+            }         
+            if (!left)                         // 3. [a,b]与该区间相交，需考虑其子树
             {
-                left = new SegTree(begin,mid,tracked);
-                right = new SegTree(mid,end,tracked);
-            }
-            int leftTracked, rightTracked;
-            if (a<mid)            
-                leftTracked = left->setTracking(a,b,tracking);
-            else
-                leftTracked = left->tracked;
-            if (b>mid)
-                rightTracked = right->setTracking(a,b,tracking);
-            else
-                rightTracked = right->tracked;
-            return tracked = max(leftTracked, rightTracked);            
+                int mid = (end-begin)/2+begin;
+                left = new SegTree(begin,mid,status);
+                right = new SegTree(mid,end,status);
+            }            
+            int leftStatus = left->setStatus(a,b,s);
+            int rightStatus = right->setStatus(a,b,s);
+            return status = max(leftStatus,rightStatus);
         }
-        
-        int getTracking(int a, int b)
-        {   
-            if (!left && !right) return tracked;
-            if (a<=begin && end<=b) return tracked;            
-            int mid = (end-begin)/2+begin;
-            int leftTracked, rightTracked;
-            if (a<mid)
-                leftTracked = left->getTracking(a,b);
-            else
-                leftTracked = 0;
-            if (b>mid)
-                rightTracked = right->getTracking(a,b);
-            else
-                rightTracked = 0;
-            return max(leftTracked, rightTracked);                
+                
+        int getStatus(int a, int b)
+        {
+            if (begin>=b || end<=a)            // 1. [a,b]与这个区间不相交，返回一个不影响结果的状态
+                return 0;            
+            if (a<=begin && end<=b)            // 2. [a,b]包括了整个区间，返回该区间的状态
+                return status;                        
+            if (!left)                         // 3. [a,b]与该区间相交，但又没有子树，返回整个区间状态
+                return status;
+            int mid = (end-begin)/2+begin;      // 4. [a,b]与该区间相交，需要考虑其子树
+            int leftStatus = left->getStatus(a,b);
+            int rightStatus = right->getStatus(a,b);
+            return max(leftStatus,rightStatus);            
         }
         
     };
@@ -78,8 +69,8 @@ public:
         for (int i=0; i<positions.size(); i++)
         {
             int maxH;
-            maxH = root.getTracking(positions[i].first,positions[i].first+positions[i].second);
-            root.setTracking(positions[i].first,positions[i].first+positions[i].second, positions[i].second+maxH);
+            maxH = root.getStatus(positions[i].first,positions[i].first+positions[i].second);
+            root.setStatus(positions[i].first,positions[i].first+positions[i].second, positions[i].second+maxH);
             curMaxH = max(curMaxH, positions[i].second+maxH);
             results.push_back(curMaxH);
         }
