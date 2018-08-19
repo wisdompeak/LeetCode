dc363aedfff75ea5aebb32dc030a1db9e4979bda
wisdompeak <guan.huifeng@gmail.com>
2018-08-19 01:26:51 -0700

    Create 239.Sliding-Window-Maximum.cpp

---
+++ Priority_Queue/239.Sliding-Window-Maximum/239.Sliding-Window-Maximum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) 
+    {        
+        vector<int>results;
+        deque<int>q;
+        for (int i=0; i<nums.size(); i++)
+        {
+            while (q.size()>0 && nums[q.back()]<nums[i])
+                q.pop_back();
+            q.push_back(i);
+            if (q.front()==i-k)
+                q.pop_front();
+            if (i>=k-1)
+                results.push_back(nums[q.front()]);
+        }
+        return results;
+    }
+};
