2d200856cd9a68faad27c127d7e63c18cc27a67d
wisdompeak <guan.huifeng@gmail.com>
2018-08-20 20:56:40 -0700

    Create 269.Alien-Dictionary.cpp

---
+++ BFS/269.Alien-Dictionary/269.Alien-Dictionary.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    string alienOrder(vector<string>& words) 
+    {
+        map<char,set<char>>Next;
+        map<char,int>inDegree;
+        
+        int totalChar=0;
+        for (int i=0; i<words.size(); i++)
+            for (int j=0; j<words[i].size(); j++)
+                inDegree[words[i][j]] = 0;               
+        
+        for (int i=1; i<words.size(); i++)
+        {
+            string word1 = words[i-1];
+            string word2 = words[i];
+            
+            for (int i=0; i<min(word1.size(),word2.size()); i++)
+            {
+                if (word1[i]==word2[i]) continue;
+                
+                if (Next[word1[i]].find(word2[i])==Next[word1[i]].end())
+                {
+                    Next[word1[i]].insert(word2[i]);
+                    inDegree[word2[i]]++;
+                }                    
+                break;
+            }
+        }
+        
+        priority_queue<char,vector<int>,greater<char>>q;
+        for (auto a:inDegree)
+            if (a.second==0) q.push(a.first);
+        string result;
+        
+        while (!q.empty())
+        {
+            char current = q.top();
+            q.pop();
+            result+=current;
+            
+            for (auto a: Next[current])
+            {
+                inDegree[a]--;
+                if (inDegree[a]==0)                
+                    q.push(a);                
+            }            
+        }
+               
+        if (result.size()==inDegree.size())
+            return result;
+        else
+            return "";
+    }
+};
