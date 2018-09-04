29bcfaff95c9a82d3bb2b86445997fbbfc1e0b87
wisdompeak <guan.huifeng@gmail.com>
2018-09-03 19:09:06 -0700

    Create 713.Subarray-Product-Less-Than-K.cpp

---
+++ Two_Pointers/713.Subarray-Product-Less-Than-K/713.Subarray-Product-Less-Than-K.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) 
+    {
+        if (k<=1) return 0;
+        int j=0;
+        int prod = 1;
+        int count = 0;
+        for (int i=0; i<nums.size(); i++)
+        {            
+            if (j<i)
+            {
+                j = i;
+                prod = 1;
+            }
+            
+            while (j<nums.size() && prod*nums[j]<k)
+            {
+                prod = prod*nums[j];
+                j++;
+            }
+            count += j-i;
+            prod = prod/nums[i];
+        }
+        return count;
+    }
+};
