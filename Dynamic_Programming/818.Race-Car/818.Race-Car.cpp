3f5cbee5b59ed70b8429925626bc62431092a4c7
wisdompeak <guan.huifeng@gmail.com>
2018-09-20 11:25:56 -0700

    Update 818.Race-Car.cpp

--- Dynamic_Programming/818.Race-Car/818.Race-Car.cpp
+++ Dynamic_Programming/818.Race-Car/818.Race-Car.cpp
@@ -1,25 +1,27 @@
-class Solution {    
-public:
+class Solution {
     int dp[10001]={0};
+public:
     int racecar(int target) 
     {
-        if (dp[target]!=0)
-            return dp[target];                
+        if (dp[target]!=0) return dp[target];
         
         int n = log2(target+1);
+        if (pow(2,n)-1==target)
+        {
+            dp[target] = n;
+            return n;   
+        }
         
-        if ((1<<n)-1==target)
+        // run past the taget
+        dp[target] = (n+1)+1+racecar(pow(2,n+1)-1-target);
+        
+        // stop before the target
+        for (int k = 0; k<n; k++)
         {
-            dp[target]=n;
-            return dp[target];
+            int temp = n+1+k+1+ racecar( target - (pow(2,n)-1) + (pow(2,k)-1) );
+            dp[target] = min(dp[target],temp);
         }
-                
-        dp[target] = (n+1)+1+racecar((1<<(n+1))-1-target);
-        for (int m=0; m<n; m++)
-        {
-            dp[target] = min(dp[target], racecar(target-((1<<n)-1)+((1<<m)-1))+n+1+m+1);            
-        }
+        
         return dp[target];
-        
     }
 };
