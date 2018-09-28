04825fca42f1fdedc1bda3cf7bcbd801f64fbde0
wisdompeak <guan.huifeng@gmail.com>
2018-09-28 00:08:48 -0700

    Update 076.Minimum-Window-Substring.cpp

--- Two_Pointers/076.Minimum-Window-Substring/076.Minimum-Window-Substring.cpp
+++ Two_Pointers/076.Minimum-Window-Substring/076.Minimum-Window-Substring.cpp
@@ -2,39 +2,34 @@ class Solution {
 public:
     string minWindow(string s, string t) 
     {
-        string result;
-        int len = INT_MAX;
-        
-        unordered_map<char,int>Table;        
-        for (auto ch: t)        
-            Table[ch]++;        
-        int COUNT = Table.size();
+        unordered_map<char,int>Table;
+        for (int i=0; i<t.size(); i++)
+            Table[t[i]]++;
+        int M = Table.size();
         
         unordered_map<char,int>Map;
-        int i = 0;
+        int i=0;
+        int Len = INT_MAX;
+        string result;
         int count = 0;
-        
         for (int j=0; j<s.size(); j++)
-        {        
+        {
             Map[s[j]]++;
             if (Map[s[j]]==Table[s[j]])
                 count++;
-            
-            while (count==COUNT)
+            while (count==M)
             {
-                if (j-i+1<len)
+                if (Len>j-i+1)
                 {
-                    len = j-i+1;
-                    result = s.substr(i,j-i+1);
+                    Len = j-i+1;
+                    result = s.substr(i,Len);
                 }
-                
                 Map[s[i]]--;
-                if (Map[s[i]]<Table[s[i]])
+                if (Map[s[i]]==Table[s[i]]-1)
                     count--;
-                i++;                
+                i++;
             }
         }
-            
         return result;
     }
 };
