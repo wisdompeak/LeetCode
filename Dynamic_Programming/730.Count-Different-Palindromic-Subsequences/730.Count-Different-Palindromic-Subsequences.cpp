a7fe5bdf21b0919deba4f7a339b6dae3f99c3855
wisdompeak <guan.huifeng@gmail.com>
2018-10-01 13:20:28 -0700

    Update 730.Count-Different-Palindromic-Subsequences.cpp

--- Dynamic_Programming/730.Count-Different-Palindromic-Subsequences/730.Count-Different-Palindromic-Subsequences.cpp
+++ Dynamic_Programming/730.Count-Different-Palindromic-Subsequences/730.Count-Different-Palindromic-Subsequences.cpp
@@ -1,22 +1,38 @@
-###  730.Count-Different-Palindromic-Subsequences
-
-此题最大的特点是，不同的回文序列的定义是：只有字符不同的才算不同．所以我们定义dp[i][j]的时候，往内层递归时，需要分四种字符来讨论．
-
-假设我们在考察dp[i][j].我们需要额外定义first[i][k]，表示第i个字符开始，后面第一次出现k的位置．同理last[i][k]，表示第i个字符开始，最后一次出现k的位置．当然这里的first[i][k]和last[i][k]都是在闭区间[i,j]里面．也就是说，每次遇到一个[i,j]，根据j来更新first[i][k]和last[i][k]．
-
-所以可以有转移方程：
-```cpp
-for (int k=0; k<4; k++)
-  dp[i][j]+= dp[first[i][k]+1][last[i][k]-1];
-```
-注意，上述的方程不够完善．因为可能first[i][k]或second[i][k]不存在．所以更完善的表达是：
-```cpp
-for (int k=0; k<4; k++)
-{
-  if (last[i][k]!=-1)
-    dp[i][j]+= dp[first[i][k]+1][last[i][k]-1];
-  if (first[i][k]!=-1)
-    dp[i][j]+= 1;
-}
-```
-第二个判断语句是因为，字符ｋ在[i,j]区间内不能和另外一个匹配成回文，但是它自身可以单独出现作为回文．所以dp[i][j]需要自加１．
+class Solution {
+public:
+    int countPalindromicSubsequences(string S) 
+    {
+        int N = S.size();
+        auto dp = vector<vector<int>>(N,vector<int>(N,0));
+        
+        auto first = vector<vector<int>>(N,vector<int>(4,-1));
+        auto last = vector<vector<int>>(N,vector<int>(4,-1));
+        
+        int MOD = 1e9+7;
+        
+        for (int len = 1; len<=N; len++)
+            for (int i=0; i+len<=N; i++)
+            {
+                int j = i+len-1;
+                
+                if (first[i][S[j]-'a']==-1)
+                    first[i][S[j]-'a'] = j;
+                else
+                    last[i][S[j]-'a'] = j;
+                
+                for (int k=0; k<4; k++)
+                {
+                    if (last[i][k]>=0)                    
+                        dp[i][j]+=dp[first[i][k]+1][last[i][k]-1]+1;                    
+                    if (first[i][k]>=0)
+                        dp[i][j]+=1;                    
+                    dp[i][j]%=MOD;
+                }                
+                
+                //cout<<i<<" "<<j<<" "<<dp[i][j]<<endl;
+            }
+        
+        return dp[0][N-1];
+        
+    }
+};
