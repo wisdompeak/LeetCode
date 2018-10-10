ecb5ffa7f3e5a9d616eeaf2f6852e5255faeb605
wisdompeak <guan.huifeng@gmail.com>
2018-10-10 00:28:52 -0700

    Update 363.Max Sum of Rectangle No Larger Than K.cpp

--- Hash/363.Max-Sum-of-Rectangle-No-Larger-Than-K/363.Max Sum of Rectangle No Larger Than K.cpp
+++ Hash/363.Max-Sum-of-Rectangle-No-Larger-Than-K/363.Max Sum of Rectangle No Larger Than K.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int maxSumSubmatrix(vector<vector<int>>& matrix, int k) 
     {
-        int M=matrix.size();
-        int N=matrix[0].size();
+        int M = matrix.size();
+        if (M==0) return 0;
+        int N = matrix[0].size();
         
         if (M>N)
         {
@@ -14,44 +15,34 @@ public:
             return maxSumSubmatrix(matrix2,k);
         }
         
-        vector<vector<int>>sums(M+1,vector<int>(N,0));
-        for (int i=1; i<=M; i++)
-            for (int j=0; j<N; j++)
-                sums[i][j]=sums[i-1][j]+matrix[i-1][j];
+        int result = INT_MIN;
         
-        int result=INT_MIN;
-        
-        
-        for (int a=1; a<=M; a++)
-            for (int b=1; b<=a; b++)
+        for (int i=0; i<M; i++)        
+        {
+            vector<int>row(N,0);
+            for (int j=i; j<M; j++)
             {
-                vector<int>sum(N,0);
-                for (int j=0; j<N; j++)
-                    sum[j]=sums[a][j]-sums[b-1][j];
-                
-                //cout<<a<<" "<<b<<endl;
-
-                set<int>Set;
-                Set.insert(0);
-                int CurSum=0;
-                for (int j=0; j<N; j++)
-                {
-                    CurSum+=sum[j];
-                    
-                    auto it = Set.lower_bound(CurSum-k);
-                    if (it!=Set.end()) 
-                        result = max(result,CurSum-*it);
-                    Set.insert(CurSum);
-                    
-                    //cout<<result<<" ";
-                    
-                }
-                
-                cout<<endl;
+                for (int k=0; k<N; k++)
+                    row[k]+=matrix[j][k];
+                result = max(result,helper(row,k));
             }
-            
+        }        
         return result;
-        
-        
     }
+    
+    int helper(vector<int>&row, int k)
+    {
+        set<int>Set({0});
+        int preSum = 0;
+        int result = INT_MIN;
+        for (int i=0; i<row.size(); i++)
+        {
+            preSum+=row[i];
+            auto iter = Set.lower_bound(preSum-k);
+            if (iter!=Set.end()) 
+                result = max(result,preSum-*iter);
+            Set.insert(preSum);
+        }
+        return result;
+    }    
 };
