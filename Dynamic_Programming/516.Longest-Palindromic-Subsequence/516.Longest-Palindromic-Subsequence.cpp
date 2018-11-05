85468b2a04cda1dcc2be3716e793b6464f66ce56
wisdompeak <guan.huifeng@gmail.com>
2018-11-04 17:52:36 -0800

    Update 516.Longest-Palindromic-Subsequence.cpp

--- Dynamic_Programming/516.Longest-Palindromic-Subsequence/516.Longest-Palindromic-Subsequence.cpp
+++ Dynamic_Programming/516.Longest-Palindromic-Subsequence/516.Longest-Palindromic-Subsequence.cpp
@@ -2,30 +2,18 @@ class Solution {
 public:
     int longestPalindromeSubseq(string s) 
     {
-        int N=s.size();
-        auto dp=vector<vector<int>>(N,vector<int>(N,0));
+        int N = s.size();
+        auto dp = vector<vector<int>>(N,vector<int>(N,0));        
         
-        for (int i=0; i<N; i++)
-            dp[i][i]=1;
-        
-        for (int i=0; i<N-1; i++)
-        {
-            if (s[i]==s[i+1])
-                dp[i][i+1]=2;
-            else
-                dp[i][i+1]=1;
-        }
-        
-        for (int len=3; len<=N; len++)
-        {
+        for (int len=1; len<=N; len++)
             for (int i=0; i<=N-len; i++)
             {
-                if (s[i]==s[i+len-1])
-                    dp[i][i+len-1] = 2+dp[i+1][i+len-2];
+                int j = i+len-1;
+                if (len==1)
+                    dp[i][j] = 1;
                 else
-                    dp[i][i+len-1] = max(dp[i+1][i+len-1],dp[i][i+len-2]);                
+                    dp[i][j] = (s[i]==s[j])? dp[i+1][j-1]+2:max(dp[i][j-1],dp[i+1][j]);
             }
-        }
         return dp[0][N-1];
     }
 };
