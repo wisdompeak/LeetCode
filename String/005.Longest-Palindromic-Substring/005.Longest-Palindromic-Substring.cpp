f96dd1b4ad5227b7c31f2bdabc36027629465cf9
wisdompeak <guan.huifeng@gmail.com>
2018-11-11 21:20:11 -0800

    Create 005.Longest-Palindromic-Substring.cpp

---
+++ String/005.Longest-Palindromic-Substring/005.Longest-Palindromic-Substring.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string longestPalindrome(string s) 
+    {
+        string t="#";
+        for (int i=0; i<s.size(); i++)
+            t+= s.substr(i,1)+"#";
+        
+        int N = t.size();
+        vector<int>P(N,0);
+        int maxCenter = -1;
+        int maxRight = -1;
+        
+        for (int i=0; i<N; i++)
+        {
+            int k;
+            if (i<maxRight)
+            {
+                k = min(P[maxCenter*2-i],maxRight-i);
+                while (i-k>=0 && i+k<N && t[i-k]==t[i+k]) k++;                
+            }
+            else
+            {
+                k = 0;
+                while (i-k>=0 && i+k<N && t[i-k]==t[i+k]) k++;
+            }
+            P[i] = k-1;
+            if (i+k-1>maxRight)
+            {
+                maxRight = i+k-1;
+                maxCenter = i;
+            }
+        }
+        
+        for (auto x:P) cout<<x<<" ";
+        
+        int maxLen = -1;
+        int center;
+        for (int i=0; i<P.size(); i++)
+        {
+            if (P[i]>maxLen)
+            {
+                maxLen = P[i];
+                center = i;                
+            }
+        }
+        return s.substr((center - maxLen)/2, maxLen);
+    }
+};
