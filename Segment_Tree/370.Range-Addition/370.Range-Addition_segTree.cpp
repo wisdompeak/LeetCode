831caf0a599f21d74a7184b10920d39c21f798de
wisdompeak <guan.huifeng@gmail.com>
2019-01-29 04:09:44 -0800

    Update 370.Range-Addition_segTree.cpp

--- Segment_Tree/370.Range-Addition/370.Range-Addition_segTree.cpp
+++ Segment_Tree/370.Range-Addition/370.Range-Addition_segTree.cpp
@@ -1,76 +1,50 @@
 class Solution {
-    class SegNode
+    class SegTree
     {
         public:
+        int start,end,status;
+        SegTree* left;
+        SegTree* right;
+        SegTree(int a, int b, int s):start(a),end(b),status(s),left(NULL),right(NULL){}
         
-        int begin, end, status;
-        SegNode* left;
-        SegNode* right;
-        SegNode(int a, int b, int s)
+        void setStatus(int a, int b, int s)
         {
-            begin = a;
-            end = b;
-            status = s;
-            left = NULL;
-            right = NULL;
-        }
-        
-        void remove(SegNode* & node)
-        {
-            if (node==NULL) return;
-            remove(node->left);
-            remove(node->right);
-            delete node;
-            node=NULL;
-        }
-        
-        int setStatus(int a, int b, int s)
-        {
-            if (b<=begin || a>=end)
+            if (a<=start && b>=end && left==NULL)   // bottom node condition 1;
             {
-                return status;
-            }
-            if (a<=begin && end<=b && left==NULL)
-            {                
                 status += s;
-                return status;
+                return;
             }
-            int mid = (end-begin)/2+begin;
-            if (left==NULL)
-            {              
-                left = new SegNode(begin,mid,status);
-                right = new SegNode(mid,end,status);
-            }
-            int leftStatus = left->setStatus(a,b,s);
-            int rightStatus = right->setStatus(a,b,s);
-            return max(leftStatus,rightStatus);
+            if (a>=end || b<=start)         // bottom node condition 2;
+                return;
+            int mid = start+(end-start)/2;  
+            if (left==NULL)                 // no children? create them
+            {
+                left = new SegTree(start,mid,status);
+                right = new SegTree(mid,end,status);
+            }                               // recursion
+            left->setStatus(a,b,s);
+            right->setStatus(a,b,s);
         }
     };
-    
 public:
     vector<int> getModifiedArray(int length, vector<vector<int>>& updates) 
     {
-        SegNode* root = new SegNode(0,length,0);
-        
-        for (int i=0; i<updates.size(); i++)
-            root->setStatus(updates[i][0],updates[i][1]+1,updates[i][2]);
-        
+        SegTree* root = new SegTree(0,length,0);
+        for (auto x:updates)        
+            root->setStatus(x[0],x[1]+1,x[2]);
         vector<int>results(length);
         DFS(root,results);
         return results;
     }
-    
-    void DFS(SegNode* node, vector<int>& results)
+    void DFS(SegTree* node, vector<int>&results)
     {
-        if (node==NULL)
-            return;
         if (node->left!=NULL)
         {
             DFS(node->left,results);
             DFS(node->right,results);
             return;
         }
-        for (int i=node->begin; i<node->end; i++)
+        for (int i=node->start; i<node->end; i++)
             results[i] = node->status;
     }
 };
