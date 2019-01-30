8d65a9a4b02a2f975d67d5c6a49e3047e6e9c97d
wisdompeak <guan.huifeng@gmail.com>
2019-01-30 00:49:43 -0800

    Update 715.Range-Module_segTree.cpp

--- Segment_Tree/715.Range-Module/715.Range-Module_segTree.cpp
+++ Segment_Tree/715.Range-Module/715.Range-Module_segTree.cpp
@@ -2,73 +2,64 @@ class RangeModule {
     class SegTree
     {
         public:
-        int begin,end, status;
+        int start, end;
+        bool status;
         SegTree* left;
         SegTree* right;
-        SegTree(int a, int b, int s)
-        {
-            begin = a;
-            end = b;
-            status = s;
-            left = NULL;
-            right = NULL;
-        }
+        SegTree(int a, int b, bool T):start(a),end(b),status(T),left(NULL),right(NULL){}
         
         void remove(SegTree* &node)
         {
-            if (!node) return;
+            if (node==NULL) return;
             remove(node->left);
             remove(node->right);
             delete node;
             node = NULL;
+            return;
         }
         
-        
-        int setStatus(int a, int b, int s)
+        bool setStatus(int a, int b, bool T)
         {
-            if (begin>=b || end<=a)            // 1. [a,b]与这个区间不相交，返回原先的状态
-                return status;                        
-            if (a<=begin && end<=b)            // 2. [a,b]包括了整个区间，将该区间抹平
+            if (a<=start && b>=end) // bottom condition 1: [a,b)>[start,end)
             {
                 remove(left);
                 remove(right);
-                return status = s;
-            }         
-            if (!left)                         // 3. [a,b]与该区间相交，需考虑其子树
-            {
-                int mid = (end-begin)/2+begin;
-                left = new SegTree(begin,mid,status);
-                right = new SegTree(mid,end,status);
-            }            
-            int leftStatus = left->setStatus(a,b,s);
-            int rightStatus = right->setStatus(a,b,s);
-            return status = leftStatus && rightStatus;
-        }
-                
-        int getStatus(int a, int b)
-        {
-            if (begin>=b || end<=a)            // 1. [a,b]与这个区间不相交，返回一个不影响结果的状态
-                return true;            
-            if (a<=begin && end<=b)            // 2. [a,b]包括了整个区间，返回该区间的状态
-                return status;                        
-            if (!left)                         // 3. [a,b]与该区间相交，但又没有子树，返回整个区间状态
+                return status = T;
+            }
+            if (a>=end || b<=start) // bottom condition 2: [a,b) do not intersect with [start,end)
                 return status;
-            int mid = (end-begin)/2+begin;      // 4. [a,b]与该区间相交，需要考虑其子树
-            int leftStatus = left->getStatus(a,b);
-            int rightStatus = right->getStatus(a,b);
-            return leftStatus && rightStatus;            
-        }
-            
+            int mid = start+(end-start)/2;
+            if (left==NULL)         // no children? create them!
+            {
+                left = new SegTree(start,mid,status);
+                right = new SegTree(mid,end,status);
+            }
+            bool L = left->setStatus(a,b,T);
+            bool R = right->setStatus(a,b,T);
+            return status = L&&R;
+        }        
+        
+        bool getStatus(int a, int b)
+        {            
+            if (a<=start && b>=end)   // bottom condition 1: [a,b)>[start,end)
+                return status;
+            if (a>=end || b<=start)   // bottom condition 2: [a,b) do not intersect with [start,end)
+                return true;
+            if (left==NULL)           // bottom condition 3: [a,b) intersect with [start,end)
+                return status;
+            int mid = start+(end-start)/2;
+            bool L = left->getStatus(a,b);
+            bool R = right->getStatus(a,b);
+            return L&&R;
+        }        
     };
-    
     
 public:
     
-    SegTree root = SegTree(0,INT_MAX,false);
+    SegTree root = SegTree(0,1e9,false);
     
     RangeModule() 
     {
-        
     }
     
     void addRange(int left, int right) 
@@ -77,7 +68,7 @@ public:
     }
     
     bool queryRange(int left, int right) 
-    {
+    {        
         return root.getStatus(left,right);
     }
     
