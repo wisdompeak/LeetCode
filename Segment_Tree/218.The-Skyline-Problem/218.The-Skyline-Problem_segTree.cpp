a6f2a75d1fb53e8ca345a8288a9e7cc02e80e3a9
wisdompeak <guan.huifeng@gmail.com>
2019-02-04 00:15:47 -0800

    Update 218.The-Skyline-Problem_segTree.cpp

--- Segment_Tree/218.The-Skyline-Problem/218.The-Skyline-Problem_segTree.cpp
+++ Segment_Tree/218.The-Skyline-Problem/218.The-Skyline-Problem_segTree.cpp
@@ -2,93 +2,77 @@ class Solution {
     class SegTree
     {
         public:
-        int begin, end, status;
+        int start,end,status;
         SegTree* left;
         SegTree* right;
-        SegTree(int a, int b, int T)
-        {
-            begin = a;
-            end = b;            
-            status = T;
-            left = NULL;
-            right = NULL;
-        }                
+        SegTree(int a, int b, int s):start(a),end(b),status(s),left(NULL),right(NULL){}
         
         void remove(SegTree* &node)
         {
-            if (!node) return;
+            if (node==NULL) return;
             remove(node->left);
             remove(node->right);
             delete node;
             node = NULL;
+            return;            
         }
         
-        int setStatus(int a, int b, int s)
+        void setStatus(int a, int b, int s)
         {
-            if (begin>=b || end<=a)            // 1. [a,b]与这个区间不相交，返回原先的状态
-                return status;                        
-            if (a<=begin && end<=b && status<=s)            // 2. [a,b]包括了整个区间，并且该区间的status<s,则将该区间抹平
+            if (a>=end || b<=start)
+                return;
+            if (a<=start && b>=end && s>=status)
             {
                 remove(left);
                 remove(right);
-                return status = s;
-            }         
-            if (a<=begin && end<=b && status>s && !left)    // 3. [a,b]包括了整个区间，并且该区间的status>s,且无子树，则整体不更新
+                status = s;
+                return;
+            }
+            if (a<=start && b>=end && s<status && left==NULL)
+                return;
+            if (left==NULL)
             {
-                return status;
-            }         
-            if (!left)                         // 4. 其他情况，需考虑其子树
-            {
-                int mid = (end-begin)/2+begin;
-                left = new SegTree(begin,mid,status);
+                int mid = start+(end-start)/2;
+                left = new SegTree(start,mid,status);
                 right = new SegTree(mid,end,status);
-            }            
-            int leftStatus = left->setStatus(a,b,s);
-            int rightStatus = right->setStatus(a,b,s);
-            return status = max(leftStatus,rightStatus);
+            }
+            left->setStatus(a,b,s);
+            right->setStatus(a,b,s);
+            status = max(left->status,right->status);            
         }
-        
     };
-    
 public:
-    vector<pair<int,int>>Set;
+    vector<pair<int,int>>results;
     vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) 
     {
-        if (buildings.size()==0)
-            return {};
+        if (buildings.size()==0) return {};
         
-        SegTree root = SegTree(0,INT_MAX,0);
-        for (int i=0; i<buildings.size(); i++)
-            root.setStatus(buildings[i][0],buildings[i][1],buildings[i][2]);        
+        SegTree* root = new SegTree(0,INT_MAX,0);
+        for (auto q:buildings)
+            root->setStatus(q[0],q[1],q[2]);
         
-        SegTree* node = &root;
-        DFS(node);
+        DFS(root);
+        if (results.back().second!=0) results.push_back({INT_MAX,0});
         
-        if (Set.back().second!=0)
-            Set.push_back({INT_MAX,0});
-        int i = Set.size()-1;
-        while (i>=0 && Set[i].second==0)
-            i--;
-        Set.resize(i+2);
-        return Set;
+        vector<pair<int,int>>filteredResults;
+        for (auto p: results)
+        {
+            if (filteredResults.size()!=0 && p.second==filteredResults.back().second)
+                continue;
+            filteredResults.push_back({p.first,p.second});
+        }
+        if (filteredResults.size()!=0 && filteredResults[0].second==0) filteredResults.erase(filteredResults.begin());
+        return filteredResults;
     }
     
     void DFS(SegTree* node)
-    {
-        if (node==NULL) return;
-        if (node->left!=NULL)
+    {        
+        if (node->left==NULL)
+            results.push_back({node->start,node->status});
+        else
         {
             DFS(node->left);
             DFS(node->right);
-        }
-        else
-        {
-            if (Set.size()==0 && node->status==0)
-                return;
-            else if (Set.size()>0 && node->status==Set.back().second)
-                return;
-            else
-                Set.push_back({node->begin,node->status});                
         }
     }
 };
