9dfb8db3dc9937e1722b2a637f392c3d97683cd7
wisdompeak <guan.huifeng@gmail.com>
2019-02-06 10:10:25 -0800

    Update 850.Rectangle-Area-II.cpp

--- Others/850.Rectangle-Area-II/850.Rectangle-Area-II.cpp
+++ Others/850.Rectangle-Area-II/850.Rectangle-Area-II.cpp
@@ -1,46 +1,44 @@
 class Solution {
-    long long MOD = 1e9+7;
 public:
     int rectangleArea(vector<vector<int>>& rectangles) 
     {
-        int N=rectangles.size();
-        vector<int>X;
-        vector<int>Y;
-        for (int i=0; i<N; i++)
+        set<int>SetX;
+        set<int>SetY;
+        for (auto coordinate: rectangles)
         {
-            X.push_back(rectangles[i][0]);
-            X.push_back(rectangles[i][2]);
-            Y.push_back(rectangles[i][1]);
-            Y.push_back(rectangles[i][3]);
+            SetX.insert(coordinate[0]);
+            SetX.insert(coordinate[2]);
+            SetY.insert(coordinate[1]);
+            SetY.insert(coordinate[3]);
         }
-        sort(X.begin(),X.end());
-        X.erase(unique(X.begin(),X.end()),X.end());
-        sort(Y.begin(),Y.end());
-        Y.erase(unique(Y.begin(),Y.end()),Y.end());
+        vector<int>X(SetX.begin(),SetX.end());
+        vector<int>Y(SetY.begin(),SetY.end());
         
-        vector<vector<bool>>points(X.size(),vector<bool>(Y.size(),false));
-        for (int i=0; i<N; i++)
+        int M = X.size();
+        int N = Y.size();
+        auto points = vector<vector<bool>>(M,vector<bool>(N,0));
+        
+        for (int k=0; k<rectangles.size(); k++)
         {
-            int idx_X_0 = lower_bound(X.begin(),X.end(),rectangles[i][0])-X.begin();
-            int idx_Y_0 = lower_bound(Y.begin(),Y.end(),rectangles[i][1])-Y.begin();
-            int idx_X_1 = lower_bound(X.begin(),X.end(),rectangles[i][2])-X.begin();
-            int idx_Y_1 = lower_bound(Y.begin(),Y.end(),rectangles[i][3])-Y.begin();
-            
-            for (int u=idx_X_0; u<idx_X_1; u++)
-                for (int v=idx_Y_0; v<idx_Y_1; v++)
-                    points[u][v]=true;            
+            int idx_X_0 = lower_bound(X.begin(),X.end(),rectangles[k][0]) - X.begin();
+            int idx_Y_0 = lower_bound(Y.begin(),Y.end(),rectangles[k][1]) - Y.begin();
+            int idx_X_1 = lower_bound(X.begin(),X.end(),rectangles[k][2]) - X.begin();
+            int idx_Y_1 = lower_bound(Y.begin(),Y.end(),rectangles[k][3]) - Y.begin();
+            for (int i=idx_X_0+1; i<=idx_X_1; i++)
+                for (int j=idx_Y_0+1; j<=idx_Y_1; j++)
+                    points[i][j] = 1;
         }
         
-        long long result = 0;
-        for (int i=0; i<X.size(); i++)
-            for (int j=0; j<Y.size(); j++)
+        long long  MOD = 1e9+7;
+        long long sum = 0;
+        for (int i=1; i<M; i++)
+            for (int j=1; j<N; j++)
             {
                 if (points[i][j])
-                {
-                    result = result + (long long)(X[i+1]-X[i])*(long long)(Y[j+1]-Y[j])%MOD;
-                }
-                    
+                    sum = (sum + (long long)(X[i]-X[i-1])*(long long)(Y[j]-Y[j-1])) % MOD;
+                
             }
-        return result%MOD;                
+                
+        return sum;
     }
 };
