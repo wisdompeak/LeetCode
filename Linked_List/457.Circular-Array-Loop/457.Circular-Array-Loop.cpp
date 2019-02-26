2190804148fdb34e702c6ca579818c5d7c0522f8
wisdompeak <guan.huifeng@gmail.com>
2019-02-25 23:19:43 -0800

    Update and rename 457.Circular-Array-Loop.py to 457.Circular-Array-Loop.cpp

---
+++ Linked_List/457.Circular-Array-Loop/457.Circular-Array-Loop.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool circularArrayLoop(vector<int>& nums) 
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 0) continue; 
+            
+            int slow = i, fast = i, dir = nums[i];
+            do {
+                slow = advanceBy(1, slow, nums, dir);
+                fast = advanceBy(2, fast, nums, dir);
+            } while (slow >= 0 && fast >= 0 && slow != fast);
+                
+            if (slow >= 0 && fast >= 0 && slow != advanceBy(1, slow, nums, dir)) 
+                return true;
+            
+            slow = i;
+            while (advanceBy(1, slow, nums, dir)>=0)
+            {
+                int tmp = slow;
+                nums[tmp] = 0;
+                slow = tmp;
+            }
+        }
+        return false;
+    }
+    
+    int advanceBy(int step, int j, vector<int>& nums, int dir) 
+    {
+        int n = nums.size();
+        for (int k=0; k<step; k++) 
+        {
+            j = (j + nums[j] + n) % n;
+            j = (j + n) % n;
+            if (nums[j] * dir <= 0)
+                return -1;
+        }
+        return j;
+    }
+
+
+};
