63d2df2ee6833bf8be057df692944315e4436869
wisdompeak <guan.huifeng@gmail.com>
2019-04-22 18:54:55 -0700

    Update 576.Out-of-Boundary-Paths.cpp

--- Dynamic_Programming/576.Out-of-Boundary-Paths/576.Out-of-Boundary-Paths.cpp
+++ Dynamic_Programming/576.Out-of-Boundary-Paths/576.Out-of-Boundary-Paths.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int findPaths(int m, int n, int N, int i, int j) 
     {
-        auto p = vector<vector<long long>>(m,vector<long long>(n,0));
-        vector<vector<long long>> q =p;
-        long long M=pow(10,9)+7;
+        auto p = vector<vector<int>>(m,vector<int>(n,0));
+        auto q =p;
+        int M=pow(10,9)+7;
         
         for (int k=0; k<N; k++)
         {
             for (int i=0; i<m; i++)
              for (int j=0; j<n; j++)
              {
-                 long long n1= (i==0)?1:q[i-1][j];
-                 long long n2= (i==m-1)?1:q[i+1][j];
-                 long long n3= (j==0)?1:q[i][j-1];
-                 long long n4= (j==n-1)?1:q[i][j+1];
+                 long n1= (i==0)?1:q[i-1][j];
+                 long n2= (i==m-1)?1:q[i+1][j];
+                 long n3= (j==0)?1:q[i][j-1];
+                 long n4= (j==n-1)?1:q[i][j+1];
                  p[i][j]=(n1+n2+n3+n4)%M;
              }
             q=p;
