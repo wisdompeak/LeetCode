1f4cd44e7e837bb515de7506e3cb35cadc3b2664
wisdompeak <guan.huifeng@gmail.com>
2019-05-06 18:42:59 -0700

    Create 1035.Uncrossed-Lines.cpp

---
+++ Dynamic_Programming/1035.Uncrossed-Lines/1035.Uncrossed-Lines.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& A, vector<int>& B) 
+    {
+        int m = A.size();
+        int n = B.size();
+        auto dp = vector<vector<int>>(m+1,vector<int>(n+1,0));
+        A.insert(A.begin(),0);
+        B.insert(B.begin(),0);
+        
+        for (int i=1; i<=m; i++)
+            for (int j=1; j<=n; j++)
+            {
+                if (A[i]==B[j])
+                    dp[i][j] = dp[i-1][j-1]+1;
+                else
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);                    
+            }
+        return dp[m][n];
+    }
+};
