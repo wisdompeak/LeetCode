6410422bd0f94b98f74da6bf229d98a2761dbecb
wisdompeak <guan.huifeng@gmail.com>
2019-05-07 23:35:35 -0700

    Create 312.Burst-Balloons-DP.cpp

---
+++ DFS/312.Burst-Balloons/312.Burst-Balloons-DP.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) 
+    {
+        int N = nums.size();
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        
+        auto dp = vector<vector<int>>(N+2,vector<int>(N+2,0));
+        
+        for (int len=1; len<=N; len++)
+            for (int i=1; i<=N-len+1; i++)
+            {
+                int j = i+len-1;
+                for (int k=i; k<=j; k++)
+                    dp[i][j] = max(dp[i][j], dp[i][k-1]+dp[k+1][j]+nums[i-1]*nums[k]*nums[j+1]);                
+            }        
+        
+        return dp[1][N];
+    }
+};
