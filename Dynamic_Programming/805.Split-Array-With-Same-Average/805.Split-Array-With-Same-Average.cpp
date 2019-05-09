f58afe902e88686ca8e6e125cf5af34d4383b134
wisdompeak <guan.huifeng@gmail.com>
2019-05-09 00:45:17 -0700

    Update 805.Split-Array-With-Same-Average.cpp

--- Dynamic_Programming/805.Split-Array-With-Same-Average/805.Split-Array-With-Same-Average.cpp
+++ Dynamic_Programming/805.Split-Array-With-Same-Average/805.Split-Array-With-Same-Average.cpp
@@ -6,28 +6,26 @@ public:
         for (int i=0; i<A.size(); i++)
             sum+=A[i];
         int N=A.size();
-        auto dp = vector<vector<bool>>(N+1,vector<bool>(sum+1,0));        
+        A.insert(A.begin(),0);
+        auto dp = vector<vector<bool>>(N+1,vector<bool>(sum/2+1,0));        
         dp[0][0]=1;
         
-        for (int i=0; i<N; i++)
-        {
-            vector<pair<int,int>>temp;
-            for (int Anum=0; Anum<=N; Anum++)
-                for (int Asum=0; Asum<=sum; Asum++)
-                {                    
-                    if (dp[Anum][Asum]==1 && Anum+1<=N && Asum+A[i]<=sum)
-                        temp.push_back({Anum+1,Asum+A[i]});
-                }
-            for (int j=0; j<temp.size(); j++)
-                dp[temp[j].first][temp[j].second]=1;
-        }
         
-        for (int Anum=1; Anum<N; Anum++)
-            for (int Asum=0; Asum<=sum; Asum++)
-            {                    
-                if (dp[Anum][Asum]==1 && Asum*1.0/Anum==(sum-Asum)*1.0/(N-Anum))
-                   return true;                                                
-            }
+        for (int i=1; i<=N; i++)
+        {
+            auto temp = dp;
+            
+            for (int Asum=0; Asum<=sum/2; Asum++)
+                for (int Anum=1; Anum<=i; Anum++)
+                {                    
+                    if (Anum-1>=0 && Asum-A[i]>=0 && temp[Anum-1][Asum-A[i]])
+                    {
+                        dp[Anum][Asum]=1;
+                        if (Anum!=N && (Asum*1.0/Anum==sum*1.0/N))                        
+                            return true;                        
+                    }                    
+                }            
+        }
         
         return false;
     }
