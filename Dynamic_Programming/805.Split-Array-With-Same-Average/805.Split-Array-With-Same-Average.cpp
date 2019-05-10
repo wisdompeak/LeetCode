856302c536a4ec89362e37d9befc93d660c3c4f8
wisdompeak <guan.huifeng@gmail.com>
2019-05-09 19:09:32 -0700

    Update 805.Split-Array-With-Same-Average.cpp

--- Dynamic_Programming/805.Split-Array-With-Same-Average/805.Split-Array-With-Same-Average.cpp
+++ Dynamic_Programming/805.Split-Array-With-Same-Average/805.Split-Array-With-Same-Average.cpp
@@ -2,31 +2,30 @@ class Solution {
 public:
     bool splitArraySameAverage(vector<int>& A) 
     {
-        int sum=0;
-        for (int i=0; i<A.size(); i++)
-            sum+=A[i];
-        int N=A.size();
+        int N = A.size();
+        int SUM = accumulate(A.begin(),A.end(),0);
+        double targetAvg = SUM*1.0/N;
+        auto dp = vector<vector<bool>>(SUM/2+1,vector<bool>(N+1,false));
         A.insert(A.begin(),0);
-        auto dp = vector<vector<bool>>(N+1,vector<bool>(sum/2+1,0));        
-        dp[0][0]=1;
         
-        
+        dp[0][0] = 1;        
+            
         for (int i=1; i<=N; i++)
         {
-            auto temp = dp;
-            
-            for (int Asum=0; Asum<=sum/2; Asum++)
-                for (int Anum=1; Anum<=i; Anum++)
-                {                    
-                    if (Anum-1>=0 && Asum-A[i]>=0 && temp[Anum-1][Asum-A[i]])
+            for (int sum = SUM/2; sum>=0; sum--)
+                for (int num = i; num>=1; num--)
+                {
+                    if (sum-A[i]<0) continue;
+                    if (dp[sum-A[i]][num-1])
                     {
-                        dp[Anum][Asum]=1;
-                        if (Anum!=N && (Asum*1.0/Anum==sum*1.0/N))                        
-                            return true;                        
-                    }                    
-                }            
+                        dp[sum][num] = true;
+                        if (num!=N && (sum *1.0/ num == targetAvg))
+                            return true;
+                    }
+                }
         }
-        
+    
         return false;
+        
     }
 };
