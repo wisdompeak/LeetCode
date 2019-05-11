19a06189cfe44c1b9e0ee9cbd97541f3ba0e3c2e
wisdompeak <guan.huifeng@gmail.com>
2019-05-11 01:41:35 -0700

    Update 591.Tag-Validator_s2.cpp

--- Stack/591.Tag-Validator/591.Tag-Validator_s2.cpp
+++ Stack/591.Tag-Validator/591.Tag-Validator_s2.cpp
@@ -2,74 +2,69 @@ class Solution {
 public:
     bool isValid(string code) 
     {
-        int i=0;
         stack<string>Stack;
-        bool head=0;
         
-        while (i<code.size())        
+        int i=0;
+        bool ever = 0;
+        
+        while (i<code.size())
         {
             if (i+8<code.size() && code.substr(i,9)=="<![CDATA[")
             {
                 i+=9;
+                int i0=i;
                 while (i+2<code.size() && code.substr(i,3)!="]]>")
                     i++;
                 if (i+2==code.size()) return false;
-                i+=3;
-            }         
+                i+=3;                
+            }
+            
             else if (i+1<code.size() && code.substr(i,2)=="</")
             {
                 i+=2;
                 int i0=i;
                 while (i<code.size() && code[i]!='>')
                     i++;
-                if (i==code.size() || code[i]!='>')
-                    return false;
-                string tagName=code.substr(i0,i-i0);
-                if (validTag(tagName)==false)
-                    return false;
-                if (Stack.empty() || Stack.top()!=tagName)
+                if (i==code.size()) return false;
+                string tagname = code.substr(i0,i-i0);
+                if (Stack.empty() || Stack.top()!=tagname)
                     return false;
                 Stack.pop();
-                if (Stack.empty() && i!=code.size()-1)
-                    return false;
-                i++;
+                i++; 
+                if (Stack.empty() && i!=code.size()) return false;                             
             }
+            
             else if (code[i]=='<')
             {
-                head=1;
                 i++;
                 int i0=i;
                 while (i<code.size() && code[i]!='>')
                     i++;
-                if (i==code.size() || code[i]!='>')
-                    return false;
-                string tagName=code.substr(i0,i-i0);
-                if (validTag(tagName)==false)
-                    return false;
-                if (Stack.empty() && i0!=1)
-                    return false;
-                Stack.push(tagName);
+                if (i==code.size()) return false;
+                string tagname = code.substr(i0,i-i0);
+                if (!isValidTag(tagname)) return false;                
+                if (ever==false && i0!=1) return false;
+                ever = true;                
+                Stack.push(tagname);
                 i++;
-            }               
+            }
+            
             else
                 i++;
         }
         
-        if (head==0 || !Stack.empty())
-            return false;
-        else
-            return true;
+        if (!Stack.empty()) return false;
+        if (ever==0) return false;
+        
+        return true;
     }
     
-    bool validTag(string tag)
+    bool isValidTag(string tagname)
     {
-        if (tag.size()==0 || tag.size()>9) return false;
-        
-        for (int i=0; i<tag.size(); i++)
-        {
-            if (tag[i]>'Z' || tag[i]<'A') return false;
-        }
-        
-        return true;         
+        if (tagname.size()<1 || tagname.size()>9) return false;
+        for (auto ch:tagname)
+            if (ch<'A'|| ch>'Z') return false;
+        return true;
     }
+    
 };
