a3d41409032583eb30d206f38e99b6a9b9457f47
wisdompeak <guan.huifeng@gmail.com>
2019-05-13 21:55:32 -0700

    Create 1040.Moving-Stones-Until-Consecutive-II.cpp

---
+++ Greedy/1040.Moving-Stones-Until-Consecutive-II/1040.Moving-Stones-Until-Consecutive-II.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> numMovesStonesII(vector<int>& stones) 
+    {
+        sort(stones.begin(),stones.end());
+        int n = stones.size();
+        
+        int a = (stones[n-2]-stones[0]+1)-(n-1);
+        int b = (stones[n-1]-stones[1]+1)-(n-1);
+        int y = max(a,b);
+        
+        int j = 0;
+        int x = INT_MAX;
+        for (int i=0; i<n; i++)
+        {
+            while (j<n && stones[j]-stones[i]+1<n)
+                j++;
+            if (j==n) break;
+            
+            int gap;
+            if (stones[j]-stones[i]+1==n)
+                gap = n-(j-i+1);
+            else if (n-(j-i)==1)
+                gap = n-(j-i)+1;
+            else
+                gap = n-(j-i);
+            
+            x = min(x,gap);
+        }
+        
+        return {x,y};
+    }
+};
