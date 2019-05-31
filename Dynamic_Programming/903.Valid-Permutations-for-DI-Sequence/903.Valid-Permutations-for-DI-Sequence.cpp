8323db6e949b52689dae3909287669b81079cb89
wisdompeak <guan.huifeng@gmail.com>
2019-05-30 23:50:57 -0700

    Update 903.Valid-Permutations-for-DI-Sequence.cpp

--- Dynamic_Programming/903.Valid-Permutations-for-DI-Sequence/903.Valid-Permutations-for-DI-Sequence.cpp
+++ Dynamic_Programming/903.Valid-Permutations-for-DI-Sequence/903.Valid-Permutations-for-DI-Sequence.cpp
@@ -2,28 +2,37 @@ class Solution {
 public:
     int numPermsDISequence(string S) 
     {
+        int N = S.size();
         S = "#"+S;
-        int N = S.size();        
-        auto dp = vector<vector<int>>(N+1,vector<int>(N+1,0));
-        dp[1][1] = 1;
-        int MOD = 1e9+7;
-        for (int i=2; i<=N; i++)
-            for (int j=1; j<=i; j++)
+        auto dp = vector<vector<int>>(N+1,vector<int>(N+1));
+        dp[0][0] = 1;
+        int M = 1e9+7;
+        
+        for (int i=1; i<=N; i++)        
+            for (int j=0; j<=i; j++)
             {
-                if (S[i-1]=='I')
+                if (S[i]=='D')
                 {
-                    for (int k=1; k<j; k++)
-                        dp[i][j] = (dp[i][j]+dp[i-1][k])%MOD;
+                    for (int k=j; k<=i-1; k++)
+                    {
+                        dp[i][j] += dp[i-1][k];
+                        dp[i][j]%=M;
+                    }                        
                 }
                 else
                 {
-                    for (int k=j; k<=i; k++)
-                        dp[i][j] = (dp[i][j]+dp[i-1][k])%MOD;
-                }
+                    for (int k=0; k<j; k++)
+                    {
+                        dp[i][j] += dp[i-1][k];
+                        dp[i][j]%=M;
+                    }                        
+                }                    
             }
+        
         int result = 0;
-        for (int j=1; j<=N; j++)
-            result = (result+dp[N][j])%MOD;
+        for (int j=0; j<=N; j++)
+            result = (result+dp[N][j])%M;
+        
         return result;
     }
 };
