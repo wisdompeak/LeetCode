a2872ef7592c97baed755d48b962e547c0a6fa9e
wisdompeak <guan.huifeng@gmail.com>
2019-06-30 16:08:51 -0700

    Create 1105.Filling-Bookcase-Shelves.cpp

---
+++ Dynamic_Programming/1105.Filling-Bookcase-Shelves/1105.Filling-Bookcase-Shelves.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelf_width) 
+    {
+        int N = books.size();
+        books.insert(books.begin(),{0,0});        
+        vector<int>dp(N+1,INT_MAX/2);
+        dp[0] = 0;
+        
+        for (int i=1; i<=N; i++)
+        {
+            int width = books[i][0];
+            int height = books[i][1];
+            dp[i] = dp[i-1]+height;
+            
+            int j = i-1;
+            while (j>=1 && books[j][0]+width<=shelf_width)
+            {                
+                width += books[j][0];
+                height = max(height, books[j][1]);
+                dp[i] = min(dp[i], dp[j-1]+height);
+                j--;
+            }
+            
+            cout<<i<<" "<<dp[i]<<endl;
+        }
+        return dp[N];
+    }
+};
