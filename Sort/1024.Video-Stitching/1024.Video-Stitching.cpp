f3de31292150da574c63be11eac06676fd8493dc
wisdompeak <guan.huifeng@gmail.com>
2019-07-09 21:27:45 -0700

    Update 1024.Video-Stitching.cpp

--- Sort/1024.Video-Stitching/1024.Video-Stitching.cpp
+++ Sort/1024.Video-Stitching/1024.Video-Stitching.cpp
@@ -1,41 +1,40 @@
 class Solution {
     static bool cmp(vector<int>&a, vector<int>&b)
     {
-        if (a[0]==b[0]) return a[1]>b[1];
-        else return a[0]<b[0];
+        if (a[0]==b[0])
+            return a[1]>b[1];
+        else
+            return a[0]<b[0];
     }
 public:
-    int videoStitching(vector<vector<int>>& q, int T) 
+    int videoStitching(vector<vector<int>>& clips, int T) 
     {
-        sort(q.begin(),q.end(), cmp);
+        sort(clips.begin(),clips.end(),cmp);
         
-        if (q[0][0]>0) return -1;
-        int left = q[0][0];
-        int right = q[0][1];
-        int far = q[0][1];
+        if (clips[0][0]!=0) return -1;
+        int right = clips[0][1];
+        int idx = 0;
         int count = 1;
+        if (clips[0][1]>=T) return count;
         
-        int i = 1;
-        while (far<T && i<q.size())
+        while (idx < clips.size())
         {
-            int originFar = far;
-            while (i<q.size() && q[i][0]<=right)
+            int farReach = right;
+            while (idx<clips.size() && clips[idx][0]<=right)
             {
-                if (q[i][1]>=far)
-                {
-                    far = q[i][1];
-                }
-                i++;
+                farReach = max(farReach, clips[idx][1]);
+                idx++;
             }
+            if (farReach == right)
+                return -1;
             
-            if (far==originFar) break;
-            
-            left = right;
-            right = far;
             count++;
+            
+            if (farReach>=T) return count;
+            right = farReach;
         }
         
-        if (far>=T) return count;
-        else return -1;
+        return -1;
+        
     }
 };
