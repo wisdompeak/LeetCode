b8639947b1655b43904d4432adff17ed0080c5ac
wisdompeak <guan.huifeng@gmail.com>
2019-08-14 20:27:04 -0700

    Update 115.Distinct-Subsequences.cpp

--- Dynamic_Programming/115.Distinct-Subsequences/115.Distinct-Subsequences.cpp
+++ Dynamic_Programming/115.Distinct-Subsequences/115.Distinct-Subsequences.cpp
@@ -1,31 +1,23 @@
-class Solution {    
+class Solution {
 public:
     int numDistinct(string s, string t) 
     {
-        int M=s.size();
-        int N=t.size();
-        if (M==0||N==0) return 0;
+        int m = s.size();
+        int n = t.size();
+        auto dp = vector<vector<long>>(m+1,vector<long>(n+1,0));
+        s = "#"+s;
+        t = "#"+t;
         
-        auto dp=vector<vector<int>>(M,vector<int>(N,0));
-                
-        int count=0;
-        for (int i=0; i<M; i++)
-        {
-            if (s[i]==t[0])            
-                count++;
-            dp[i][0]=count;                            
-        }
+        for (int i=0; i<=m; i++) dp[i][0] = 1;
         
-                
-        for (int j=1; j<N; j++)
-         for (int i=j; i<M; i++)
-         {             
-             if (s[i]==t[j])
-                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-             else
-                dp[i][j]=dp[i-1][j];                  
-         }
-              
-        return dp[M-1][N-1];
+        for (int i=1; i<=m; i++)
+            for (int j=1; j<=n; j++)
+            {
+                if (s[i]==t[j])
+                    dp[i][j] += dp[i-1][j-1];
+                dp[i][j] += dp[i-1][j];
+            }
+        
+        return dp[m][n];
     }
 };
