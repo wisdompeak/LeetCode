3ece153549d13a6bf96be1b5c15cd4eb817fcf63
wisdompeak <guan.huifeng@gmail.com>
2019-09-02 23:42:15 -0700

    Create 210.Course-Schedule-II.cpp

---
+++ BFS/210.Course-Schedule-II/210.Course-Schedule-II.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        int n = numCourses;
+        unordered_map<int,vector<int>>nextCourses(n);
+        unordered_map<int,int>degree(n);
+        
+        for (auto edge: prerequisites)
+        {
+            int a = edge[0];
+            int b = edge[1];
+            nextCourses[b].push_back(a);
+            degree[a]+=1;
+        }
+        
+        queue<int>q;
+        for (int i=0; i<n; i++)
+            if (degree[i]==0)
+                q.push(i);
+        
+        vector<int>rets;
+        while (!q.empty())
+        {
+            int cur = q.front();
+            q.pop();
+            rets.push_back(cur);
+            
+            for (auto next: nextCourses[cur])
+            {
+                degree[next] -= 1;
+                if (degree[next]==0)
+                    q.push(next);
+            }
+        }
+        
+        if (rets.size()!=n)
+            return {};
+        else
+            return rets;
+    }
+};
