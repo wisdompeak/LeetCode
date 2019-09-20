a03c5645eec965999decc8ac1219ae49a1f94272
wisdompeak <guan.huifeng@gmail.com>
2019-09-20 00:35:25 -0700

    Update 007.Reverse-Integer.cpp

--- Others/007.Reverse-Integer/007.Reverse-Integer.cpp
+++ Others/007.Reverse-Integer/007.Reverse-Integer.cpp
@@ -2,17 +2,23 @@ class Solution {
 public:
     int reverse(int x) 
     {
-        if (x==INT_MIN) return 0;        
-        int flag = (x>=0)? 1:-1;
-        x = abs(x);
-        int result = 0;
+        if (x==INT_MIN) return 0;
+        
+        int sign = 1;
+        if (x<0) sign = -1;
+        
+        x = abs(x); 
+        
+        int ret = 0;
         while (x>0)
         {
-            int temp  = result*10+x%10;
-            if (temp/10!=result) return 0;
-            result = temp;
+            if (ret > INT_MAX/10) return 0;
+            if (ret*10 > INT_MAX - x%10 ) return 0;
+            
+            ret = ret*10 + x%10;
             x = x/10;
         }
-        return flag*result;
+        
+        return ret*sign;
     }
 };
