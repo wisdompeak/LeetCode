d09da5783de250a03734b0bba3a220afd4b1172f
wisdompeak <guan.huifeng@gmail.com>
2019-10-13 02:06:09 -0700

    Update 765.Couples-Holding-Hands_v2.cpp

--- Greedy/765.Couples-Holding-Hands/765.Couples-Holding-Hands_v2.cpp
+++ Greedy/765.Couples-Holding-Hands/765.Couples-Holding-Hands_v2.cpp
@@ -1,48 +1,51 @@
 class Solution {
+    vector<int>Father;
 public:
     int minSwapsCouples(vector<int>& row) 
-    {        
-        if (row.size()==0) return 0;
-        
+    {
         int N = row.size();
-        unordered_map<int,int>Pos;
-        unordered_map<int,int>coupleID;
+        Father.resize(N);
         
-        for (int i=0; i<N; i++)
+        for (int i=0; i<N; i+=2)
         {
-            Pos[row[i]] = i;
-            coupleID[i] = i/2;
-        }
-            
-        int count = 0;
-        
-        for (int k=0; k<N; k+=2)
-        {           
-            if (coupleID[row[k]]==coupleID[row[k+1]])
-                continue;
-
-            int flag=0;       
-            
-            int holder = row[k];
-            int partner = coupleID[holder]*2==holder? holder+1:holder-1;
-            int curPos = k+1;
-            
-            while (flag==0)
-            {                
-                swap(row[Pos[partner]],row[curPos]);                
-                count++;
-                
-                holder = row[Pos[partner]];
-                int holder_pos = Pos[partner];
-                
-                curPos = (holder_pos%2==0) ? holder_pos+1:holder_pos-1;
-                partner = coupleID[holder]*2==holder? holder+1:holder-1;
-                                
-                flag = row[curPos]==partner;
-            }
-            
+            Father[i] = i;
+            Father[i+1] = i;
         }
         
-        return count;
+        for (int i=0; i<N; i+=2)
+        {
+            int a = row[i];
+            int b = row[i+1];
+            if (FindFather(a)!=FindFather(b))
+                Union(a,b);
+        }
+        
+        unordered_map<int,int>count;
+        for (int i=0; i<N; i++)
+            count[FindFather(i)] += 1;
+        
+        int ret = 0;
+        for (auto x: count)
+           ret += x.second/2-1;
+        
+        return ret;
+    }
+    
+    int FindFather(int x)
+    {
+        if (Father[x]!=x)
+            Father[x] = FindFather(Father[x]);
+        return Father[x];
+    }
+    
+    void Union(int x, int y)
+    {
+        x = Father[x];
+        y = Father[y];
+        if (x<y)
+            Father[y] = x;
+        else
+            Father[x] = y;
     }
 };
+
