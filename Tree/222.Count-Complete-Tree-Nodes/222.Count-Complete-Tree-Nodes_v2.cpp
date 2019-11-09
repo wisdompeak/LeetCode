0cec9132354b5ef3716a73fd8a4731dce6684c2c
wisdompeak <guan.huifeng@gmail.com>
2019-11-09 00:59:39 -0800

    Create 222.Count-Complete-Tree-Nodes_v2.cpp

---
+++ Tree/222.Count-Complete-Tree-Nodes/222.Count-Complete-Tree-Nodes_v2.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) 
+    {
+        if (root==NULL) return 0;
+        int h = 0;
+        TreeNode* node = root;
+        while (node!=NULL)
+        {
+            h++;
+            node = node->left;            
+        }
+        int low = 1;
+        int hi = (1<<h)-1;
+        
+        while (low<hi)
+        {
+            int mid = low+(hi-low+1)/2;
+            if (hasK(root,mid))
+                low = mid;
+            else
+                hi = mid-1;
+        }
+        return low;
+    }
+    
+    bool hasK(TreeNode* root, int K)
+    {
+        vector<int>path;
+        while (K>0)
+        {
+            path.push_back(K);
+            K = K/2;
+        }
+        for (int i=path.size()-1; i>=0; i--)
+        {
+            if (root==NULL) return false;
+            if (i==0) return true;
+            if (path[i-1]==path[i]*2)
+                root = root->left;
+            else
+                root = root->right;            
+        }
+        return true;
+    }
+};
