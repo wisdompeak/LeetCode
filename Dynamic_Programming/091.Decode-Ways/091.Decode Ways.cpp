ab36f90c9426d1b20c82f2637cc27217568eaf29
wisdompeak <guan.huifeng@gmail.com>
2019-11-12 19:20:39 -0800

    Update 091.Decode Ways.cpp

--- Dynamic_Programming/091.Decode-Ways/091.Decode Ways.cpp
+++ Dynamic_Programming/091.Decode-Ways/091.Decode Ways.cpp
@@ -2,39 +2,34 @@ class Solution {
 public:
     int numDecodings(string s) 
     {
-        s.insert(s.begin(),'0');
-        int N=s.size();
-        vector<int>dp(s.size(),0);
+        if (s.size()==0) return 0;
         
-        if (N==1) return 0;
+        int N =s.size();
+        s = "#"+s;
+        vector<int>dp(N+1,0);
         
-        dp[0]=1;
+        dp[0] = 1;
+        dp[1] = s[1]=='0'? 0:1;
         
-        if (s[1]=='0') 
-            dp[1]=0;
-        else
-            dp[1]=1;
-        if (N==1) return dp[1];
-                
-        for (int i=2; i<s.size(); i++)
+        for (int i=2; i<=N; i++)
         {
-            int part1, part2;
+            // Check if s[i] alone can stand for a letter
+            if (s[i]!='0')
+            {
+                dp[i] += dp[i-1];
+            }
             
-            if (s[i]=='0')
-                part1 = 0;
-            else
-                part1 = dp[i-1];
-                
-            int num = stoi(s.substr(i-1,2));
-            if (num>=10 && num<=26)
-                part2 = dp[i-2];
-            else
-                part2 = 0;
-                
-            dp[i] = part1+part2;
+            // Check if s[i-1:i] can stand for a letter
+            if (s[i]>='0' && s[i]<='6' && (s[i-1]=='1'||s[i-1]=='2'))
+                dp[i]+=dp[i-2];
+            else if (s[i]>='7' && s[i]<='9' && s[i-1]=='1')
+                dp[i]+=dp[i-2];
+            
+            // cout<<i<<" "<<dp[i]<<endl;
         }
         
-        return dp.back();
+        return dp[N];
         
     }
 };
+
