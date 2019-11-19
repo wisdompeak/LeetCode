74e424cb28caffa679e069c96a79fff52a4fb5e0
wisdompeak <guan.huifeng@gmail.com>
2019-11-19 00:40:34 -0800

    Update 829.Consecutive-Numbers-Sum.cpp

--- Math/829.Consecutive-Numbers-Sum/829.Consecutive-Numbers-Sum.cpp
+++ Math/829.Consecutive-Numbers-Sum/829.Consecutive-Numbers-Sum.cpp
@@ -6,9 +6,7 @@ public:
         
         for (int m=1; m<=sqrt(2*N)+1; m++)
         {
-            if ((2*N-m*(m-1))%(2*m)!=0)
-                continue;
-            if ((2*N-m*(m-1))/(2*m)>=1)
+            if ((2*N-m*(m-1))%(2*m)==0 && 2*N-m*(m-1)>0)
                 count++;
         }
         return count;
