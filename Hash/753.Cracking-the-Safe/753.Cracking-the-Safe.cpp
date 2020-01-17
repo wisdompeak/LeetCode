88679aa77bfdf4d097502237444d458bf83977d3
wisdompeak <guan.huifeng@gmail.com>
2020-01-17 05:04:18 -0800

    Update 753.Cracking-the-Safe.cpp

--- Hash/753.Cracking-the-Safe/753.Cracking-the-Safe.cpp
+++ Hash/753.Cracking-the-Safe/753.Cracking-the-Safe.cpp
@@ -3,18 +3,17 @@ public:
     string crackSafe(int n, int k) 
     {
         string ans;
-        ans.assign(n,'0');
+        ans.assign(n-1,'0');
         unordered_map<string,int>record;
         
         int total = pow(k,n);
         
-        for (int i=1; i<total; i++)
+        for (int i=0; i<total; i++)
         {
             string temp = ans.substr(ans.size()-(n-1),(n-1));
             record[temp] = (record[temp]+1)%k;;
             ans+= '0'+record[temp];
         }
-        return ans;
-        
+        return ans;        
     }
 };
