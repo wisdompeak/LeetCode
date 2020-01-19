a99626f046e5450401eac299ece4f50954d52254
wisdompeak <guan.huifeng@gmail.com>
2020-01-19 02:16:58 -0800

    Create 1312.Minimum-Insertion-Steps-to-Make-a-String-Palindrome.cpp

---
+++ Dynamic_Programming/1312.Minimum-Insertion-Steps-to-Make-a-String-Palindrome/1312.Minimum-Insertion-Steps-to-Make-a-String-Palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minInsertions(string s) 
+    {
+        int n = s.size();
+        auto dp = vector<vector<int>>(1+n,vector<int>(1+n,0));
+        string t = s;
+        reverse(t.begin(),t.end());
+        s = "#"+s;
+        t = "#"+t;
+        
+        for (int i=1; i<=n; i++)
+        {
+            dp[i][0] = i;
+            dp[0][i] = i;
+        }
+        
+        for (int i=1; i<=n; i++)
+            for (int j=1; j<=n; j++)
+            {
+                if (s[i]==t[j])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = min(min(dp[i-1][j]+1, dp[i][j-1]+1), dp[i-1][j-1]+2);
+                //cout<<i<<" "<<j<<" "<<dp[i][j]<<endl;
+            }
+        return dp[n][n]/2;
+    }
+};
