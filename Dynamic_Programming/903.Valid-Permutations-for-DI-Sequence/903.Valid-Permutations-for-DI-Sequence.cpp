d43c535f15558aaf541ae59642af06931ea5bb4e
wisdompeak <guan.huifeng@gmail.com>
2020-01-31 21:51:10 -0800

    Update 903.Valid-Permutations-for-DI-Sequence.cpp

--- Dynamic_Programming/903.Valid-Permutations-for-DI-Sequence/903.Valid-Permutations-for-DI-Sequence.cpp
+++ Dynamic_Programming/903.Valid-Permutations-for-DI-Sequence/903.Valid-Permutations-for-DI-Sequence.cpp
@@ -4,35 +4,30 @@ public:
     {
         int N = S.size();
         S = "#"+S;
-        auto dp = vector<vector<int>>(N+1,vector<int>(N+1));
+        auto dp = vector<vector<int>>(N+1,vector<int>(N+1,0));
+
         dp[0][0] = 1;
         int M = 1e9+7;
-        
-        for (int i=1; i<=N; i++)        
+
+        for (int i=1; i<=N; i++)
             for (int j=0; j<=i; j++)
             {
-                if (S[i]=='D')
+                if (S[i]=='I')
                 {
-                    for (int k=j; k<=i-1; k++)
-                    {
-                        dp[i][j] += dp[i-1][k];
-                        dp[i][j]%=M;
-                    }                        
+                    for (int k=j; k<=N; k++)
+                        dp[i][j] = (dp[i][j] + dp[i-1][k]) % M;
                 }
                 else
                 {
                     for (int k=0; k<j; k++)
-                    {
-                        dp[i][j] += dp[i-1][k];
-                        dp[i][j]%=M;
-                    }                        
-                }                    
+                        dp[i][j] = (dp[i][j] + dp[i-1][k]) % M;
+                }
             }
-        
-        int result = 0;
-        for (int j=0; j<=N; j++)
-            result = (result+dp[N][j])%M;
-        
-        return result;
+
+        int ret = 0;
+        for (int i=0; i<=N; i++)
+            ret = (ret+dp[N][i])%M;
+        return ret;
+            
     }
 };
