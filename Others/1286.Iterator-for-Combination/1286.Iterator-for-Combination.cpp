945e2f2e3ad7698eff884338bbf8d9d985b6332d
wisdompeak <guan.huifeng@gmail.com>
2020-02-07 23:02:03 -0800

    Create 1286.Iterator-for-Combination.cpp

---
+++ Others/1286.Iterator-for-Combination/1286.Iterator-for-Combination.cpp
@@ -0,0 +1,42 @@
+class CombinationIterator {
+    string cur;
+    string end;
+    bool flag;
+    string characters;
+    int combinationLength;
+    
+public:
+    CombinationIterator(string characters, int combinationLength) 
+    {
+        cur = characters.substr(0,combinationLength);
+        end = characters.substr(characters.size()-combinationLength);
+        flag = 1;
+        this->characters = characters;
+        this->combinationLength = combinationLength;        
+    }
+    
+    string next() 
+    {
+        if (flag)
+        {
+            flag = 0;
+            return cur;
+        }
+        
+        int i = cur.size()-1;
+        while (i>=0 && cur[i]==characters[i+characters.size()-combinationLength])
+            i--;
+        int j = 0;
+        while (cur[i]!=characters[j])
+            j++;
+        for (int k=i; k<cur.size(); k++)
+            cur[k] = characters[j+1 + k-i];  // cur[i] = characters[j+1]
+        
+        return cur;
+    }
+    
+    bool hasNext() 
+    {
+        return cur!=end;
+    }
+};
