a8e51d2d9f3fd0de081c3a4815278eac838a9072
wisdompeak <guan.huifeng@gmail.com>
2020-02-08 22:48:52 -0800

    Create 1348.Tweet-Counts-Per-Frequency.cpp

---
+++ Heap/1348.Tweet-Counts-Per-Frequency/1348.Tweet-Counts-Per-Frequency.cpp
@@ -0,0 +1,61 @@
+class TweetCounts {
+    unordered_map<string,multiset<int>>Map;
+public:
+    TweetCounts() {
+        
+    }
+    
+    void recordTweet(string tweetName, int time) 
+    {
+        Map[tweetName].insert(time);
+    }
+    
+    vector<int> getTweetCountsPerFrequency(string freq, string tweetName, int startTime, int endTime) 
+    {
+        vector<int>rets;
+                
+        int count = 0;
+        int delta = 0;
+        if (freq=="minute")
+            delta = 60;
+        else if (freq=="hour")
+            delta = 60*60;
+        else
+            delta = 60*60*24;
+        
+        auto iter = Map[tweetName].lower_bound(startTime);
+                
+        
+        while (startTime <= endTime)
+        {
+            if (iter==Map[tweetName].end() || *iter>endTime)
+            {
+                rets.push_back(count);
+                count = 0;
+                startTime += delta;
+                continue;
+            }
+            
+            if (*iter < startTime+delta)
+            {
+                count++;
+                iter = next(iter,1);
+            }
+            else
+            {
+                rets.push_back(count);
+                count = 0;
+                startTime += delta;
+            }
+        }
+        
+        return rets;
+    }
+};
+
+/**
+ * Your TweetCounts object will be instantiated and called as such:
+ * TweetCounts* obj = new TweetCounts();
+ * obj->recordTweet(tweetName,time);
+ * vector<int> param_2 = obj->getTweetCountsPerFrequency(freq,tweetName,startTime,endTime);
+ */
