57ac4dade80f0ada811e306a40459d709e0c6ffc
wisdompeak <guan.huifeng@gmail.com>
2020-02-29 00:20:04 -0800

    Update 518.Coin-Change-2.cpp

--- Dynamic_Programming/518.Coin-Change-2/518.Coin-Change-2.cpp
+++ Dynamic_Programming/518.Coin-Change-2/518.Coin-Change-2.cpp
@@ -1,21 +1,17 @@
 class Solution {
-    int result;
 public:
     int change(int amount, vector<int>& coins) 
     {
-        if (amount==0) return 1;
-        if (coins.size()==0) return 0;
-        
         vector<int>dp(amount+1,0);
-        dp[0]=1;
-        
-        for (int i=0; i<coins.size(); i++)
+        dp[0] = 1;
+        for (int coin: coins) 
         {
-            for (int j = coins[i]; j<=amount; j++)
-                dp[j] += dp[j-coins[i]];
+            for (int i=1; i<=amount; i++)
+            {
+                if (i>=coin)
+                    dp[i] += dp[i-coin];
+            }
         }
-        
-        return dp[amount];        
-    }
-    
+        return dp[amount];
+    }    
 };
