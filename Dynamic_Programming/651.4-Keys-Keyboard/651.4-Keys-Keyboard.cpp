0b257b51b8cc7bc39d0c7ddb7622d63d68be1aa1
wisdompeak <guan.huifeng@gmail.com>
2020-03-03 23:59:16 -0800

    Update 651.4-Keys-Keyboard.cpp

--- Dynamic_Programming/651.4-Keys-Keyboard/651.4-Keys-Keyboard.cpp
+++ Dynamic_Programming/651.4-Keys-Keyboard/651.4-Keys-Keyboard.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int maxA(int N) 
     {
+        if (N==1) return 1;
+        
         vector<int>dp(N+1,0);
-                
-        dp[1]=1;
-        dp[2]=2;
-                    
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        
         for (int i=3; i<=N; i++)
-        {
-            dp[i]=i;
-            for (int j=1; j<i; j++)
+        {            
+            for (int j=0; j<=i-1; j++)
             {
-                dp[i]=max(dp[i],dp[j]+i-j);
-                if (j>2)
-                    dp[i]=max(dp[i],dp[j]*(i-j-2+1));
+                dp[i] = max(dp[i], dp[j]+i-j);
+                if (i-j>=2)
+                    dp[i] = max(dp[i], dp[j]*(i-j-2+1));
             }
         }
-        
-        return dp[N];
+        return dp[N];        
     }
 };
