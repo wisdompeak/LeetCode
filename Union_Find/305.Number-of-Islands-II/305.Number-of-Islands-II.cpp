f48f7b70be9d8ba1377e04c2a05d530d6a04cfbb
wisdompeak <guan.huifeng@gmail.com>
2020-03-07 00:04:09 -0800

    Update 305.Number-of-Islands-II.cpp

--- Union_Find/305.Number-of-Islands-II/305.Number-of-Islands-II.cpp
+++ Union_Find/305.Number-of-Islands-II/305.Number-of-Islands-II.cpp
@@ -1,41 +1,47 @@
 class Solution {
-    unordered_map<int,int>Father;
-    int M;
-    int N;
+    vector<int>Father;
 public:
-    vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) 
+    vector<int> numIslands2(int m, int n, vector<vector<int>>& positions) 
     {
-        M=m;
-        N=n;
-        for (int i=0; i<m*n; i++)        
-            Father[i]=-1;
+        Father.resize(m*n);
+        for (int i=0; i<m*n; i++)         
+             Father[i]=-1;
         
         vector<pair<int,int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
         int count=0;
-        vector<int>results;
+        vector<int>result;
         
-        for (int i=0; i<positions.size(); i++)        
+        for (int i=0; i<positions.size(); i++)
         {
-            int x=positions[i].first;
-            int y=positions[i].second;
-            Father[x*N+y]=x*N+y;
-            count++;
+            int x=positions[i][0];
+            int y=positions[i][1];
+            if (Father[x*n+y]!=-1)
+            {
+                result.push_back(result.back());
+                continue;
+            }
+            
+            Father[x*n+y]=x*n+y;
+            count+=1;
             
             for (int k=0; k<4; k++)
             {
                 int a=x+dir[k].first;
                 int b=y+dir[k].second;
-                if (a<0||a>=M||b<0||b>=N) continue;
-                if (Father[a*N+b]==-1) continue;
-                if (FindSet(x*N+y)!=FindSet(a*N+b))
+                if (a<0||a>=m||b<0||b>=n)
+                    continue;
+                if (Father[a*n+b]==-1)
+                    continue;
+                if (FindSet(a*n+b)!=FindSet(x*n+y))
                 {
-                    Union(x*N+y,a*N+b);
                     count--;
-                }                
+                    Union(a*n+b,x*n+y);                    
+                }                    
             }
-            results.push_back(count);
+            result.push_back(count);
         }
-        return results;
+        
+        return result;
     }
     
     int FindSet(int x)
@@ -49,17 +55,10 @@ public:
     {
         x=Father[x];
         y=Father[y];
-        bool flag;
-        if (x/N<y<N)
-            flag=1;
-        else if (x/N==y<N && x%N<y%N)
-            flag=1;
-        else
-            flag=0;
         
-        if (flag)
+        if (x<y)
             Father[y]=x;
         else
-            Father[x]=y;
+            Father[x]=y;        
     }
 };
