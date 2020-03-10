ad3dab783d8e8c35c5289953d5dca25126f0c516
wisdompeak <guan.huifeng@gmail.com>
2020-03-10 00:12:05 -0700

    Update 213.House-Robber-II_v2.cpp

--- Dynamic_Programming/213.House-Robber-II/213.House-Robber-II_v2.cpp
+++ Dynamic_Programming/213.House-Robber-II/213.House-Robber-II_v2.cpp
@@ -1,26 +1,22 @@
 class Solution {
-    map<vector<int>,int>Map;
 public:
     int rob(vector<int>& nums) 
     {
         int n = nums.size();
         if (n==0) return 0;
-        return max(nums[0]+DFS(2,n-2,nums), DFS(1,n-1,nums));
-    }
-    
-    int DFS(int i, int j, vector<int>&nums)
-    {
-        if (i>j) return 0;
+        if (n==1) return nums[0];
         
-        if (i==j) return nums[i];
+        auto dp = vector<vector<int>>(n, vector<int>(n,0));
+        for (int i=0; i<n; i++)
+            dp[i][i] = nums[i];
         
-        if (Map.find({i,j})!=Map.end())
-            return Map[{i,j}];
+        for (int len = 2; len <=n; len++)
+            for (int i= 0; i+len-1<n; i++)
+            {
+                int j = i+len-1;
+                dp[i][j] = max(nums[i]+ ((i+2>j)?0:dp[i+2][j]), dp[i+1][j]);
+            }
         
-        int ret = max(nums[i]+DFS(i+2,j,nums), DFS(i+1,j,nums));
-        
-        Map[{i,j}] = ret;
-        
-        return ret;
+        return max(nums[0]+((2>n-2)?0:dp[2][n-2]), dp[1][n-1]);
     }
 };
