e368a1bffcbc2db858c61de8bbfafdb5a0c7f8c1
wisdompeak <guan.huifeng@gmail.com>
2020-03-16 23:22:37 -0700

    Update 740.Delete-and-Earn.cpp

--- Dynamic_Programming/740.Delete-and-Earn/740.Delete-and-Earn.cpp
+++ Dynamic_Programming/740.Delete-and-Earn/740.Delete-and-Earn.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) 
     {
+        vector<int>gain(10001,0);
+        for (auto x: nums)
+            gain[x] += x;
+
         vector<int>dp(10001,0);
-        for (int i=0; i<nums.size(); i++)
-            dp[nums[i]]+=nums[i];
-        
-        for (int i=2; i<=10000; i++)             
-            dp[i]=max(dp[i]+dp[i-2],dp[i-1]);
+        dp[1] = gain[1];
+
+        for (int i=2; i<=10000; i++)
+        {
+            dp[i] = max(dp[i-2]+gain[i], dp[i-1]);
+        }
         return dp[10000];
     }
 };
