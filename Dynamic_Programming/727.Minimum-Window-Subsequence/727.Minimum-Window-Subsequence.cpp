b6b0dc2e02acb6b5b190e420e5eb6d769cee74dc
wisdompeak <guan.huifeng@gmail.com>
2020-03-30 22:42:19 -0700

    Update 727.Minimum-Window-Subsequence.cpp

--- Dynamic_Programming/727.Minimum-Window-Subsequence/727.Minimum-Window-Subsequence.cpp
+++ Dynamic_Programming/727.Minimum-Window-Subsequence/727.Minimum-Window-Subsequence.cpp
@@ -2,43 +2,41 @@ class Solution {
 public:
     string minWindow(string S, string T) 
     {
-        int M=S.size();
-        int N=T.size();
-        S.insert(S.begin(),'0');
-        T.insert(T.begin(),'0');
-        int inf=100000;
-        auto dp=vector<vector<int>>(N+1,vector<int>(M+1,inf));
+        int m = S.size();
+        int n = T.size();
+        S = "#"+S;
+        T = "#"+T;
+        auto dp = vector<vector<int>>(m+1,vector<int>(n+1,0));
         
-        for (int j=0; j<=M; j++)
-            dp[0][j]=0;
+        for (int j=1; j<=n; j++)
+            dp[0][j] = INT_MAX/2;
+        for (int i=0; i<=m; i++)
+            dp[i][0] = 0;
         
-        for (int i=1; i<=N; i++)
-         for (int j=1; j<=M; j++)
-         {
-             if (T[i]==S[j])
-                 dp[i][j]=dp[i-1][j-1]+1;
-             else
-                 dp[i][j]=dp[i][j-1]+1;
-         }
-        
-        int len_min=INT_MAX;
-        for (int j=1; j<=M; j++)
-            len_min=min(len_min,dp[N][j]);
-        
-        if (len_min>=inf) return "";
-        
-        int k;
-        for (int j=1; j<=M; j++)
-        {
-            if (len_min==dp[N][j])
+        for (int i=1; i<=m; i++)
+            for (int j=1; j<=n; j++)
             {
-                k=j;
-                break;
+                if (S[i]==T[j])
+                    dp[i][j] = dp[i-1][j-1]+1;
+                else
+                    dp[i][j] = dp[i-1][j]+1;
+            }
+        
+        int len  = INT_MAX/2;
+        int pos;
+        for (int i=1; i<=m; i++)
+        {
+            if (dp[i][n] < len)
+            {
+                len = dp[i][n];
+                pos = i;
             }
         }
         
-        //cout<<k<<endl;
-        //cout<<len_min<<endl;
-        return S.substr(k-len_min+1,len_min);
+        if (len>=INT_MAX/2)
+            return "";
+        else
+            return S.substr(pos-len+1,len);
+        
     }
 };
