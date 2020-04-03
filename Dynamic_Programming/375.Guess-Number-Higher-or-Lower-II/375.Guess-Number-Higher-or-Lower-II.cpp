4cc9b9c2276e00dc7bc089a1522fa482939fdf4c
wisdompeak <guan.huifeng@gmail.com>
2020-04-02 19:24:25 -0700

    Update 375.Guess-Number-Higher-or-Lower-II.cpp

--- Dynamic_Programming/375.Guess-Number-Higher-or-Lower-II/375.Guess-Number-Higher-or-Lower-II.cpp
+++ Dynamic_Programming/375.Guess-Number-Higher-or-Lower-II/375.Guess-Number-Higher-or-Lower-II.cpp
@@ -2,25 +2,17 @@ class Solution {
 public:
     int getMoneyAmount(int n) 
     {
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-                
-        for (int i=1; i<=n-1; i++)
-            dp[i][i+1]=i;        
-        
-        for (int len=3; len<=n; len++)
-        {
-            for (int start=1; start<=n-len+1; start++)
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));                
+               
+        for (int len=2; len<=n; len++)        
+            for (int i=1; i+len-1<=n; i++)
             {
-                int temp = INT_MAX;
-                for (int x=start+1; x<start+len-1; x++)
-                {                    
-                    temp = min( temp , x+max(dp[start][x-1], dp[x+1][start+len-1]));
-                }
-                dp[start][start+len-1]=temp;
-            }
-        }
-        
-        return dp[1][n];
-        
+                int j = i+len-1;
+                dp[i][j] = INT_MAX/2;
+                                
+                for (int k=i; k<=j; k++)                                   
+                    dp[i][j] = min(dp[i][j] , k + max(dp[i][k-1], dp[k+1][j]));                
+            }            
+        return dp[1][n];        
     }
 };
