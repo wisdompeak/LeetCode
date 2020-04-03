0ce906f36ecfa99680eec7b6291fe99f4f892242
wisdompeak <guan.huifeng@gmail.com>
2020-04-02 18:23:09 -0700

    Update 410.Split Array Largest Sum_dp.cpp

--- Dynamic_Programming/410.Split-Array-Largest-Sum/410.Split Array Largest Sum_dp.cpp
+++ Dynamic_Programming/410.Split-Array-Largest-Sum/410.Split Array Largest Sum_dp.cpp
@@ -3,20 +3,19 @@ public:
     int splitArray(vector<int>& nums, int m) 
     {
         int N = nums.size();
-        auto dp = vector<vector<long>>(N+1,vector<long>(m+1));
+        auto dp = vector<vector<long>>(N+1,vector<long>(m+1, INT_MAX));
         nums.insert(nums.begin(),0);
         
-        for (int i=1; i<=N; i++)
-            dp[i][1] = dp[i-1][1]+nums[i];
+        dp[0][0] = 0;
         
         for (int i=1; i<=N; i++)
-            for (int k=2; k<=min(m,i); k++)
+            for (int k=1; k<=min(m,i); k++)
             {
-                dp[i][k] = INT_MAX;
-                for (int j=1; j<=i-1; j++)
+                long sum = 0;
+                for (int j=i; j>=k; j--)
                 {
-                    long temp = max(dp[j][k-1],dp[i][1]-dp[j][1]);
-                    dp[i][k] = min(dp[i][k], temp);
+                    sum += nums[j];
+                    dp[i][k] = min(dp[i][k], max(dp[j-1][k-1], sum));
                 }
             }
         
