f6e62c876630cc23ce8c1902c9514329398aab99
wisdompeak <guan.huifeng@gmail.com>
2020-04-02 17:55:20 -0700

    Update 813.Largest-Sum-of-Averages.cpp

--- Dynamic_Programming/813.Largest-Sum-of-Averages/813.Largest-Sum-of-Averages.cpp
+++ Dynamic_Programming/813.Largest-Sum-of-Averages/813.Largest-Sum-of-Averages.cpp
@@ -2,30 +2,28 @@ class Solution {
 public:
     double largestSumOfAverages(vector<int>& A, int K) 
     {
-        int N=A.size();
+        int N = A.size();
+        A.insert(A.begin(), 0);
+        auto dp = vector<vector<double>>(N+1, vector<double>(K+1,0));
         
-        auto dp=vector<vector<double>>(N,vector<double>(K+1,0));
+        dp[0][0] = 0;        
+        for (int i=1; i<=N; i++)
+            dp[i][0] = INT_MIN/2;        
         
-        double sum=0;
-        for (int i=0; i<N; i++)
-        {
-            sum+=A[i];
-            dp[i][1]=sum*1.0/(i+1);
-        }
-        
-        for (int k=2; k<=K; k++)
-        {
-            for (int i=k-1; i<N; i++)
+        for (int i=1; i<=N; i++)
+            for (int k=1; k<=min(i,K); k++)
             {
-                int sum = 0;
-                for (int j=i; j>=k-1; j--)
+                double sum = 0;
+                for (int j=i; j>=k; j--)
                 {
-                    sum+=A[j];
-                    dp[i][k]=max(dp[i][k], sum*1.0/(i-j+1)+dp[j-1][k-1]);
+                    sum += A[j];
+                    dp[i][k] = max(dp[i][k], dp[j-1][k-1]+sum*1.0/(i-j+1));
                 }
             }
-        }
         
-        return dp[N-1][K];
+        double ret = 0;
+        for (int k=1; k<=K; k++)
+            ret = max(ret, dp[N][k]);
+        return ret;
     }
 };
