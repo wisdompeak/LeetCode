80a4fb8b60b498186baf6aef2188f8b697679ddf
wisdompeak <guan.huifeng@gmail.com>
2020-04-15 01:54:39 -0700

    Update 516.Longest-Palindromic-Subsequence.cpp

--- Dynamic_Programming/516.Longest-Palindromic-Subsequence/516.Longest-Palindromic-Subsequence.cpp
+++ Dynamic_Programming/516.Longest-Palindromic-Subsequence/516.Longest-Palindromic-Subsequence.cpp
@@ -6,14 +6,12 @@ public:
         s = "#" + s;
         auto dp = vector<vector<int>>(N+1,vector<int>(N+1,0));
         
-        for (int len=1; len<=N; len++)
+        for (int i=1; i<=N; i++)
+            dp[i][i] = 1;
+        
+        for (int len=2; len<=N; len++)
             for (int i=1; i+len-1<=N; i++)
-            {
-                if (len==1)
-                {
-                    dp[i][i] = 1;
-                    continue;
-                }
+            {                
                 int j = i+len-1;
                 if (s[i]==s[j])
                     dp[i][j] = dp[i+1][j-1]+2;
