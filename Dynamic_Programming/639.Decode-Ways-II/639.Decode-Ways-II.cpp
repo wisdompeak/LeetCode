fb7ec0f44877e29ba5bb2602fbd89bd793d857e5
wisdompeak <guan.huifeng@gmail.com>
2020-04-23 23:59:29 -0700

    Update 639.Decode-Ways-II.cpp

--- Dynamic_Programming/639.Decode-Ways-II/639.Decode-Ways-II.cpp
+++ Dynamic_Programming/639.Decode-Ways-II/639.Decode-Ways-II.cpp
@@ -2,71 +2,42 @@ class Solution {
 public:
     int numDecodings(string s) 
     {
-        long long N=pow(10,9)+7;
-        s = "x"+s;
-        long long k, k1, k2 = 1;
-        
-        if (s[1]=='*') k1=9;
-        if (s[1]=='0') k1=0;
-        if (s[1]>='1' && s[1]<='9') k1=1;
-        if (s.size()==2) return k1;        
-        
-        for (int i=2; i<s.size(); i++)
-        {                
-            if (s[i]=='0')
+        int n = s.size();
+        s = "#"+s;
+        vector<long>dp(n+1,0);
+        long M = 1e9+7;
+
+        dp[0] = 1;
+        if (s[1] == '*') dp[1] = 9;
+        else if (s[1] == '0') dp[1] = 0;
+        else dp[1] = 1;
+
+        for (int i=2; i<=n; i++)
+        {
+            if (isdigit(s[i]))
             {
-                if (s[i-1]=='0' || s[i-1]>='3' && s[i-1]<='9')
-                    return 0;
-                else if (s[i-1]=='1'||s[i-1]=='2')
-                    k=k2;
-                else if (s[i-1]=='*')
-                    k=k2*2;
-            }
-            else if (s[i]>='1' && s[i]<='6')
-            {
-                k=k1;
-                if (s[i-1]=='*')
-                    k+=k2*2;
-                else if (s[i-1]=='0')
-                    ;
+                dp[i] += s[i]=='0' ? 0:dp[i-1];
+                if (isdigit(s[i-1]))
+                {
+                    int num = (s[i-1]-'0')*10+(s[i]-'0');
+                    if (num>=10 && num<=26)
+                        dp[i]+=dp[i-2];
+                }
                 else
                 {
-                    int num=stoi(s.substr(i-1,2));
-                    if (num>=11 && num<=26)
-                    k+=k2;
+                    if (s[i]<='6') dp[i] += dp[i-2]*2;
+                    else dp[i] += dp[i-2];
                 }                
             }
-            else if (s[i]>='7' && s[i]<='9')
+            else
             {
-                k=k1;
-                if (s[i-1]=='*')
-                    k+=k2;
-                else if (s[i-1]=='0')
-                    ;
-                else
-                {
-                    int num=stoi(s.substr(i-1,2));
-                    if (num>=11 && num<=26)
-                    k+=k2;
-                }
+                dp[i] += dp[i-1]*9;
+                if (s[i-1]=='1') dp[i]+=dp[i-2]*9;
+                else if (s[i-1]=='2') dp[i]+=dp[i-2]*6;
+                else if (s[i-1]=='*') dp[i]+=dp[i-2]*15;
             }
-            else if (s[i]=='*')
-            {
-                k=k1*9;
-                if (s[i-1]=='*')
-                    k+=k2*15;
-                else if (s[i-1]=='0' || s[i-1]>='3' && s[i-1]<='9')
-                    ;
-                else if (s[i-1]=='1')
-                    k+=k2*9;
-                else if (s[i-1]=='2')
-                    k+=k2*6;                
-            }            
-            
-            k = k%N;
-            k2 = k1;
-            k1 = k;            
-        }        
-        return k;
+            dp[i]%=M;
+        }
+        return dp[n];
     }
 };
