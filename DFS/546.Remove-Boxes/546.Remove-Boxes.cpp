dc39be4f2b7471d3249c376803c37c01570bc2a6
wisdompeak <guan.huifeng@gmail.com>
2020-05-03 13:35:38 -0700

    Update 546.Remove-Boxes.cpp

--- DFS/546.Remove-Boxes/546.Remove-Boxes.cpp
+++ DFS/546.Remove-Boxes/546.Remove-Boxes.cpp
@@ -1,35 +1,39 @@
 class Solution {    
+    vector<int>boxes;
+    int dp[100][100][100];
     
 public:
     int removeBoxes(vector<int>& boxes) 
     {                
-        int dp[100][100][100]={0};
-        
-        return DFS(boxes, dp, 0, boxes.size()-1, 0);        
+        this->boxes = boxes;       
+        return DFS(0, boxes.size()-1, 0);        
     }
     
-    int DFS(vector<int>& boxes, int dp[100][100][100], int l, int r, int k)
+    int DFS(int l, int r, int k)
     {
         if (l>r) return 0;
         
         if (dp[l][r][k]!=0)
             return dp[l][r][k];                
         
-        while (r>l && boxes[r]==boxes[r-1]) 
+        int i = r;
+        int count = k;
+        while (i>=l && boxes[i]==boxes[r]) 
         {
-            r--;
-            k++;
-        }
+            i--;
+            count++;
+        }        
+        dp[l][r][k] = DFS(l,i,0) + count*count;
         
-        dp[l][r][k] = DFS(boxes,dp,l,r-1,0) + (k+1)*(k+1);
-        
-        for (int i=l; i<r; i++)
+        for (int j=i; j>=l; j--)
         {
-            if (boxes[i]==boxes[r])
-            {
-                dp[l][r][k] = max(dp[l][r][k], DFS(boxes,dp,l,i,k+1) + DFS(boxes,dp,i+1,r-1,0));
-            }
+            if (boxes[j]!=boxes[r]) continue;
+            if (boxes[j]==boxes[r] && boxes[j+1]==boxes[r]) continue;
+            dp[l][r][k] = max(dp[l][r][k], DFS(l,j,count)+DFS(j+1,i,0));
         }
+
+        // cout<<l<<" "<<r<<" "<<k<<":"<<dp[l][r][k]<<endl;
+
         return dp[l][r][k];
     }
 };
