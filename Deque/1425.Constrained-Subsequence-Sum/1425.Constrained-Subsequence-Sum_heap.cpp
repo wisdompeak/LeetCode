1a111d3652db023e7d80fc4d04fcd9586245b3ea
wisdompeak <guan.huifeng@gmail.com>
2020-05-09 16:20:16 -0700

    Update 1425.Constrained-Subsequence-Sum_heap.cpp

--- Deque/1425.Constrained-Subsequence-Sum/1425.Constrained-Subsequence-Sum_heap.cpp
+++ Deque/1425.Constrained-Subsequence-Sum/1425.Constrained-Subsequence-Sum_heap.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     int constrainedSubsetSum(vector<int>& nums, int k) 
     {
-        int n = nums.size();
-        nums.insert(nums.begin(),0);
-        vector<int>dp(n+1,0);
+        int n = nums.size();        
+        vector<int>dp(n,0);
         multiset<int>Set({0});
 
         int ret = INT_MIN;
-        for (int i=1; i<=n; i++)
+        for (int i=0; i<n; i++)
         {
-            if (Set.size()>k)            
+            if (Set.size()>k && i-k-1>=0)            
                 Set.erase(Set.lower_bound(dp[i-k-1]));            
 
-            dp[i] = max(nums[i], *Set.rbegin()+nums[i]);
+            dp[i] = nums[i];
+            if (Set.size()>0) dp[i] = max(dp[i], *Set.rbegin()+nums[i]);
             ret = max(ret, dp[i]);
             Set.insert(dp[i]);
         }
-
         return ret;
     }
 };
