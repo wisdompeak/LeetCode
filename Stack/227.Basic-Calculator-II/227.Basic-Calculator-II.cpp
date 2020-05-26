75b7cd0d29fd2c5e200abcd04da58daf6c6a6936
wisdompeak <guan.huifeng@gmail.com>
2020-05-25 18:51:59 -0700

    Create 227.Basic-Calculator-II.cpp

---
+++ Stack/227.Basic-Calculator-II/227.Basic-Calculator-II.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int calculate(string s) 
+    {
+        string S = "+";
+        for (auto ch:s)
+        {
+            if (ch==' ') continue;
+            S.push_back(ch);
+            if (ch=='(')
+                S+="+";
+        }
+        s = S;
+
+        vector<int>nums;
+        vector<int>signs;
+        int sign;
+
+        for (int i=0; i<S.size(); i++)
+        {
+            if (s[i]=='+' || s[i]=='-')
+            {
+                signs.push_back(s[i]=='+'?1:-1);
+            }
+            else if (s[i]=='*' || s[i]=='/')
+            {
+                int j = i+1;
+                while (j<s.size() && isdigit(s[j]))
+                    j++;
+                int num = stoi(s.substr(i+1,j-i-1));                
+                if (s[i]=='*') nums.back() *= num;
+                else if (s[i]=='/') nums.back() /= num;
+                i = j-1;
+            }
+            else if (isdigit(s[i]))
+            {
+                int j = i;
+                while (j<s.size() && isdigit(s[j]))
+                    j++;
+                int num = stoi(s.substr(i,j-i));                
+                nums.push_back(num);
+                i = j-1;
+            }
+        }
+
+        int ret = 0;
+        for (int i=0; i<nums.size(); i++)
+            ret+=nums[i]*signs[i];
+        return ret;
+   }
+};
