a09d58a5b86c84306b536858e84ba275ac0f089a
wisdompeak <guan.huifeng@gmail.com>
2020-05-31 14:06:47 -0700

    Update 1381.Design-a-Stack-With-Increment-Operation.cpp

--- Design/1381.Design-a-Stack-With-Increment-Operation/1381.Design-a-Stack-With-Increment-Operation.cpp
+++ Design/1381.Design-a-Stack-With-Increment-Operation/1381.Design-a-Stack-With-Increment-Operation.cpp
@@ -1,42 +1,43 @@
-class CustomStack {    
-    int count = 0;
-    int maxSize;
-    vector<int>offset;
+class CustomStack {
     vector<int>nums;
+    vector<int>offset;
+    int maxSize;
+    int count = 0;
     int diff = 0;
 public:
-    CustomStack(int maxSize) {
-        this->maxSize = maxSize;
-        nums.resize(maxSize,0);
-        offset.resize(maxSize,0);
+    CustomStack(int maxSize) 
+    {
+        this->maxSize =  maxSize;
+        nums.resize(maxSize, 0);
+        offset.resize(maxSize, 0);
     }
     
     void push(int x) 
-    {   
-        if (count == maxSize) return;
-
-        if (count>=1 && diff!=0)
+    {
+        if (count==maxSize) return;
+        
+        if (count>=1)
             offset[count-1] += diff;
+        diff = 0;
         
         nums[count] = x;
-        count++;
-        offset[count-1] = 0;
-        diff = 0;                
+        offset[count] = 0;
+        count++;        
     }
     
     int pop() 
     {
         if (count==0) return -1;
-
+        
         diff += offset[count-1];
-        int ret = nums[count-1]+diff;        
-        count--;
-        return ret;
+        int ret = nums[count-1] + diff;
+        count -= 1;
+        return ret;        
     }
     
     void increment(int k, int val) 
     {
-        if (count==0) return;
+        if (count == 0) return;
         offset[min(k-1, count-1)] += val;
     }
 };
