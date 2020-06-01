5276ea1377c124f96f011296b803fd6116a14b20
wisdompeak <guan.huifeng@gmail.com>
2020-05-31 22:58:08 -0700

    Update 1087.Brace-Expansion.cpp

--- Stack/1087.Brace-Expansion/1087.Brace-Expansion.cpp
+++ Stack/1087.Brace-Expansion/1087.Brace-Expansion.cpp
@@ -3,24 +3,23 @@ public:
     vector<string> expand(string S) 
     {
         string s;
-        for (int i=0; i<S.size(); i++)
+        for (auto ch: S)
         {
-            if (isalpha(S[i]))
+            if (isalpha(ch))
             {
                 s.push_back('{');
-                s.push_back(S[i]);
+                s.push_back(ch);
                 s.push_back('}');
             }
             else
-                s.push_back(S[i]);                
-        }        
-        S = "{"+s+"}";
-        cout<<S<<endl;
+                s.push_back(ch);
+        }
+        S = s;
         
-        stack<vector<string>>stackStr;
         stack<int>stackOp;
+        stack<vector<string>>stackStr;
         vector<string>cur;
-        
+            
         for (int i=0; i<S.size(); i++)
         {
             if (S[i]=='{')
@@ -34,52 +33,58 @@ public:
                 stackStr.push(cur);
                 stackOp.push(1);
                 cur.clear();
-            }            
+            }
             else if (isalpha(S[i]))
             {
                 string temp;
                 temp.push_back(S[i]);
-                cur.push_back(temp);                
+                cur.push_back(temp);
             }
             else if (S[i]=='}')
             {
                 while (stackOp.top()==1)
                 {
-                    cur = combine(stackStr.top(), cur);                                 
-                    stackOp.pop();
+                    cur = combine(stackStr.top(), cur);
                     stackStr.pop();
-                }                    
+                    stackOp.pop();
+                }
                 if (stackOp.top()==0)
                 {
                     cur = crossProduct(stackStr.top(), cur);
-                    stackOp.pop();
                     stackStr.pop();
+                    stackOp.pop();
                 }
             }
         }
+        
         sort(cur.begin(), cur.end());
         return cur;        
     }
     
-    vector<string> crossProduct(vector<string>&s, vector<string>&t)
+    vector<string>combine(vector<string>&s, vector<string>&t)
     {
         vector<string>ret;
-        if (s.size()==0) s.push_back("");
-        if (t.size()==0) t.push_back("");
-        for (int i=0; i<s.size(); i++)
-            for (int j=0; j<t.size(); j++)
-                ret.push_back(s[i]+t[j]);
-        return ret;        
-    }
-    
-    vector<string> combine(vector<string>&s, vector<string>&t)
-    {
-        vector<string>ret;        
         for (int i=0; i<s.size(); i++)
             ret.push_back(s[i]);
         for (int i=0; i<t.size(); i++)
-            ret.push_back(t[i]);        
-        return ret;        
-    }    
+            ret.push_back(t[i]);
+        return ret;
+    }
+    
+    vector<string>crossProduct(vector<string>&s, vector<string>&t)
+    {
+        if (s.size()==0) s.push_back("");
+        if (t.size()==0) t.push_back("");
+        vector<string>ret;
+        for (int i=0; i<s.size(); i++)
+            for (int j=0; j<t.size(); j++)
+            {
+                ret.push_back(s[i]+t[j]);
+            }
+        return ret;
+    }
 };
+
+
+
 
