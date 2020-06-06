4530d7f74c81d186cb263523512fdca0fe6bddea
wisdompeak <guan.huifeng@gmail.com>
2020-06-06 16:58:12 -0700

    Update 255.Verify Preorder Sequence in Binary Search Tree_stack.cpp

--- Tree/255.Verify-Preorder-Sequence-in-Binary-Search-Tree/255.Verify Preorder Sequence in Binary Search Tree_stack.cpp
+++ Tree/255.Verify-Preorder-Sequence-in-Binary-Search-Tree/255.Verify Preorder Sequence in Binary Search Tree_stack.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     bool verifyPreorder(vector<int>& preorder) 
     {
-        if (preorder.size()<=1) return true;
-        
         stack<int>Stack;
-        Stack.push(preorder[0]);
-        int small = INT_MIN;
-
-        for (int i=1; i<preorder.size(); i++)
+        int small=INT_MIN;
+        for (int i=0; i<preorder.size(); i++)
         {
             if (preorder[i]<small) return false;
             
-            while (!Stack.empty() && preorder[i]>Stack.top())
+            if (Stack.empty()||preorder[i]<Stack.top())
+                Stack.push(preorder[i]);
+            else
             {
-                small=max(small,Stack.top());
-                Stack.pop();
-            }
-            
-            Stack.push(preorder[i]);
+                while (!Stack.empty() && preorder[i]>Stack.top())
+                {
+                    small=Stack.top();
+                    Stack.pop();
+                }
+                Stack.push(preorder[i]);
+            }                
         }
-        
         return true;
     }
 };
