c58575ea53af20eea0a2cc9ba5f7bb7cbcdd90a2
wisdompeak <guan.huifeng@gmail.com>
2020-06-27 18:25:28 -0700

    Create 843.Guess-the-Word.cpp

---
+++ Others/843.Guess-the-Word/843.Guess-the-Word.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void findSecretWord(vector<string>& wordlist, Master& master) 
+    {
+        for (int i=0; i<10; i++)
+        {
+            int r = rand() % wordlist.size();
+            string x = wordlist[r];
+            int count = master.guess(x);
+            if (count==6) return;
+            
+            vector<string>temp;
+            for (auto s:wordlist)
+            {
+                if (s==x) continue;
+                if (sameCount(s,x)==count) temp.push_back(s);
+            }
+            wordlist = temp;
+        }
+    }
+    
+    int sameCount(string A, string B)
+    {
+        int count=0;
+        for (int i=0; i<6; i++)
+            if (A[i]==B[i]) count++;
+        return count;
+    }
+};
