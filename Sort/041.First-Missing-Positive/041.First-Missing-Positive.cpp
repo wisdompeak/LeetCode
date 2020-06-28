00a138514bb2fe28c7e3b980955c94b50cde5446
wisdompeak <guan.huifeng@gmail.com>
2020-06-27 17:35:50 -0700

    Update 041.First-Missing-Positive.cpp

--- Sort/041.First-Missing-Positive/041.First-Missing-Positive.cpp
+++ Sort/041.First-Missing-Positive/041.First-Missing-Positive.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) 
     {
-        if (nums.size()==0) return 1;        
         nums.insert(nums.begin(),0);
-        
-        for (int i=1; i<nums.size(); i++)
+        for (int i=0; i<nums.size(); i++)
         {
-            while (!(nums[i]<=0 || nums[i]>=nums.size() || nums[i]==i || nums[i]==nums[nums[i]]))
-                swap(nums[i],nums[nums[i]]);
+            while (nums[i]!=i && nums[i]<nums.size() && nums[i]!=nums[nums[i]])
+                swap(nums[i], nums[nums[i]]);
         }
-        
         for (int i=1; i<nums.size(); i++)
             if (nums[i]!=i) return i;     
         
