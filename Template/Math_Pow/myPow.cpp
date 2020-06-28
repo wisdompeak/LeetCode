b48f796fdd466ac70d80774f870ab1a531762a78
wisdompeak <guan.huifeng@gmail.com>
2020-06-27 21:32:15 -0700

    Create myPow.cpp

---
+++ Template/Math_Pow/myPow.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double quickMul(double x, long long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+};
