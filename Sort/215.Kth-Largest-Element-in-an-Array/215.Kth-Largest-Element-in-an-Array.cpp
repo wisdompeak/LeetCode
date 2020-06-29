7e27d10b051ed716c1291e58fd91798be0b7cf14
wisdompeak <guan.huifeng@gmail.com>
2020-06-28 23:45:27 -0700

    Update 215.Kth-Largest-Element-in-an-Array.cpp

--- Sort/215.Kth-Largest-Element-in-an-Array/215.Kth-Largest-Element-in-an-Array.cpp
+++ Sort/215.Kth-Largest-Element-in-an-Array/215.Kth-Largest-Element-in-an-Array.cpp
@@ -2,32 +2,23 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) 
     {
-        return QuickSort(nums,nums.size()+1-k);
-    }
-    
-    int QuickSort(vector<int>&nums, int n)
-    {
-        int pivot=nums[nums.size()/2];
-        
-        int count = 0;
-        vector<int>small;
-        vector<int>large;
-        
-        for (int k=0; k<nums.size(); k++)
+        long left = INT_MIN, right = INT_MAX;
+        while (left<right)
         {
-            if (nums[k]==pivot)
-                count++;
-            else if (nums[k]>pivot)
-                large.push_back(nums[k]);
+            int mid = right-(right-left)/2;
+            if (count(nums,mid) >=k)            
+                left = mid;
             else
-                small.push_back(nums[k]);
-        }
-        
-        if (n<=small.size())
-            return QuickSort(small,n);
-        else if (n>small.size()+count)
-            return QuickSort(large,n-small.size()-count);
-        else
-            return pivot;
+                right = mid-1;
+        }  
+        return left;
+    }
+
+    int count(vector<int>&nums, int t)
+    {
+        int ret = 0;
+        for (auto x: nums)
+            ret += (x>=t);
+        return ret;
     }
 };
