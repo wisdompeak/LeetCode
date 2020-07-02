6d623b97e2c2b25f50e17dcd3742904f8d1eb672
wisdompeak <guan.huifeng@gmail.com>
2020-07-01 23:37:08 -0700

    Update 441.Arranging-Coins.cpp

--- Math/441.Arranging-Coins/441.Arranging-Coins.cpp
+++ Math/441.Arranging-Coins/441.Arranging-Coins.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int arrangeCoins(int n) 
     {
+        if (n==0) return 0;
         long m = n;
-        for (long k = sqrt(2*m)-2;  k < sqrt(2*m)+2; k++)
+        for (long k = sqrt(2*m)-1;  k < sqrt(2*m); k++)
         {
             if (k*(k+1) <= 2*m && 2*m < (k+1)*(k+2))
                 return k;
         }
-        return -1;
-        
+        return -1;        
     }
 };
