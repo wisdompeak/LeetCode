73543485e0e95d14da379f7dd1fb8eb773b6b848
wisdompeak <guan.huifeng@gmail.com>
2020-07-03 00:08:51 -0700

    Create 321.Create-Maximum-Number_dp.cpp

---
+++ Greedy/321.Create-Maximum-Number/321.Create-Maximum-Number_dp.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) 
+    {
+        int K = k;
+        int m = nums1.size();
+        int n = nums2.size();
+        nums1.insert(nums1.begin(), 0);
+        nums2.insert(nums2.begin(), 0);
+
+        string dp[m+1][n+1][K+1];        
+
+        for (int i=0; i<=m; i++)
+            for (int j=0; j<=n; j++)
+                for (int k=0; k<=min(K,i+j); k++)
+                {
+                    if (k==0) 
+                    {
+                        dp[i][j][k] = "";
+                        continue;
+                    }
+                    dp[i][j][k] = "";
+                    if (i-1>=0 && k-1>=0 && dp[i-1][j][k-1]!="#") dp[i][j][k] = max(dp[i][j][k], dp[i-1][j][k-1]+to_string(nums1[i]));
+                    if (j-1>=0 && k-1>=0 && dp[i][j-1][k-1]!="#") dp[i][j][k] = max(dp[i][j][k], dp[i][j-1][k-1]+to_string(nums2[j]));
+                    if (i-1>=0 && dp[i-1][j][k]!="#") dp[i][j][k] = max(dp[i][j][k], dp[i-1][j][k]);
+                    if (j-1>=0 && dp[i][j-1][k]!="#") dp[i][j][k] = max(dp[i][j][k], dp[i][j-1][k]);                    
+                }
+
+        vector<int>ret;
+        for (int i=0; i<dp[m][n][K].size(); i++)
+            ret.push_back(dp[m][n][K][i]-'0');
+
+        return ret;
+    }
+};
