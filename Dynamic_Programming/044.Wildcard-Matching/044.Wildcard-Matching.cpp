44e85cf868972bf7af45140341b7cb4a23d6e269
wisdompeak <guan.huifeng@gmail.com>
2020-07-05 13:27:31 -0700

    Update 044.Wildcard-Matching.cpp

--- Dynamic_Programming/044.Wildcard-Matching/044.Wildcard-Matching.cpp
+++ Dynamic_Programming/044.Wildcard-Matching/044.Wildcard-Matching.cpp
@@ -6,25 +6,28 @@ public:
         int N = p.size();
         s="0"+s;
         p="0"+p;
-        auto dp = vector<vector<int>>(M+1,vector<int>(N+1,0));
-        dp[0][0] = 1;
-        int k = 1;
-        while (k<=N && p[k]=='*')
-        {
-            dp[0][k] = 1;
-            k++;
-        }
+        auto dp = vector<vector<int>>(M+1,vector<int>(N+1,0));        
 
+        dp[0][0] = 1;
         for (int j=1; j<=N; j++)
-            for (int i=1; i<=M; i++)
+        {
+            if (p[j]!='*') break;
+            dp[0][j] = 1;
+        }
+        
+        for (int i=1; i<=M; i++)
+            for (int j=1; j<=N; j++)
             {
                 if (p[j]=='?')
                     dp[i][j] = dp[i-1][j-1];
                 else if (p[j]=='*')
+                {
                     dp[i][j] = dp[i][j-1]||dp[i-1][j];
+                }
                 else if (s[i]==p[j])
                     dp[i][j] = dp[i-1][j-1];
             }
+        
         return dp[M][N];
     }
 };
