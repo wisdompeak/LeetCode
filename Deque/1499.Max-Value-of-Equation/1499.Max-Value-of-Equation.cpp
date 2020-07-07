6dc3a8e63c787aa8d9609053fa5ab1b8ce1bae3f
wisdompeak <guan.huifeng@gmail.com>
2020-07-07 00:04:37 -0700

    Create 1499.Max-Value-of-Equation.cpp

---
+++ Deque/1499.Max-Value-of-Equation/1499.Max-Value-of-Equation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaxValueOfEquation(vector<vector<int>>& points, int k) 
+    {
+        int ret = INT_MIN;
+        deque<int>q;
+        for (int i=0; i<points.size(); i++)
+        {
+            while (q.size()>0 && points[q.front()][0] < points[i][0]-k)
+                q.pop_front();
+            
+        	if (q.size() > 0)
+        	    ret = max(ret, -points[q.front()][0]+points[q.front()][1] + points[i][0]+points[i][1]);
+
+            while (q.size()>0 && -points[q.back()][0]+points[q.back()][1] < -points[i][0]+points[i][1])
+                q.pop_back();
+            q.push_back(i);
+            
+        }
+        return ret;
+
+    }
+};
