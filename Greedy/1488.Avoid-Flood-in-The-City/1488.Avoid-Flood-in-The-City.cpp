5f0d4207ecf9f240fec1d7de8274120c8a4257e5
wisdompeak <guan.huifeng@gmail.com>
2020-07-11 23:33:33 -0700

    Update 1488.Avoid-Flood-in-The-City.cpp

--- Greedy/1488.Avoid-Flood-in-The-City/1488.Avoid-Flood-in-The-City.cpp
+++ Greedy/1488.Avoid-Flood-in-The-City/1488.Avoid-Flood-in-The-City.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     vector<int> avoidFlood(vector<int>& rains) 
     {
-        unordered_map<int,int>pos; // pos[x] is the last day that rain fell on lake x        
-        set<int>Set;         // the days that are dry, i.e. when we can drain
+        unordered_map<int,int>fill; // fill[x] is the last day that rain filled lake x        
+        set<int>dryDays;         // the days that are dry, i.e. when we can drain
         vector<int>rets(rains.size(),1);
         
         for (int i=0; i<rains.size(); i++)
         {
             int x = rains[i];
             if (x==0)            
-                Set.insert(i);            
+                dryDays.insert(i);            
             else
             {
                 rets[i] = -1;
-                if (pos.find(x)!=pos.end())
+                if (fill.find(x)!=fill.end())
                 {
-                    auto iter = Set.lower_bound(pos[x]);
-                    if (iter==Set.end()) return {};                    
+                    auto iter = dryDays.lower_bound(fill[x]);
+                    if (iter==dryDays.end()) return {};                    
                     rets[*iter] = x;  
-                    Set.erase(*iter);                    
+                    dryDays.erase(*iter);                    
                 }
-                pos[x] = i;
+                fill[x] = i;
             }            
         }        
         return rets;        
