f0cceeb1bd26723bed08b7ea1ad9a6c932918c06
wisdompeak <guan.huifeng@gmail.com>
2020-07-14 23:18:23 -0700

    Update 1039.Minimum-Score-Triangulation-of-Polygon.cpp

--- Dynamic_Programming/1039.Minimum-Score-Triangulation-of-Polygon/1039.Minimum-Score-Triangulation-of-Polygon.cpp
+++ Dynamic_Programming/1039.Minimum-Score-Triangulation-of-Polygon/1039.Minimum-Score-Triangulation-of-Polygon.cpp
@@ -9,7 +9,7 @@ public:
             dp[i][i+1] = 0;
         
         for (int len=3; len<=N; len++)
-            for (int i=0; i<=N-len; i++)
+            for (int i=0; i+len-1<N; i++)
             {
                 int j = i+len-1;
                 for (int k=i+1; k<=j-1; k++)
