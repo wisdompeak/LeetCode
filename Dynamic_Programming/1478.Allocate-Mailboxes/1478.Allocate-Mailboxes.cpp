4cd13414226a27038ddf92721c6734b44bfb7185
wisdompeak <guan.huifeng@gmail.com>
2020-07-17 03:03:43 -0700

    Update 1478.Allocate-Mailboxes.cpp

--- Dynamic_Programming/1478.Allocate-Mailboxes/1478.Allocate-Mailboxes.cpp
+++ Dynamic_Programming/1478.Allocate-Mailboxes/1478.Allocate-Mailboxes.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    int minDistance(vector<int>& houses, int k) 
+    int minDistance(vector<int>& houses, int K) 
     {
-        sort(houses.begin(), houses.end());
-        
         int n = houses.size();
-        int range[n][n];
-        for (int i=0; i<n; i++)
-            for (int j=i; j<n; j++)
+        sort(houses.begin(), houses.end());
+        houses.insert(houses.begin(), INT_MIN);
+        vector<vector<int>>dp(n+1, vector<int>(K+1, INT_MAX/2));
+
+        int range[n+1][n+1];
+        for (int i=1; i<=n; i++)
+            for (int j=i; j<=n; j++)
             {
                 int sum = 0;
                 for (int k=i; k<=j; k++)
-                    sum += abs(houses[(i+j)/2]-houses[k]);
+                    sum += abs(houses[k] - houses[(i+j)/2]);
                 range[i][j] = sum;
             }
-        
-        int K = k;
-        int dp[n][K+1];
-        for (int i=0; i<n; i++)
-            dp[i][1] = range[0][i];
-        
-        
-        for (int i=0; i<n; i++)
+
+        for (int i=1; i<=n; i++)
+            dp[i][1] = range[1][i];
+
+        for (int i=1; i<=n; i++)
             for (int k=2; k<=K; k++)
             {
-                dp[i][k] = INT_MAX/3;
-                for (int j=0; j<i; j++)
-                    dp[i][k] = min(dp[i][k], dp[j][k-1]+range[j+1][i]);
+                for (int j=1; j+1<=i; j++)
+                    dp[i][k] = min(dp[i][k],  dp[j][k-1]+range[j+1][i]);
             }
-        
-        return dp[n-1][K];
-        
+
+        return dp[n][K];
+
     }
 };
