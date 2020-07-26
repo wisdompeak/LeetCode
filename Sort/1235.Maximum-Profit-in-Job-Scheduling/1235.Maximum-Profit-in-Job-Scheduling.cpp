ec6ae4afb6f21d960a758baed57daea26fcbb015
wisdompeak <guan.huifeng@gmail.com>
2020-07-25 18:09:30 -0700

    Update 1235.Maximum-Profit-in-Job-Scheduling.cpp

--- Sort/1235.Maximum-Profit-in-Job-Scheduling/1235.Maximum-Profit-in-Job-Scheduling.cpp
+++ Sort/1235.Maximum-Profit-in-Job-Scheduling/1235.Maximum-Profit-in-Job-Scheduling.cpp
@@ -2,27 +2,31 @@ class Solution {
 public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) 
     {
-        vector<vector<int>>p;
+        vector<vector<int>>jobs;
+        set<int>endTimes({0});
         for (int i=0; i<startTime.size(); i++)
-            p.push_back({endTime[i], startTime[i], profit[i]});
-        
-        sort(p.begin(), p.end());
-        vector<pair<int,int>>q;
-        q.push_back({0,0});
-        
-        for (int i=0; i<p.size(); i++)
-        {            
-            int end = p[i][0];
-            int start = p[i][1];
-            int pro = p[i][2];
-            
-            auto iter = upper_bound(q.begin(),q.end(), make_pair(start, INT_MAX));
-            iter = prev(iter,1);
-            int newPro = iter->second + p[i][2];
-            if (q.size()>0 && q.back().second < newPro)
-                q.push_back({end, newPro});
+        {
+            jobs.push_back({endTime[i],startTime[i],profit[i]});
+            endTimes.insert(endTime[i]);
         }
-        
-        return q.back().second;
+        sort(jobs.begin(), jobs.end());
+
+        unordered_map<int,int>time2val;
+        time2val[0] = 0;
+        int curVal = 0;
+
+        for (auto job: jobs)
+        {
+            int end = job[0];
+            int start = job[1];
+            int val = job[2];
+
+            auto iter = endTimes.upper_bound(start);
+            int lastEnd = *prev(iter,1);            
+            time2val[end] = max(curVal, time2val[lastEnd]+val);
+
+            curVal = time2val[end];
+        }
+        return curVal;
     }
 };
