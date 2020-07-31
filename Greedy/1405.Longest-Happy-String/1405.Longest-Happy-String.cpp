d056d35e4ef47505dcfb346dbe7d7e5f5c2acbf2
wisdompeak <guan.huifeng@gmail.com>
2020-07-31 00:04:34 -0700

    Create 1405.Longest-Happy-String.cpp

---
+++ Greedy/1405.Longest-Happy-String/1405.Longest-Happy-String.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) 
+    {
+        priority_queue<pair<int,char>>pq;
+        if (a!=0) pq.push({a,'a'});
+        if (b!=0) pq.push({b,'b'});
+        if (c!=0) pq.push({c,'c'});
+
+        string ret;
+        while (!pq.empty())
+        {
+            if (pq.size()==1) 
+            {
+                int k = min(2, pq.top().first);
+                for (int i=0; i<k; i++)                    
+                    ret.push_back(pq.top().second);
+                return ret;
+            }
+
+            auto x = pq.top();
+            pq.pop();
+            auto y = pq.top();
+            pq.pop();
+
+            int k = min(x.first-y.first, 2);
+            for (int i=0; i<k; i++)
+                ret.push_back(x.second);
+            ret.push_back(y.second);
+
+            x.first-=k;
+            y.first-=1;
+            if (x.first!=0) pq.push(x);
+            if (y.first!=0) pq.push(y);
+        }
+        return ret;
+        
+    }
+};
