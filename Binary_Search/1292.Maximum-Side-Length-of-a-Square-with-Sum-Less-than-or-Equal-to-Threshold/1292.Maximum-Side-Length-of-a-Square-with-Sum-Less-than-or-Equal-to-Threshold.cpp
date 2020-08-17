a3c10014ca2d9b82a8ec4d95f406a6faebda8d69
wisdompeak <guan.huifeng@gmail.com>
2020-08-16 23:04:41 -0700

    Create 1292.Maximum-Side-Length-of-a-Square-with-Sum-Less-than-or-Equal-to-Threshold.cpp

---
+++ Binary_Search/1292.Maximum-Side-Length-of-a-Square-with-Sum-Less-than-or-Equal-to-Threshold/1292.Maximum-Side-Length-of-a-Square-with-Sum-Less-than-or-Equal-to-Threshold.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    int m,n;
+    vector<vector<int>>presum;
+public:
+    int maxSideLength(vector<vector<int>>& mat, int threshold) 
+    {
+        m = mat.size();
+        n = mat[0].size();
+        
+        presum.resize(m+1);
+        for (int i=0; i<=m; i++)
+            presum[i].resize(n+1);
+                       
+        for (int i=1; i<=m; i++)
+            for (int j=1; j<=n; j++)
+                presum[i][j] = presum[i-1][j]+presum[i][j-1]-presum[i-1][j-1]+mat[i-1][j-1];
+
+        int left = 1, right = min(m,n);
+        while (left < right)
+        {
+            int mid = right-(right-left)/2;
+            if (isOK(mid, threshold))
+                left = mid;
+            else
+                right = mid-1;
+        }
+        if (isOK(left, threshold)) return left;
+        else return 0;
+    }
+
+    bool isOK(int len, int threshold)
+    {
+        for (int i=len; i<=m; i++)
+            for (int j=len; j<=n; j++)
+            {
+                int sum = presum[i][j] - presum[i][j-len] - presum[i-len][j] + presum[i-len][j-len];
+                if (sum <= threshold)
+                    return true;
+            }
+        return false;
+    }
+};
