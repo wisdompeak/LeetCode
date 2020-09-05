df4da51f401cd9dd1d82cb361e464f057069e35b
wisdompeak <guan.huifeng@gmail.com>
2020-09-05 01:02:41 -0700

    Update 1263.Minimum-Moves-to-Move-a-Box-to-Their-Target-Location.cpp

--- BFS/1263.Minimum-Moves-to-Move-a-Box-to-Their-Target-Location/1263.Minimum-Moves-to-Move-a-Box-to-Their-Target-Location.cpp
+++ BFS/1263.Minimum-Moves-to-Move-a-Box-to-Their-Target-Location/1263.Minimum-Moves-to-Move-a-Box-to-Their-Target-Location.cpp
@@ -1,50 +1,48 @@
 class Solution {
-struct Node {
-    int bx, by, px, py;
-};    
+    struct Node {
+        int bx, by, px, py;        
+    };
 public:
     int minPushBox(vector<vector<char>>& grid) 
     {
         int m = grid.size();
-        int n = grid[0].size();        
+        int n = grid[0].size();
+        int bx,by,px,py,tx,ty;
         
-        int px,py,bx,by,tx,ty;
         for (int i=0; i<m; i++)
             for (int j=0; j<n; j++)
             {
-                if (grid[i][j]=='B')
+                if (grid[i][j]=='S')
                 {
-                    bx = i;
-                    by = j;
+                    px = i, py = j;
                     grid[i][j] = '.';
                 }
-                else if (grid[i][j]=='S')
+                else if (grid[i][j]=='B')
                 {
-                    px = i;
-                    py = j;
+                    bx = i, by = j;
                     grid[i][j] = '.';
                 }
                 else if (grid[i][j]=='T')
                 {
-                    tx = i;
-                    ty = j;
+                    tx = i, ty = j;
                     grid[i][j] = '.';
-                }                
+                }
             }
-        
-        int flag[m][n][m][n];
-        memset(flag, 255, sizeof(flag));
-        flag[bx][by][px][py] = 0;
-        
-        auto dir=vector<pair<int,int>>({{1,0},{-1,0},{0,1},{0,-1}});
         
         deque<Node>q;
         q.push_back({bx,by,px,py});
+        int memo[21][21][21][21];
+        memset(memo, 255, sizeof(memo));
+        memo[bx][by][px][py] = 0;
+        
+        auto dir = vector<pair<int,int>>({{1,0},{-1,0},{0,1},{0,-1}});
+        
         while (!q.empty())
         {
-            auto [bx,by,px,py] = q.front();
+            auto [bx, by, px, py] = q.front();
             q.pop_front();
-            if (bx==tx && by==ty) return flag[bx][by][px][py];
+            if (bx==tx && by==ty)
+                return memo[bx][by][px][py];
             
             for (int k=0; k<4; k++)
             {
@@ -53,30 +51,27 @@ public:
                 if (x<0||x>=m||y<0||y>=n) continue;
                 if (grid[x][y]!='.') continue;
                 if (x==bx && y==by) continue;
-                if (flag[bx][by][x][y]>=0) continue;
-                flag[bx][by][x][y] = flag[bx][by][px][py];
-                q.push_front({bx,by,x,y});
+                if (memo[bx][by][x][y]>=0) continue;
+                memo[bx][by][x][y] = memo[bx][by][px][py];
+                q.push_front({bx,by,x,y});               
             }
-            if (abs(px - bx) + abs(py - by) == 1)
+            if (abs(px-bx)+abs(py-by)==1)
             {
                 for (int k=0; k<4; k++)
                 {
-                    if (px+dir[k].first!=bx || py+dir[k].second!=by)
-                        continue;
-                
-                    int x = bx+dir[k].first;
-                    int y = by+dir[k].second;                   
-                    if (x<0||x>=m||y<0||y>=n) continue;
-                    if (grid[x][y]!='.') continue;
-                    if (flag[x][y][bx][by]>=0) continue;
-                    flag[x][y][bx][by] = flag[bx][by][px][py]+1;
-                    q.push_back({x,y,bx,by});                
+                    if (px+dir[k].first==bx && py+dir[k].second==by)
+                    {
+                        int bx2 = bx+dir[k].first;
+                        int by2 = by+dir[k].second;
+                        if (bx2<0||bx2>=m||by2<0||by2>=n) continue;
+                        if (grid[bx2][by2]!='.') continue;
+                        if (memo[bx2][by2][bx][by]>=0) continue;
+                        memo[bx2][by2][bx][by] = memo[bx][by][px][py]+1;
+                        q.push_back({bx2,by2,bx,by});
+                    }
                 }
             }
-            
         }
-        
-        return -1;
-        
+        return -1;        
     }
 };
