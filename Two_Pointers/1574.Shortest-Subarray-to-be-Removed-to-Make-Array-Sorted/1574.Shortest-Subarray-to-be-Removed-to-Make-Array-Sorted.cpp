9cf4a6ea3ff14106fb9146250b0f3a2ff9bb1aad
wisdompeak <guan.huifeng@gmail.com>
2020-09-05 12:19:32 -0700

    Create 1574.Shortest-Subarray-to-be-Removed-to-Make-Array-Sorted.cpp

---
+++ Two_Pointers/1574.Shortest-Subarray-to-be-Removed-to-Make-Array-Sorted/1574.Shortest-Subarray-to-be-Removed-to-Make-Array-Sorted.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& arr) 
+    {
+        int n = arr.size();
+        int ret = n-1;
+        
+        int j = n-1;
+        while (j-1>=0 && arr[j-1]<=arr[j])
+            j--;
+        ret = min(ret, j);        
+        if (ret==0) return 0;
+        
+        for (int i=0; i<n; i++)
+        {
+            if (i>=1 && arr[i]<arr[i-1]) break;
+            while (j<n && arr[j]<arr[i])
+                j++;
+            ret = min(ret, j-i-1);
+        }
+        
+        return ret;
+    }
+};
