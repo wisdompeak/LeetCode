f08d977a8b8c1b8b1049ec8daea168d754147ad9
wisdompeak <guan.huifeng@gmail.com>
2020-09-05 16:33:39 -0700

    Create 1575.Count-All-Possible-Routes_v2.cpp

---
+++ Dynamic_Programming/1575.Count-All-Possible-Routes/1575.Count-All-Possible-Routes_v2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    long dp[201][100];
+public:    
+    int countRoutes(vector<int>& locations, int start, int finish, int fuel) 
+    {
+        int n = locations.size();
+        dp[fuel][start] = 1;
+        long M = 1e9+7;
+        long ret = 0;
+        
+        for (int f=fuel; f>=0; f--)
+            for (int c = 0; c<n; c++)
+            {
+                for (int d = 0; d<n; d++)
+                {
+                    if (d==c) continue;
+                    if (f+abs(locations[d]-locations[c])>fuel) continue;
+                    dp[f][c] = (dp[f][c] + dp[f+abs(locations[d]-locations[c])][d])%M;
+                }
+                if (c==finish) ret = (ret+dp[f][c])%M;
+            }
+        
+        return ret;
+    }
+};
