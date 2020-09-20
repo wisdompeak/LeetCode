ddf4225c7bb62bcca7155092aaec6aaa1b0acc24
wisdompeak <guan.huifeng@gmail.com>
2020-09-20 11:42:16 -0700

    Update 340.Longest-Substring-with-At-Most-K-Distinct-Characters_v1.cpp

--- Two_Pointers/340.Longest-Substring-with-At-Most-K-Distinct-Characters/340.Longest-Substring-with-At-Most-K-Distinct-Characters_v1.cpp
+++ Two_Pointers/340.Longest-Substring-with-At-Most-K-Distinct-Characters/340.Longest-Substring-with-At-Most-K-Distinct-Characters_v1.cpp
@@ -1,34 +1,25 @@
 class Solution {
-    int count = 0;
 public:
     int lengthOfLongestSubstringKDistinct(string s, int k) 
     {
-        vector<int>freq(256,0);        
+        vector<int>freq(256,0);
         int n = s.size();
         int j = 0;
         int ret = 0;
-        for (int i=0; i<s.size(); i++)
-        {                                     
-            while (j<n && getCount(freq, s[j]) <= k)
+        int count = 0;
+        for (int i=0; i<n; i++)
+        {
+            while (j<n && count + (freq[s[j]]+1 == 1) <= k)
             {
                 freq[s[j]]++;
-                count += (freq[s[j]]==1);
-                j++;                
+                count += (freq[s[j]] == 1);
+                j++;
             }
-                
             ret = max(ret, j-i);
-                        
+            
             freq[s[i]]--;
-            count -= (freq[s[i]]==0);
+            count -= (freq[s[i]] == 0);
         }
         return ret;        
-    }
-    
-    int getCount(vector<int>&freq, char ch)
-    {                
-        if (freq[ch]==0)
-            return count+1;
-        else
-            return count;
     }
 };
