b24bcc6e6c34fd8eb0031b1ad06637ed13adc504
wisdompeak <guan.huifeng@gmail.com>
2020-09-20 00:13:29 -0700

    Update 992.Subarrays-with-K-Different-Integers_v1.cpp

--- Two_Pointers/992.Subarrays-with-K-Different-Integers/992.Subarrays-with-K-Different-Integers_v1.cpp
+++ Two_Pointers/992.Subarrays-with-K-Different-Integers/992.Subarrays-with-K-Different-Integers_v1.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    int lengthOfLongestSubstringKDistinct(string s, int k) 
+    int subarraysWithKDistinct(vector<int>& A, int K) 
     {
-        vector<int>freq(256,0);
-        int count = 0;
-        int j = 0;
-        int ret = 0;
-        for (int i=0; i<s.size(); i++)
-        {                        
-            while (count <= k)
-            {
-                ret = max(ret, j-i);
-                if (j==s.size()) break;                
-                freq[s[j]]++;
-                if (freq[s[j]]==1)
-                    count++;     
-                j++;
-            }
-            
-            freq[s[i]]--;
-            if (freq[s[i]]==0)
-                count--;
-        }
-        return ret;        
+        return atMostK(A,K) - atMostK(A,K-1);
     }
+    
+    int atMostK(vector<int>& A, int K)
+    {
+        unordered_map<int,int>Map;
+        int count=0;
+        int i = 0;
+        
+        for (int j=0; j<A.size(); j++)
+        {
+            Map[A[j]]++;
+            
+            while (Map.size()>K)
+            {
+                Map[A[i]]--;
+                if (Map[A[i]]==0)
+                    Map.erase(A[i]);
+                i++;
+            }            
+            count+= j-i+1;
+        }        
+        return count;            
+    }    
 };
+
