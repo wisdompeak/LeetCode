918e05d88451e2fa267e8a7d857ed9ddb1ad1a0e
wisdompeak <guan.huifeng@gmail.com>
2020-09-20 23:29:13 -0700

    Create 1595.Minimum-Cost-to-Connect-Two-Groups-of-Points.cpp

---
+++ Dynamic_Programming/1595.Minimum-Cost-to-Connect-Two-Groups-of-Points/1595.Minimum-Cost-to-Connect-Two-Groups-of-Points.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    int dp[12][1<<12];
+public:
+    int connectTwoGroups(vector<vector<int>>& cost) 
+    {
+        int m = cost.size();
+        int n = cost[0].size();
+
+        for (int i=0; i<m; i++)
+            dp[i][0] = INT_MAX/2;
+        for (int state = 1; state < (1<<n); state++)
+        {
+            int sum = 0;
+            for (int j=0; j<n; j++)
+            {
+                if (((state>>j)&1)==1)
+                    sum += cost[0][j];
+            }
+            dp[0][state] = sum;
+        }
+
+        for (int i=1; i<m; i++)
+        {
+            for (int state = 1; state < (1<<n); state++)
+            {
+                dp[i][state] = INT_MAX/2;
+                for (int subset=state; subset>0; subset=(subset-1)&state)
+                {
+                    int sum = 0;
+                    for (int j=0; j<n; j++)
+                    {
+                        if (((subset>>j)&1)==1)
+                            sum += cost[i][j];
+                    }
+                    dp[i][state] = min(dp[i][state], dp[i-1][state-subset]+sum);
+                }
+
+                int minPath = INT_MAX;
+                for (int j=0; j<n; j++)
+                    minPath = min(minPath, cost[i][j]);
+                dp[i][state] = min(dp[i][state], dp[i-1][state] + minPath);                                 
+            }            
+        }
+
+        return dp[m-1][(1<<n)-1];
+    }
+};
