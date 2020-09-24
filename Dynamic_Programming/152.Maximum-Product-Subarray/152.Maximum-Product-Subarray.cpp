9fbcaf2c125b41986e9f1526e9c7cd851c851af5
wisdompeak <guan.huifeng@gmail.com>
2020-09-23 22:02:53 -0700

    Update 152.Maximum-Product-Subarray.cpp

--- Dynamic_Programming/152.Maximum-Product-Subarray/152.Maximum-Product-Subarray.cpp
+++ Dynamic_Programming/152.Maximum-Product-Subarray/152.Maximum-Product-Subarray.cpp
@@ -2,27 +2,17 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) 
     {
-        if (nums.size()==1) return nums[0];
+        long MAX = 1;
+        long MIN = 1;
+        long ret =  INT_MIN;
         
-        vector<int>f(nums.size(),0);
-        vector<int>g(nums.size(),0);
-        
-        f[0]=nums[0];
-        g[0]=nums[0];
-        
-        for (int i=1; i<nums.size(); i++)
-        {
-            f[i] = max(nums[i],max(f[i-1]*nums[i],g[i-1]*nums[i]));
-            g[i] = min(nums[i],min(f[i-1]*nums[i],g[i-1]*nums[i]));
-        }
-
-        
-        int result=INT_MIN;
         for (int i=0; i<nums.size(); i++)
-            result=max(result,f[i]);
-            
+        {
+            long MAX_tmp = MAX, MIN_tmp = MIN;
+            MAX = max(max(MAX_tmp*nums[i],MIN_tmp*nums[i]),(long)nums[i]);            
+            MIN = min(min(MAX_tmp*nums[i],MIN_tmp*nums[i]),(long)nums[i]);
+            ret = max(ret,MAX);
+        }
         return result;
-        
-        
     }
 };
