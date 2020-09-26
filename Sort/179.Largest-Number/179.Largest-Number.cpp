973777ccd091cf5cdee27c30d639f9bf00ab0fce
wisdompeak <guan.huifeng@gmail.com>
2020-09-25 23:54:31 -0700

    Update 179.Largest-Number.cpp

--- Sort/179.Largest-Number/179.Largest-Number.cpp
+++ Sort/179.Largest-Number/179.Largest-Number.cpp
@@ -1,23 +1,21 @@
 class Solution {
-    static bool cmp(string a, string b)
+    static bool cmp(string&a, string&b)
     {
-        string c=a+b;
-        string d=b+a;
-        return stoll(c)>stoll(d);
+        return (a+b)>(b+a);
     }
 public:
     string largestNumber(vector<int>& nums) 
     {
-        vector<string>Nums;
+        vector<string>strs;
         for (int i=0; i<nums.size(); i++)
-            Nums.push_back(to_string(nums[i]));
-            
-        sort(Nums.begin(),Nums.end(),cmp);
-        
+            strs.push_back(to_string(nums[i]));
+
+        sort(strs.begin(), strs.end(), cmp);
+
         string result;
-        for (int i=0; i<Nums.size(); i++)
-            result+=Nums[i];
-            
+        for (int i=0; i<strs.size(); i++)
+            result+=strs[i];
+
         int i=0;
         while (result[i]=='0') i++;
         if (i==result.size())
