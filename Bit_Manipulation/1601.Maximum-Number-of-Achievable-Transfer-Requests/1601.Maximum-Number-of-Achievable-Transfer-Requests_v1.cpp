e8cfd2aae52ba525929bc9f1521a15d78c84b2df
wisdompeak <guan.huifeng@gmail.com>
2020-09-26 23:38:56 -0700

    Update 1601.Maximum-Number-of-Achievable-Transfer-Requests_v1.cpp

--- Bit_Manipulation/1601.Maximum-Number-of-Achievable-Transfer-Requests/1601.Maximum-Number-of-Achievable-Transfer-Requests_v1.cpp
+++ Bit_Manipulation/1601.Maximum-Number-of-Achievable-Transfer-Requests/1601.Maximum-Number-of-Achievable-Transfer-Requests_v1.cpp
@@ -1,6 +1,34 @@
 class Solution {
     vector<int>net;
 public:
+    bool check(int s, int n, vector<vector<int>>& requests, int& count)
+    {
+        int m = requests.size();     
+        std::fill(net.begin(), net.end(), 0);
+
+        for (int i=0; i<m; i++)
+        {
+            if (((s>>i)&1)==1)
+            {
+                net[requests[i][0]]++;
+                net[requests[i][1]]--;
+                count++;
+            }
+        }
+
+        int flag = 1;
+        for (int i=0; i<n; i++)
+        {
+            if (net[i]!=0)
+            {
+                flag= 0;
+                break;
+            }
+        }
+
+        return flag;
+    }
+
     int maximumRequests(int n, vector<vector<int>>& requests) 
     {
         int m = requests.size();        
@@ -8,31 +36,9 @@ public:
         net.resize(n);
         
         for (int state = 0; state < (1<<m); state++)
-        {            
-            std::fill(net.begin(), net.end(), 0);
-                            
-            int count = 0;
-            for (int i=0; i<m; i++)
-            {
-                if (((state>>i)&1)==1)
-                {
-                    net[requests[i][0]]++;
-                    net[requests[i][1]]--;
-                    count++;
-                }
-            }
-            
-            int flag = 1;
-            for (int i=0; i<n; i++)
-            {
-                if (net[i]!=0)
-                {
-                    flag= 0;
-                    break;
-                }
-            }
-            
-            if (flag==1)
+        {                      
+            int count = 0;  
+            if (check(state, n, requests, count))
                 ret = max(ret, count);
         }
         return ret;        
