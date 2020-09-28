0f937ecb5488c1bdb9001911a77f8ee52c75a0c8
wisdompeak <guan.huifeng@gmail.com>
2020-09-27 23:41:27 -0700

    Create 1306.Jump-Game-III.cpp

---
+++ DFS/1306.Jump-Game-III/1306.Jump-Game-III.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int visited[50001];
+public:
+    bool canReach(vector<int>& arr, int start) 
+    {
+        if (start<0 || start>=arr.size()) 
+            return false;
+        if (arr[start]==0)
+            return true;
+        if (visited[start]==1) 
+            return false;
+
+        visited[start] = 1;
+        if (canReach(arr, start-arr[start]))
+            return true;
+        
+        if (canReach(arr, start+arr[start]))
+            return true;
+
+        visited[start] = 1;
+
+        return false;
+    }
+};
