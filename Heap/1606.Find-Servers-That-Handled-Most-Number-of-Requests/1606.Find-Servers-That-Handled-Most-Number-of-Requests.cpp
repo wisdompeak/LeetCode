525dbbe6d959fef0dbc23fe97e761f13719ec322
wisdompeak <guan.huifeng@gmail.com>
2020-10-03 13:07:07 -0700

    Update 1606.Find-Servers-That-Handled-Most-Number-of-Requests.cpp

--- Heap/1606.Find-Servers-That-Handled-Most-Number-of-Requests/1606.Find-Servers-That-Handled-Most-Number-of-Requests.cpp
+++ Heap/1606.Find-Servers-That-Handled-Most-Number-of-Requests/1606.Find-Servers-That-Handled-Most-Number-of-Requests.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> busiestServers(int k, vector<int>& arrival, vector<int>& load) 
     {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>busy;
         set<int>free;
         for (int i=0; i<k; i++)
             free.insert(i);
@@ -10,25 +10,26 @@ public:
         
         for (int i=0; i<arrival.size(); i++)
         {
-            while (!pq.empty() && pq.top().first <= arrival[i])
+            while (!busy.empty() && busy.top().first <= arrival[i])
             {                
-                free.insert(pq.top().second);
-                pq.pop();            
+                free.insert(busy.top().second);
+                busy.pop();
             }
             
             if (free.empty()) continue;
-                   
+            
             auto iter = free.lower_bound(i%k);
-            if (iter==free.end())
+            if (iter == free.end())
                 iter = free.begin();
+            
             int id = *iter;
-            count[id]+=1;
+            count[id] += 1;
             free.erase(id);
-            pq.push({arrival[i]+load[i],id});
+            busy.push({arrival[i]+load[i], id});            
         }
         
-        int maxCount = *max_element(count.begin(), count.end());
         vector<int>ret;
+        int maxCount = *max_element(count.begin(), count.end());
         for (int i=0; i<k; i++)
             if (count[i]==maxCount)
                 ret.push_back(i);
