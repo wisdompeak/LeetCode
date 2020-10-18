d9ddbbb0508cc4f5c84fcd9f5c8bada76aaca176
wisdompeak <guan.huifeng@gmail.com>
2020-10-17 18:25:38 -0700

    Update Inverse_Element.cpp

--- Template/Inverse_Element/Inverse_Element.cpp
+++ Template/Inverse_Element/Inverse_Element.cpp
@@ -1,16 +1,40 @@
 #include<bits/stdc++.h>
-#define ll long long
+#define LL long long
 using namespace std;
-const ll N=1e6+7,mod=998244353;
-ll inv[N];
+const LL N = 1e6+7, mod = 998244353;
+LL inv[N];
 
-int main() 
-{
+/*********************************/
+// Linear method to compute inv[i]
+void main() 
+{    
     int i;
     for(inv[1]=1,i=2;i<N;++i)
-        inv[i]=(mod-mod/i)*inv[mod%i]%mod;
+        inv[i]=(mod-mod/i) * inv[mod%i] % mod;
     
-    // ....
+    ret = ret* inv[n] % mod;   // ret = (ret / n) % mod;    
+}
 
-    ret = ret* inv[n]%mod;   // ret = (ret / n) % M;    
+/*********************************/
+// Qucik Pow
+
+LL quickPow(int x, int y) 
+{
+    LL ret = 1;
+    LL cur = x;
+    while (y) 
+    {
+        if (y & 1) 
+        {
+            ret = (LL)ret * cur % mod;
+        }
+        cur = (LL)cur * cur % mod;
+        y >>= 1;
+    }
+    return ret;
+}
+
+LL inv(LL x) 
+{
+    return quickPow(x, mod - 2);
 }
