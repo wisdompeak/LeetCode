448f02c58f219a8ee0c9417b8c20b76ab3b17e4f
wisdompeak <guan.huifeng@gmail.com>
2020-10-25 23:42:43 -0700

    Update 1632.Rank-Transform-of-a-Matrix_v2.cpp

--- Union_Find/1632.Rank-Transform-of-a-Matrix/1632.Rank-Transform-of-a-Matrix_v2.cpp
+++ Union_Find/1632.Rank-Transform-of-a-Matrix/1632.Rank-Transform-of-a-Matrix_v2.cpp
@@ -25,52 +25,50 @@ public:
         for (int i=0; i<m; i++)
             for (int j=0; j<n; j++)
                 Father[i*n+j] = i*n+j;
-
-        vector<pair<int,int>>nums;
-        for (int i=0; i<m; i++)
-            for (int j=0; j<n; j++)
-                nums.push_back({matrix[i][j],{i*n+j}});
-
+                        
         for (int i=0; i<m; i++)
         {
-            unordered_map<int,int>Map;
+            vector<pair<int,int>>temp;
             for (int j=0; j<n; j++)
+                temp.push_back({matrix[i][j], i*n+j});
+            sort(temp.begin(), temp.end());
+            for (int j=1; j<n; j++)
             {
-                int val = matrix[i][j];
-                if (Map.find(val)==Map.end())
-                    Map[val] = i*n+j;
-                else
+                if (temp[j].first == temp[j-1].first)
                 {
-                    if (FindFather(Map[val])!=FindFather(i*n+j))
-                        Union(Map[val], i*n+j);
-                }                
+                    if (FindFather(temp[j-1].second)!=FindFather(temp[j].second))
+                        Union(temp[j-1].second, temp[j].second);
+                }                    
             }
         }
-
+        
         for (int j=0; j<n; j++)
         {
-            unordered_map<int,int>Map;
+            vector<pair<int,int>>temp;
             for (int i=0; i<m; i++)
-            {
-                int val = matrix[i][j];
-                if (Map.find(val)==Map.end())
-                    Map[val] = i*n+j;
-                else
+                temp.push_back({matrix[i][j], i*n+j});
+            sort(temp.begin(), temp.end());
+            for (int i=1; i<m; i++)
+            {  
+                if (temp[i].first==temp[i-1].first)
                 {
-                    if (FindFather(Map[val])!=FindFather(i*n+j))
-                        Union(Map[val], i*n+j);
-                }                
+                    if (FindFather(temp[i-1].second)!=FindFather(temp[i].second))
+                        Union(temp[i-1].second, temp[i].second);
+                }                    
             }
         }
 
-        unordered_map<int, vector<int>>Group;
+        vector<pair<int,int>>nums;                
+        vector<vector<int>>group(m*n);
         for (int i=0; i<m; i++)
             for (int j=0; j<n; j++)
             {
-                Group[FindFather(i*n+j)].push_back(i*n+j);
+                int root = FindFather(i*n+j);
+                group[root].push_back(i*n+j);
+                nums.push_back({matrix[i][j], i*n+j});
             }
-                                        
         sort(nums.begin(), nums.end());
+
         vector<int>rowRank(m,0);
         vector<int>colRank(m,0);
         auto rets = vector<vector<int>>(m, vector<int>(n));
@@ -83,18 +81,18 @@ public:
             if (rets[x][y]!=0) continue; 
             
             int r = 0;            
-            for (auto p: Group[Father[x*n+y]])
+            for (auto p: group[Father[x*n+y]])
             {
                 r = max(r, rowRank[p/n]);
                 r = max(r, colRank[p%n]);
             }
-            for (auto p: Group[Father[x*n+y]])
+            for (auto p: group[Father[x*n+y]])
             {
                 rowRank[p/n] = r+1;
                 colRank[p%n] = r+1;
                 rets[p/n][p%n] = r+1;
             }
         }
-        return rets;
+        return rets;        
     }
 };
