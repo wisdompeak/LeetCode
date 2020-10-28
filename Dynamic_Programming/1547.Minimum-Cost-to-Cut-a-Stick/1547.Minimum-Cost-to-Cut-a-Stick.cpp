8251d3bc04c8ba2eab0a0cb534b9732833c31fd6
wisdompeak <guan.huifeng@gmail.com>
2020-10-28 00:09:51 -0700

    Update 1547.Minimum-Cost-to-Cut-a-Stick.cpp

--- Dynamic_Programming/1547.Minimum-Cost-to-Cut-a-Stick/1547.Minimum-Cost-to-Cut-a-Stick.cpp
+++ Dynamic_Programming/1547.Minimum-Cost-to-Cut-a-Stick/1547.Minimum-Cost-to-Cut-a-Stick.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     int minCost(int n, vector<int>& cuts) 
     {
-        int m = cuts.size()+1;
         sort(cuts.begin(), cuts.end());
-        cuts.insert(cuts.begin(),0);
+        cuts.insert(cuts.begin(), 0);
         cuts.push_back(n);
         
-        auto dp = vector<vector<int>>(m+1, vector<int>(m+1, INT_MAX/2));
-        for (int i=0; i<m; i++)
+        int m = cuts.size();
+        auto dp = vector<vector<int>>(m, vector<int>(m,INT_MAX));
+        
+        for (int i=0; i+1<m; i++)
             dp[i][i+1] = 0;
         
-        for (int len=2; len<=m; len++)
-            for (int i=0; i+len<=m; i++)
+        for (int len = 3; len <= m; len++)
+            for (int i=0; i+len-1<m; i++)
             {
-                int j = i+len;
+                int j = i+len-1;
                 for (int k=i+1; k<j; k++)
-                    dp[i][j] = min(dp[i][j], dp[i][k]+dp[k][j]+cuts[j]-cuts[i]);                
+                    dp[i][j] = min(dp[i][j], cuts[j]-cuts[i]+dp[i][k]+dp[k][j]);
             }
-        return dp[0][m];                
+        
+        return dp[0][m-1];
     }
 };
