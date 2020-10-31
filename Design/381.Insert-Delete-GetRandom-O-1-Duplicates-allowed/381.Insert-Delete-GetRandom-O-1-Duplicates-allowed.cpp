999d71afa105008f6047a1819f600c160703d384
wisdompeak <guan.huifeng@gmail.com>
2020-10-31 11:02:06 -0700

    Update 381.Insert-Delete-GetRandom-O-1-Duplicates-allowed.cpp

--- Design/381.Insert-Delete-GetRandom-O-1-Duplicates-allowed/381.Insert-Delete-GetRandom-O-1-Duplicates-allowed.cpp
+++ Design/381.Insert-Delete-GetRandom-O-1-Duplicates-allowed/381.Insert-Delete-GetRandom-O-1-Duplicates-allowed.cpp
@@ -10,11 +10,9 @@ public:
     /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
     bool insert(int val) 
     {
-        int flag = val2pos.find(val)==val2pos.end();
-        
+        int flag = val2pos.find(val)==val2pos.end();        
         nums.push_back(val);
         val2pos[val].insert(nums.size()-1);
-        
         return flag;
     }
     
@@ -23,29 +21,32 @@ public:
     {
         int flag = val2pos.find(val)!=val2pos.end();
         if (flag==false) return false;
-        
-        int pos = *(val2pos[val].begin());
+
         int val2 = nums.back();
         
         if (val2==val)
         {
-            nums.pop_back();
+            val2pos[val].erase(nums.size()-1);
+            nums.pop_back();            
+
+            if (val2pos[val].size()==0)
+                val2pos.erase(val);            
         }
         else
-        {
+        {   
             int pos2 = nums.size()-1;
+            int pos = *(val2pos[val].begin());            
             nums[pos] = val2;
-            nums.pop_back();
             val2pos[val2].erase(pos2);
             val2pos[val2].insert(pos);
+            val2pos[val].erase(pos);
+            nums.pop_back();
+
+            if (val2pos[val].size()==0)
+                val2pos.erase(val);
         }
         
-        val2pos[val].erase(pos);
-        if (val2pos[val].size()==0)
-            val2pos.erase(val);
-        
         return flag;
-        
     }
     
     /** Get a random element from the collection. */
@@ -63,4 +64,3 @@ public:
  * bool param_2 = obj->remove(val);
  * int param_3 = obj->getRandom();
  */
-
