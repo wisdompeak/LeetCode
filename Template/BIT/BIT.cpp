eff635edda96cfa9c8069ed97f5f6f86ca4b70db
wisdompeak <guan.huifeng@gmail.com>
2020-11-08 23:18:51 -0800

    Create BIT.cpp

---
+++ Template/BIT/BIT.cpp
@@ -0,0 +1,33 @@
+const int MAX_N = 100000;
+
+class Solution {
+public:    
+    long long bitArr[MAX_N+1];
+    long long nums[MAX_N+1];  // Note: nums is 1-index
+    long long M = 1e9+7;
+
+    void updateDelta(int i, long long delta) {
+        int idx = i;
+        while (idx <= MAX_N)
+        {
+            bitArr[idx]+=delta;
+            bitArr[idx] %= M;
+            idx+=idx&(-idx);
+        }
+    }
+
+    long long queryPreSum(int idx){
+        long long result = 0;
+        while (idx){
+            result += bitArr[idx];
+            result %= M;
+            idx-=idx&(-idx);
+        }
+        return result;
+    }
+
+    long long sumRange(int i, int j) {
+    // sum of a range inclusively
+        return queryPreSum(j)-queryPreSum(i-1);
+    }    
+};
