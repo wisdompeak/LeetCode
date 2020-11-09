9484224e8a5ea4eadced0aadcd53aa1d162cdbb3
wisdompeak <guan.huifeng@gmail.com>
2020-11-08 22:58:25 -0800

    Create ordered_set.cpp

---
+++ Template/RB_Tree/ordered_set.cpp
@@ -0,0 +1,22 @@
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+
+typedef tree<
+int ,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+int main() {
+    ordered_set Set;    
+    vector<int>nums({1,3,2,2,4,4,8,6,7,3});
+    for (int i=0; i<nums.size(); i++)
+    {
+        Set.insert(nums[i]);
+        cout<<nums[i]<<":"<<Set.order_of_key(nums[i])<<endl;
+    }    
+}
+// If you want to make it like a multiset, you may design the base type of the ordered_set as pair<int,int>
