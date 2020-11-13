3fcdcf7300d99435c571fceceb0709130290aaa0
wisdompeak <guan.huifeng@gmail.com>
2020-11-12 23:50:07 -0800

    Update 493.Reverse-Pairs.cpp

--- Divide_Conquer/493.Reverse-Pairs/493.Reverse-Pairs.cpp
+++ Divide_Conquer/493.Reverse-Pairs/493.Reverse-Pairs.cpp
@@ -1,63 +1,56 @@
 class Solution {
-    int result=0;
+    int ret = 0;
+    int temp[50001];
 public:
     int reversePairs(vector<int>& nums) 
     {
-        if (nums.size()<=1) return 0;
-        vector<long>Nums;
-        for (auto a:nums)
-            Nums.push_back(long(a));
-        divideConquer(Nums,0,nums.size()-1);
-        return result;        
+        int n = nums.size();
+        vector<int>sorted = nums;
+        helper(nums, sorted, 0, n-1);
+        return ret;
     }
-    
-    void divideConquer(vector<long>&nums, int a, int b)
+
+    void helper(vector<int>& nums, vector<int>& sorted, int a, int b)
     {
-        if (a==b) return;
-        
+        if (a>=b) return;
         int mid = a+(b-a)/2;
-        divideConquer(nums,a,mid);
-        divideConquer(nums,mid+1,b);
-        
-        for (int i=mid+1; i<=b; i++)
+        helper(nums, sorted, a, mid);
+        helper(nums, sorted, mid+1, b);
+
+        for (int j=mid+1; j<=b; j++)
         {
-            auto pos=upper_bound(nums.begin()+a,nums.begin()+mid+1,2*nums[i]);
-            result+= nums.begin()+mid+1-pos;
+            auto iter = upper_bound(sorted.begin()+a, sorted.begin()+mid+1, 2*(long)nums[j]);
+            ret += sorted.begin()+mid+1 - iter;
         }
-        
-        //sort(nums.begin()+a,nums.begin()+b+1);            
-        MergeSort(nums,a,mid,b);
-    }
-    
-    void MergeSort(vector<long>&nums, int a, int mid, int b)
-    {
-        vector<int>array(b-a+1);
-        int p1=a;
-        int p2=mid+1;
-        for (int i=0; i<b-a+1; i++)
+        // sort(sorted.begin()+a, sorted.begin()+b+1);
+        int i=a, j=mid+1, p = 0;        
+        while (i<=mid && j<=b)
         {
-            if (p1>mid)
+            if (sorted[i]<=sorted[j])
             {
-                array[i]=nums[p2];
-                p2++;
-            }
-            else if (p2>b)
-            {
-                array[i]=nums[p1];
-                p1++;
-            }
-            else if (nums[p1]<=nums[p2])
-            {
-                array[i]=nums[p1];
-                p1++;
-            }
+                temp[p] = sorted[i];
+                i++;
+            }                
             else
             {
-                array[i]=nums[p2];
-                p2++;
-            }                
+                temp[p] = sorted[j];
+                j++;
+            } 
+            p++;
         }
-        for (int i=0; i<array.size(); i++)
-            nums[i+a]=array[i];        
+        while (i<=mid)
+        {
+            temp[p] = sorted[i];
+            i++;
+            p++;
+        }
+        while (j<=b)
+        {
+            temp[p] = sorted[j];
+            j++;
+            p++;
+        }
+        for (int i=0; i<b-a+1; i++)
+            sorted[a+i] = temp[i];
     }
 };
