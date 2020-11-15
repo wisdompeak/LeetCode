6050a52c655f5e849e412a42d6d3e96fbf0ae3c3
wisdompeak <guan.huifeng@gmail.com>
2020-11-15 00:53:12 -0800

    Create 1655.Distribute-Repeating-Integers.cpp

---
+++ Dynamic_Programming/1655.Distribute-Repeating-Integers/1655.Distribute-Repeating-Integers.cpp
@@ -0,0 +1,45 @@
+class Solution {    
+    bool dp[51][1<<10]; // dp[i][state] whether the first i objects can satisfy customers of state
+public:
+    bool canDistribute(vector<int>& nums, vector<int>& quantity) 
+    {
+        unordered_map<int,int>Map;
+        for (auto x: nums)
+            Map[x]++;
+        vector<int>count;
+        for (auto x:Map)
+            count.push_back(x.second);
+        sort(count.begin(), count.end());
+        int n = count.size();
+        count.insert(count.begin(),0);
+                       
+        int m = quantity.size();
+        for (int i=0; i<=n; i++)
+            dp[i][0] = true;        
+        
+        for (int i=1; i<=n; i++)        
+            for (int state = 1; state < (1<<m); state++)
+            {
+                for (int subset=state; subset>0; subset=(subset-1)&state)
+                {
+                    if (dp[i-1][state-subset]==false) continue;
+                    if (!canFormSubset(count[i], quantity, subset)) continue;
+                    dp[i][state]=true;
+                    break;
+                }
+            }        
+        return dp[n][(1<<m)-1];
+    }
+
+    bool canFormSubset(int count, vector<int>& quantity, int subset)
+    {
+        int sum = 0;
+        int m = quantity.size();
+        for (int i=0; i<m; i++)
+        {
+            if ((subset>>i)&1) 
+                sum+=quantity[i];
+        }
+        return count>=sum;
+    }
+};
