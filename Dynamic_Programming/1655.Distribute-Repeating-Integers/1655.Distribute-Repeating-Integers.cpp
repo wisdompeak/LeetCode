97737288f5163d59a99f8f9fd9ec8680388b7af7
wisdompeak <guan.huifeng@gmail.com>
2020-11-15 02:34:27 -0800

    Update 1655.Distribute-Repeating-Integers.cpp

--- Dynamic_Programming/1655.Distribute-Repeating-Integers/1655.Distribute-Repeating-Integers.cpp
+++ Dynamic_Programming/1655.Distribute-Repeating-Integers/1655.Distribute-Repeating-Integers.cpp
@@ -1,5 +1,5 @@
-class Solution {    
-    bool dp[51][1<<10]; // dp[i][state] whether the first i objects can satisfy customers of state
+class Solution {
+    bool dp[51][1024];
 public:
     bool canDistribute(vector<int>& nums, vector<int>& quantity) 
     {
@@ -7,38 +7,48 @@ public:
         for (auto x: nums)
             Map[x]++;
         vector<int>count;
-        for (auto x:Map)
+        for (auto x: Map)
             count.push_back(x.second);
-        sort(count.begin(), count.end());
-        int n = count.size();
-        count.insert(count.begin(),0);
-                       
-        int m = quantity.size();
-        for (int i=0; i<=n; i++)
-            dp[i][0] = true;        
         
-        for (int i=1; i<=n; i++)        
-            for (int state = 1; state < (1<<m); state++)
+        int n = count.size();
+        int m = quantity.size();        
+        
+        count.insert(count.begin(), 0);
+                
+        for (int i=0; i<=n; i++)
+            dp[i][0] = true;
+        
+        for (int i=1; i<=n; i++)
+            for (int state=1; state<(1<<m); state++)
             {
+                if (dp[i-1][state]==1) 
+                {
+                    dp[i][state] = true;
+                    continue;
+                }
+                
                 for (int subset=state; subset>0; subset=(subset-1)&state)
                 {
                     if (dp[i-1][state-subset]==false) continue;
-                    if (!canFormSubset(count[i], quantity, subset)) continue;
-                    dp[i][state]=true;
-                    break;
+                    if (canSatisfySubset(count[i], quantity, subset))
+                    {
+                        dp[i][state] = true;
+                        break;
+                    }
                 }
-            }        
+            }
         return dp[n][(1<<m)-1];
+        
     }
-
-    bool canFormSubset(int count, vector<int>& quantity, int subset)
+    
+    bool canSatisfySubset(int count, vector<int>& quantity, int subset)
     {
-        int sum = 0;
         int m = quantity.size();
+        int sum = 0;
         for (int i=0; i<m; i++)
         {
-            if ((subset>>i)&1) 
-                sum+=quantity[i];
+            if ((subset>>i)&1)
+                sum += quantity[i];
         }
         return count>=sum;
     }
