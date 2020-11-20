f891c90723c13de5543c55f5824a0f4a58a600c2
wisdompeak <guan.huifeng@gmail.com>
2020-11-19 19:12:35 -0800

    Update 1540.Can-Convert-String-in-K-Moves.cpp

--- Greedy/1540.Can-Convert-String-in-K-Moves/1540.Can-Convert-String-in-K-Moves.cpp
+++ Greedy/1540.Can-Convert-String-in-K-Moves/1540.Can-Convert-String-in-K-Moves.cpp
@@ -4,25 +4,17 @@ public:
     {
         if (s.size()!=t.size()) return false;
         int n = s.size();
-        vector<int>times(27);
+        vector<int>count(26);
         
-        for (int i=0; i<n; i++)
+        for (int i=0; i<n; i++)        
+            count[(t[i]-s[i]+26)%26] += 1;                
+        
+        int ret = 0;
+        for (int i=1; i<26; i++)        
         {
-            times[(t[i]-s[i]+26)%26] += 1;
+            if (count[i]>=1)
+                ret = max(ret, (count[i]-1)*26+i);        
         }
-        times[26] = times[0];
-        
-        int maxNum = 0;
-        int loop = 0;
-        for (int i=1; i<26; i++)
-        {
-            if (times[i] >= loop)
-            {
-                loop = times[i];
-                maxNum = i;
-            }
-        }
-        
-        return (loop-1)*26+maxNum <=k;            
+        return ret<=k;
     }
 };
