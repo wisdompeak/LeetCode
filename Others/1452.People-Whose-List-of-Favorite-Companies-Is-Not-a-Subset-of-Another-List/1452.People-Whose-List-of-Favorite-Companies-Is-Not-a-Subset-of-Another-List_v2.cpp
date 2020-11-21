36e891b377428d7dd4d5adffa91f3f785229f186
wisdompeak <guan.huifeng@gmail.com>
2020-11-21 00:49:43 -0800

    Update 1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List_v2.cpp

--- Others/1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List/1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List_v2.cpp
+++ Others/1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List/1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List_v2.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> peopleIndexes(vector<vector<string>>& favoriteCompanies) 
-    {        
+    {
         int n = favoriteCompanies.size();
-        unordered_map<string,bitset<100>>c2p;
+        unordered_map<string, bitset<100>>c2p;
         for (int i=0; i<n; i++)
             for (int j=0; j<favoriteCompanies[i].size(); j++)
-                c2p[favoriteCompanies[i][j]][i] = 1;
-
-        vector<int>rets;
-        bitset<100>state;
+                c2p[favoriteCompanies[i][j]][i]=1;
+        
+        vector<int>ret;
         for (int i=0; i<n; i++)
-        {            
-            state.set();
+        {
+            bitset<100>bs;
+            bs.set();
             
             for (string c: favoriteCompanies[i])
             {
-                state = state & c2p[c];
+                bs &= c2p[c];
             }
-            if (state.count()==1)
-                rets.push_back(i);
-        }        
-        return rets;
+            if (bs.count()==1)
+                ret.push_back(i);
+        }
+        return ret;        
     }
 };
