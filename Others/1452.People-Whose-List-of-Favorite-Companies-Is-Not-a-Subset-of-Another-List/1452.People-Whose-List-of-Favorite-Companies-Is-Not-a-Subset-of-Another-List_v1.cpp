2a6c0c414aee04caa9d82822272351d738375bcb
wisdompeak <guan.huifeng@gmail.com>
2020-11-20 22:18:57 -0800

    Create 1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List_v1.cpp

---
+++ Others/1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List/1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List_v1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    static bool cmp(pair<unordered_set<string>, int>&a, pair<unordered_set<string>, int>&b)
+    {
+        return a.first.size() > b.first.size();
+    }
+
+public:
+    vector<int> peopleIndexes(vector<vector<string>>& favoriteCompanies) {
+        vector<pair<unordered_set<string>, int>>arr;
+        for (int i=0; i<favoriteCompanies.size(); i++)
+        {
+            unordered_set<string>Set(favoriteCompanies[i].begin(), favoriteCompanies[i].end());
+            arr.push_back({Set, i});
+        }
+        sort(arr.begin(), arr.end(), cmp);
+
+        vector<int>rets;
+        for (int i=0; i<arr.size(); i++)
+        {
+            int flag = 1;
+            for (int j=0; j<i; j++)
+            {                
+                bool include = 1;
+                for (auto c: arr[i].first)
+                {
+                    if (arr[j].first.find(c)==arr[j].first.end())
+                    {            
+                        include = 0;            
+                        break;
+                    }                        
+                }
+                if (include == 1) 
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag==1) rets.push_back(arr[i].second);
+        }
+
+        sort(rets.begin(), rets.end());
+        return rets;
+    }  
+
+};
