a1dd0e2e4649cba991c98d1b3fe0c073bc27e608
wisdompeak <guan.huifeng@gmail.com>
2020-11-21 00:19:28 -0800

    Update 1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List_v1.cpp

--- Others/1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List/1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List_v1.cpp
+++ Others/1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List/1452.People-Whose-List-of-Favorite-Companies-Is-Not-a-Subset-of-Another-List_v1.cpp
@@ -1,29 +1,25 @@
 class Solution {
-    static bool cmp(pair<unordered_set<string>, int>&a, pair<unordered_set<string>, int>&b)
-    {
-        return a.first.size() > b.first.size();
-    }
-
+    
 public:
     vector<int> peopleIndexes(vector<vector<string>>& favoriteCompanies) {
-        vector<pair<unordered_set<string>, int>>arr; // {companiyset, personId}
+        vector<unordered_set<string>>arr; 
         for (int i=0; i<favoriteCompanies.size(); i++)
         {
-            unordered_set<string>Set(favoriteCompanies[i].begin(), favoriteCompanies[i].end());
-            arr.push_back({Set, i});
+            unordered_set<string>Set(favoriteCompanies[i].begin(),favoriteCompanies[i].end());
+            arr.push_back(Set);
         }
-        sort(arr.begin(), arr.end(), cmp);
 
         vector<int>rets;
         for (int i=0; i<arr.size(); i++)
         {
             int flag = 1;
-            for (int j=0; j<i; j++)
+            for (int j=0; j<arr.size(); j++)            
             {                
+                if (i==j) continue;
                 bool include = 1;  // check if i-th set is included in j-th set
-                for (auto c: arr[i].first)
+                for (auto c: arr[i])
                 {
-                    if (arr[j].first.find(c)==arr[j].first.end())
+                    if (arr[j].find(c)==arr[j].end())
                     {            
                         include = 0;            
                         break;
@@ -35,10 +31,9 @@ public:
                     break;
                 }
             }
-            if (flag==1) rets.push_back(arr[i].second);
+            if (flag==1) rets.push_back(i);
         }
 
-        sort(rets.begin(), rets.end());
         return rets;
     }  
 
