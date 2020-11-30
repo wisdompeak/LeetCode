b2d9be107c361ed343dc0fa3718d5ef47faf0095
wisdompeak <guan.huifeng@gmail.com>
2020-11-29 19:02:23 -0800

    Create 1674.Minimum-Moves-to-Make-Array-Complementary_v2.cpp

---
+++ Others/1674.Minimum-Moves-to-Make-Array-Complementary/1674.Minimum-Moves-to-Make-Array-Complementary_v2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums, int limit) 
+    {
+        map<int,int>diff;
+        int n = nums.size();
+        for (int i=0; i<n/2; i++)
+        {            
+            int a = min(nums[i],nums[n-1-i]);
+            int b = max(nums[i],nums[n-1-i]);
+
+            diff[2]+=2;
+            diff[1+a]-=1;
+            diff[a+b]-=1;
+            diff[a+b+1]+=1;
+            diff[limit+b+1]+=1;
+        }
+
+        int ret = INT_MAX;
+        int count = 0;
+        for (auto x: diff)
+        {
+            count += x.second;
+            ret = min(ret, count);
+        }
+        return ret;
+    }
+};
