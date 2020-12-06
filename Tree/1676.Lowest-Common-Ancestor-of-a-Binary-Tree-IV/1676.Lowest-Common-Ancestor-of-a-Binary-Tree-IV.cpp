6070f0cc0dc1029e419f5846f2f18933d0ff4a98
wisdompeak <guan.huifeng@gmail.com>
2020-12-06 00:14:10 -0800

    Create 1676.Lowest-Common-Ancestor-of-a-Binary-Tree-IV.cpp

---
+++ Tree/1676.Lowest-Common-Ancestor-of-a-Binary-Tree-IV/1676.Lowest-Common-Ancestor-of-a-Binary-Tree-IV.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    unordered_set<TreeNode*>Set;
+    TreeNode* ret = NULL;
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, vector<TreeNode*> &nodes) 
+    {
+        for (auto x: nodes)
+            Set.insert(x);
+        
+        dfs(root);
+        return ret;        
+    }
+    
+    int dfs(TreeNode* node)
+    {
+        if (node==NULL) return 0;
+        int left = dfs(node->left);
+        int right = dfs(node->right);
+        int cur = Set.find(node)!=Set.end();
+        if (ret==NULL && left+right+cur==Set.size())
+            ret = node;
+        return left+right+cur;
+    }
+};
