39571d20bea94d5e11858508e847bf62dd656c6b
wisdompeak <guan.huifeng@gmail.com>
2020-12-10 00:46:20 -0800

    Update 996.Number-of-Squareful-Arrays.cpp

--- DFS/996.Number-of-Squareful-Arrays/996.Number-of-Squareful-Arrays.cpp
+++ DFS/996.Number-of-Squareful-Arrays/996.Number-of-Squareful-Arrays.cpp
@@ -1,44 +1,49 @@
 class Solution {
-    int result = 0;
-    unordered_map<int,vector<int>>Map;
+    vector<vector<int>>next;
+    vector<int>visited;
+    int n;
+    int ret = 0;
 public:
     int numSquarefulPerms(vector<int>& A) 
     {
-        int N = A.size();
-        sort(A.begin(),A.end());        
-        for (int i=0; i<N; i++)
-            for (int j=0; j<N; j++)
+        n = A.size();
+        next.resize(n);
+        visited.resize(n);
+        sort(A.begin(), A.end());
+        for (int i=0; i<n; i++)
+            for (int j=0; j<n; j++)
             {
                 if (i==j) continue;
-                if ((int)sqrt(A[i]+A[j])==sqrt(A[i]+A[j]))
-                    Map[i].push_back(j);                
+                if (sqrt(A[i]+A[j])==(int)sqrt(A[i]+A[j]))
+                    next[i].push_back(j);
             }
-        unordered_set<int>visited;
-        for (int i=0; i<N; i++)
+        for (int i=0; i<n; i++)
         {
-            if (i>=1 && A[i]==A[i-1]) continue;
-            visited.insert(i);
-            DFS(A,visited,i);
-            visited.erase(i);
-        }        
-        return result;        
+            if (i>0 && A[i]==A[i-1]) continue;
+            visited[i] = 1;
+            dfs(A, i, 1);
+            visited[i] = 0;
+        }
+        return ret;
     }
-    
-    void DFS(vector<int>&A, unordered_set<int>&visited, int cur)
+
+    void dfs(vector<int>&A, int cur, int count)
     {
-        if (visited.size()==A.size())
+        if (count==n)
         {
-            result++;
+            ret++;
             return;
         }
-        for (int i=0; i<Map[cur].size(); i++)
+
+        int last = -1;
+        for (auto i: next[cur])
         {
-            if (visited.find(Map[cur][i])!=visited.end()) continue;
-            if (i>=1 && A[Map[cur][i]]==A[Map[cur][i-1]] && visited.find(Map[cur][i-1])==visited.end())
-                continue;
-            visited.insert(Map[cur][i]);
-            DFS(A,visited,Map[cur][i]);
-            visited.erase(Map[cur][i]);            
+            if (visited[i]==1) continue;
+            if (A[i]==last) continue;
+            visited[i] = 1;
+            last = A[i];
+            dfs(A, i, count+1);
+            visited[i] = 0;
         }
     }
 };
