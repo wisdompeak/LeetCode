e6e499c038c32ad6ff000551d321257a28fb800a
wisdompeak <guan.huifeng@gmail.com>
2020-12-12 00:02:10 -0800

    Update 473.Matchsticks-to-Square.cpp

--- DFS/473.Matchsticks-to-Square/473.Matchsticks-to-Square.cpp
+++ DFS/473.Matchsticks-to-Square/473.Matchsticks-to-Square.cpp
@@ -1,35 +1,37 @@
 class Solution {
-    int sum = 0;
+    bool visited[15];
+    int total;
 public:
     bool makesquare(vector<int>& nums) 
     {
         if (nums.size()==0) return false;
-        sort(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.end());                
-        
-        for (auto x:nums) sum+=x;
-        if (sum%4!=0) return false;
-        
-        sum = sum/4;
-        vector<int>visited(nums.size(),0);
-        return DFS(nums,0,0,0,visited);
+        total = accumulate(nums.begin(), nums.end(), 0);
+        if (total%4!=0) return false;
+        sort(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.end());
+
+        return dfs(nums, 0, 0, 0);
     }
-    
-    bool DFS(vector<int>& nums, int curIdx, int curEdge, int curSum, vector<int>& visited)
+
+    bool dfs(vector<int>& nums, int cur, int group, int sum)
     {
-        if (curEdge==4) return true;
-        if (curSum==sum) return DFS(nums, 0, curEdge+1, 0, visited);
-        if (curSum>sum) return false;
-        if (curIdx==nums.size()) return false;
+        if (group==4) return true;
+        if (sum > total/4) return false;
+        if (sum == total/4)
+            return dfs(nums, 0, group+1, 0);
         
-        for (int i=curIdx; i<nums.size(); i++)
+        int last = -1;
+        for (int i=cur; i<nums.size(); i++)
         {
             if (visited[i]==1) continue;
-            visited[i] = 1;
-            if (DFS(nums,i+1,curEdge,curSum+nums[i],visited))
+            if (nums[i]==last) continue;
+            visited[i] = 1;            
+            last = nums[i];
+            if (dfs(nums, i+1, group, sum+nums[i]))
                 return true;
             visited[i] = 0;
         }
-        return false;        
+        return false;
     }
+
 };
