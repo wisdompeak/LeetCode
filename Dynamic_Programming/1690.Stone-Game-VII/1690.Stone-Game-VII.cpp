c1af26b4beede7094caf1dcec0c5a614e327d1da
wisdompeak <guan.huifeng@gmail.com>
2020-12-14 00:09:36 -0800

    Create 1690.Stone-Game-VII.cpp

---
+++ Dynamic_Programming/1690.Stone-Game-VII/1690.Stone-Game-VII.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    int dp[1005][1005];
+public:
+    int stoneGameVII(vector<int>& stones) 
+    {
+        int n = stones.size();
+        stones.insert(stones.begin(), 0);
+        stones.push_back(0);
+        
+        vector<int>presum(n+2,0);
+        for (int i=1; i<=n+1; i++)
+            presum[i] = presum[i-1] + stones[i];        
+        
+        for (int i=1; i<=n; i++)
+            dp[i][i] = 0;
+        for (int i=1; i+1<=n; i++)
+            dp[i][i+1] = max(stones[i], stones[i+1]);
+                
+        for (int len = 3; len<=n; len++)
+        {
+            for (int i=1; i+len-1<=n; i++)
+            {
+                int j = i+len-1;
+                dp[i][j] = max(presum[j]-presum[i] - dp[i+1][j], presum[j-1]-presum[i-1] - dp[i][j-1]);
+            }
+        }
+        return dp[1][n];        
+    }
+};
