116e3ff1a01d4dd614a43e8f57118782493752c1
wisdompeak <guan.huifeng@gmail.com>
2020-12-21 22:22:20 -0800

    Update 395.Longest-Substring-with-At-Least-K-Repeating-Characters.cpp

--- Recursion/395.Longest-Substring-with-At-Least-K-Repeating-Characters/395.Longest-Substring-with-At-Least-K-Repeating-Characters.cpp
+++ Recursion/395.Longest-Substring-with-At-Least-K-Repeating-Characters/395.Longest-Substring-with-At-Least-K-Repeating-Characters.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    int longestSubstring(string s, int k) 
-    {
-        if (s=="") return 0;
-        
-        unordered_map<char,int>Map;
+    int longestSubstring(string s, int k) {
+        unordered_map<int,int>Map;
         for (int i=0; i<s.size(); i++)
             Map[s[i]]++;
-                
-        unordered_set<char>Set;
-        for (auto a:Map)
-        {
-            if (a.second>=k)
-                Set.insert(a.first);            
-        }        
-        if (Set.size()==Map.size())
-            return s.size();
-        
-        int result=0;
+            
+        int result = 0;
         for (int i=0; i<s.size(); i++)
         {
-            int i0=i;
-            while (i<s.size() && Set.find(s[i])!=Set.end())
-                i++;
-            result = max(result, longestSubstring(s.substr(i0,i-i0),k));
+            if (Map[s[i]]<k) continue;
+            int j = i;
+            while (j<s.size() && Map[s[j]]>=k)
+                j++;
+            if (j-i==s.size())
+                return s.size();
+            else
+                result = max(result, longestSubstring(s.substr(i,j-i),k));
+            i = j-1;
         }
         
         return result;
-            
     }
 };
