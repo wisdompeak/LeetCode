cd94b56b7e8b7b74c4efca7c9873d8bb8a2821ed
wisdompeak <guan.huifeng@gmail.com>
2020-12-28 12:14:41 -0800

    Update 421.Maximum-XOR-of-Two-Numbers-in-an-Array.cpp

--- Trie/421.Maximum-XOR-of-Two-Numbers-in-an-Array/421.Maximum-XOR-of-Two-Numbers-in-an-Array.cpp
+++ Trie/421.Maximum-XOR-of-Two-Numbers-in-an-Array/421.Maximum-XOR-of-Two-Numbers-in-an-Array.cpp
@@ -1,63 +1,44 @@
 class Solution {
-public:
     class TrieNode
     {
         public:
-         TrieNode* next[2];
-         TrieNode()
-         {
-             next[0]=NULL;
-             next[1]=NULL;
-         }
+        TrieNode* next[2];
     };
-    
-    TrieNode* buildTree(vector<int>nums)
-    {
-        TrieNode* root=new TrieNode();
-        TrieNode* current;
-        for (int i=0; i<nums.size(); i++)
-        {
-            current = root;
-            for (int j=31; j>=0; j--)
-            {
-                int bit = ((nums[i]>>j)&1);
-                if (current->next[bit]==NULL)
-                    current->next[bit]=new TrieNode();
-                current=current->next[bit];
-            }
-        }
-        return root;
-    }
-    
-    int oppositePath(TrieNode* root, int num)
-    {
-        TrieNode* node =root;
-        int result=0;
-        for (int i=31; i>=0; i--)
-        {
-            int bit= (num>>i)&1;
-            if (node->next[1-bit]!=NULL)
-            {
-                node = node->next[1-bit];
-                result += (1-bit)<<i;
-            }
-            else
-            {
-                node = node->next[bit];
-                result += (bit)<<i;
-            }
-        }
-        return result^num;
-    }
-    
+public:
     int findMaximumXOR(vector<int>& nums) 
     {
-        TrieNode* root=buildTree(nums);
-        int result=0;
-        for (int i=0; i<nums.size(); i++)
+        TrieNode* root = new TrieNode();
+        for (auto num: nums)
         {
-            result = max(result, oppositePath(root,nums[i]));
+            TrieNode* node = root;
+            for (int k=31; k>=0; k--)
+            {
+                if (node->next[(num>>k)&1]==NULL)
+                    node->next[(num>>k)&1] = new TrieNode();
+                node = node->next[(num>>k)&1];
+            }
         }
-        return result;
+
+        int ret = 0;
+        for (auto num: nums)
+        {
+            TrieNode* node = root;
+            int ans = 0;
+            for (int k=31; k>=0; k--)
+            {
+                if (node->next[1 - (num>>k)&1]!=NULL)
+                {
+                    ans = ans*2+1;
+                    node = node->next[1 - (num>>k)&1];
+                }                    
+                else
+                {
+                    ans = ans*2+0;
+                    node = node->next[(num>>k)&1];
+                }
+            }
+            ret = max(ret, ans);
+        }
+        return ret;
     }
 };
