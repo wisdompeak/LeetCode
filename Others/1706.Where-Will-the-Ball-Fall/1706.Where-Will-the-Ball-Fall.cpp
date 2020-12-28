56d7477a924751ca2564bc6eac6f0c28b86ff39f
wisdompeak <guan.huifeng@gmail.com>
2020-12-27 22:57:52 -0800

    Update 1706.Where-Will-the-Ball-Fall.cpp

--- Others/1706.Where-Will-the-Ball-Fall/1706.Where-Will-the-Ball-Fall.cpp
+++ Others/1706.Where-Will-the-Ball-Fall/1706.Where-Will-the-Ball-Fall.cpp
@@ -4,31 +4,37 @@ public:
     {
         int m = grid.size();
         int n = grid[0].size();
-
-        vector<int>ret(n,-1);
+        vector<int>ret(n, -1);
+        
         for (int j=0; j<n; j++)
         {
             int x = 0, y = j;
             while (x < m)
             {
-                if (grid[x][y]==1 && y==n-1)
-                    break;
-                else if (grid[x][y]==-1 && y==0)
-                    break;
-                else if (grid[x][y]==1 && grid[x][y+1]==-1)
-                    break;
-                else if (grid[x][y]==-1 && grid[x][y-1]==1)
-                    break;
-                else if (grid[x][y]==1)
+                if (grid[x][y]==1)
                 {
-                    x++;
-                    y++;
+                    if (y==n-1)
+                        break;
+                    else if (grid[x][y+1]==-1)
+                        break;
+                    else
+                    {
+                        x++;
+                        y++;
+                    }                        
                 }
                 else
                 {
-                    x++;
-                    y--;
-                }
+                    if (y==0)
+                        break;
+                    else if (grid[x][y-1]==1)
+                        break;
+                    else
+                    {
+                        x++;
+                        y--;
+                    }                        
+                }                
             }
             if (x==m)
                 ret[j] = y;
