660bc7a9cee2aa4415d2a433acc807211ae69c82
wisdompeak <guan.huifeng@gmail.com>
2021-01-26 14:48:24 -0800

    Update 1739.Building-Boxes.cpp

--- Math/1739.Building-Boxes/1739.Building-Boxes.cpp
+++ Math/1739.Building-Boxes/1739.Building-Boxes.cpp
@@ -1,41 +1,44 @@
 typedef long long ll;
 
 class Solution {
-    int a[1000000];
 public:
     int minimumBoxes(int n) 
     {
-        ll left = 1, right = 1e9;
+        int left = 1, right = 1e9;
         while (left < right)
         {
-            ll mid = left+(right-left)/2;
+            int mid = left+(right-left)/2;
             if (cal(mid) >= n)
                 right = mid;
             else
                 left = mid+1;
-        }       
-        return left;
+        }
+        return left;        
     }
     
-    ll cal(ll t)
-    {        
-        ll n = (int)sqrt(2*t);       
-        if ((1+n)*n/2 > t)
-            n--;
-            
-        for (int i=1; i<=n; i++)
-            a[i] = n-i+1;
-        ll diff = t - (1+n)*n/2;
-        for (int i=1; i<=diff; i++)
-            a[i] += 1;
-
-        ll count = 0;
+    ll cal(ll area)
+    {
+        ll d = (int)sqrt(2*area);
+        while ((1+d)*d/2 > area)
+            d--;
+        ll diff = area - (1+d)*d/2;
+        
+        vector<ll>a(d);
+        for (int i=0; i<d; i++)
+            a[i] = d - i;
+        for (int i=0; i<diff; i++)
+            a[i] += 1;        
+        
+        ll total = 0;
         ll sufsum = 0;
-        for (ll k = n; k >= 1; k--)
+        for (int i=d-1; i>=0; i--)
         {
-            sufsum += a[k];
-            count += sufsum;
+            sufsum += a[i];
+            total += sufsum;
         }
-        return count;
+        
+        // cout<<area<<" "<<d<<" "<<total<<endl;
+        
+        return total;        
     }
 };
