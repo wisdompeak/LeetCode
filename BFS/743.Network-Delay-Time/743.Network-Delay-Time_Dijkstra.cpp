88b48a8a4a31c7d4e6ac0b796155124ca01a09a4
wisdompeak <guan.huifeng@gmail.com>
2021-02-12 18:07:58 -0800

    Update 743.Network-Delay-Time_Dijkstra.cpp

--- BFS/743.Network-Delay-Time/743.Network-Delay-Time_Dijkstra.cpp
+++ BFS/743.Network-Delay-Time/743.Network-Delay-Time_Dijkstra.cpp
@@ -1,34 +1,36 @@
 typedef pair<int,int> PII;
+
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) 
-    {
-        unordered_map<int, vector<PII>>Map;
+    int networkDelayTime(vector<vector<int>>& times, int n, int K) 
+    {        
+        vector<vector<PII>>Map(n+1);
         for (auto t: times)
             Map[t[0]].push_back({t[1], t[2]});
-
-
+            
         priority_queue<PII, vector<PII>, greater<>>pq;
-        pq.push({0,k});
+        pq.push({0, K});
         vector<int>visited(n+1,0);
-        
         int ret = 0;
+        
         while (!pq.empty())
         {
             auto [d, cur] = pq.top();
             pq.pop();
-            ret = max(ret, d);
-
-            if (visited[cur]) continue;
+            if (visited[cur]==1) continue;
             visited[cur] = 1;
+            ret = max(ret, d);  // dist[cur] = d
             
-            for (auto next: Map[cur])
-                pq.push({d+next.second, next.first});
+            for (auto [next, weight]: Map[cur])
+            {
+                if (visited[next]) continue;
+                pq.push({d+weight, next});
+            }            
         }
-
+        
         for (int i=1; i<=n; i++)
             if (visited[i]==0) return -1;
-
+        
         return ret;
     }
 };
