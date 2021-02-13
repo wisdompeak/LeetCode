1e1ad98809a4ce0ade32005c1a2876e53357c025
wisdompeak <guan.huifeng@gmail.com>
2021-02-12 16:33:41 -0800

    Update 778.Swim-in-Rising-Water.cpp

--- BFS/778.Swim-in-Rising-Water/778.Swim-in-Rising-Water.cpp
+++ BFS/778.Swim-in-Rising-Water/778.Swim-in-Rising-Water.cpp
@@ -1,14 +1,14 @@
+typedef vector<int> VI;
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>pq;
+        priority_queue<VI, vector<VI>, greater<VI>>pq;
         pq.push({grid[0][0],0,0});
         
         int N = grid.size();
         auto visited = vector<vector<int>>(N,vector<int>(N,0));
-        visited[0][0] = 1;
         
-        int t = 0;
+        int ret = 0;
         auto dir = vector<pair<int,int>>({{1,0},{-1,0},{0,1},{0,-1}});
         
         while (!pq.empty())
@@ -16,48 +16,27 @@ public:
             int h = pq.top()[0];
             int x = pq.top()[1];
             int y = pq.top()[2];
+            ret = max(ret, h);
             pq.pop();
             
-            t = h;
+            if (visited[x][y]) continue;            
+            visited[x][y] = 1;
             
             if (x==N-1 && y==N-1)
-                return t;
+                return ret;
             
-            queue<pair<int,int>>q;
-            q.push({x,y});
-            
-            while (!q.empty())
+            for (int k = 0; k<4; k++)
             {
-                int i = q.front().first;
-                int j = q.front().second;
-                q.pop();
-                
-                for (int k = 0; k<4; k++)
-                {
-                    int xx = i+dir[k].first;
-                    int yy = j+dir[k].second;
-                    if (xx<0||xx>=N||yy<0||yy>=N)
-                        continue;
-                    if (visited[xx][yy]==1)
-                        continue;
-                    if (grid[xx][yy] > t)
-                    {
-                        pq.push({grid[xx][yy],xx,yy});
-                        visited[xx][yy] = 1;
-                    }
-                    else   // grid[xx][yy] <= t
-                    {
-                        if (xx==N-1 && yy==N-1)
-                            return t;
-                        q.push({xx,yy});
-                        visited[xx][yy] = 1;
-                    }
-                }
-            }
-            
+                int xx = x+dir[k].first;
+                int yy = y+dir[k].second;
+                if (xx<0||xx>=N||yy<0||yy>=N)
+                    continue;
+                if (visited[xx][yy]==1)
+                    continue;
+                pq.push({grid[xx][yy],xx,yy});
+            }            
         }
         
         return -1;
     }
 };
-
