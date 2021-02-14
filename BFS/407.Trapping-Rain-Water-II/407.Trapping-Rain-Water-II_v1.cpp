a5600872c95938eb804454b7d6f86570eeb56539
wisdompeak <guan.huifeng@gmail.com>
2021-02-13 16:10:53 -0800

    Update 407.Trapping-Rain-Water-II_v1.cpp

--- BFS/407.Trapping-Rain-Water-II/407.Trapping-Rain-Water-II_v1.cpp
+++ BFS/407.Trapping-Rain-Water-II/407.Trapping-Rain-Water-II_v1.cpp
@@ -1,71 +1,46 @@
+typedef vector<int> VI;
 class Solution {
 public:
     int trapRainWater(vector<vector<int>>& heightMap) 
     {
-        int M = heightMap.size();
-        if (M==0) return 0;
-        int N = heightMap[0].size();
-        
-        set<vector<int>>Set;
-        
-        for (int i=0; i<M; i++)
-        {
-            Set.insert({heightMap[i][0],i,0});
-            Set.insert({heightMap[i][N-1],i,N-1});
-            heightMap[i][0] = -1;
-            heightMap[i][N-1] = -1;
-        }
-        for (int j=0; j<N; j++)
-        {
-            Set.insert({heightMap[0][j],0,j});
-            Set.insert({heightMap[M-1][j],M-1,j});
-            heightMap[0][j] = -1;
-            heightMap[M-1][j] = -1;
-        }
-        
-        int ret = 0;
-        auto dir = vector<pair<int,int>>({{1,0},{-1,0},{0,1},{0,-1}});
-        while (!Set.empty())
-        {
-            int h = (*Set.begin())[0];
-            int x = (*Set.begin())[1];
-            int y = (*Set.begin())[2];
-            Set.erase(Set.begin());
-            
-            queue<pair<int,int>>q;
-            q.push({x,y});
-            
-            while (!q.empty())
+        int m = heightMap.size();
+        int n = heightMap[0].size();
+
+        auto resolved = vector<vector<int>>(m, vector<int>(n+1));
+
+        priority_queue<VI, vector<VI>, greater<>>pq;
+        for (int i=0; i<m; i++)
+            for (int j=0; j<n; j++)
             {
-                int i = q.front().first;
-                int j = q.front().second;
-                q.pop();
-                
-                for (int k=0; k<4; k++)
-                {
-                    int m = i+dir[k].first;
-                    int n = j+dir[k].second;
-                    
-                    if (m<0||m>=M||n<0||n>=N)
-                        continue;
-                    if (heightMap[m][n]==-1)
-                        continue;
-                    
-                    if (heightMap[m][n] <= h)
-                    {
-                        ret += h - heightMap[m][n];
-                        q.push({m,n});
-                        heightMap[m][n] = -1;
-                    }
-                    else
-                    {
-                        Set.insert({heightMap[m][n],m,n});
-                        heightMap[m][n] = -1;
-                    }
-                        
-                }
+                if (i==0 || i==m-1 || j==0 || j==n-1)
+                    pq.push({heightMap[i][j],i,j});
+            }
+
+        int ret = 0;
+        int cur = 0;        
+        auto dir = vector<pair<int,int>>({{1,0},{-1,0},{0,1},{0,-1}});
+
+
+        while (!pq.empty())
+        {
+            int h = pq.top()[0];
+            int x = pq.top()[1];
+            int y = pq.top()[2];
+            pq.pop();
+            if (resolved[x][y]) continue;
+            resolved[x][y] = 1;
+
+            cur = max(cur, h);
+            ret += cur - h;
+            for (int k=0; k<4; k++)
+            {
+                int i = x+dir[k].first;
+                int j = y+dir[k].second;
+                if (i<0||i>=m||j<0||j>=n) continue;
+                if (resolved[i][j]) continue;
+                pq.push({heightMap[i][j],i,j});                
             }
         }
-        return ret;
+        return ret;        
     }
 };
