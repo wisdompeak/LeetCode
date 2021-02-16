d4d8c477dcaee6af8257e7621541924d5f88a66c
wisdompeak <guan.huifeng@gmail.com>
2021-02-15 16:33:53 -0800

    Update 1066.Campus-Bikes-II.cpp

--- BFS/1066.Campus-Bikes-II/1066.Campus-Bikes-II.cpp
+++ BFS/1066.Campus-Bikes-II/1066.Campus-Bikes-II.cpp
@@ -1,44 +1,46 @@
+typedef pair<int,int> PII;
+
 class Solution {
+    int dist[10][10];
+    int visited[1024];
 public:
-    int dis(vector<vector<int>>& workers, vector<vector<int>>& bikes, int i, int j)
-    {
-        int x1=workers[i][0];
-        int y1=workers[i][1];
-        int x2=bikes[j][0];
-        int y2=bikes[j][1];
-        return abs(x1-x2)+abs(y1-y2);
-    }
-    
-    
     int assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) 
     {
-        set<vector<int>>Set;
-        Set.insert({0,0,0});
+        int m = workers.size();
+        int n = bikes.size();
+        for (int i=0; i<m; i++)
+            for (int j=0; j<n; j++)
+            {
+                int x1=workers[i][0];
+                int y1=workers[i][1];
+                int x2=bikes[j][0];
+                int y2=bikes[j][1];
+                dist[i][j] = abs(x1-x2)+abs(y1-y2);
+            }
         
-        set<vector<int>>visited;
+        priority_queue<PII, vector<PII>, greater<>>pq;        
+        pq.push({0, 0});        
         
-        while (true)
+        while (!pq.empty())
         {
-            int cost = (*Set.begin())[0];
-            int i = (*Set.begin())[1];
-            int taken = (*Set.begin())[2];
-            Set.erase(Set.begin());
+            auto [cost, state] = pq.top();
+            pq.pop();
+                        
+            if (visited[state]) continue;
+            visited[state] = 1;
             
-            if (i==workers.size())
-                return cost;
-            
-            if (visited.find({i,taken})!=visited.end())
-                continue;
-            visited.insert({i,taken});
-            
+            int i = __builtin_popcount(state);
+            if (i==m) return cost;
+                
             for (int j=0; j<bikes.size(); j++)
             {
-                if ((taken&(1<<j)) !=0)
-                    continue;
-                Set.insert({cost+dis(workers, bikes, i,j), i+1, taken+(1<<j)});
-            }            
+                if ((state>>j)&1) continue;
+                int newState = state+(1<<j);
+                if (visited[newState]) continue;                    
+                pq.push({cost+dist[i][j], newState});
+            }
         }
-        
+                
         return 0;
     }
 };
