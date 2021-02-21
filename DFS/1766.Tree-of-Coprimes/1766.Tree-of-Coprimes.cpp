2eb8e2e33e247aa6b19aee05b1cc3a5d22906562
wisdompeak <guan.huifeng@gmail.com>
2021-02-20 17:45:15 -0800

    Update 1766.Tree-of-Coprimes.cpp

--- DFS/1766.Tree-of-Coprimes/1766.Tree-of-Coprimes.cpp
+++ DFS/1766.Tree-of-Coprimes/1766.Tree-of-Coprimes.cpp
@@ -1,56 +1,52 @@
 class Solution {
-    vector<int>nums;
-    vector<vector<int>>edges;
-    vector<int>Map[100000];
-    vector<int>next[100000];
-    vector<int>path;
-    vector<int>ret;
+    vector<int> path; // path[i] is the i-th node idx along the dfs path
+    vector<int> records[51]; // records[i] contains the depths of all the nodes whose num = i
+    vector<int> rets;
+    vector<int> next[100000];
     int visited[100000];
-        
+    vector<int>nums;
 public:
     vector<int> getCoprimes(vector<int>& nums, vector<vector<int>>& edges) 
     {
         this->nums = nums;
-        this->edges = edges;        
         int n = nums.size();
-        ret.resize(n);
+        rets.resize(n);
         
-        for (auto e: edges)
+        for (auto e:edges)
         {
             next[e[0]].push_back(e[1]);
             next[e[1]].push_back(e[0]);
         }
         
         visited[0] = 1;
-        ret[0] = -1;
         dfs(0, 0);
-        return ret;
+        return rets;
     }
     
-    void dfs(int cur, int depth)
+    void dfs(int curIdx, int depth)
     {
-        if (depth>0)
+        int i = -1;
+        for (int d=1; d<=50; d++)
         {
-            int ans = -1;
-            for (int d=1; d<=50; d++)
+            if (records[d].size() > 0 && gcd(d, nums[curIdx])==1)
             {
-                if (Map[d].size()>0 && gcd(d, nums[cur])==1)
-                    ans = max(ans, Map[d].back());
+                i = max(i, records[d].back());
             }
-            if (ans!=-1) ret[cur] = path[ans];
-            else ret[cur] = -1;
+        }
+        rets[curIdx] = (i==-1) ? -1 : path[i];
+        
+        path.push_back(curIdx);
+        records[nums[curIdx]].push_back(depth);                        
+        
+        for (int nextIdx : next[curIdx])
+        {
+            if (visited[nextIdx]) continue;
+            visited[nextIdx] = 1;
+            dfs(nextIdx, depth+1);
+            visited[nextIdx] = 0;
         }
         
-        path.push_back(cur);
-        Map[nums[cur]].push_back(depth);
-        for (auto x: next[cur])
-        {
-            if (visited[x]) continue;
-            visited[x] = 1;
-            dfs(x, depth+1);            
-            visited[x] = 0;
-        }
         path.pop_back();
-        Map[nums[cur]].pop_back();        
+        records[nums[curIdx]].pop_back();                        
     }
 };
