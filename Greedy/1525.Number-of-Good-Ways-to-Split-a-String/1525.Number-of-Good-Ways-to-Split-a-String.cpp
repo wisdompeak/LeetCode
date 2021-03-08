224018985e57c1a8555784ed3d02c12599c3108e
wisdompeak <guan.huifeng@gmail.com>
2021-03-07 22:44:28 -0800

    Create 1525.Number-of-Good-Ways-to-Split-a-String.cpp

---
+++ Greedy/1525.Number-of-Good-Ways-to-Split-a-String/1525.Number-of-Good-Ways-to-Split-a-String.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int numSplits(string s) 
+    {
+        int n = s.size();
+        vector<int>left(n,0);
+        vector<int>right(n,0);
+        
+        vector<int>count(26,0);
+        int sum = 0;
+        for (int i=0; i<s.size(); i++)
+        {
+            count[s[i]-'a']+=1;
+            if (count[s[i]-'a']==1)
+                sum+=1;
+            left[i] = sum;
+        }
+        
+        count.clear();
+        count.resize(26);
+        sum = 0;
+        for (int i=s.size()-1; i>=0; i--)
+        {
+            count[s[i]-'a']+=1;
+            if (count[s[i]-'a']==1)
+                sum+=1;
+            right[i] = sum;
+        }
+        
+        int ret = 0;
+        for (int i=0; i<s.size()-1; i++)
+        {
+            if (left[i]==right[i+1])
+                ret+=1;
+        }
+        return ret;
+    }
+};
