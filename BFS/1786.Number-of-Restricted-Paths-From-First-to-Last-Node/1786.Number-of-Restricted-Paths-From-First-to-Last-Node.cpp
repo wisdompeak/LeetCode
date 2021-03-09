b829a5d2b8500f627dcf042b95246a218a78575d
wisdompeak <guan.huifeng@gmail.com>
2021-03-09 01:13:42 -0800

    Update 1786.Number-of-Restricted-Paths-From-First-to-Last-Node.cpp

--- BFS/1786.Number-of-Restricted-Paths-From-First-to-Last-Node/1786.Number-of-Restricted-Paths-From-First-to-Last-Node.cpp
+++ BFS/1786.Number-of-Restricted-Paths-From-First-to-Last-Node/1786.Number-of-Restricted-Paths-From-First-to-Last-Node.cpp
@@ -1,64 +1,62 @@
 typedef pair<int,int> PII;
+
 class Solution {
     vector<PII> next[20001];
     int dist[20001];
     int visited[20001];
-    long path[20001];
+    int pathNum[20001];
     long M = 1e9+7;
     int n;
 public:
     int countRestrictedPaths(int n, vector<vector<int>>& edges) 
     {
-        this-> n = n;
+        this->n = n;
         for (auto e:edges)
         {
             int a = e[0]-1;
             int b = e[1]-1;
             next[a].push_back({b, e[2]});
-            next[b].push_back({a, e[2]});
+            next[b].push_back({a, e[2]});            
         }
         
-        for (int i=0; i<n; i++)
-            path[i] = -1;
-        
         priority_queue<PII, vector<PII>, greater<>>pq;
-        pq.push({0, n-1});
-                
+        pq.push({0,n-1});        
+        
         while (!pq.empty())
         {
             auto [d, cur] = pq.top();
             pq.pop();
-            if (visited[cur]==1) continue;
-            visited[cur] = 1;
+            if (visited[cur]) continue;
             dist[cur] = d;
+            visited[cur] = 1;
             
-            for (auto [nxt, len] :next[cur])
+            for (auto [nxt, len] : next[cur])
             {
-                if (visited[nxt]==1) continue;
-                pq.push({d+len, nxt});
-            }
+                if (visited[nxt]) continue;
+                pq.push({ d + len, nxt});
+            }            
         }
-
+        
+        for (int i=0; i<n; i++)
+            pathNum[i] = -1;
+        
         long ret = dfs(0);
         return ret;
     }
     
     long dfs(int cur)
     {
-        if (cur==n-1)
-            return 1;        
-        if (path[cur]!=-1)
-            return path[cur];
+        if (cur==n-1) return 1;
+        if (pathNum[cur]!=-1) return pathNum[cur];
+        
         long sum = 0;
         for (auto [nxt, len]: next[cur])
         {
-            if (dist[nxt]>=dist[cur]) continue;
-            visited2[nxt] = 1;
+            if (dist[nxt] >= dist[cur]) continue;
             sum += dfs(nxt);
             sum %= M;
         }
-        path[cur] = sum;
+        pathNum[cur] = sum;
         return sum;
     }
-    
 };
