7bddb645e8a54237f907d53f027e86f6192c148e
wisdompeak <guan.huifeng@gmail.com>
2021-03-23 15:48:51 -0700

    Update 456.132-Pattern.cpp

--- Stack/456.132-Pattern/456.132-Pattern.cpp
+++ Stack/456.132-Pattern/456.132-Pattern.cpp
@@ -3,29 +3,24 @@ public:
     bool find132pattern(vector<int>& nums) 
     {
         int n = nums.size();
-        if (n<3) return false;
-        
         vector<int>leftMin(n);
         leftMin[0] = INT_MAX;
         for (int i=1; i<n; i++)
-            leftMin[i] = min(leftMin[i-1],nums[i-1]);
+        {
+            leftMin[i] = min(leftMin[i-1], nums[i-1]);
+        }
         
         stack<int>Stack;
         for (int i=n-1; i>=0; i--)
         {
-            int second = INT_MIN;
-            while (!Stack.empty() && Stack.top()<nums[i])
+            while (!Stack.empty() && nums[i]>nums[Stack.top()])
             {
-                second = Stack.top();
+                if (leftMin[i] < nums[Stack.top()])
+                    return true;
                 Stack.pop();
-            }
-                        
-            if (leftMin[i]<second)
-                return true;
-            Stack.push(nums[i]);            
+            }            
+            Stack.push(i);
         }
-        
         return false;
     }
 };
-
