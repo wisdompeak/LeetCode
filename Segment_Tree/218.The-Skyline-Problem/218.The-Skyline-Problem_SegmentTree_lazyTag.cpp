b0f69f65d78f4f49e7ebbb6685911abc8624c750
wisdompeak <guan.huifeng@gmail.com>
2021-04-04 23:49:26 -0700

    Update 218.The-Skyline-Problem_SegmentTree_lazyTag.cpp

--- Segment_Tree/218.The-Skyline-Problem/218.The-Skyline-Problem_SegmentTree_lazyTag.cpp
+++ Segment_Tree/218.The-Skyline-Problem/218.The-Skyline-Problem_SegmentTree_lazyTag.cpp
@@ -24,98 +24,101 @@ class Solution {
             node->right = new SegTreeNode(mid+1, b);
         }
         init(node->left, a, mid);
-        init(node->right, mid+1, b);                
+        init(node->right, mid+1, b);
+                
+        node->info = 0;  // write your own logic
     }
     
     void updateRange(SegTreeNode* node, int a, int b, int val)
     {        
-        if (b < node->start || a > node->end ) // no intersection
+        if (b < node->start || a > node->end ) 
             return;
-        if (node->start == node->end)   // leaf node
+        if (node->start == node->end)
         {
             node->info = max(node->info, val);
-            node->tag = 0;
+            return;
+        }        
+        if (a <= node->start && node->end <=b && val >= node->info)
+        {
+            // write your own logic            
+            node->info = val;    
+            node->tag = 1;   
             return;
         }
-        if (a <= node->start && node->end <=b && node->info <= val) // no need to dive, tag lazy
-        {            
-            node->info = val;
-            node->tag = 1;
-            return;
-        }        
         
-        if (node->tag == 1) // if current node tagged lazy, push information down
-        {
-            node->tag = 0;            
-            node->left->info = node->info;
-            node->right->info = node->info;
-            node->left->tag = 1;
-            node->right->tag = 1;            
-        }        
+        pushDown(node);        
         node->info = max(node->info, val);
         
         updateRange(node->left, a, b, val);
-        updateRange(node->right, a, b, val);        
+        updateRange(node->right, a, b, val);                
     }
-        
-    SegTreeNode* root;
-    vector<vector<int>>arr;
     
+   
+    void pushDown(SegTreeNode* node)
+    {
+        if (node->tag!=0)
+        {
+            node->left->info = node->info;
+            node->right->info = node->info;
+            node->left->tag = 1;
+            node->right->tag = 1;
+            node->tag = 0;
+        }        
+    }    
+    
+    vector<pair<int,int>>height; // {idx, h}
 public:
-    
     vector<vector<int>> getSkyline(vector<vector<int>>& buildings) 
     {
         set<int>Set;
-        for (auto& building: buildings)
+        for (auto & building: buildings)
         {
             Set.insert(building[0]);
             Set.insert(building[1]);
         }
-        int i = 0;
+        int id = 0;
         unordered_map<int,int>pos2idx;
-        vector<int>idx2pos;
-        for (auto x: Set)
+        unordered_map<int,int>idx2pos;
+        for (auto x:Set)
         {
-            pos2idx[x] = i;
-            idx2pos.push_back(x);
-            i++;
+            pos2idx[x] = id;
+            idx2pos[id] = x;
+            id++;
         }
         
         int n = pos2idx.size();
         SegTreeNode* root = new SegTreeNode(0, n-1);
         init(root, 0, n-1);
         
-        for (auto & building : buildings)
+        sort(buildings.begin(), buildings.end(), [](vector<int>&a, vector<int>&b){return a[2]<b[2];});
+        
+        for (auto & building: buildings)
         {
-            updateRange(root, pos2idx[building[0]], pos2idx[building[1]]-1, building[2]);
+            updateRange(root, pos2idx[building[0]], pos2idx[building[1]]-1, building[2]); 
         }
         
         DFS(root);
-        
+                
         vector<vector<int>>rets;
-        for (int i=0; i<arr.size(); i++)
+        for (int i=0; i<height.size(); i++)
         {
-            rets.push_back({idx2pos[arr[i][0]], arr[i][1]});
-            while (i+1 < arr.size() && arr[i+1][1]==arr[i][1])
-            {
+            auto [idx, h] = height[i];
+            rets.push_back({idx2pos[idx], h});
+            while (i+1 < height.size() && height[i+1].second == height[i].second)
                 i++;
-            }
         }
         return rets;
     }
     
     void DFS(SegTreeNode* node)
     {
-        if (node->start == node->end || node->tag == 1)
+        if (node->start==node->end || node->tag==1)
         {
-            arr.push_back({node->start, node->info});            
+            height.push_back({node->start, node->info});
+            return;
         }
-        else
-        {
-            DFS(node->left);
-            DFS(node->right);                    
-        }
-        
+        DFS(node->left);
+        DFS(node->right);
     }
     
 };
