52faeea6e7387b4ecd5826d71f65c7fe1aa011b0
wisdompeak <guan.huifeng@gmail.com>
2021-04-17 00:47:52 -0700

    Update 1074.Number-of-Submatrices-That-Sum-to-Target.cpp

--- Hash/1074.Number-of-Submatrices-That-Sum-to-Target/1074.Number-of-Submatrices-That-Sum-to-Target.cpp
+++ Hash/1074.Number-of-Submatrices-That-Sum-to-Target/1074.Number-of-Submatrices-That-Sum-to-Target.cpp
@@ -2,37 +2,29 @@ class Solution {
 public:
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) 
     {
-        int M = matrix.size();
-        int N = matrix[0].size();
+        int m = matrix.size(), n = matrix[0].size();
+        int count = 0;
         
-        int count = 0;
-        for (int i=0; i<M; i++)
+        for (int i=0; i<m; i++)
         {
-            vector<int>temp(N,0);
-            for (int j=i; j<M; j++)
+            vector<int>row(n);
+            for (int t=i; t<m; t++)
             {
-                for (int k=0; k<N; k++)
-                    temp[k] += matrix[j][k];
-                count += helper(temp,target);
-            }
-        }
-        return count;                        
-    }
-    
-    int helper(vector<int>&nums, int target)
-    {
-        int N = nums.size();
-        int presum = 0;
-        unordered_map<int,int>presumMap;
-        presumMap[0] = 1;
-        int count = 0;
-        for (int j=0; j<N; j++)
-        {
-            presum += nums[j];
-            if (presumMap.find(presum - target)!=presumMap.end())
-                count+=presumMap[presum - target];
-            presumMap[presum]+=1;
+                for (int j=0; j<n; j++)
+                    row[j] += matrix[t][j];
+                
+                unordered_map<int,int>Map;
+                Map[0] = 1;
+                int presum = 0;
+                for (int j=0; j<n; j++)
+                {
+                    presum += row[j];
+                    count += Map[presum - target];
+                    Map[presum]+=1;
+                }
+            }            
         }
         return count;
+        
     }
 };
