33e50747ca164d3eb5bb0f35c7ca83089a88b802
wisdompeak <guan.huifeng@gmail.com>
2021-04-24 16:43:45 -0700

    Update and rename 212.Word-Search-II.cpp to 212.Word-Search-II_v1.cpp

--- Trie/212.Word-Search-II/212.Word-Search-II_v1.cpp
+++ Trie/212.Word-Search-II/212.Word-Search-II_v1.cpp
@@ -12,27 +12,34 @@ class Solution {
         }
     };
     TrieNode* root;
-    set<string>Set;
-    int M;
-    int N;
+    unordered_set<string>Set;
+    int M, N;
+    bool visited[12][12];
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) 
     {
-        root=new TrieNode();
-        
-        for (int i=0; i<words.size(); i++)
-            build(words[i]);
-                
         M=board.size();
-        N=board[0].size();            
-        auto visited=vector<vector<int>>(M,vector<int>(N,0));
+        N=board[0].size(); 
+        root=new TrieNode();        
+        for (int i=0; i<words.size(); i++)
+        {
+            TrieNode* node=root;
+            for (auto ch: words[i])
+            {            
+                if (node->next[ch-'a']==NULL)
+                    node->next[ch-'a']=new TrieNode();
+                node=node->next[ch-'a'];
+            }
+            node->isEnd=true;
+        }            
         
-        string word;
         for (int i=0; i<M; i++)
          for (int j=0; j<N; j++)
-         {             
+         {   
+             TrieNode* node = root;          
+             string word;
              visited[i][j]=1;
-             DFS(i,j,root,word,visited,board);
+             DFS(i,j,node,word,board);
              visited[i][j]=0;
          }
         
@@ -40,25 +47,11 @@ public:
         return results;
     }
     
-    void build(string word)
+    void DFS(int i, int j, TrieNode* node, string& word, vector<vector<char>>& board)
     {
-        TrieNode* node=root;
-        for (int i=0; i<word.size(); i++)
-        {
-            char ch=word[i];
-            if (node->next[ch-'a']==NULL)
-                node->next[ch-'a']=new TrieNode();
-            node=node->next[ch-'a'];
-        }
-        node->isEnd=true;
-    }
-    
-    void DFS(int i, int j, TrieNode* node, string word, vector<vector<int>>&visited, vector<vector<char>>& board)
-    {
-        if (node->next[board[i][j]-'a']==NULL) return;
-        
+        if (node->next[board[i][j]-'a']==NULL) return;        
         node = node->next[board[i][j]-'a'];
-        word += board[i][j];
+        word.push_back(board[i][j]);
         
         if (node->isEnd==true) Set.insert(word);
         
@@ -71,9 +64,11 @@ public:
             if (x<0||x>=M||y<0||y>=N) continue;
             if (visited[x][y]==1) continue;
             
-            visited[x][y]=1;
-            DFS(x,y,node,word,visited,board);
-            visited[x][y]=0;                            
+            visited[x][y]=1;            
+            DFS(x,y,node,word,board);
+            visited[x][y]=0;                           
         }        
+
+        word.pop_back();
     }
 };
