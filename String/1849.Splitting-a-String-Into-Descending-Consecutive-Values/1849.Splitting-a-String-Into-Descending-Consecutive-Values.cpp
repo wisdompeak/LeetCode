80192f12152e1fe44226f731453fbef19d2ae6f5
wisdompeak <guan.huifeng@gmail.com>
2021-05-02 17:57:14 -0700

    Update 1849.Splitting-a-String-Into-Descending-Consecutive-Values.cpp

--- String/1849.Splitting-a-String-Into-Descending-Consecutive-Values/1849.Splitting-a-String-Into-Descending-Consecutive-Values.cpp
+++ String/1849.Splitting-a-String-Into-Descending-Consecutive-Values/1849.Splitting-a-String-Into-Descending-Consecutive-Values.cpp
@@ -2,28 +2,26 @@ typedef long long ll;
 class Solution {
 public:
     bool splitString(string s) 
-    {
-        ll num = 0;
-        for (int i = 0; i < s.size(); i++)
-        {   num  = num*10 + (s[i] - '0');
-            if (num > 1e10) break;
-            if (dfs(s, i+1, num, 1))
+    {        
+        for (int len=1; len < s.size(); len++)
+        {
+            ll num = stoll(s.substr(0, len));
+            if (num >= 1e10) break;
+            if (dfs(s, len, num))
                 return true;
         }
         return false;
     }
     
-    bool dfs(string&s, int cur, ll num, int count)
+    bool dfs(string&s, int cur, ll num)
     {
-        if (cur==s.size())  return count>=2;
-                
-        ll k = 0;
-        for (int i=cur; i<s.size(); i++)                
+        if (cur==s.size()) return true;
+        
+        for (int len = 1; len + cur <= s.size(); len++)                
         {
-            k = k*10 + (s[i]-'0');
-            if (k > 1e10) break;
-            if (k > num) break;            
-            if (num-1 == k && dfs(s, i+1, k, count+1))
+            ll num2 = stoll(s.substr(cur, len));
+            if (num2 >= 1e10) break;            
+            if (num-1 == num2 && dfs(s, cur+len, num2))
                 return true;                
         }
         return false;
