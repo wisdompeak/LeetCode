7fe47f67075f756d668def32461db042b19cdeb0
wisdompeak <guan.huifeng@gmail.com>
2021-05-02 17:30:01 -0700

    Update 1849.Splitting-a-String-Into-Descending-Consecutive-Values.cpp

--- String/1849.Splitting-a-String-Into-Descending-Consecutive-Values/1849.Splitting-a-String-Into-Descending-Consecutive-Values.cpp
+++ String/1849.Splitting-a-String-Into-Descending-Consecutive-Values/1849.Splitting-a-String-Into-Descending-Consecutive-Values.cpp
@@ -3,13 +3,11 @@ class Solution {
 public:
     bool splitString(string s) 
     {
-        int cur = 0;
-        while (cur<s.size() && s[cur]=='0')
-            cur++;
-        
-        for (int len=1; cur + len + len-1 <= s.size(); len++)
-        {            
-            if (dfs(s, cur+len, stoll(s.substr(cur, len)), 1))
+        ll num = 0;
+        for (int i = 0; i < s.size(); i++)
+        {   num  = num*10 + (s[i] - '0');
+            if (num > 1e11) break;
+            if (dfs(s, i+1, num, 1))
                 return true;
         }
         return false;
@@ -17,19 +15,15 @@ public:
     
     bool dfs(string&s, int cur, ll num, int count)
     {
-        if (cur==s.size()) 
+        if (cur==s.size())  return count>=2;
+                
+        ll k = 0;
+        for (int i=cur; i<s.size(); i++)                
         {
-           if (count>=2) return true;
-           if (count==1 && num==1 && s.back()=='0')  return true;
-           return false;
-        }
-        if (s[cur]=='0') return dfs(s, cur+1, num, count);
-        
-        for (int len = 1; cur + len <= s.size(); len++)                
-        {
-            if (len > to_string(num).size()) break;
-            if (len < to_string(num).size()-1) continue;
-            if (num-1 == stoll(s.substr(cur, len)) && dfs(s, cur+len, num-1, count+1))
+            k = k*10 + (s[i]-'0');
+            if (k > 1e10) break;
+            if (k > num) break;            
+            if (num-1 == k && dfs(s, i+1, k, count+1))
                 return true;                
         }
         return false;
