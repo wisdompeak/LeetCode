92d1f590e03a756aac05d64446e9b23444c707e2
wisdompeak <guan.huifeng@gmail.com>
2021-05-15 18:02:47 -0700

    Create 1862.Sum-of-Floored-Pairs.cpp

---
+++ Others/1862.Sum-of-Floored-Pairs/1862.Sum-of-Floored-Pairs.cpp
@@ -0,0 +1,44 @@
+typedef long long ll;
+class Solution {
+public:
+    int sumOfFlooredPairs(vector<int>& nums) 
+    {
+        int MaxN = 100000;
+        vector<int>count(MaxN+1);        
+        for (auto x: nums)
+            count[x]++;
+        
+        vector<int>presum(MaxN+1);        
+        for (int i=1; i<=MaxN; i++)
+            presum[i] = presum[i-1]+count[i];
+        
+        sort(nums.begin(), nums.end());
+                
+        ll ret = 0;
+        ll M = 1e9+7;
+        for (int i=0; i<nums.size(); i++)
+        {
+            if (i>=1 && nums[i]==nums[i-1]) continue;
+            
+            int x = nums[i];            
+            ll ans = 0;
+            
+            int k;  // [k*x, (k+1)*x-1]
+            for (k=1; x*k+x-1<=MaxN; k++)
+            {
+                ans = ans + (presum[x*k+x-1]-presum[x*k-1])*k%M;
+                ans %= M;
+            }
+            
+            if (x*k+x-1>MaxN && x*k-1<=MaxN)
+            {                
+                ans = ans + (presum[MaxN] - presum[x*k-1]) * k % M;
+                ans %= M;
+            }
+                        
+            ret = (ret + ans * count[x]) % M;            
+        }
+        
+        return ret;
+    }
+};
