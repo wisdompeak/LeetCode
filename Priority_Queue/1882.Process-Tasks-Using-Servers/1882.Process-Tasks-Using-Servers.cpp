aa4938249da0e69168fe65c335ae6db02a7d7e22
wisdompeak <guan.huifeng@gmail.com>
2021-05-30 22:45:02 -0700

    Update 1882.Process-Tasks-Using-Servers.cpp

--- Priority_Queue/1882.Process-Tasks-Using-Servers/1882.Process-Tasks-Using-Servers.cpp
+++ Priority_Queue/1882.Process-Tasks-Using-Servers/1882.Process-Tasks-Using-Servers.cpp
@@ -1,37 +1,34 @@
 typedef array<int,2> AI2;
 typedef array<int,3> AI3;
-
 class Solution {
 public:
     vector<int> assignTasks(vector<int>& servers, vector<int>& tasks) 
     {
-        priority_queue<AI2, vector<AI2>, greater<>>pq1;
-        priority_queue<AI3, vector<AI3>, greater<>>pq2;
+        priority_queue<AI2,vector<AI2>,greater<>>FreePQ;
+        priority_queue<AI3,vector<AI3>,greater<>>BusyPQ;
+        
         for (int i=0; i<servers.size(); i++)
-            pq1.push({servers[i], i});
-        
-        
+            FreePQ.push({servers[i],i});
         queue<int>jobs;
-        vector<int>rets(tasks.size(),-1);
+        vector<int>rets(tasks.size());
         
         for (int j=0; j<tasks.size(); j++)
         {
             jobs.push(j);
-            while (!pq2.empty() && pq2.top()[0] <= j)
+            while (!BusyPQ.empty() && BusyPQ.top()[0]<=j)
             {
-                auto [t,w,idx] = pq2.top();
-                pq2.pop();
-                pq1.push({w,idx});
+                auto [t,w,idx] = BusyPQ.top();
+                BusyPQ.pop();
+                FreePQ.push({w, idx});
             }
-            
-            while (!jobs.empty() && !pq1.empty())
+            while (!jobs.empty() && !FreePQ.empty())
             {
                 int job = jobs.front();
                 jobs.pop();
-                auto [w, idx] = pq1.top();
-                pq1.pop();
-                pq2.push({j+tasks[job], w, idx});                
+                auto [w,idx] = FreePQ.top();
+                FreePQ.pop();
                 rets[job] = idx;
+                BusyPQ.push({j+tasks[job], w, idx});
             }
         }
         
@@ -39,10 +36,10 @@ public:
         {
             int job = jobs.front();
             jobs.pop();
-            auto [t, w, idx] = pq2.top();
-            pq2.pop();
-            pq2.push({t+tasks[job], w, idx});                
+            auto [t,w,idx] = BusyPQ.top();
+            BusyPQ.pop();
             rets[job] = idx;
+            BusyPQ.push({t+tasks[job], w, idx});            
         }
         
         return rets;
