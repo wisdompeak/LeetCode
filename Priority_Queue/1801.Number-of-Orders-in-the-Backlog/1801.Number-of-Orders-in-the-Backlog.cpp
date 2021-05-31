64b6744115e49a69fd9e3ec091700527c4bf55f4
wisdompeak <guan.huifeng@gmail.com>
2021-05-30 22:39:08 -0700

    Create 1801.Number-of-Orders-in-the-Backlog.cpp

---
+++ Priority_Queue/1801.Number-of-Orders-in-the-Backlog/1801.Number-of-Orders-in-the-Backlog.cpp
@@ -0,0 +1,52 @@
+typedef pair<long,long> PII;
+class Solution {
+public:
+    int getNumberOfBacklogOrders(vector<vector<int>>& orders) 
+    {
+        priority_queue<PII>buy;
+        priority_queue<PII, vector<PII>, greater<>>sell;
+        
+        long ret = 0;
+        long M = 1e9+7;
+        for (auto order: orders)
+        {            
+            ret = (ret+order[1])%M;
+            
+            if (order[2]==0)
+            {
+                while (!sell.empty() && order[1]>0 && sell.top().first <= order[0])
+                {
+                    auto [price, amount] = sell.top();
+                    sell.pop();
+                    long num = min(amount, (long)order[1]);
+                    amount -= num;
+                    order[1] -= num;
+                    ret = (ret-num*2+M)%M;
+                    if (amount >0)
+                        sell.push({price, amount});
+                }
+                if (order[1]>0)
+                    buy.push({order[0], order[1]});                
+            }
+            else
+            {
+                while (!buy.empty() && order[1]>0 && buy.top().first >= order[0])
+                {
+                    auto [price, amount] = buy.top();
+                    buy.pop();
+                    long num = min(amount, (long)order[1]);
+                    amount -= num;
+                    order[1] -= num;
+                    ret = (ret-num*2+M)%M;
+                    if (amount >0)
+                        buy.push({price, amount});
+                }
+                if (order[1]>0)
+                    sell.push({order[0], order[1]});                
+            }            
+        }
+        
+        return ret;
+        
+    }
+};
