b83fdf26eec35a01eef6bbeec74d96fc8133947e
wisdompeak <guan.huifeng@gmail.com>
2021-06-01 01:11:18 -0700

    Update 798.Smallest-Rotation-with-Highest-Score.cpp

--- Others/798.Smallest-Rotation-with-Highest-Score/798.Smallest-Rotation-with-Highest-Score.cpp
+++ Others/798.Smallest-Rotation-with-Highest-Score/798.Smallest-Rotation-with-Highest-Score.cpp
@@ -3,22 +3,22 @@ public:
     int bestRotation(vector<int>& A) 
     {
         int N = A.size();
-        vector<int>change(N+1,0);
+        vector<int>diff(N,0);
         for (int i=0; i<N; i++)
         {
-            change[(i-A[i]+1+N)%N]-=1;
-            change[(i+1)%N]+=1;
+            diff[(i-A[i]+1+N)%N]-=1;
+            diff[(i+1)%N]+=1;
         }
         
         int sum = 0;
-        int result = 0;
+        int ret = 0;
         int K = 0;
         for (int i=1; i<N; i++)
         {
-            sum+=change[i];
-            if (sum>result)
+            sum += diff[i];
+            if (sum > ret)
             {
-                result = sum;
+                ret = sum;
                 K = i;
             }
         }
