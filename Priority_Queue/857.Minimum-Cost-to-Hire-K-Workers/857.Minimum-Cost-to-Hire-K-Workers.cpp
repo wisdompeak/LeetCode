aa8bd4f19be87260e7f6a9645f082cb28640156d
wisdompeak <guan.huifeng@gmail.com>
2021-06-11 18:19:23 -0700

    Update 857.Minimum-Cost-to-Hire-K-Workers.cpp

--- Priority_Queue/857.Minimum-Cost-to-Hire-K-Workers/857.Minimum-Cost-to-Hire-K-Workers.cpp
+++ Priority_Queue/857.Minimum-Cost-to-Hire-K-Workers/857.Minimum-Cost-to-Hire-K-Workers.cpp
@@ -1,29 +1,38 @@
 class Solution {
-public:
-    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int K) 
+    static bool cmp(pair<int,int>&a, pair<int,int>&b)
     {
-        vector<pair<double,int>>q;
+        return a.second*1.0 / a.first < b.second*1.0 / b.first; 
+    }
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) 
+    {
+        vector<pair<int,int>>persons;
         for (int i=0; i<quality.size(); i++)
-            q.push_back({wage[i]*1.0/quality[i],quality[i]});
-        sort(q.begin(),q.end());
-        
-        int totalQuality = 0;
-        double result = INT_MAX;
-        priority_queue<int>pq;
-        for (int i=0; i<q.size(); i++)
+            persons.push_back({quality[i], wage[i]});
+
+        sort(persons.begin(), persons.end(), cmp);
+
+        priority_queue<int>pq_quality;
+        int sum_quality = 0;
+        double ret = 1e20;
+
+        for (int i=0; i<persons.size(); i++)
         {
-            totalQuality+=q[i].second;
-            pq.push(q[i].second);
-            
-            if (pq.size()==K)
+            sum_quality += persons[i].first;
+            pq_quality.push(persons[i].first);
+            if (pq_quality.size() > k)
             {
-                double unitWage = q[i].first;
-                result = min(result,unitWage*totalQuality);
-                totalQuality-=pq.top();
-                pq.pop();
+                sum_quality -= pq_quality.top();
+                pq_quality.pop();
             }
-        }        
-        
-        return result;        
+            if (pq_quality.size() == k)
+            {
+                double unitWage = persons[i].second * 1.0 / persons[i].first;
+                ret = min(ret, unitWage * sum_quality);
+            }                
+        }
+
+        return ret;
+
     }
 };
