61d5535247a9b771a77266bfc56a64003ddc8caa
wisdompeak <guan.huifeng@gmail.com>
2021-06-14 17:00:44 -0700

    Create 1439.Find-the-Kth-Smallest-Sum-of-a-Matrix-With-Sorted-Rows_v2.cpp

---
+++ Heap/1439.Find-the-Kth-Smallest-Sum-of-a-Matrix-With-Sorted-Rows/1439.Find-the-Kth-Smallest-Sum-of-a-Matrix-With-Sorted-Rows_v2.cpp
@@ -0,0 +1,40 @@
+typedef pair<int,vector<int>> PIV;
+class Solution {    
+public:
+    int kthSmallest(vector<vector<int>>& mat, int k) 
+    {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        set<vector<int>>visited;
+        vector<int>idx(m,0);
+        visited.insert(idx);
+
+        int sum = 0;
+        for (int i=0; i<m; i++)
+            sum += mat[i][0];
+        priority_queue<PIV,vector<PIV>,greater<>>pq;
+        pq.push({sum, idx});
+
+        for (int t=0; t<k; t++)
+        {            
+            auto [sum, idx] = pq.top();
+            pq.pop();
+            if (t==k-1) return sum;
+
+            for (int i=0; i<m; i++)
+            {
+                cout<<i<<endl;
+                idx[i]++;
+                if (idx[i]<n && visited.find(idx)==visited.end())
+                {
+                    visited.insert(idx);
+                    pq.push({sum + mat[i][idx[i]] - mat[i][idx[i]-1], idx});
+                }
+                idx[i]--;
+            }
+        }
+
+        return -1;
+    }        
+};
