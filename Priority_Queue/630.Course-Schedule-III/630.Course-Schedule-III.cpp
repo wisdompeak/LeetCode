28faa74ecbb12e6559940040e1faa3fe0f380224
wisdompeak <guan.huifeng@gmail.com>
2021-06-17 22:01:50 -0700

    Update 630.Course-Schedule-III.cpp

--- Priority_Queue/630.Course-Schedule-III/630.Course-Schedule-III.cpp
+++ Priority_Queue/630.Course-Schedule-III/630.Course-Schedule-III.cpp
@@ -1,36 +1,26 @@
 class Solution {
-    static bool cmp(vector<int>a,vector<int>b)
+    static bool cmp(vector<int>&a, vector<int>&b)
     {
         return a[1]<b[1];
     }
 public:
     int scheduleCourse(vector<vector<int>>& courses) 
     {
-        sort(courses.begin(),courses.end(),cmp);        
+        sort(courses.begin(),courses.end(),cmp);
+        priority_queue<int>pq;
         
-        priority_queue<int>pq;        
-        
-        int day=0;
-        
+        int cur = 0;
         for (int i=0; i<courses.size(); i++)
         {
-            if (day+courses[i][0]<=courses[i][1])
+            pq.push(courses[i][0]);
+            cur += courses[i][0];
+            
+            if (cur>courses[i][1])
             {
-                day+=courses[i][0];
-                pq.push(courses[i][0]);
-            }
-            else
-            {
-                if (!pq.empty() && pq.top()>courses[i][0])
-                {
-                    day=day-pq.top()+courses[i][0];
-                    pq.pop();
-                    pq.push(courses[i][0]);
-                }
-            }
+                cur -= pq.top();
+                pq.pop();
+            }            
         }
-        
         return pq.size();
-        
     }
 };
