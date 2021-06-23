af865b353fcda84de00daae544bca8c897483d0d
wisdompeak <guan.huifeng@gmail.com>
2021-06-23 00:04:24 -0700

    Create 524.Longest-Word-in-Dictionary-through-Deleting.cpp

---
+++ Greedy/524.Longest-Word-in-Dictionary-through-Deleting/524.Longest-Word-in-Dictionary-through-Deleting.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string findLongestWord(string S, vector<string>& dictionary) 
+    {
+        int m = S.size();
+        S = "#"+S;
+        int next[m+1][26];
+        
+        for (int k=0; k<26; k++)
+            next[m][k] = -1;
+        for (int i=m; i>=1; i--)
+        {
+            for (int k=0; k<26; k++)
+                next[i-1][k] = next[i][k];
+            next[i-1][S[i]-'a'] = i;
+        }
+        
+        string ret = "";
+        for (auto word: dictionary)
+        {
+            int flag = 1;
+            int i = 0;
+            for (auto ch: word)
+            {
+                i = next[i][ch-'a'];
+                if (i==-1)
+                {
+                    flag = 0;
+                    break;
+                }                
+            }
+            if (flag)
+            {
+                if (ret=="" || word.size() > ret.size() || (word.size() == ret.size() && word < ret))
+                    ret = word;
+            }
+        }
+        return ret;        
+    }
+};
