d4f90886a115b0267e5a3a856546ab925d081b32
wisdompeak <guan.huifeng@gmail.com>
2021-06-26 14:28:04 -0700

    Update 1055.Shortest-Way-to-Form-String.cpp

--- Greedy/1055.Shortest-Way-to-Form-String/1055.Shortest-Way-to-Form-String.cpp
+++ Greedy/1055.Shortest-Way-to-Form-String/1055.Shortest-Way-to-Form-String.cpp
@@ -9,38 +9,29 @@ public:
         for (int k=0; k<26; k++)
             next[n][k] = -1;
         
-        for (int i=n-1; i>=0; i--)        
+        for (int i=n-1; i>=0; i--)      
+        {
             for (int k=0; k<26; k++)
-            {
-                if (k == source[i+1]-'a')
-                    next[i][k] = i+1;
-                else
-                    next[i][k] = next[i+1][k];                
-            }
+                next[i][k] = next[i+1][k];
+            next[i][source[i+1]-'a'] = i+1;
+            
+        }
         
         int j = 0;
-        int count = 0;
-        for (int i=0; i<target.size();)
-        {       
-            if (next[0][target[i]-'a']==-1)
-                return -1;
-                        
-            j = next[j][target[i]-'a'];
-            
-            if (j==-1)
+        int count = 1;
+        for (int i=0; i<target.size(); i++)
+        {               
+            if (next[j][target[i]-'a'] != -1)
             {
+                j =  next[j][target[i]-'a'];
+            }
+            else
+            {
+                if (j==0) return -1;
                 j = 0;
-                count++;                    
-            }            
-            else if ( j!= -1)  
-            {                
-                i += 1;
-                if (i==target.size())
-                {
-                    count++;
-                    break;
-                }                    
-            }    
+                count++;
+                i--;
+            }                
         }
         return count;        
     }
