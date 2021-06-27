bc34581a4ff82367ae494f35f36d324a937f3c0a
wisdompeak <guan.huifeng@gmail.com>
2021-06-26 18:08:54 -0700

    Update 1909.Remove-One-Element-to-Make-the-Array-Strictly-Increasing.cpp

--- Dynamic_Programming/1909.Remove-One-Element-to-Make-the-Array-Strictly-Increasing/1909.Remove-One-Element-to-Make-the-Array-Strictly-Increasing.cpp
+++ Dynamic_Programming/1909.Remove-One-Element-to-Make-the-Array-Strictly-Increasing/1909.Remove-One-Element-to-Make-the-Array-Strictly-Increasing.cpp
@@ -2,26 +2,32 @@ class Solution {
 public:
     bool canBeIncreasing(vector<int>& nums) 
     {
-        int n = nums.size();        
-        vector<int>dp0(n);
-        vector<int>dp1(n);
+        int n = nums.size();
         
-        dp0[0] = 1;
+        nums.insert(nums.begin(), INT_MIN); // nums[1:n]
+        vector<int>dp0(n+1);
+        vector<int>dp1(n+1);
+        
+        dp0[0] = 0;
         dp1[0] = 0;
         
-        for (int i=1; i<n; i++)
+        for (int i=1; i<=n; i++)
         {
-            dp0[i] = nums[i]>nums[i-1] ? (dp0[i-1] + 1) : 1;
-            
-            if (i>=2 && nums[i]>nums[i-2])
-                dp1[i] = max(dp1[i], dp0[i-2] + 1);
             if (nums[i]>nums[i-1])
-                dp1[i] = max(dp1[i], dp1[i-1] + 1);
-                        
-            if (dp0[i]>=n-1 || dp1[i]>=n-1) return true;
+                dp0[i] = dp0[i-1] + 1;
+            else     
+                dp0[i] = 1;
+            
+            dp1[i] = 0;
+            if (nums[i]>nums[i-1])
+                dp1[i] = max(dp1[i], dp1[i-1] + 1);            
+            if (i>=2 && nums[i]>nums[i-2])
+                dp1[i] =  max(dp1[i], dp0[i-2] + 1);  
+            
+            if (dp0[i]>=n-1 || dp1[i]>=n-1)
+                return true;
         }
         
         return false;
-        
     }
 };
