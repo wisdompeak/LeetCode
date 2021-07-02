f3b9b5aeb910d39e25db4a3ca536f12cea975323
wisdompeak <guan.huifeng@gmail.com>
2021-07-01 23:45:18 -0700

    Update 1909.Remove-One-Element-to-Make-the-Array-Strictly-Increasing_v3.cpp

--- Dynamic_Programming/1909.Remove-One-Element-to-Make-the-Array-Strictly-Increasing/1909.Remove-One-Element-to-Make-the-Array-Strictly-Increasing_v3.cpp
+++ Dynamic_Programming/1909.Remove-One-Element-to-Make-the-Array-Strictly-Increasing/1909.Remove-One-Element-to-Make-the-Array-Strictly-Increasing_v3.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    bool canBeIncreasing(vector<int>& nums) 
+    {
+        for (int i=0; i+1<nums.size(); i++)
+        {
+            if (nums[i] >= nums[i+1])
+            {
+                return check(nums,i) || check(nums,i+1);
+            }            
+        }
+        return true;
+    }
 
+    bool check(vector<int>&nums, int k)
+    {
+        int cur = INT_MIN;
+        for (int i=0; i<nums.size(); i++)
+        {
+            if (i==k) continue;
+            if (cur >= nums[i]) return false;
+            cur = nums[i];
+        }
+        return true;
+    }
+};
