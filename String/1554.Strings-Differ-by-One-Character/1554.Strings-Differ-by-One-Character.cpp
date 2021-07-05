41337e64f85ea8405b7add906882927eba446de0
wisdompeak <guan.huifeng@gmail.com>
2021-07-04 23:42:38 -0700

    Update 1554.Strings-Differ-by-One-Character.cpp

--- String/1554.Strings-Differ-by-One-Character/1554.Strings-Differ-by-One-Character.cpp
+++ String/1554.Strings-Differ-by-One-Character/1554.Strings-Differ-by-One-Character.cpp
@@ -3,31 +3,31 @@ class Solution {
 public:
     bool differByOne(vector<string>& dict) 
     {
-        ULL base = 26;        
+        ULL base = 26;
         int n = dict.size();
         int m = dict[0].size();
         vector<ULL>hash(n);
         for (int i=0; i<n; i++)
         {
             ULL h = 0;
-            for (int j=0; j<m; j++)
-                h = h*base+dict[i][j]-'a';
+            for (auto ch: dict[i])            
+                h = h * base + (ch-'a');
             hash[i] = h;
         }
         
-        ULL del = 1;
+        ULL power = 1;
         for (int j=m-1; j>=0; j--)
-        {
-            unordered_set<ULL>Set;            
-            
+        {                        
+            unordered_set<ULL>Set;
             for (int i=0; i<n; i++)
             {
-                ULL h = hash[i]  - (dict[i][j]-'a') * del;
-                if (Set.find(h)!=Set.end())
+                ULL hash_new = hash[i] - (dict[i][j]-'a')*power;
+                if (Set.find(hash_new)!=Set.end())
                     return true;
-                Set.insert(h);
-            }
-            del = del*base;
+                Set.insert(hash_new);
+            }            
+            
+            power *= base;
         }
         
         return false;
