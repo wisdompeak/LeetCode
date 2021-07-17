096f18a80374109ceb9492d00ef6ad74fb41354d
wisdompeak <guan.huifeng@gmail.com>
2021-07-17 15:46:27 -0700

    Update 795.Number-of-Subarrays-with-Bounded-Maximum.cpp

--- Others/795.Number-of-Subarrays-with-Bounded-Maximum/795.Number-of-Subarrays-with-Bounded-Maximum.cpp
+++ Others/795.Number-of-Subarrays-with-Bounded-Maximum/795.Number-of-Subarrays-with-Bounded-Maximum.cpp
@@ -3,24 +3,24 @@ public:
     int numSubarrayBoundedMax(vector<int>& A, int L, int R) 
     {
         long ret = 0;
-        int start = -1, lastLR = -1;
+        int start = -1, validMax = -1;
      
         for (int i = 0; i < A.size(); i++) 
         { 
             if (A[i] > R) 
             {                
                 start = -1;
-                lastLR = -1;
+                validMax = -1;
             }
             else
             {
                 if (start == -1)
                     start = i;
                 if (A[i]<=R && A[i]>=L)
-                    lastLR = i;                                
+                    validMax = i;                                
                 
-                if (start!=-1 && lastLR!=-1)
-                    ret += (lastLR-start+1);                
+                if (start!=-1 && validMax!=-1)
+                    ret += (validMax-start+1);                
             }
         }
  
