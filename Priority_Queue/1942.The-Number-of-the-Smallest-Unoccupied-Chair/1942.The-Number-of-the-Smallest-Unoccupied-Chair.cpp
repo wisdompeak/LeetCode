ac603c584ea03543e373fe9a72960a444a1d34f6
wisdompeak <guan.huifeng@gmail.com>
2021-07-24 14:02:52 -0700

    Create 1942.The-Number-of-the-Smallest-Unoccupied-Chair.cpp

---
+++ Priority_Queue/1942.The-Number-of-the-Smallest-Unoccupied-Chair/1942.The-Number-of-the-Smallest-Unoccupied-Chair.cpp
@@ -0,0 +1,37 @@
+using PII = pair<int,int>;
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) 
+    {
+        priority_queue<int, vector<int>, greater<>>empty;
+        for (int i=0; i<10005; i++)
+            empty.push(i);
+        
+        for (int i=0; i<times.size(); i++)
+            times[i].push_back(i);
+        sort(times.begin(), times.end());
+        
+        priority_queue<PII, vector<PII>, greater<>> used;
+        
+        for (auto time: times)
+        {
+            int start = time[0];
+            int end = time[1];
+            int people = time[2];
+            
+            while (!used.empty() && used.top().first <= start)
+            {
+                empty.push(used.top().second);
+                used.pop();
+            }
+            
+            int chair = empty.top();
+            empty.pop();
+            if (people==targetFriend) return chair;
+            
+            used.push({end, chair});
+        }
+        
+        return -1;
+    }
+};
