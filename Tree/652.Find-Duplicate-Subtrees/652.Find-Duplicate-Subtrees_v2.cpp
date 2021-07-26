2bbe7cdf4a7278f8730d271e7b940129201874b5
wisdompeak <guan.huifeng@gmail.com>
2021-07-25 21:03:28 -0700

    Update 652.Find-Duplicate-Subtrees_v2.cpp

--- Tree/652.Find-Duplicate-Subtrees/652.Find-Duplicate-Subtrees_v2.cpp
+++ Tree/652.Find-Duplicate-Subtrees/652.Find-Duplicate-Subtrees_v2.cpp
@@ -9,35 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-typedef long long ll;
+ using LL = long long;
 class Solution {
     vector<TreeNode*>rets;
-    unordered_map<ll, int>key2Id;
-    unordered_map<ll, int>key2count;
+    unordered_map<LL, int>key2Id;
+    unordered_map<LL, int>key2count;
 public:
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) 
     {
-        getId(root);
-        return rets;        
+        getID(root);
+        return rets;
     }
-    
-    ll getId(TreeNode* node)
+
+    LL getID(TreeNode* node)
     {
         if (node==NULL) return 0;
-        
-        ll key = (getId(node->left)<<32) + (getId(node->right)<<16) + node->val;
-        key2count[key]+=1;
-        if (key2count[key]==1)
+        LL key = (getID(node->left)<<32) + (getID(node->right)<<16) + node->val;
+
+        if (key2Id.find(key)==key2Id.end())
+        {                        
+            key2Id[key] = key2Id.size();
+            key2count[key] = 1;
+        }
+        else
         {
-            key2Id[key] = key2Id.size()+1;                        
+            key2count[key] += 1;            
+            if (key2count[key]==2)
+            {
+                rets.push_back(node);
+            }
         }
-        else if (key2count[key]==2)
-        {            
-            rets.push_back(node);            
-        }
-        
+
         return key2Id[key];
     }
-    
-    
 };
