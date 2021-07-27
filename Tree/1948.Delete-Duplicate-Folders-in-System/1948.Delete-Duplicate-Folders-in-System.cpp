269426c6b5585dbd3dc1b094ea95b098e414956c
wisdompeak <guan.huifeng@gmail.com>
2021-07-26 22:53:38 -0700

    Update 1948.Delete-Duplicate-Folders-in-System.cpp

--- Tree/1948.Delete-Duplicate-Folders-in-System/1948.Delete-Duplicate-Folders-in-System.cpp
+++ Tree/1948.Delete-Duplicate-Folders-in-System/1948.Delete-Duplicate-Folders-in-System.cpp
@@ -1,17 +1,16 @@
 class Solution {
-   struct TreeNode {
-      string val;
-      unordered_map<string, TreeNode*> next;
-      TreeNode(string s):val(s) {}
+    struct TreeNode {
+        string val;
+        map<string, TreeNode*> next;  
+        TreeNode(string s) : val(s) {}
     };
-    
     TreeNode* root;
-    vector<vector<string>>rets;
-    unordered_map<TreeNode*, string>node2key;
     unordered_map<string, int>key2Id;
     unordered_map<string, int>key2count;
+    unordered_map<TreeNode*, string> node2key;
+    vector<vector<string>>rets;
     
-    public:
+public:
     vector<vector<string>> deleteDuplicateFolder(vector<vector<string>>& paths) 
     {
         root = new TreeNode("/");
@@ -31,47 +30,54 @@ class Solution {
         vector<string>path;
         dfs(root, path);
         
-        return rets;
-        
+        return rets;        
     }
     
     int getId(TreeNode* node)
     {
-        if (node==NULL) return 0;
-                
+        if (node==NULL)
+            return 0;
+        
         string key;
         for (auto x: node->next)
-            key += to_string(getId(x.second)) + "#" + x.second->val;
+        {
+            TreeNode* child = x.second;
+            key += to_string(getId(child)) + "#" + child->val + "#";
+        }
         
         node2key[node] = key;
-                
-        key2count[key]+=1;
+        
+        key2count[key] += 1;
         if (key2count[key]==1)
         {
             key2Id[key] = key2Id.size()+1;
         }
+        
         return key2Id[key];
     }
-    
+        
     void dfs(TreeNode* node, vector<string>&path)
-    {                
+    {
         string key = node2key[node];
-
-        if (key!="" && key2count[key] >= 2) 
-            return;       
+        
+        if (key!="" && key2count[key]>=2)
+            return;
         
         if (node->val!="/")
         {
             path.push_back(node->val);
-            rets.push_back(path);
+            rets.push_back(path);            
         }
-                    
-        for (auto x: node->next)
+        
+        for (auto x : node->next)
         {
             dfs(x.second, path);
         }
         
-        if (node->val!="/")            
-            path.pop_back();
+        if (node->val!="/")
+        {
+            path.pop_back();            
+        }        
     }
+    
 };
