b10b2cc881997e794d74c8ad66693238a22b7e59
wisdompeak <guan.huifeng@gmail.com>
2021-07-31 12:23:12 -0700

    Update 1066.Campus-Bikes-II_v1.cpp

--- BFS/1066.Campus-Bikes-II/1066.Campus-Bikes-II_v1.cpp
+++ BFS/1066.Campus-Bikes-II/1066.Campus-Bikes-II_v1.cpp
@@ -3,35 +3,41 @@ class Solution {
 public:
     int assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) 
     {
-        int m = workers.size();
-        int n = bikes.size();
+        int m = bikes.size();
+        int n = workers.size();
         
         for (int i=0; i<m; i++)
             for (int j=0; j<n; j++)
             {
-                int x1 = workers[i][0];
-                int y1 = workers[i][1];
-                int x2 = bikes[j][0];
-                int y2 = bikes[j][1];
+                int x1 = bikes[i][0];
+                int y1 = bikes[i][1];
+                int x2 = workers[j][0];
+                int y2 = workers[j][1];
                 dist[i][j] = abs(x1-x2)+abs(y1-y2);
             }
         
+        int ret = INT_MAX/2;
         vector<int>dp(1<<m, INT_MAX/2);
-        vector<int>dp2;
         dp[0] = 0;
         for (int j=0; j<n; j++)
-        {
-            dp2 = dp;
-            for (int state = 0; state < (1<<m); state++)
+        {            
+            int state = (1 << (j+1)) - 1;            
+            while (state < (1 << m))  // iterate all state that contain j+1 bits of "1"
             {
                 for (int i=0; i<m; i++)
                 {
                     if ((state >> i) &1)
-                        dp[state] = min(dp[state], dp2[state- (1<<i)] + dist[i][j]);                    
-                }                
+                        dp[state] = min(dp[state], dp[state- (1<<i)] + dist[i][j]);
+                    
+                    if (j==n-1) ret = min(ret, dp[state]);
+                }     
+
+                int c = state & - state;
+                int r = state + c;
+                state = (((r ^ state) >> 2) / c) | r;
             }
         }
         
-        return dp[(1<<m)-1];
+        return ret;
     }
 };
