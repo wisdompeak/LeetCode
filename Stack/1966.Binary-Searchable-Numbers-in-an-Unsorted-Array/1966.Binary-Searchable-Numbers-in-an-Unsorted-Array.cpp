d5be01f8641c10281eb7dc160d1027cd9206add1
wisdompeak <guan.huifeng@gmail.com>
2021-08-11 22:58:05 -0700

    Update 1966.Binary-Searchable-Numbers-in-an-Unsorted-Array.cpp

--- Stack/1966.Binary-Searchable-Numbers-in-an-Unsorted-Array/1966.Binary-Searchable-Numbers-in-an-Unsorted-Array.cpp
+++ Stack/1966.Binary-Searchable-Numbers-in-an-Unsorted-Array/1966.Binary-Searchable-Numbers-in-an-Unsorted-Array.cpp
@@ -1,1 +1,38 @@
-
+class Solution {
+public:
+    int binarySearchableNumbers(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<int>nextSmaller(n, n);
+        stack<int>Stack;
+        for (int i=0; i<n; i++)
+        {
+            while (!Stack.empty() && nums[i]<nums[Stack.top()])
+            {
+                nextSmaller[Stack.top()] = i;
+                Stack.pop();
+            }
+            Stack.push(i);
+        }
+                
+        vector<int>prevGreater(n, -1);
+        while (!Stack.empty()) Stack.pop();
+        for (int i=n-1; i>=0; i--)
+        {
+            while (!Stack.empty() && nums[i]>nums[Stack.top()])
+            {
+                prevGreater[Stack.top()] = i;
+                Stack.pop();
+            }
+            Stack.push(i);
+        }
+        
+        int count = 0;
+        for (int i=0; i<n; i++)
+        {
+            if (nextSmaller[i]==n && prevGreater[i]==-1)
+                count++;
+        }
+        return count;        
+    }
+};
