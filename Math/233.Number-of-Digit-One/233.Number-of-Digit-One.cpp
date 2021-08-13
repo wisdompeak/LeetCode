fec06f904f1ef46a588a0e988e5882072eb91712
wisdompeak <guan.huifeng@gmail.com>
2021-08-12 22:14:49 -0700

    Update 233.Number-of-Digit-One.cpp

--- Math/233.Number-of-Digit-One/233.Number-of-Digit-One.cpp
+++ Math/233.Number-of-Digit-One/233.Number-of-Digit-One.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     int countDigitOne(int n) 
     {
-        if (n<0) return 0;
+        string N = to_string(n);
+        long len = N.size();
+        long count = 0;
         
-        int count = 0;
-        string str = to_string(n); 
-        
-        reverse(str.begin(),str.end());
-        
-        for (int i=1; i<=str.size(); i++)
-        {
-            int a = n/(int)pow(10,i);
-            count += a*(int)pow(10,i-1);
+        for (int i=1; i<=len; i++)
+        {   
+            long divisor = pow(10,i);
+            count += n/divisor*1*pow(10,i-1);
+
+            int digit = N[len-i]-'0';
             
-            int digit = str[i-1]-'0';
-            
-            if (digit>1)
+            if (digit > 1)
                 count += pow(10,i-1);
-            else if (digit==1)
-                count += n%(int)pow(10,i-1)+1;
+            else if (digit == 1)
+                count += n%(int)pow(10,i-1) + 1;
         }
-        
         return count;
     }
 };
