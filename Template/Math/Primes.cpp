76f3a84a5cfd5e820d0c38a3468c36d917f503bc
wisdompeak <guan.huifeng@gmail.com>
2021-09-05 20:36:18 -0700

    Create Primes.cpp

---
+++ Template/Math/Primes.cpp
@@ -0,0 +1,25 @@
+  // Find all primes <= n.
+  vector<int>Eratosthenes(int n)
+  {
+      auto q=vector<int>(n+1,0);
+
+      for (int i=2; i<=sqrt(n); i++)
+      {
+          if (q[i]==0)
+          {
+              int j=i*2;
+              while (j<n)
+              {
+                  q[j]=1;
+                  j+=i;
+              }
+          }
+      }
+      vector<int>primes;
+      for (int i=2; i<=n; i++)
+      {
+          if (q[i]==0)
+              primes.push_back(i);                
+      }
+      return primes;
+  }
