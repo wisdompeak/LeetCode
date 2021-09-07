6160937133fa032e02eb597cd7fe4a8008143721
wisdompeak <guan.huifeng@gmail.com>
2021-09-06 17:11:02 -0700

    Update 1997.First-Day-Where-You-Have-Been-in-All-the-Rooms.cpp

--- Others/1997.First-Day-Where-You-Have-Been-in-All-the-Rooms/1997.First-Day-Where-You-Have-Been-in-All-the-Rooms.cpp
+++ Others/1997.First-Day-Where-You-Have-Been-in-All-the-Rooms/1997.First-Day-Where-You-Have-Been-in-All-the-Rooms.cpp
@@ -1,15 +1,12 @@
 class Solution {
     long dp[100000];    
     long M = 1e9+7;
-    int n;
 public:
     int firstDayBeenInAllRooms(vector<int>& nextVisit) 
     {
         n = nextVisit.size();
         dp[0] = 0;
-        dp[1] = 2;
-        long ret = 2;
-        for (int i=1; i<n-1; i++)
+        for (int i=0; i<n-1; i++)
         {
             int prev = nextVisit[i];            
             dp[i+1] = dp[i] + (dp[i]-dp[prev] + M +1) % M + 1;            
@@ -17,6 +14,4 @@ public:
         }
         return dp[n-1];        
     }
-    
-    
 };
