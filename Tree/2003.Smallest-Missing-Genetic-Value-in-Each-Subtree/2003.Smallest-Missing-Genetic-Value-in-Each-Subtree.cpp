74d252c22d592372b396fc56e968386dc294c0ea
wisdompeak <guan.huifeng@gmail.com>
2021-09-12 17:51:39 -0700

    Create 2003.Smallest-Missing-Genetic-Value-in-Each-Subtree.cpp

---
+++ Tree/2003.Smallest-Missing-Genetic-Value-in-Each-Subtree/2003.Smallest-Missing-Genetic-Value-in-Each-Subtree.cpp
@@ -0,0 +1,68 @@
+class Solution {    
+    int visited[100001];
+    int vals[100005];
+    vector<int>children[100001];
+    int q = 1;
+public:
+    vector<int> smallestMissingValueSubtree(vector<int>& parents, vector<int>& nums) 
+    {
+        int n = parents.size();                
+        for (int i=1; i<n; i++)
+        {
+            if (parents[i]!=-1)
+                children[parents[i]].push_back(i);
+        }
+        
+        vector<int>val2node(100001, -1);
+        vector<int>rets(n, 1);
+        
+        for (int i=0; i<n; i++)
+            val2node[nums[i]] = i;
+        
+        if (val2node[1]==-1)
+            return rets;
+        
+        int node = val2node[1];
+        for (int child: children[node])
+            dfs1(child, rets);                        
+        while (node!=0)
+        {
+            int p = parents[node];
+            for (int child: children[p])
+            {
+                if (child!=node)
+                    dfs1(child, rets);
+            }
+            node = p;
+        }
+                                
+        node = val2node[1];
+        while (node!=-1)
+        {
+            dfs(node, nums);                            
+            while (vals[q]==1)
+                q++;
+            rets[node] = q;
+            node = parents[node];
+        }
+        
+        return rets;        
+    }
+    
+    void dfs1(int node, vector<int>&rets)
+    {
+        rets[node] = 1;    
+        for (auto child: children[node])        
+            dfs1(child, rets);                
+    }
+    
+    void dfs(int node, vector<int>&nums)
+    {
+        if (visited[node]==1) return;
+        visited[node] = 1;
+        
+        vals[nums[node]] = 1;        
+        for (int child: children[node])
+            dfs(child, nums);
+    }
+};
