4081185942cbd449a918be4c4f8b16551394ed8b
wisdompeak <guan.huifeng@gmail.com>
2021-09-15 01:09:21 -0700

    Update 978.Longest-Turbulent-Subarray.cpp

--- Others/978.Longest-Turbulent-Subarray/978.Longest-Turbulent-Subarray.cpp
+++ Others/978.Longest-Turbulent-Subarray/978.Longest-Turbulent-Subarray.cpp
@@ -12,15 +12,22 @@ public:
                 q[i] = 1;
         }
         
+        if (A.size()==1) return 1;
+        if (A.size()==2) return q[0]==0? 1: 2;
+        
         int count = 1;
         int start = 0;
-        for (int i=0; i<A.size()-1; i++)
+        for (int i=0; i+1<A.size()-1; i++)
         {
-            start = i;
-            while (i+1<A.size()-1 && q[i+1]*q[i]==-1)
-                i++;
             if (q[i]!=0)
+                count = max(count, 2);
+            
+            start = i;
+            while (i+1<A.size()-1 && q[i]*q[i+1]==-1)
+            {
+                i++;
                 count = max(count, i-start+2);
+            }
         }
         
         return count;
