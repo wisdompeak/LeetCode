aa91389a818566ebfa07f3deb3e2360e391f461e
wisdompeak <guan.huifeng@gmail.com>
2021-09-19 00:08:15 -0700

    Update 2008.Maximum-Earnings-From-Taxi_v1.cpp

--- Greedy/2008.Maximum-Earnings-From-Taxi/2008.Maximum-Earnings-From-Taxi_v1.cpp
+++ Greedy/2008.Maximum-Earnings-From-Taxi/2008.Maximum-Earnings-From-Taxi_v1.cpp
@@ -1,27 +1,21 @@
 using LL = long long;
 class Solution {
-    static bool cmp(vector<int>&a, vector<int>&b)
-    {
-        return a[1]<b[1];
-    }
 public:
     long long maxTaxiEarnings(int n, vector<vector<int>>& rides) 
     {
         vector<LL>dp(n+1);
-        sort(rides.begin(), rides.end(), cmp);
-        int k = 0;
+        unordered_map<int, vector<pair<int,int>>>Map; // endTime -> {startTime, gain}
+        for (auto ride: rides)
+            Map[ride[1]].push_back({ride[0], ride[1]-ride[0]+ride[2]});
         
         for (int i=1; i<=n; i++)
         {
             dp[i] = dp[i-1];
-            while (k<rides.size() && rides[k][1] < i)
-                k++;
-            while (k<rides.size() && rides[k][1]==i)
+            for (auto ride: Map[i])
             {
-                int start = rides[k][0];
-                dp[i] = max(dp[i], dp[start]+rides[k][1]-rides[k][0]+rides[k][2]);
-                k++;
-            }
+                int start = ride.first, gain = ride.second;
+                dp[i] = max(dp[i], dp[start] + ride.second);
+            }           
         }
         return dp[n];
     }
