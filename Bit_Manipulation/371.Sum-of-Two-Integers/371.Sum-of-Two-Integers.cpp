87e11f3d6adc11f4b7ea30e84cf547b36fa12c71
wisdompeak <guan.huifeng@gmail.com>
2021-09-25 18:30:41 -0700

    Update 371.Sum-of-Two-Integers.cpp

--- Bit_Manipulation/371.Sum-of-Two-Integers/371.Sum-of-Two-Integers.cpp
+++ Bit_Manipulation/371.Sum-of-Two-Integers/371.Sum-of-Two-Integers.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    int getSum(int a, int b) 
-    {        
-        while (b!=0)
-        {
-            int aa= a^b;  // 不进位的加法
-            int bb= (a&b)<<1; // 各个位上的进位值
-            a = aa;
-            b = bb;
-        }        
-        return a;        
+    int getSum(int a, int b) {
+        while (b != 0) {
+            unsigned int carry = (unsigned int)(a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
     }
 };
