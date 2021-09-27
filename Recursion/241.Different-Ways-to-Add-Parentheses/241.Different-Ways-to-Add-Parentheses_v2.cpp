a6615480df5b09f46fbd999f798dc24d6ccf6af1
wisdompeak <guan.huifeng@gmail.com>
2021-09-26 21:43:36 -0700

    Update 241.Different-Ways-to-Add-Parentheses_v2.cpp

--- Recursion/241.Different-Ways-to-Add-Parentheses/241.Different-Ways-to-Add-Parentheses_v2.cpp
+++ Recursion/241.Different-Ways-to-Add-Parentheses/241.Different-Ways-to-Add-Parentheses_v2.cpp
@@ -1,7 +1,7 @@
 class Solution {
     vector<int>nums;
     vector<char>ops;
-    
+    vector<int> dp[21][21];
 public:
     vector<int> diffWaysToCompute(string input) 
     {
@@ -10,18 +10,17 @@ public:
             int j = i;
             while (j<input.size() && isdigit(input[j]))
                 j++;
-            nums.push_back(stoi(input.substr(i,j-i)));
-            ops.push_back(input[j]);
-            i = j;
+            nums.push_back(stoi(input.substr(i, j-i)));
+            if (j<input.size()) ops.push_back(input[j]);
+            i = j;            
         }
-
+        
         int n = nums.size();
-        vector<vector<vector<int>>>dp(n, vector<vector<int>>(n));
-        helper(dp, 0, n-1);
-        return dp[0][n-1];
+        helper(0, n-1);
+        return dp[0][n-1];        
     }
-
-    void helper(vector<vector<vector<int>>>&dp, int a, int b)
+    
+    void helper(int a, int b)
     {
         if (!dp[a][b].empty())
             return;
@@ -33,10 +32,10 @@ public:
         
         for (int i=a; i<b; i++)
         {
-            helper(dp, a, i);
-            helper(dp, i+1, b);
-            for (auto x: dp[a][i])
-                for (auto y: dp[i+1][b])
+            helper(a,i);
+            helper(i+1,b);
+            for (int x: dp[a][i])
+                for (int y: dp[i+1][b])
                 {
                     if (ops[i]=='+')
                         dp[a][b].push_back(x+y);
@@ -46,5 +45,5 @@ public:
                         dp[a][b].push_back(x*y);
                 }
         }
-    }
+    }    
 };
