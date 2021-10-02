95bf313afc5a750653d14e913c6105af2c860e19
wisdompeak <guan.huifeng@gmail.com>
2021-10-02 15:37:19 -0700

    Update 424.Longest-Repeating-Character-Replacement_v1.cpp

--- Hash/424.Longest-Repeating-Character-Replacement/424.Longest-Repeating-Character-Replacement_v1.cpp
+++ Hash/424.Longest-Repeating-Character-Replacement/424.Longest-Repeating-Character-Replacement_v1.cpp
@@ -1,32 +1,25 @@
-class Solution {
-    vector<int>count;
+class Solution {    
 public:
     int characterReplacement(string s, int k) 
     {        
         int ret = 0;
         int j = 0;
-        count.resize(26);
+        vector<int>count(26);
         for (int i=0; i<s.size(); i++)
         {
-            while (j<s.size() && checkOK(s, j, j-i+1, k))
-                j++;            
-            ret = max(ret, j-i);            
+            while (j-i - *max_element(count.begin(), count.end()) <= k)
+            {
+                ret = max(ret, j-i);
+                if (j<s.size())
+                {
+                    count[s[j]-'A']++;
+                    j++;
+                }
+                else
+                    break;
+            }                            
             count[s[i]-'A']--;
         }
         return ret;
-    }
-
-    bool checkOK(string&s, int j, int total, int k)
-    {
-        count[s[j]-'A']++;
-        int maxCount = *max_element(count.begin(), count.end());
-        
-        if (total - maxCount <= k)
-            return true;
-        else
-        {
-            count[s[j]-'A']--;
-            return false;
-        }
     }
 };
