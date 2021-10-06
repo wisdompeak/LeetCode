871192afa18373f1db9f08d5d2408407e7824092
wisdompeak <guan.huifeng@gmail.com>
2021-10-05 22:17:49 -0700

    Update 2030.Smallest-K-Length-Subsequence-With-Occurrences-of-a-Letter.cpp

--- Stack/2030.Smallest-K-Length-Subsequence-With-Occurrences-of-a-Letter/2030.Smallest-K-Length-Subsequence-With-Occurrences-of-a-Letter.cpp
+++ Stack/2030.Smallest-K-Length-Subsequence-With-Occurrences-of-a-Letter/2030.Smallest-K-Length-Subsequence-With-Occurrences-of-a-Letter.cpp
@@ -1,65 +1,50 @@
 class Solution {
 public:
     string smallestSubsequence(string s, int k, char letter, int repetition) 
-    {        
-        int count = 0;
-        for (auto ch:s)
-        {
-            if (ch==letter)
-                count++;
-        }
-        
-        int k1 = count - repetition;
+    {
         int k0 = s.size() - k;
+        int count = 0;
+        for (auto ch: s)
+            if (ch==letter) count++;
+        int k1 = count - repetition;
         
-        stack<char>Stack;
-        int count0 = 0;
-        int count1 = 0;
-            
+        int count0 = 0; //the total number of letters deleted
+        int count1 = 0; // the total number of "letter" deleted
+        
+        stack<int>Stack;
         for (int i=0; i<s.size(); i++)
-        {
-            if (Stack.empty()||s[i]>=Stack.top())
+        {            
+            while (!Stack.empty() && s[i]<Stack.top() && count0<k0 && (Stack.top()!=letter|| (Stack.top()==letter && count1<k1)))
             {
-                Stack.push(s[i]);
+                if (Stack.top() == letter)
+                    count1++;
+                count0++;
+                Stack.pop();
             }
-            else if (s[i]<Stack.top())
-            {
-                while (!Stack.empty() && s[i]<Stack.top() && count0<k0 && ((Stack.top()!=letter || (Stack.top()==letter && count1<k1))))
-                {
-                    if (Stack.top()==letter)
-                        count1++;
-                    count0++;
-                    Stack.pop();                    
-                }
-                Stack.push(s[i]);
-            }
+            Stack.push(s[i]);            
         }
-        
         
         string result;
         while (!Stack.empty())
         {
-            result+=Stack.top();
+            result += Stack.top();
             Stack.pop();
         }
-                                        
-        string ans;
         
+        string ans;
         for (int i=0; i<result.size(); i++)
         {
-            if (count0 == k0 || (result[i]==letter && count1==k1))
+            if (count0==k0 || (result[i]==letter && count1==k1))
                 ans.push_back(result[i]);
             else
             {
                 count0++;
                 if (result[i]==letter)
-                    count1++;                
+                    count1++;
             }
         }
         
         reverse(ans.begin(), ans.end());
-        
         return ans;
-        
     }
 };
