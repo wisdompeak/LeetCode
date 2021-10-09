47b9d866766412b211bbd426a79534d925482d51
wisdompeak <guan.huifeng@gmail.com>
2021-10-08 23:30:40 -0700

    Create 1989.Maximum-Number-of-People-That-Can-Be-Caught-in-Tag.cpp

---
+++ Two_Pointers/1989.Maximum-Number-of-People-That-Can-Be-Caught-in-Tag/1989.Maximum-Number-of-People-That-Can-Be-Caught-in-Tag.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int catchMaximumAmountofPeople(vector<int>& team, int dist) 
+    {
+        int n = team.size();        
+        int j = 0;
+        int ret = 0;
+        for (int i=0; i<n; i++)
+        {
+            if (team[i]!=0) continue;
+            j = max(j, i-dist);
+            while (j<n && team[j]!=1)
+                j++;
+            if (j<n && abs(j-i)<=dist)
+            {
+                ret++;
+                team[j] = 2;                
+            }
+        }
+        return ret;        
+    }
+};
