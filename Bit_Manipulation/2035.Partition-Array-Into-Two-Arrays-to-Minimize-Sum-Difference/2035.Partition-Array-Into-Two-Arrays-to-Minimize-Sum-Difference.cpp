fb4b9b92eeb9d85f6272b5a32377b7ce1929c517
wisdompeak <guan.huifeng@gmail.com>
2021-10-10 16:26:00 -0700

    Update 2035.Partition-Array-Into-Two-Arrays-to-Minimize-Sum-Difference.cpp

--- Bit_Manipulation/2035.Partition-Array-Into-Two-Arrays-to-Minimize-Sum-Difference/2035.Partition-Array-Into-Two-Arrays-to-Minimize-Sum-Difference.cpp
+++ Bit_Manipulation/2035.Partition-Array-Into-Two-Arrays-to-Minimize-Sum-Difference/2035.Partition-Array-Into-Two-Arrays-to-Minimize-Sum-Difference.cpp
@@ -32,27 +32,31 @@ public:
         for (int i=n; i<2*n; i++)
             nums2.push_back(nums[i]);
         
-        unordered_map<int, vector<LL>>Map1 = helper(nums1);
         unordered_map<int, vector<LL>>Map2 = helper(nums2);
         
         LL sum = accumulate(nums.begin(), nums.end(), 0LL);
         LL ret = LLONG_MAX;
-        
-        for (int i=0; i<=n; i++)
+
+        for (int state=0; state<(1<<n); state++)
         {
-            int j = n-i;
-            for (LL x: Map1[i])
+            LL x = 0;
+            int i = __builtin_popcount(state);
+            for (int k=0; k<n; k++)
             {
-                auto iter = lower_bound(Map2[j].begin(), Map2[j].end(), sum/2-x);
-                if (iter!=Map2[j].end())
-                    ret = min(ret, abs(*iter*2+2*x-sum));
-                if (iter!=Map2[j].begin())
-                {
-                    iter = prev(iter);
-                    ret = min(ret, abs(*iter*2+2*x-sum));
-                }
+                if ((state>>k)&1)
+                    x += (LL)nums1[k];
             }
+            int j = n - i;
+            auto iter = lower_bound(Map2[j].begin(), Map2[j].end(), sum/2-x);
+            if (iter!=Map2[j].end())
+                ret = min(ret, abs(*iter*2+2*x-sum));
+            if (iter!=Map2[j].begin())
+            {
+                iter = prev(iter);
+                ret = min(ret, abs(*iter*2+2*x-sum));
+            }
+            
         }
-        return ret;
+      return ret;
     }
 };
