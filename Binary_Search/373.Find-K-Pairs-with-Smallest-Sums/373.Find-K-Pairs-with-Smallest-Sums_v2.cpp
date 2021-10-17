cf76c36f7b184bb6c87cc6a5594007ceb8258ce0
wisdompeak <guan.huifeng@gmail.com>
2021-10-17 16:23:11 -0700

    Update 373.Find-K-Pairs-with-Smallest-Sums_v2.cpp

--- Binary_Search/373.Find-K-Pairs-with-Smallest-Sums/373.Find-K-Pairs-with-Smallest-Sums_v2.cpp
+++ Binary_Search/373.Find-K-Pairs-with-Smallest-Sums/373.Find-K-Pairs-with-Smallest-Sums_v2.cpp
@@ -1,69 +1,50 @@
-class Solution {
-    static bool cmp(vector<int>&a, vector<int>&b)
-    {
-        return a[0]+a[1]<b[0]+b[1];
-    }
+class Solution {    
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) 
     {
-        int M = nums1.size();
-        int N = nums2.size();
-        if (M==0 || N==0) return {};
-        
-        auto matrix = vector<vector<int>>(M,vector<int>(N));
-        for (int i=0; i<M; i++)
-            for (int j=0; j<N; j++)
-                matrix[i][j] = nums1[i]+nums2[j];
-        
-        int left = matrix[0][0];
-        int right = matrix[M-1][N-1];
-        while (left<right)
+        long left = INT_MIN, right = INT_MAX;
+        while (left < right)
         {
-            int mid = left+(right-left)/2;
-            if (EqualOrSmaller(matrix, mid) < k)
-                 left = mid+1;
+            long mid = left+(right-left)/2;
+            long count = countSmallerOrEqual(mid, nums1, nums2);
+            if (count < k)
+                left = mid+1;
             else
                 right = mid;
         }
-        
-        int x = left;
-        
-        vector<vector<int>>results;
-        int i = M-1, j = 0;
-        while (i>=0 && j<N)
+        int m = left;
+
+        vector<vector<int>>rets1;
+        vector<vector<int>>rets2;
+        for (int i=0; i<nums1.size(); i++)
         {
-            if (matrix[i][j]<=x)
+            int j=0;
+            while (j<nums2.size() && nums1[i]+nums2[j]<=m)
             {
-                for (int k=i; k>=0; k--)
-                    results.push_back({nums1[k],nums2[j]});
+                if (nums1[i]+nums2[j]<m)
+                    rets1.push_back({nums1[i], nums2[j]});
+                else
+                    rets2.push_back({nums1[i], nums2[j]});
                 j++;
-            }
-            else
-                i--;
+            }                
         }
         
-        sort(results.begin(), results.end(), cmp);
-        if (k<results.size())
-            results.resize(k);
-        return results;
+        int t = min(rets2.size(), k - rets1.size());
+        for (int i=0; i<t; i++)
+            rets1.push_back(rets2[i]);
+        return rets1;
     }
-    
-    int EqualOrSmaller(vector<vector<int>>& matrix, int x)
+
+    long countSmallerOrEqual(int m, vector<int>& nums1, vector<int>& nums2)
     {
-        int M = matrix.size();
-        int N = matrix[0].size();
-        int i = M-1, j = 0;
-        int count = 0;
-        while (i>=0 && j<N)
+        int j = nums2.size()-1;
+        long ret = 0;
+        for (int i=0; i<nums1.size(); i++)
         {
-            if (matrix[i][j]<=x)
-            {
-                count += i+1;
-                j++;
-            }
-            else
-                i--;
+            while (j>=0 && nums1[i]+nums2[j]>m)
+                j--;
+            ret += j+1;
         }
-        return count;        
+        return ret;
     }
 };
