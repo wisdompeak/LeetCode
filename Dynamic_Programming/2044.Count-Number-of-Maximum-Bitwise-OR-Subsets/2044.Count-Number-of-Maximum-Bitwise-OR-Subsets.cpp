7434572719ba0a6da79206bdecdae456a461d1c3
wisdompeak <guan.huifeng@gmail.com>
2021-10-17 17:44:47 -0700

    Update 2044.Count-Number-of-Maximum-Bitwise-OR-Subsets.cpp

--- Dynamic_Programming/2044.Count-Number-of-Maximum-Bitwise-OR-Subsets/2044.Count-Number-of-Maximum-Bitwise-OR-Subsets.cpp
+++ Dynamic_Programming/2044.Count-Number-of-Maximum-Bitwise-OR-Subsets/2044.Count-Number-of-Maximum-Bitwise-OR-Subsets.cpp
@@ -3,21 +3,21 @@ public:
     int countMaxOrSubsets(vector<int>& nums) 
     {
         int n = nums.size();
-        int k = 17;
-        vector<int>dp(1<<k);
+        int M = (1<<17);
+        vector<int>dp(M+1);
         dp[0] = 1;
         
         for (int x : nums)
         {
             auto dp2 = dp;
-            for (int s = 0; s<(1<<k); s++)
+            for (int s = 0; s<M; s++)
             {
                 dp2[s|x] += dp[s];
             }
             dp = std::move(dp2);
         }
         
-        for (int s=(1<<k)-1; s>=0; s--)
+        for (int s=M; s>=0; s--)
         {
             if (dp[s]>0)
                 return dp[s];
