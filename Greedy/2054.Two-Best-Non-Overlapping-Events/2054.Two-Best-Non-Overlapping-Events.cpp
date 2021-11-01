f2048fd66c88e810f5bca3e3cee3282cb0d4930e
wisdompeak <guan.huifeng@gmail.com>
2021-10-31 20:50:34 -0700

    Update 2054.Two-Best-Non-Overlapping-Events.cpp

--- Greedy/2054.Two-Best-Non-Overlapping-Events/2054.Two-Best-Non-Overlapping-Events.cpp
+++ Greedy/2054.Two-Best-Non-Overlapping-Events/2054.Two-Best-Non-Overlapping-Events.cpp
@@ -3,33 +3,38 @@ class Solution {
     {
         return a[1] < b[1];
     }
+
 public:
     int maxTwoEvents(vector<vector<int>>& events) 
     {
         int n = events.size();
         sort(events.begin(), events.end(), cmp);
         
-        vector<pair<int,int>>dp; // {time, value}
-        dp.push_back({events[0][1], events[0][2]});
-        int rollingMax = events[0][2];
-            
-        int ret = events[0][2];
+        vector<int>rollingMax(n);
+        int m = 0;
+        for (int i=0; i<n; i++)
+        {
+            m = max(m, events[i][2]);
+            rollingMax[i] = m;
+        }
         
-        for (int i=1; i<n; i++)
+        vector<int>endTimes;
+        int ret = 0;
+        
+        for (int i=0; i<n; i++)
         {
             int a = events[i][0]-1, b = events[i][1], v = events[i][2];
+            ret = max(ret, v);
             
-            int lastMax = 0;
-            auto iter = upper_bound(dp.begin(), dp.end(), make_pair(a, INT_MAX));
-            if (iter!=dp.begin())
+            auto iter = upper_bound(endTimes.begin(), endTimes.end(), a);
+            if (iter!=endTimes.begin())
             {
                 iter = prev(iter);
-                lastMax = iter->second;
-            }                        
-            ret = max(ret, lastMax + v);
+                int idx = iter - endTimes.begin();
+                ret = max(ret, rollingMax[idx] + v);
+            }
             
-            rollingMax = max(rollingMax, v);
-            dp.push_back({b, rollingMax});
+            endTimes.push_back(b);
         }
         
         return ret;        
