7ed47019129e07510d16359c3bc848853210df24
refinedcoding <63949022+refinedcoding@users.noreply.github.com>
2021-12-07 22:32:20 -0800

    Update 479.Largest-Palindrome-Product.cpp

--- Others/479.Largest-Palindrome-Product/479.Largest-Palindrome-Product.cpp
+++ Others/479.Largest-Palindrome-Product/479.Largest-Palindrome-Product.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
+    // Rank: 34.85% -> 81.82%
     int largestPalindrome(int n) 
     {
         if (n==1) return 9;
-        
-        int upper = pow(10,n)-1;
-        int lower = pow(10,n-1);
+        int lower = pow(10,n-1), upper = lower * 10 - 1;
         
         for (int i=upper; i>=lower; i--)
         {
-            long long palindrome = createPalindrome(i);
-            
-            for (int j=upper; j>=sqrt(palindrome); j--)
+            long long palindrome = i;
+            for (int j = i; j > 0; j /= 10) 
             {
-                if (palindrome%j==0 && palindrome/j>=pow(10,n-1))
+                palindrome = palindrome * 10 + j % 10;
+            }
+            
+            for (int j=sqrt(palindrome); j <= upper; j++)
+            {
+                if (palindrome%j==0 && palindrome/j<=upper)
                     return palindrome%1337;
             }
         }
         
         return -1;
-    }
-    
-    long long createPalindrome(int i)
-    {
-        string temp = to_string(i);
-        reverse(temp.begin(),temp.end());
-        return stol(to_string(i)+temp);
     }
 };
