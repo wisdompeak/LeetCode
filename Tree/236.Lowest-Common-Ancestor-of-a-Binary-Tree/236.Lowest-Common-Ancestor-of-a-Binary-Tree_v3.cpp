26c1e4dca806a5549a59851d9e5563d535ea4643
wisdompeak <guan.huifeng@gmail.com>
2021-12-11 13:55:04 -0800

    Create  236.Lowest-Common-Ancestor-of-a-Binary-Tree_v3.cpp

---
+++ Tree/236.Lowest-Common-Ancestor-of-a-Binary-Tree/236.Lowest-Common-Ancestor-of-a-Binary-Tree_v3.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    int count = 0;
+    TreeNode* ret = NULL;
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        dfs(root, p, q);
+        return ret;
+    }
+    
+    int dfs(TreeNode* node, TreeNode* p, TreeNode* q) 
+    {
+        if (node==NULL) return 0;
+        int count = dfs(node->left, p, q) + dfs(node->right, p, q);
+        if (node==p || node==q) count++;        
+        if (count == 2 && ret == NULL)
+            ret = node;
+        return count;        
+    }
+};
