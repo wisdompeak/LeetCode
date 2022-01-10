6dbddb2e1bb8f4feb9c3c582c67e8e8c1b56e020
wisdompeak <guan.huifeng@gmail.com>
2022-01-10 00:28:25 -0800

    Update 2132.Stamping-the-Grid_v1.cpp

--- Others/2132.Stamping-the-Grid/2132.Stamping-the-Grid_v1.cpp
+++ Others/2132.Stamping-the-Grid/2132.Stamping-the-Grid_v1.cpp
@@ -1,6 +1,7 @@
-class Solution {
+class RegionSum {
+    vector<vector<int>>presum;
 public:
-    vector<vector<int>>getPresum(vector<vector<int>>& A)
+    RegionSum(vector<vector<int>>& A)
     {
         int m = A.size();
         int n = A[0].size();
@@ -13,10 +14,9 @@ public:
                 int c = (i==0||j==0)?0:presum[i-1][j-1];
                 presum[i][j] = a + b - c + A[i][j];
             }
-        return presum;
+        this->presum = presum;
     }
-
-    int queryPresum(vector<vector<int>>& presum, int i, int j, int x, int y)
+    int query(int i, int j, int x, int y)
     {
         int a = j==0?0:presum[x][j-1];
         int b = i==0?0:presum[i-1][y];
@@ -24,32 +24,34 @@ public:
         int area = presum[x][y] - a - b + c;
         return area;
     }
+};
 
+class Solution {
+public:
     bool possibleToStamp(vector<vector<int>>& grid, int stampHeight, int stampWidth) 
     {
         int m = grid.size(), n = grid[0].size();
-        auto gridPresum = getPresum(grid);
+        vector<vector<int>>stamps(m, vector<int>(n));
 
-        vector<vector<int>>stamp(m, vector<int>(n));
-        for (int i=stampHeight-1; i<m; i++)
-            for (int j=stampWidth-1; j<n; j++)
+        RegionSum Grid(grid);        
+        for (int i=0; i+stampHeight-1<m; i++)
+            for (int j=0; j+stampWidth-1<n; j++)
             {
-                int x = i-stampHeight+1;
-                int y = j-stampWidth+1; 
-                int area = queryPresum(gridPresum, x, y, i, j);
+                int x = i+stampHeight-1;
+                int y = j+stampWidth-1; 
+                int area = Grid.query(i, j, x, y);
                 if (area == 0)
-                    stamp[i][j] = 1;
+                    stamps[x][y] = 1;
             }
         
-        auto stampPresum = getPresum(stamp);
-                
+        RegionSum Stamps(stamps);                
         for (int i=0; i<m; i++)
             for (int j=0; j<n; j++)
             {
                 if (grid[i][j]==1) continue;
                 int x = min(m-1, i+stampHeight-1);
                 int y = min(n-1, j+stampWidth-1);
-                int area = queryPresum(stampPresum, i, j, x, y);
+                int area = Stamps.query(i, j, x, y);
                 if (area == 0) return false;
             }
         return true;
