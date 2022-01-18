46adc6d2e02a70be8c1b0d145ca7fbb8c8f7ff9c
wisdompeak <guan.huifeng@gmail.com>
2022-01-18 00:44:01 -0800

    Update 2140.Solving-Questions-With-Brainpower_v2.cpp

--- Dynamic_Programming/2140.Solving-Questions-With-Brainpower/2140.Solving-Questions-With-Brainpower_v2.cpp
+++ Dynamic_Programming/2140.Solving-Questions-With-Brainpower/2140.Solving-Questions-With-Brainpower_v2.cpp
@@ -1,21 +1,30 @@
 using LL = long long;
 class Solution {
 public:
-    long long mostPoints(vector<vector<int>>& questions) 
-    {
+    long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        LL ret = 0;
-        vector<LL>dp(n, 0);    // dp[i]: the maximum gain before making deicisions on i-th problem
+        vector<vector<LL>> dp(n, vector<LL>(2));        
+        dp[0][1] = questions[0][0];
+
+        vector<pair<int, int>>endTimes;
         for (int i=0; i<n; i++)
+            endTimes.push_back({i+questions[i][1], i});
+        sort(endTimes.begin(), endTimes.end());
+        int p = 0;
+        
+        LL maxPre = 0;
+        for (int i = 1; i < n; ++i)
         {
-            int skip = questions[i][1];
-            if (i+1<n)
-                dp[i+1] = max(dp[i+1], dp[i]);
-            if (i+skip+1<n)
-                dp[i+skip+1] = max(dp[i+skip+1], dp[i] + questions[i][0]);
-            ret = max(ret, dp[i]+questions[i][0]);
+            while (p<n && endTimes[p].first < i)
+            {                
+                maxPre = max(maxPre, dp[endTimes[p].second][1]);
+                p++;
+            }
+
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = maxPre + questions[i][0];
         }
 
-        return ret;        
+        return max(dp[n - 1][0] , dp[n - 1][1]);
     }
 };
