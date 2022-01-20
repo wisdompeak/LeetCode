79bb71ce81adf725341c4d2898370e9fc0d2f784
wisdompeak <guan.huifeng@gmail.com>
2022-01-20 01:52:30 -0800

    Update 2140.Solving-Questions-With-Brainpower_v1.cpp

--- Dynamic_Programming/2140.Solving-Questions-With-Brainpower/2140.Solving-Questions-With-Brainpower_v1.cpp
+++ Dynamic_Programming/2140.Solving-Questions-With-Brainpower/2140.Solving-Questions-With-Brainpower_v1.cpp
@@ -4,12 +4,12 @@ public:
     long long mostPoints(vector<vector<int>>& questions) 
     {
         int n = questions.size();
-        vector<LL>dp(n+1, 0);    
+        vector<LL>dp(n+1);
         for (int i=n-1; i>=0; i--)
         {
-            int skip = questions[i][1];
-            dp[i] = max(dp[i+1], dp[min(i+skip+1, n)] + questions[i][0]);
+            int j = i+questions[i][1]+1;            
+            dp[i] = max(dp[i+1], (j<n?dp[j]:0) + questions[i][0]);
         }
-        return dp[0];        
+        return dp[0];
     }
 };
