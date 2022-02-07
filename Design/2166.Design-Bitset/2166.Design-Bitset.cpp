2888194132c15e9100a3781c6d8c3760cd4db1c4
wisdompeak <guan.huifeng@gmail.com>
2022-02-06 18:12:04 -0800

    Update 2166.Design-Bitset.cpp

--- Design/2166.Design-Bitset/2166.Design-Bitset.cpp
+++ Design/2166.Design-Bitset/2166.Design-Bitset.cpp
@@ -1,10 +1,10 @@
-class Bitset {    
+class Bitset {
     int flips[100000];
-    int m;
     int totalFlip = 0;
+    int m;
     int cnt = 0;
 public:
-    Bitset(int size) {        
+    Bitset(int size) {
         m = size;
         for (int i=0; i<m; i++)
             flips[i] = 0;
@@ -14,65 +14,43 @@ public:
     {
         if ((flips[idx]+totalFlip)%2==0)
         {
-            flips[idx] += 1;
-            flips[idx] %= 2;            
+            flips[idx]+=1;
             cnt++;
         }
     }
     
-    void unfix(int idx) 
-    {
+    void unfix(int idx) {
         if ((flips[idx]+totalFlip)%2==1)
         {
-            flips[idx] += 1;
-            flips[idx] %= 2;            
+            flips[idx]+=1;
             cnt--;
         }
     }
     
-    void flip() 
-    {
+    void flip() {
         totalFlip += 1;
-        totalFlip %= 2;
         cnt = m - cnt;
     }
     
-    bool all() 
-    {
+    bool all() {
         return cnt == m;
     }
     
-    bool one() 
-    {
+    bool one() {
         return cnt >= 1;
     }
     
-    int count() 
-    {
+    int count() {
         return cnt;
     }
     
-    string toString() 
-    {
+    string toString() {
         string s;
         for (int i=0; i<m; i++)
         {
-            int t = (flips[i]+totalFlip)%2;            
-            s += (t==1?"1":"0");
+            int t = (flips[i]+totalFlip)%2;
+            s += t==0?"0":"1";
         }
-            
         return s;
     }
 };
-
-/**
- * Your Bitset object will be instantiated and called as such:
- * Bitset* obj = new Bitset(size);
- * obj->fix(idx);
- * obj->unfix(idx);
- * obj->flip();
- * bool param_4 = obj->all();
- * bool param_5 = obj->one();
- * int param_6 = obj->count();
- * string param_7 = obj->toString();
- */
