9b7d58e500050fa36f00131fd2248380a563292d
Fenghe Xu <jxphxufh@gmail.com>
2022-02-09 00:44:44 +0800

    fix: 1774 brute-force TLE
    Remove duplicates of topping combinations to prevent TLE.

--- Bit_Manipulation/1774.Closest-Dessert-Cost/1774.Closest-Dessert-Cost.cpp
+++ Bit_Manipulation/1774.Closest-Dessert-Cost/1774.Closest-Dessert-Cost.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
-    int closestCost(vector<int>& baseCosts, vector<int>& toppingCosts, int target) 
-    {
-        int cost;
-        int diff = INT_MAX;
-        int m = toppingCosts.size();
+    int closestCost(vector<int>& baseCosts, vector<int>& toppingCosts, int target) {
+        set<int> toppingsSet;
+
+        int n = toppingCosts.size();
+
+        for(int state = 0; state < (1 << (2 * n)); ++state) {
+            int sum = 0;
+            for(int i = 0; i < n; ++i) {
+                int cur = (state >> (2 * i)) & 3;
+                if(!cur || cur == 3) continue;
+                sum += toppingCosts[i] * cur;
+            }
+            toppingsSet.insert(sum);
+        }
         
-        for (int baseCost: baseCosts)
-        {            
-            for (int state = 0; state < (1<<(2*m)); state++)
-            {
-                int sum = baseCost;
-                for (int i=0; i<m; i++)
-                {
-                    if ((state>>(i*2))&1)
-                        sum += toppingCosts[i];
-                    if ((state>>(i*2+1))&1)
-                        sum += toppingCosts[i];
-                }
-                if (abs(sum-target)<diff || abs(sum-target)==diff && sum<cost)
-                {
-                    diff = abs(sum-target);
-                    cost = sum;
-                }
-            }            
-        }        
-        return cost;        
+        int ret = INT_MAX;
+        for(int base: baseCosts) {
+            int targetTopping = target - base;
+
+            auto it1 = toppingsSet.upper_bound(targetTopping);
+            auto it2 = it1;
+            if(it2 != toppingsSet.begin()) --it2;
+            if(it1 == toppingsSet.end()) --it1;
+            int cur;
+            int curDiff1 = abs(*it1 + base - target);
+            int curDiff2 = abs(*it2 + base - target);
+            if(curDiff1  < curDiff2) {
+                cur = *it1 + base;
+            } else {
+                cur = *it2 + base;
+            }
+            int retDiff = abs(ret - target);
+            int curDiff = abs(cur - target);
+            if(curDiff < retDiff || (curDiff == retDiff && cur < ret)) ret = cur;
+        }
+        return ret;
     }
 };
