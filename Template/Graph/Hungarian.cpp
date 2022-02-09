3a37256542e2aa3301486fb82f7f2c8512a05f4e
wisdompeak <guan.huifeng@gmail.com>
2022-02-08 22:11:43 -0800

    Update Hungarian.cpp

--- Template/Graph/Hungarian.cpp
+++ Template/Graph/Hungarian.cpp
@@ -1,100 +1,42 @@
-// https://www.renfei.org/blog/bipartite-matching.html
-// 顶点、边的编号均从 0 开始
-// 邻接表储存
-
-struct Edge
-{
-    int from;
-    int to;
-    int weight;
-
-    Edge(int f, int t, int w):from(f), to(t), weight(w) {}
-};
-
-vector<int> G[__maxNodes]; /* G[i] 存储顶点 i 出发的边的编号 */
-vector<Edge> edges;
-typedef vector<int>::iterator iterator_t;
-int num_nodes;
-int num_left;
-int num_right;
-int num_edges;
-int matching[__maxNodes]; /* 存储求解结果 */
-int check[__maxNodes];
-
-/********* DFS ******************************/
-
-bool dfs(int u)
-{
-    for (iterator_t i = G[u].begin(); i != G[u].end(); ++i) { // 对 u 的每个邻接点
-        int v = edges[*i].to;
-        if (!check[v]) {     // 要求不在交替路中
-            check[v] = true; // 放入交替路
-            if (matching[v] == -1 || dfs(matching[v])) {
-                // 如果是未盖点，说明交替路为增广路，则交换路径，并返回成功
-                matching[v] = u;
-                matching[u] = v;
+int num_nodes = 100000;
+class Solution {
+    vector<int> next[num_nodes];
+    int match[num_nodes];
+public:
+    int minimumOperations(vector<vector<int>>& grid) 
+    {
+        // construct next[]
+        
+        for (int i=0; i<num_nodes; i++)
+            match[i] = -1;
+        
+        vector<bool>visited(num_nodes);
+        
+        int count = 0;
+        for (int i=0; i<num_nodes; i++)
+        {
+            if (match[i]!=-1) continue;
+            visited.assign(num_nodes, 0);
+            if (dfs(i, visited))
+                count++;            
+        }
+        
+        return count;
+    }
+                                        
+    bool dfs(int i, vector<bool>&visited)
+    {
+        for (int j: next[i])
+        {
+            if (visited[j]) continue;
+            visited[j] = true;
+            if (match[j]==-1 || dfs(match[j], visited))
+            {
+                match[i] = j;
+                match[j] = i;
                 return true;
             }
         }
-    }
-    return false; // 不存在增广路，返回失败
-}
-
-int hungarian()
-{
-    int ans = 0;
-    memset(matching, -1, sizeof(matching));
-    for (int u=0; u < num_left; ++u) {
-        if (matching[u] == -1) {
-            memset(check, 0, sizeof(check));
-            if (dfs(u))
-                ++ans;
-        }
-    }
-    return ans;
-}
-
-/********* BFS ******************************/
-
-queue<int> Q;
-int prev[__maxNodes];
-int Hungarian()
-{
-    int ans = 0;
-    memset(matching, -1, sizeof(matching));
-    memset(check, -1, sizeof(check));
-    for (int i=0; i<num_left; ++i) {
-        if (matching[i] == -1) {
-            while (!Q.empty()) Q.pop();
-            Q.push(i);
-            prev[i] = -1; // 设 i 为路径起点
-            bool flag = false; // 尚未找到增广路
-            while (!Q.empty() && !flag) {
-                int u = Q.front();
-                for (iterator_t ix = G[u].begin(); ix != G[u].end() && !flag; ++ix) {
-                    int v = edges[*ix].to;
-                    if (check[v] != i) {
-                        check[v] = i;
-                        Q.push(matching[v]);
-                        if (matching[v] >= 0) { // 此点为匹配点
-                            prev[matching[v]] = u;
-                        } else { // 找到未匹配点，交替路变为增广路
-                            flag = true;
-                            int d=u, e=v;
-                            while (d != -1) {
-                                int t = matching[d];
-                                matching[d] = e;
-                                matching[e] = d;
-                                d = prev[d];
-                                e = t;
-                            }
-                        }
-                    }
-                }
-                Q.pop();
-            }
-            if (matching[i] != -1) ++ans;
-        }
-    }
-    return ans;
-}
+        return false;
+    }                                        
+};
