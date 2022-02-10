efa9557191bedb486482d0c1d1d1b5870d8205fe
wisdompeak <guan.huifeng@gmail.com>
2022-02-10 10:44:04 -0800

    Update 416.Partition-Equal-Subset-Sum_dfs_v1.cpp

--- Dynamic_Programming/416.Partition-Equal-Subset-Sum/416.Partition-Equal-Subset-Sum_dfs_v1.cpp
+++ Dynamic_Programming/416.Partition-Equal-Subset-Sum/416.Partition-Equal-Subset-Sum_dfs_v1.cpp
@@ -4,22 +4,28 @@ public:
     bool canPartition(vector<int>& nums) 
     {
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        sort(nums.rbegin(), nums.rend());
+        sort(nums.begin(), nums.end());
         if (sum%2!=0) return false;
         return dfs(nums, 0, 0, sum/2);        
     }
     
     bool dfs(vector<int>& nums, int cur, int curSum, int targetSum)
     {
-        if (curSum == targetSum) return true;
-        if (curSum > targetSum) return false;
-        int hash = curSum * 20005 + cur;
+        int hash = curSum * 1000 + cur;
         if (visited.find(hash)!=visited.end()) 
             return false;
         
+        if (curSum == targetSum) return true;
+        if (curSum > targetSum) return false;
+        if (cur==nums.size()) return false;
+        
         for (int i=cur; i<nums.size(); i++)
         {
-            if (i>cur && nums[i]==nums[i-1]) continue;
+            if (i>cur && nums[i]==nums[i-1])
+            {
+                visited.insert((curSum+nums[i])*1000 + i);
+                continue;
+            }
             if (dfs(nums, i+1, curSum+nums[i], targetSum))
                 return true;            
         }
