6e3ba1cf30262fd20274564e5fd1e80d687fb5e5
wisdompeak <guan.huifeng@gmail.com>
2022-02-10 10:32:50 -0800

    Create 416.Partition-Equal-Subset-Sum_dfs_v2.cpp

---
+++ Dynamic_Programming/416.Partition-Equal-Subset-Sum/416.Partition-Equal-Subset-Sum_dfs_v2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    unordered_set<int>visited;
+public:
+    bool canPartition(vector<int>& nums) 
+    {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum%2!=0) return false;
+        return dfs(nums, sum/2, 0, 0);        
+    }
+    
+    bool dfs(vector<int>& nums, int targetSum, int i, int sum)
+    {
+        int hash = sum * 20005 + i;
+        if (visited.find(hash)!=visited.end())
+            return false;
+        
+        if (sum== targetSum)
+            return true;
+        
+        if (sum> targetSum)
+            return false;
+        
+        if (i == nums.size())
+            return false;
+        
+        if (dfs(nums, targetSum, i + 1, sum+ nums[i]) || dfs(nums, targetSum, i + 1, sum))
+            return true;
+        
+        visited.insert(hash);
+        return false;        
+    }
+};
