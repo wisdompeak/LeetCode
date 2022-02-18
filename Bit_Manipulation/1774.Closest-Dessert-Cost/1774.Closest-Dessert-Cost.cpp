f0dfa1aa3636fdc0ff2ef80b3892f575ad515bf9
wisdompeak <guan.huifeng@gmail.com>
2022-02-17 23:07:41 -0800

    Update 1774.Closest-Dessert-Cost.cpp

--- Bit_Manipulation/1774.Closest-Dessert-Cost/1774.Closest-Dessert-Cost.cpp
+++ Bit_Manipulation/1774.Closest-Dessert-Cost/1774.Closest-Dessert-Cost.cpp
@@ -3,52 +3,29 @@ public:
     int closestCost(vector<int>& baseCosts, vector<int>& toppingCosts, int target) {
         int m = toppingCosts.size();
         
-        unordered_set<int> toppingsSet;        
-        for(int state = 0; state < pow(3,m); ++state) 
-        {
-            int s = state;
-            int sum = 0;
-            for(int i = 0; i < m; ++i) 
-            {
-                int cur = s%3;                
-                sum += toppingCosts[i] * cur;
-                s/=3;
-            }
-            toppingsSet.insert(sum);
-        }
-        vector<int>toppings(toppingsSet.begin(), toppingsSet.end());
-        sort(toppings.begin(), toppings.end());
-        
         int ret = INT_MAX;
         int ret_diff = INT_MAX;
-        for(int base: baseCosts) {
-            int targetTopping = target - base;
-
-            auto iter = lower_bound(toppings.begin(), toppings.end(), targetTopping);
-            if (iter!=toppings.end())
+        
+        for (int base: baseCosts)
+            for(int state = 0; state < pow(3,m); ++state) 
             {
-                int cand = *iter;
-                if (abs(base+cand-target) < ret_diff)
+                int s = state;
+                int topping = 0;
+                for(int i = 0; i < m; ++i) 
                 {
-                    ret = base+cand;
-                    ret_diff = abs(base+cand-target);
+                    int cur = s%3;                
+                    topping += toppingCosts[i] * cur;
+                    s/=3;
+                }
+                
+                if (abs(base+topping-target) < ret_diff)
+                {
+                    ret = base+topping;
+                    ret_diff = abs(base+topping-target);
                 }                    
-                else if (abs(base+cand-target) == ret_diff && base+cand < ret)
-                    ret = base+cand;                    
+                else if (abs(base+topping-target) == ret_diff && base+topping < ret)
+                    ret = base+topping;                    
             }
-            
-            if (iter!=toppings.begin())
-            {
-                int cand = *prev(iter);
-                if (abs(base+cand-target) < ret_diff)
-                {
-                    ret = base+cand;
-                    ret_diff = abs(base+cand-target);
-                }                    
-                else if (abs(base+cand-target) == ret_diff && base+cand < ret)
-                    ret = base+cand;                    
-            }            
-        }
         
         return ret;
     }
