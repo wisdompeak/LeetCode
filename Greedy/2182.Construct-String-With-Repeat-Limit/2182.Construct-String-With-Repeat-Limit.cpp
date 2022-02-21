cdc02cd093dce58eabf38e645b5d29b04039fe05
wisdompeak <guan.huifeng@gmail.com>
2022-02-21 10:43:05 -0800

    Create 2182.Construct-String-With-Repeat-Limit.cpp

---
+++ Greedy/2182.Construct-String-With-Repeat-Limit/2182.Construct-String-With-Repeat-Limit.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) 
+    {
+        unordered_map<char,int>Map;
+        for (auto ch: s)
+            Map[ch]++;
+        
+        priority_queue<pair<char,int>>pq;
+        
+        for (auto x:Map)
+            pq.push(x);
+        
+        string ret;
+        while (!pq.empty())
+        {
+            auto x = pq.top();
+            pq.pop();
+            int k = min(x.second, repeatLimit);
+            for (int i=0; i<k; i++)
+                ret.push_back(x.first);            
+            x.second -= k;
+            
+            if (x.second == 0) continue;
+            if (pq.empty()) return ret;
+            
+            auto y = pq.top();
+            pq.pop();
+            ret.push_back(y.first);
+            y.second -= 1;
+            
+            if (x.second!=0)
+                pq.push(x);
+            if (y.second!=0)
+                pq.push(y);
+        }
+        
+        return ret;        
+    }
+};
