7a2413a88720cbdfe1b4febd74b9a942b2d51901
wisdompeak <guan.huifeng@gmail.com>
2022-03-14 23:22:49 -0700

    Update 1810.Minimum-Path-Cost-in-a-Hidden-Grid.cpp

--- BFS/1810.Minimum-Path-Cost-in-a-Hidden-Grid/1810.Minimum-Path-Cost-in-a-Hidden-Grid.cpp
+++ BFS/1810.Minimum-Path-Cost-in-a-Hidden-Grid/1810.Minimum-Path-Cost-in-a-Hidden-Grid.cpp
@@ -24,20 +24,46 @@ public:
             for (int j=0; j<201; j++)
                 cost[i][j] = -1;
         
-        visited[100][100] = 1;
-        dfs(master, 100, 100);
+        int x0 = 100, y0 = 100;
+        dfs(master, x0, y0);
+        return dijkstra(x0, y0);
+    }
+    
+    void dfs(GridMaster &master, int x, int y)
+    {
+        if(visited[x][y]==1) return;
+        visited[x][y] = 1;
         
-        for (int i=0; i<201; i++)
-            for (int j=0; j<201; j++)
-                visited[i][j] = 0;
-                
+        if (master.isTarget())
+        {
+            targetX = x;
+            targetY = y;
+        }
+        
+        for (int k=0; k<4; k++)
+        {       
+            if (master.canMove(d[k]))
+            {                
+                int i = x+dir[k].first;
+                int j = y+dir[k].second;
+                cost[i][j] = master.move(d[k]);
+                dfs(master, i,j);
+                master.move(d[3-k]);
+            }            
+        }
+    }
+    
+    int dijkstra(int x0, int y0)
+    {
+        vector<vector<int>>visited(201, vector<int>(201));        
         priority_queue<AI3,vector<AI3>, greater<>>pq;
-        pq.push({0,100,100});
+        pq.push({0,x0,y0});
                 
         while (!pq.empty())
         {
             auto [c,x,y] = pq.top();
             pq.pop();
+            if (visited[x][y]==1) continue;
             visited[x][y] = 1;        
             if (x==targetX && y==targetY)
                 return c;
@@ -46,43 +72,15 @@ public:
             {
                 int i = x+dir[k].first;
                 int j = y+dir[k].second;
-                if (cost[i][j]==-1) continue;    
                 if (i<0 || i>=200 || j<0|| j>=200) continue;
+                if (cost[i][j]==-1) continue;                    
                 if (visited[i][j]==1) continue;
                 
                 pq.push({c+cost[i][j], i, j});
             }
         }
         
-        return -1;        
+        return -1;  
     }
     
-    void dfs(GridMaster &master, int x, int y)
-    {
-        
-        if (master.isTarget())
-        {
-            targetX = x;
-            targetY = y;
-        }
-        for (int k=0; k<4; k++)
-        {       
-            int i = x+dir[k].first;
-            int j = y+dir[k].second;
-            if(visited[i][j]==1) continue;
-            
-            if (master.canMove(d[k]))
-            {                
-                int c = master.move(d[k]);
-                visited[i][j] = 1;
-                cost[i][j] = c;
-                dfs(master, i,j);
-                master.move(d[3-k]);
-            }
-            else
-            {
-                visited[i][j] = 1;                
-            }
-        }
-    }
 };
