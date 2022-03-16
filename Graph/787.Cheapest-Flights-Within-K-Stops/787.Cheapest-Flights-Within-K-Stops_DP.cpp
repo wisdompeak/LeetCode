2c19eaa359893bef1c6dc945bd128ed8449ab4ba
wisdompeak <guan.huifeng@gmail.com>
2022-03-16 00:16:28 -0700

    Update 787.Cheapest-Flights-Within-K-Stops_DP.cpp

--- Graph/787.Cheapest-Flights-Within-K-Stops/787.Cheapest-Flights-Within-K-Stops_DP.cpp
+++ Graph/787.Cheapest-Flights-Within-K-Stops/787.Cheapest-Flights-Within-K-Stops_DP.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) 
     {
-        auto dp = vector<vector<int>>(K+2, vector<int>(n, INT_MAX/2));
-        dp[0][src] = 0;
-            
-        for (int k=1; k<=K+1; k++)
-        {                   
+        vector<vector<int>>dp(n, vector<int>(K+2, INT_MAX/2));
+        dp[src][0] = 0;
+        int ret = INT_MAX/2;
+
+        for (int j=1; j<=K+1; j++)
             for (auto flight: flights)
             {
-                int a = flight[0];
-                int b = flight[1];
-                int w = flight[2];                
-                dp[k][b] = min(dp[k][b], dp[k-1][b]);
-                dp[k][b] = min(dp[k][b], dp[k-1][a]+w);
+                int a = flight[0], b = flight[1], p = flight[2];
+                dp[b][j] = min(dp[b][j], dp[a][j-1]+p);
+                if (b==dst) ret = min(ret, dp[b][j]);
             }
-        }
-        
-        int ret = dp[K+1][dst];
-        
-        return (ret>=INT_MAX/2) ? -1 : ret;
+
+        return ret >= INT_MAX/2 ? -1 : ret;
     }
 };
