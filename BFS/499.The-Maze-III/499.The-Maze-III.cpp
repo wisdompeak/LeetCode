cd1de1d46d53d0f773f1ead4333aa03b8da591af
wisdompeak <guan.huifeng@gmail.com>
2022-03-24 23:33:30 -0700

    Update 499.The-Maze-III.cpp

--- BFS/499.The-Maze-III/499.The-Maze-III.cpp
+++ BFS/499.The-Maze-III/499.The-Maze-III.cpp
@@ -1,63 +1,43 @@
-using TP = tuple<int,string,int,int>;
+using TP = tuple<int, string, int, int>;
 class Solution {
-private:
+    vector<pair<int,int>>dir = {{1,0},{0,-1},{0,1},{-1,0}};  // d,l,r,u
     int m,n;
-    vector<pair<int,int>> dir={{1,0},{0,-1},{0,1},{-1,0}};
-    
 public:
-    int Next(int i, int j, vector<vector<int>>& maze, int k, vector<int>& hole)
-    {        
-        int steps=0;
-        while (i+dir[k].first>=0 && i+dir[k].first<m && j+dir[k].second>=0 && j+dir[k].second<n && maze[i+dir[k].first][j+dir[k].second]!=1)
-        {
-            i += dir[k].first;
-            j += dir[k].second;
-            steps++;            
-            if (i==hole[0] && j==hole[1])
-                break;
-        }
-        return steps;
-    }
-    
     string findShortestWay(vector<vector<int>>& maze, vector<int>& ball, vector<int>& hole) 
     {
-        m = maze.size();
-        n = maze[0].size();
+        m = maze.size(), n = maze[0].size();
         
-        vector<vector<int>>dist(m, vector<int>(n, INT_MAX));
-        string ret;
-        
-        priority_queue<TP, vector<TP>, greater<>> pq;
+        priority_queue<TP, vector<TP>, greater<>>pq;  // {dist, string, node_x, node_y}
         pq.push({0, "", ball[0], ball[1]});
+        
+        vector<vector<int>>dist(m, vector<int>(n,INT_MAX)); 
+        string ret;
         
         while (!pq.empty())
         {
             auto [d, s, x, y] = pq.top();
             pq.pop();
             
-            if (d > dist[x][y]) continue;            
-            dist[x][y] = d;            
-                        
-            if (x==hole[0] && y==hole[1]) 
+            if (d > dist[x][y]) continue;
+            else dist[x][y] = d;
+            
+            if (x==hole[0] && y==hole[1])
             {
                 ret = s;
                 break;
             }
             
             for (int k=0; k<4; k++)
-            {                
-                int step = Next(x,y,maze,k,hole);
-                int i = x+dir[k].first*step;
-                int j = y+dir[k].second*step;
-
-                char ch='0'+k;
-                if (d+step >= dist[i][j]) continue;                
+            {
+                int step = Next(x,y,k,maze, hole);
+                int i = x + dir[k].first * step;
+                int j = y + dir[k].second * step;
+                
+                char ch = '0'+k;
+                if (d+step >= dist[i][j]) continue;
                 pq.push({d+step, s+ch, i, j});
-            }
+            }            
         }
-        
-        if (dist[hole[0]][hole[1]]==INT_MAX)
-            return "impossible";
         
         for (int i=0; i<ret.size(); i++)
         {
@@ -67,7 +47,21 @@ public:
             else if (ret[i]=='3') ret[i] = 'u';
         }
         
-        return ret;
-        
+        if (ret=="") return "impossible";
+        else return ret;        
+    }
+    
+    int Next(int x, int y, int k, vector<vector<int>>& maze, vector<int>& hole)
+    {
+        int step = 0;
+        while (x+dir[k].first >= 0 && x+dir[k].first < m && y+dir[k].second >= 0 && y+dir[k].second < n && maze[x+dir[k].first][y+dir[k].second]!=1)
+        {            
+            step++;
+            x+=dir[k].first;
+            y+=dir[k].second;
+            if (x==hole[0] && y==hole[1])
+                break;
+        }
+        return step;
     }
 };
