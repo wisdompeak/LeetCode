90a27c165c0f7f6cb8ec74f0591811e423416304
wisdompeak <guan.huifeng@gmail.com>
2022-03-27 22:43:59 -0700

    Update 2217.Find-Palindrome-With-Fixed-Length.cpp

--- Math/2217.Find-Palindrome-With-Fixed-Length/2217.Find-Palindrome-With-Fixed-Length.cpp
+++ Math/2217.Find-Palindrome-With-Fixed-Length/2217.Find-Palindrome-With-Fixed-Length.cpp
@@ -6,18 +6,14 @@ public:
         vector<LL>rets;
         for (int k: queries)
         {
-            if (intLength==1)
+            if (intLength == 1)
             {
-                if (k>9)
-                    rets.push_back(-1);
-                else
-                    rets.push_back(k);                
-                continue;
+                if (k>9) rets.push_back(-1);
+                else rets.push_back(k);
             }
-            
-            int m = intLength/2;            
-            if (intLength%2==0)
+            else if (intLength%2==0)
             {
+                int m = intLength/2;
                 LL a = getKth(m, k);
                 if (a==-1)
                 {
@@ -25,39 +21,41 @@ public:
                     continue;
                 }
                 LL b = flip(a);
-                rets.push_back(a*pow(10, m) + b);
+                rets.push_back(a*pow(10, m) + b);                
             }
             else
             {
+                int m = intLength/2; 
                 LL a = getKth(m+1, k);
                 if (a==-1)
                 {
                     rets.push_back(-1);
                     continue;
                 }
-                LL c = a%10;
+                LL c = a % 10;
                 a = a/10;
                 LL b = flip(a);
-                rets.push_back(a*pow(10, m+1) + c*pow(10, m) + b);
+                rets.push_back(a*pow(10, m+1) + c*pow(10,m) + b);   
             }
-        }
+        }    
         return rets;
     }
-
+    
     LL getKth(int m, int k)
     {
         if (k > 9*pow(10, m-1)) return -1;
         else return pow(10, m-1) + k-1;
     }
     
-    LL flip(LL x)
+    LL flip(LL a)
     {
         LL ret = 0;
-        while (x>0)
+        while (a>0)
         {
-            ret = ret*10+(x%10);
-            x/=10;
+            ret = ret*10+(a%10);
+            a/=10;
         }
         return ret;
     }
+    
 };
