0a15d6aace512c1928b9123bcac2037e06ae36e7
wisdompeak <guan.huifeng@gmail.com>
2022-05-01 18:31:50 -0700

    Update 828.Count-Unique-Characters-of-All-Substrings-of-a-Given-String.cpp

--- Hash/828.Count-Unique-Characters-of-All-Substrings-of-a-Given-String/828.Count-Unique-Characters-of-All-Substrings-of-a-Given-String.cpp
+++ Hash/828.Count-Unique-Characters-of-All-Substrings-of-a-Given-String/828.Count-Unique-Characters-of-All-Substrings-of-a-Given-String.cpp
@@ -1,26 +1,27 @@
 class Solution {
-    long long M = pow(10,9)+7;
 public:
-    int uniqueLetterString(string S) 
-    {        
-        vector<vector<int>>pos(26);
-        int result=0;
-        for (int i=0; i<S.size(); i++)
+    int uniqueLetterString(string s) 
+    {
+        int n = s.size();
+        vector<vector<int>>pos(26); // pos[k]: the pos of letter k so far (by i-th)
+        
+        int ret = 0;
+        for (int i=0; i<n; i++)
         {
-            pos[S[i]-'A'].push_back(i);
-            for (int i=0; i<26; i++)
+            pos[s[i]-'A'].push_back(i);
+            for (int k=0; k<26; k++)
             {
-                if (pos[i].size()==1)
-                    result+=pos[i][0]+1;
-                else if (pos[i].size()>1)
+                if (pos[k].size()>=2)
                 {
-                    int k = pos[i].size();
-                    result+=pos[i][k-1]-pos[i][k-2];
+                    int m = pos[k].size();
+                    ret += pos[k][m-1] - pos[k][m-2];
                 }
-                result = result%M;    
+                else if (pos[k].size()==1)
+                {
+                    ret += pos[k][0]+1;
+                }
             }
         }
-        return result;
+        return ret;        
     }
-    
 };
