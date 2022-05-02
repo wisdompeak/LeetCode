d98f5fcb5a162230178df2f323d19f2ee47255a7
wisdompeak <guan.huifeng@gmail.com>
2022-05-01 17:54:57 -0700

    Update 2262.Total-Appeal-of-A-String.cpp

--- Hash/2262.Total-Appeal-of-A-String/2262.Total-Appeal-of-A-String.cpp
+++ Hash/2262.Total-Appeal-of-A-String/2262.Total-Appeal-of-A-String.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     long long appealSum(string s) 
     {
-        vector<vector<int>>pos(26);
+        vector<int>pos(26, -1);
         long long result=0;
         for (int i=0; i<s.size(); i++)
         {
-            pos[s[i]-'a'].push_back(i);
+            pos[s[i]-'a'] = i;
             for (int i=0; i<26; i++)
             {
-                if (pos[i].size()>=1)
-                {
-                    int k = pos[i].size();
-                    result+=(long long)pos[i][k-1]+1;
-                }                
+                if (pos[i]!=-1)                                    
+                    result+=(long long)pos[i]+1;                  
             }
         }
-        return result;
-        
+        return result;        
     }
 };
