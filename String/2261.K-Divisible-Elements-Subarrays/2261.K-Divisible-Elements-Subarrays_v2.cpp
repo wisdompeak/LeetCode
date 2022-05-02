7f12c78a1ffeb54fe51b27bd46319b1619a0fd9c
wisdompeak <guan.huifeng@gmail.com>
2022-05-01 17:43:47 -0700

    Update 2261.K-Divisible-Elements-Subarrays_v2.cpp

--- String/2261.K-Divisible-Elements-Subarrays/2261.K-Divisible-Elements-Subarrays_v2.cpp
+++ String/2261.K-Divisible-Elements-Subarrays/2261.K-Divisible-Elements-Subarrays_v2.cpp
@@ -1,42 +1,43 @@
 using ULL = unsigned long long;
+
 class Solution {
-    ULL power[215];
-    ULL P = 211;
+    ULL power[211];
+    ULL B = 201;
 public:
     int countDistinct(vector<int>& nums, int k, int p) 
     {
+        int n = nums.size();
         power[0] = 1;
-        for (int i=1; i<=211; i++)
-            power[i] = power[i-1]*P;
+        for (int i=1; i<B; i++)   // power[i] = B^i;
+            power[i] = power[i-1] * B;
         
         int ret = 0;
-        
-        for (int len=1; len<=nums.size(); len++)
+        for (int len=1; len<=n; len++)
         {
             unordered_set<ULL>Set;
             ULL hash = 0;
             int count = 0;
-            for (int i=0; i<nums.size(); i++)
+            
+            for (int i=0; i<n; i++)
             {
                 if (i>=len)
                 {
-                    hash = hash - nums[i-len]*power[len-1];
-                    if (nums[i-len]%p==0) count--;
+                    hash = (hash - nums[i-len] * power[len-1] + M ) %M;
+                    count -= (nums[i-len]%p==0);
                 }
                 
-                hash = hash*P + nums[i];
-                if (nums[i]%p==0) count++;
+                hash = hash * B + nums[i];
+                count += (nums[i]%p==0);
                 
                 if (i>=len-1)
                 {
                     if (Set.find(hash)!=Set.end()) continue;
                     Set.insert(hash);
-                    if (count<=k)
-                        ret += 1;
-                }
+                    if (count <= k)
+                        ret+=1;                    
+                }                
             }
         }
-        
         return ret;        
     }
 };
