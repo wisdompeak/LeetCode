c53516b6103a66af9306b6c6cebab1f9ef8b9981
wisdompeak <guan.huifeng@gmail.com>
2022-05-08 22:28:10 -0700

    Create 2267.Check-if-There-Is-a-Valid-Parentheses-String-Path.cpp

---
+++ Dynamic_Programming/2267.Check-if-There-Is-a-Valid-Parentheses-String-Path/2267.Check-if-There-Is-a-Valid-Parentheses-String-Path.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool hasValidPath(vector<vector<char>>& grid) 
+    {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<set<int>>>dp(m, vector<set<int>>(n));
+        
+        if (grid[0][0]=='(')
+            dp[0][0] = {1};
+        else
+            dp[0][0] = {};
+        
+        for (int i=0; i<m; i++)
+            for (int j=0; j<n; j++)
+            {
+                int k;
+                if (grid[i][j]=='(')
+                    k = 1;
+                else
+                    k = -1;
+                
+                if (i>=1) 
+                {
+                    for (int x: dp[i-1][j])
+                        if (x+k>=0 && (m+n-1)-(i+j+1)>=(x+k))
+                            dp[i][j].insert(x+k);
+                }
+                if (j>=1)
+                    for (int x: dp[i][j-1])
+                    {
+                        if (x+k>=0 && (m+n-1)-(i+j+1)>=(x+k))
+                            dp[i][j].insert(x+k);
+                    }
+            }
+        
+        return dp[m-1][n-1].find(0)!=dp[m-1][n-1].end();        
+    }
+};
