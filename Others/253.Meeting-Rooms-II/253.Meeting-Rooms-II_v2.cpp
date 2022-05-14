4f2379f0b6c963f29abf6c1f2f5ad8ef8417334c
wisdompeak <guan.huifeng@gmail.com>
2022-05-14 01:32:19 -0700

    Update 253.Meeting-Rooms-II_v2.cpp

--- Others/253.Meeting-Rooms-II/253.Meeting-Rooms-II_v2.cpp
+++ Others/253.Meeting-Rooms-II/253.Meeting-Rooms-II_v2.cpp
@@ -1,34 +1,20 @@
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
 class Solution {
-    static bool cmp1(Interval a, Interval b)
-    {
-        return a.start<b.start;
-    }
 public:
-    int minMeetingRooms(vector<Interval>& intervals) 
-    {
-        sort(intervals.begin(),intervals.end(),cmp1);
-        multiset<int>Set;
-        int count=0;
-        int i=0;
-        while (i<intervals.size())
-        {            
-            while (Set.size()==0 || i<intervals.size() && *Set.begin()>intervals[i].start)    
-            {
-                Set.insert(intervals[i].end);
-                count = max(count,int(Set.size()));
-                i++;                                
-            }                                    
-            Set.erase(Set.begin());                                     
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        map<int,int>Map;
+        for (auto interval:intervals)
+        {
+            Map[interval[0]]+=1;
+            Map[interval[1]]-=1;
         }
-        return count;
+        
+        int sum = 0;
+        int ret = 0;
+        for (auto& [t,diff]: Map)
+        {
+            sum += diff;
+            ret = max(sum, ret);
+        }
+        return ret;
     }
 };
