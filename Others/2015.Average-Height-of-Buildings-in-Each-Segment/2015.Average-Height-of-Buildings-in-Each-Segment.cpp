cfdfb667a34e7aeaa9c121e92242ba43637c1cbb
wisdompeak <guan.huifeng@gmail.com>
2022-05-19 01:01:50 -0700

    Update 2015.Average-Height-of-Buildings-in-Each-Segment.cpp

--- Others/2015.Average-Height-of-Buildings-in-Each-Segment/2015.Average-Height-of-Buildings-in-Each-Segment.cpp
+++ Others/2015.Average-Height-of-Buildings-in-Each-Segment/2015.Average-Height-of-Buildings-in-Each-Segment.cpp
@@ -2,39 +2,38 @@ class Solution {
 public:
     vector<vector<int>> averageHeightOfBuildings(vector<vector<int>>& buildings) 
     {
-        map<int,pair<int,int>>Map; // pos=>{sum, count}
+        map<int, pair<int,int>>Map; // pos -> {diffHeight, diffCount}
         for (auto build: buildings)
         {
-            int start = build[0], end = build[1], height = build[2];            
-            Map[start].first += height;
-            Map[start].second += 1;
-            Map[end].first -= height;
-            Map[end].second -= 1;
-        }                        
+            int s = build[0], e = build[1], h = build[2];
+            Map[s].first += h;
+            Map[s].second += 1;
+            Map[e].first -= h;
+            Map[e].second -= 1;
+        }
         
-        int sum = 0;
-        int count = 0;
-        vector<pair<int,int>>temp;        
+        vector<pair<int,int>>seg;
+        int totalHeight = 0, totalCount = 0;
         for (auto& [pos, kv]: Map)
         {
-            int heightDiff = kv.first, countDiff = kv.second;
-            sum += heightDiff;
-            count += countDiff;
-            int avg = (count==0 ? 0 : sum / count);
-            temp.push_back({pos, avg});            
-        }        
+            int diffHeight = kv.first, diffCount = kv.second;
+            totalHeight += diffHeight;
+            totalCount += diffCount;
+            int avg = (totalCount ==0 ? 0: totalHeight / totalCount);
+            seg.push_back({pos, avg});
+        }
         
         vector<vector<int>>rets;
-        for (int i=0; i<temp.size(); i++)
+        for (int i=0; i<seg.size(); i++)
         {
-            if (temp[i].second==0) continue;
+            if (seg[i].second == 0) continue;
             int j = i;
-            while (j<temp.size() && temp[j].second == temp[i].second)
+            while (j<seg.size() && seg[j].second == seg[i].second)
                 j++;
-            rets.push_back({temp[i].first, temp[j].first, temp[i].second});
-            i = j-1;            
+            rets.push_back({seg[i].first, seg[j].first, seg[i].second});
+            i = j-1;
         }
-        return rets;
         
+        return rets;
     }
 };
