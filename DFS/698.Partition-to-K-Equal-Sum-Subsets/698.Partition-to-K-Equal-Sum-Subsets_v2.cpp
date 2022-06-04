9c45c6411034429d2c289ebdd66271b22c361795
wisdompeak <guan.huifeng@gmail.com>
2022-06-03 23:33:31 -0700

    Create 698.Partition-to-K-Equal-Sum-Subsets_v2.cpp

---
+++ DFS/698.Partition-to-K-Equal-Sum-Subsets/698.Partition-to-K-Equal-Sum-Subsets_v2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) 
+    {
+        int n = nums.size();
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum%k!=0) return false;
+        int target = sum / k;
+        
+        vector<int>dp(1<<n, -1);
+        dp[0] = 0;
+        for (int state=0; state<(1<<n); state++)
+        {
+            if (dp[state]==-1) continue;
+            for (int i=0; i<n; i++)
+            {                                
+                if (!((state>>i)&1) && (dp[state]+nums[i] <= target))
+                    dp[state + (1<<i)] = (dp[state]+nums[i]) % target;
+            }
+        }
+        
+        return dp[(1<<n)-1]==0;
+    }
+};
