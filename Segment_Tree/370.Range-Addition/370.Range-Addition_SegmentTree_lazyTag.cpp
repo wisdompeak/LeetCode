f0c7a416d42851a89d77c3a91883e001d06ea988
wisdompeak <guan.huifeng@gmail.com>
2022-06-05 22:33:28 -0700

    Update 370.Range-Addition_SegmentTree_lazyTag.cpp

--- Segment_Tree/370.Range-Addition/370.Range-Addition_SegmentTree_lazyTag.cpp
+++ Segment_Tree/370.Range-Addition/370.Range-Addition_SegmentTree_lazyTag.cpp
@@ -1,98 +1,104 @@
-class Solution {
-    class SegTreeNode
-    {
-        public:
-        SegTreeNode* left;
-        SegTreeNode* right;
-        int start, end;
-        int info;
-        int tag;
-        SegTreeNode(int a, int b):start(a),end(b),info(0),tag(0),left(NULL),right(NULL){}
-    };
-    
-    void init(SegTreeNode* node, int a, int b)  // init for range [a,b]
-    {                        
+using LL = long long;
+class SegTreeNode
+{
+    public:
+    SegTreeNode* left = NULL;
+    SegTreeNode* right = NULL;
+    int start, end;    
+    LL info;  // the sum value over the range
+    LL delta;
+    bool tag; 
+        
+    SegTreeNode(int a, int b, int val)  // init for range [a,b] with val
+    {                 
+        tag = 0;
+        delta = 0;
+        start = a, end = b;
         if (a==b)
         {
-            node->info = 0;
+            info = val;
             return;
-        }
+        }        
         int mid = (a+b)/2;
-        if (node->left==NULL)
+        if (left==NULL)
         {
-            node->left = new SegTreeNode(a, mid);
-            node->right = new SegTreeNode(mid+1, b);
+            left = new SegTreeNode(a, mid, val);
+            right = new SegTreeNode(mid+1, b, val);            
+            info = left->info + right->info;  // check with your own logic
+        }        
+    }    
+    
+    void pushDown()
+    {
+        if (tag==1 && left)
+        {
+            left->delta += delta;
+            right->delta += delta;
+            left->tag = 1;
+            right->tag = 1;
+            tag = 0;
+            delta = 0;
+        }        
+    } 
+    
+    void updateRangeBy(int a, int b, int val)     // set range [a,b] with val
+    {        
+        if (b < start || a > end ) // not covered by [a,b] at all
+            return;        
+        if (a <= start && end <=b)  // completely covered within [a,b]
+        {
+            delta += val;
+            tag = 1;
+            return;
         }
-        init(node->left, a, mid);
-        init(node->right, mid+1, b);
-                
-        node->info = 0;  // write your own logic
+
+        if (left)
+        {
+            pushDown();        
+            left->updateRangeBy(a, b, val);
+            right->updateRangeBy(a, b, val);
+            info = left->info + right->info;  // write your own logic            
+        }        
     }
     
-    void updateRange(SegTreeNode* node, int a, int b, int val)
+    LL queryRange(int a, int b)     // query the maximum value within range [a,b]
     {
-        if (b < node->start || a > node->end )
-            return;
-        if (a <= node->start && b>=node->end)
+        if (b < start || a > end )
         {
-            node->info += val * (node->end-node->start+1);
-            node->tag += val;
-            return;
+            return 0;  // check with your own logic
+        }
+        if (a <= start && end <=b)
+        {
+            return info + delta*(end-start+1);  // check with your own logic
+        }          
+        
+        if (left)
+        {
+            pushDown();     
+            LL ret = left->queryRange(a, b) + right->queryRange(a, b);        
+            info = left->info + right->info;    // check with your own logic
+            return ret;
         }
         
-        pushdown(node); // erase lazy tag and propagate down
-        updateRange(node->left, a, b, val);
-        updateRange(node->right, a, b, val);
-    }
-    
-    void pushdown(SegTreeNode* node)
-    {
-        if (node->tag != 0)
-        {
-            node->left->tag += node->tag;
-            node->right->tag += node->tag;
-            node->left->info += node->tag * (node->left->end-node->left->start+1);
-            node->right->info += node->tag * (node->right->end-node->right->start+1);
-            node->tag = 0;            
-        }
-    }
-    
-    int querySingle(SegTreeNode* node, int id)
-    {
-        if (id < node->start || id > node->end )
-        {
-            return INT_MAX;  // write your own logic
-        }
-        if (node->start==id && node->end==id)
-        {
-            return node->info;
-        }
-        
-        pushdown(node);
-        int a = querySingle(node->left, id);
-        int b = querySingle(node->right, id);
-        if (a!=INT_MAX) return a;
-        else if (b!=INT_MAX) return b;
-        else return INT_MAX;
-    }
-    
-    
-    
+        return info;   // should not reach here
+    }  
+};
+
+class Solution {       
 public:
     vector<int> getModifiedArray(int length, vector<vector<int>>& updates) 
     {
-        SegTreeNode* root = new SegTreeNode(0, length-1);
-        init(root, 0, length-1);
+        SegTreeNode* root = new SegTreeNode(0, length-1, 0);
         
         for (auto update: updates)
         {
-            updateRange(root, update[0], update[1], update[2]);
+            root->updateRangeBy(update[0], update[1], update[2]);
         }
         
         vector<int>rets(length);
         for (int i=0; i<length; i++)
         {
-            rets[i] = querySingle(root, i);
+            rets[i] = root->queryRange(i, i);
         }
             
         return rets;
