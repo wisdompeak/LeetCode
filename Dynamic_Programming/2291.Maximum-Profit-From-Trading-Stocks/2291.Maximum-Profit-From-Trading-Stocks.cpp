16691e147565417803d958b25433e03a5d6ba7eb
wisdompeak <guan.huifeng@gmail.com>
2022-06-10 17:57:36 -0700

    Create 2291.Maximum-Profit-From-Trading-Stocks.cpp

---
+++ Dynamic_Programming/2291.Maximum-Profit-From-Trading-Stocks/2291.Maximum-Profit-From-Trading-Stocks.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumProfit(vector<int>& present, vector<int>& future, int budget) 
+    {
+        int n = present.size();
+        
+        vector<int>dp(1001);
+        for (int i=0; i<n; i++)
+            for (int j=budget; j>=present[i]; j--)
+            {
+                dp[j] = max(dp[j], dp[j-present[i]]+future[i]-present[i]);
+            }
+        
+        return dp[budget];
+    }
+};
