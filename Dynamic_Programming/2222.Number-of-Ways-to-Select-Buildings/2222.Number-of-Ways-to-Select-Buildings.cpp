94b682662591a9d603a83e9a2775752a4f1ae030
wisdompeak <guan.huifeng@gmail.com>
2022-06-19 15:51:40 -0700

    Create 2222.Number-of-Ways-to-Select-Buildings.cpp

---
+++ Dynamic_Programming/2222.Number-of-Ways-to-Select-Buildings/2222.Number-of-Ways-to-Select-Buildings.cpp
@@ -0,0 +1,27 @@
+using LL = long long;
+class Solution {
+    LL dp[100005][4][2];    
+public:
+    long long numberOfWays(string s) 
+    {
+        int n = s.size();
+        s = "#"+s;
+        
+        dp[0][0][0] = 1;
+        dp[0][0][1] = 1;
+        
+        LL  ret  = 0;
+        
+        for (int i=1; i<=n; i++)
+            for (int j=0; j<=3; j++)
+                for (int k=0; k<2; k++)
+                {
+                    dp[i][j][k] = dp[i-1][j][k];
+
+                    if (j>=1 && k==(s[i]-'0'))
+                        dp[i][j][k] += dp[i-1][j-1][1-k];                    
+                }
+        
+        return dp[n][3][0] + dp[n][3][1];        
+    }
+};
