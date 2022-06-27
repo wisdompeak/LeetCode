89cbb349f3a162ec68e2c2a59bd21fca824f3af1
wisdompeak <guan.huifeng@gmail.com>
2022-06-26 23:30:58 -0700

    Update and rename 2320.Count-Number-of-Ways-to-Place-Houses.cpp to 2320.Count-Number-of-Ways-to-Place-Houses_v1.cpp

--- Dynamic_Programming/2320.Count-Number-of-Ways-to-Place-Houses/2320.Count-Number-of-Ways-to-Place-Houses_v1.cpp
+++ Dynamic_Programming/2320.Count-Number-of-Ways-to-Place-Houses/2320.Count-Number-of-Ways-to-Place-Houses_v1.cpp
@@ -1,14 +1,16 @@
 using LL = long long;
 LL M = 1e9+7;
 class Solution {
-    LL dp[10001][2];
+    LL dp[10001][2]; 
+    // dp[i][0]: the # of plans so that there is no building at the i-th plot
+    // dp[i][1]: the # of plans so that there is a building at the i-th plot
 public:
     int countHousePlacements(int n) 
     {                
-        dp[1][0] = 1;
-        dp[1][1] = 1;
+        dp[0][0] = 1;
+        dp[0][1] = 0;
         
-        for (int i=2; i<=n; i++)
+        for (int i=1; i<=n; i++)
         {
             dp[i][0] = (dp[i-1][0] + dp[i-1][1])%M;
             dp[i][1] = dp[i-1][0];            
