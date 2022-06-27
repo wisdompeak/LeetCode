289068fdcae1c3fd3916fe16894af23cdca678b9
wisdompeak <guan.huifeng@gmail.com>
2022-06-26 22:12:21 -0700

    Update 2322.Minimum-Score-After-Removals-on-a-Tree.cpp

--- Tree/2322.Minimum-Score-After-Removals-on-a-Tree/2322.Minimum-Score-After-Removals-on-a-Tree.cpp
+++ Tree/2322.Minimum-Score-After-Removals-on-a-Tree/2322.Minimum-Score-After-Removals-on-a-Tree.cpp
@@ -1,14 +1,13 @@
-class Solution {
-    unordered_set<int> next[1000];
+class Solution {    
+    unordered_set<int> next[1005];  // next[i]:  a set of adjacent nodes next to i
+    int visited[1005];
     vector<int>nums;
-    vector<int>visited;
     int n;
 public:
     int minimumScore(vector<int>& nums, vector<vector<int>>& edges) 
     {
-        this->nums = nums;
         this->n = nums.size();
-        visited.resize(n);
+        this->nums = nums;
         for (auto edge: edges)
         {
             int a = edge[0], b = edge[1];
@@ -17,70 +16,72 @@ public:
         }
         
         int ret = INT_MAX;
-        for (int i=0; i<edges.size(); i++)
+        for (auto edge: edges)
         {
-            int a = edges[i][0], b = edges[i][1];
+            int a = edge[0], b = edge[1];
             next[a].erase(b);
             next[b].erase(a);
-
+            
             int ret1 = solve(a, b);
             int ret2 = solve(b, a);
-
+            
+            ret = min(ret, min(ret1, ret2));
+            
             next[a].insert(b);
             next[b].insert(a);
-
-            ret = min(ret, min(ret1, ret2));                        
-        }            
-        return ret;        
+        }
+        
+        return ret;
     }
     
     int solve(int a, int b)
     {
-        fill(visited.begin(), visited.end(), 0);
+        fill(visited, visited+n, 0);
         visited[a] = 1;
-        int x = getAll(a);
+        int A = getAll(a);
         
-        fill(visited.begin(), visited.end(), 0);
+        fill(visited, visited+n, 0);
         visited[b] = 1;
-        int y = getAll(b);
-                
-        int ret = INT_MAX;        
-        fill(visited.begin(), visited.end(), 0);
-        visited[a] = 1;
-        dfs(a, ret, x, y);
+        int B = getAll(b);
+        
+        int ret = INT_MAX;
+        fill(visited, visited+n, 0);
+        visited[a] = 1;        
+        dfs(a, ret, A, B);        
         return ret;
     }
     
-    int dfs(int cur, int& ret, int x, int y)
-    {  
-        int total = 0;
-        for (int nxt: next[cur]) 
-        {
-           if (visited[nxt]==1) continue;
-           visited[nxt] = 1;
-            
-           int branch = dfs(nxt, ret, x, y);
-           int other = x^branch;
-           int mx = max(branch, max(other, y));
-           int mn = min(branch, min(other, y));
-           ret = min(ret, mx-mn);
-            
-           total ^= branch;
-        }
-        total^=nums[cur];
-        return total;
-    }
-    
-    int getAll(int cur)
+    int dfs(int node, int& ret, int A, int B)
     {
-        int total = 0;        
-        for (int nxt: next[cur])
+        int total = 0;
+        for (int nxt: next[node])
         {
             if (visited[nxt]==1) continue;
             visited[nxt] = 1;
-            total ^= getAll(nxt);            
-        }            
-        total ^= nums[cur];
+            
+            int C = dfs(nxt, ret, A, B);
+            int other = A^C;
+            int mx = max(other, max(C, B));
+            int mn = min(other, min(C, B));
+            ret = min(ret, mx-mn);
+            
+            total^=C;
+        }
+        total ^= nums[node];
         return total;
-    }    
+    }
+    
+    int getAll(int node)
+    {
+        int total = 0;
+        for (int nxt: next[node])
+        {
+            if (visited[nxt]==1) continue;
+            visited[nxt] = 1;
+            total ^= getAll(nxt);
+        }
+        total ^= nums[node];
+        return total;
+    }       
+    
 };
