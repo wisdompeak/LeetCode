c30a5cc13e1770b2521812589d3b26b0b7b9bcff
wisdompeak <guan.huifeng@gmail.com>
2022-06-26 17:46:39 -0700

    Update 2141.Maximum-Running-Time-of-N-Computers.cpp

--- Binary_Search/2141.Maximum-Running-Time-of-N-Computers/2141.Maximum-Running-Time-of-N-Computers.cpp
+++ Binary_Search/2141.Maximum-Running-Time-of-N-Computers/2141.Maximum-Running-Time-of-N-Computers.cpp
@@ -1,37 +1,30 @@
 using LL = long long;
-class Solution {    
+class Solution {
 public:
     long long maxRunTime(int n, vector<int>& batteries) 
     {
-        LL left = 0, right = LLONG_MAX/2;
-        // sort(batteries.rbegin(), batteries.rend());
-        
+        LL left = 0, right = LLONG_MAX/n;
         while (left < right)
         {
             LL mid = right-(right-left)/2;
-            if (checkOK(mid, batteries, n))            
-                left = mid;                            
+            if (checkOK(mid, n, batteries))
+                left = mid;
             else
-                right = mid-1;                
+                right = mid-1;
         }
-        return left;                
+        return left;        
     }
     
-    bool checkOK(LL T, vector<int>&nums, int n)
+    bool checkOK(LL T, LL n,  vector<int>& batteries)
     {
-        int count = 0;
-        LL cur = 0;        
-        for (int i=0; i<nums.size(); i++)
+        LL sum = 0;
+        for (auto x: batteries)
         {
-            cur+=min((LL)nums[i], T);
-            while (cur >= T)
-            {
-                count++;
-                cur-=T;
-            }
-            if (count >= n)
+            sum += min((LL)x, T);
+            if (sum >= T*n)
                 return true;
         }
         return false;
     }
 };
+
