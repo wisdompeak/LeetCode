0c1c9444ab03e0b77fa675ce878c214007655892
wisdompeak <guan.huifeng@gmail.com>
2022-06-28 00:19:36 -0700

    Create 2318.Number-of-Distinct-Roll-Sequences.cpp

---
+++ Dynamic_Programming/2318.Number-of-Distinct-Roll-Sequences/2318.Number-of-Distinct-Roll-Sequences.cpp
@@ -0,0 +1,46 @@
+using LL = long long;
+LL M = 1e9+7;
+class Solution {
+    LL dp[10001][7][7];
+public:
+    int distinctSequences(int n) 
+    {
+        if (n==1) return 6;
+
+        LL count = 0;        
+        for (int a=1; a<=6; a++)
+            for (int b=1; b<=6; b++)
+            {
+                if (a!=b && gcd(a,b)==1)
+                {
+                    dp[2][a][b] = 1;                
+                    count++;
+                }                    
+            }
+        if (n==2) return count;
+        
+        LL ret = 0;
+        for (int i=3; i<=n; i++)
+            for (int a=1; a<=6; a++)
+                for (int b=1; b<=6; b++)
+                {
+                    if (a==b) continue;
+                    if (gcd(a,b)>1) continue;
+
+                    for (int x=1; x<=6; x++)
+                    {
+                        if (x==a) continue;
+                        if (x==b) continue;
+                        if (gcd(x,a)>1) continue;
+
+                        dp[i][a][b] += dp[i-1][x][a];
+                        dp[i][a][b] %= M;
+                    }                   
+
+                    if (i==n)
+                        ret = (ret + dp[i][a][b]) %M;
+                }
+
+        return ret;
+    }
+};
