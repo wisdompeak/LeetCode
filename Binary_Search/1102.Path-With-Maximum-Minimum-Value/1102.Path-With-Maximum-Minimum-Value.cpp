d05b324e54b151ff40f9da1dc6fbc075088377e6
wisdompeak <guan.huifeng@gmail.com>
2022-07-02 12:08:17 -0700

    Update 1102.Path-With-Maximum-Minimum-Value.cpp

--- Binary_Search/1102.Path-With-Maximum-Minimum-Value/1102.Path-With-Maximum-Minimum-Value.cpp
+++ Binary_Search/1102.Path-With-Maximum-Minimum-Value/1102.Path-With-Maximum-Minimum-Value.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    int maximumMinimumPath(vector<vector<int>>& A) 
+    int maximumMinimumPath(vector<vector<int>>& grid) 
     {
-        int left = INT_MAX;
-        int right = INT_MIN;
-        int M = A.size();
-        int N = A[0].size();
-        for (int i=0; i<M; i++)
-            for (int j=0; j<N; j++)
-            {
-                left = min(left,A[i][j]);
-                right = max(right,A[i][j]);
-            }
-        
+        int left = 0, right = 1e9;        
         while (left<right)
         {
             int mid = right-(right-left)/2;
                         
-            if (check(A,mid))
+            if (check(grid,mid))
                 left = mid;
             else
                 right = mid-1;
@@ -25,17 +15,17 @@ public:
         return left;        
     }
     
-    bool check(vector<vector<int>> A, int K)
+    bool check(vector<vector<int>> grid, int K)
     {
-        if (A[0][0]<K) return false;
+        if (grid[0][0]<K) return false;
         auto dir = vector<pair<int,int>>({{1,0},{-1,0},{0,1},{0,-1}});
         
-        int M = A.size();
-        int N = A[0].size();
+        int M = grid.size(), N = grid[0].size();
         
         queue<pair<int,int>>q;
         q.push({0,0});
-        A[0][0] = -1;
+        vector<vector<int>>visited(M, vector<int>(N));
+        visited[0][0] = 1;
         
         while (q.size()>0)
         {
@@ -47,19 +37,16 @@ public:
             {
                 int i = x+dir[k].first;
                 int j = y+dir[k].second;
-                if (i<0||i>=M||j<0||j>=N)
-                    continue;
-                if (A[i][j]==-1)
-                    continue;                
-                if (A[i][j]<K) continue;
+                if (i<0||i>=M||j<0||j>=N) continue;
+                if (visited[i][j]) continue;                
+                if (grid[i][j]<K) continue;
                 if (i==M-1 && j==N-1) return true;
-                A[i][j]=-1;
+                visited[i][j] = 1;
                 
                 q.push({i,j});                
             }
         }
                 
         return false;
-    
     }
 };
