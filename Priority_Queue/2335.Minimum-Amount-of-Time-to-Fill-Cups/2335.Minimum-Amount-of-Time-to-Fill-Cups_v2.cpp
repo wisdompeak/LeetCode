f9bab6f490be9bd86f1e393566a12eae46d26cc6
wisdompeak <guan.huifeng@gmail.com>
2022-07-10 09:49:21 -0700

    Update 2335.Minimum-Amount-of-Time-to-Fill-Cups_v2.cpp

--- Priority_Queue/2335.Minimum-Amount-of-Time-to-Fill-Cups/2335.Minimum-Amount-of-Time-to-Fill-Cups_v2.cpp
+++ Priority_Queue/2335.Minimum-Amount-of-Time-to-Fill-Cups/2335.Minimum-Amount-of-Time-to-Fill-Cups_v2.cpp
@@ -4,7 +4,7 @@ public:
     {
         sort(amount.rbegin(), amount.rend());
         int total = accumulate(amount.begin(), amount.end(), 0);
-        if (amount[0]>=(total+1)/2)
+        if (amount[0]>=total/2+1)
             return amount[0];
         else
             return (total+1)/2;
