e70928e21701c6194ee4f2b43027814ccdf43a52
wisdompeak <guan.huifeng@gmail.com>
2022-07-10 11:18:06 -0700

    Update Primes.cpp

--- Template/Math/Primes.cpp
+++ Template/Math/Primes.cpp
@@ -1,20 +1,18 @@
 // Find all primes <= n.
 vector<int>Eratosthenes(int n)
 {
-    auto q=vector<int>(n+1,0);
+    vector<int>q(n+1,0);
+    vector<int>primes;
     for (int i=2; i<=sqrt(n); i++)
     {
-        if (q[i]==0)
+        if (q[i]==1) continue;
+        int j=i*2;
+        while (j<=n)
         {
-            int j=i*2;
-            while (j<=n)
-            {
-                q[j]=1;
-                j+=i;
-            }
+            q[j]=1;
+            j+=i;
         }
-    }
-    vector<int>primes;
+    }        
     for (int i=2; i<=n; i++)
     {
         if (q[i]==0)
