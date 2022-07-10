5208bee7c0be9d04ac3abf6f811f3d7e12894f0e
wisdompeak <guan.huifeng@gmail.com>
2022-07-10 09:30:54 -0700

    Create 2335.Minimum-Amount-of-Time-to-Fill-Cups_v2.cpp

---
+++ Priority_Queue/2335.Minimum-Amount-of-Time-to-Fill-Cups/2335.Minimum-Amount-of-Time-to-Fill-Cups_v2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fillCups(vector<int>& amount) 
+    {
+        sort(amount.rbegin(), amount.rend());
+        int total = accumulate(amount.begin(), amount.end(), 0);
+        if (amount[0]>=(total+1)/2)
+            return amount[0];
+        else
+            return (total+1)/2;
+        
+    }
+};
