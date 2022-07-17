9bb9f34086e75c566ba5a8ff71d9c06a4e9a0e14
wisdompeak <guan.huifeng@gmail.com>
2022-07-17 15:13:20 -0700

    Update 2343.Query-Kth-Smallest-Trimmed-Number.cpp

--- Greedy/2343.Query-Kth-Smallest-Trimmed-Number/2343.Query-Kth-Smallest-Trimmed-Number.cpp
+++ Greedy/2343.Query-Kth-Smallest-Trimmed-Number/2343.Query-Kth-Smallest-Trimmed-Number.cpp
@@ -4,34 +4,36 @@ public:
     {
         int m = nums.size(), n = nums[0].size();
         vector<vector<int>>ans(n+1, vector<int>(m));
-
-        for (int i=0; i<m; i++)
-            ans[0][i] = i;
         
-        for (int t=1; t<=n; t++)
+        for (int j=0; j<m; j++)
+            ans[0][j] = j;
+        
+        for (int i=1; i<=n; i++)
         {
-            vector<vector<int>>bucket(10);
-            for (int i=0; i<m; i++)
+            vector<vector<int>>buckets(10);
+            
+            for (int j=0; j<m; j++)
             {
-                int idx = ans[t-1][i];                
-                bucket[nums[idx][n-t]-'0'].push_back(idx);
+                int idx = ans[i-1][j];
+                buckets[nums[idx][nums[idx].size()-i] - '0'].push_back(idx);
             }
             
-            int i = 0;
-            for (int b=0; b<10; b++)            
-                for (int idx: bucket[b])
+            int j = 0;
+            for (int b=0; b<=9; b++)
+            {
+                for (int idx: buckets[b])
                 {
-                    ans[t][i] = idx;
-                    i++;
+                    ans[i][j] = idx;
+                    j++;
                 }
+            }
         }
-
+        
         vector<int>rets;
-        for (auto& q: queries)
+        for (auto q: queries)
         {
             rets.push_back(ans[q[1]][q[0]-1]);
-        }            
-
+        }
         return rets;
     }
 };
