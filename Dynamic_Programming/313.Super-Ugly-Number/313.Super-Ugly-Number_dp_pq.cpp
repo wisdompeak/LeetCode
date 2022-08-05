a28fb9c130e62ecf7120526d896d94dbe4df961e
wisdompeak <guan.huifeng@gmail.com>
2022-08-04 23:42:10 -0700

    Update 313.Super-Ugly-Number_dp_pq.cpp

--- Dynamic_Programming/313.Super-Ugly-Number/313.Super-Ugly-Number_dp_pq.cpp
+++ Dynamic_Programming/313.Super-Ugly-Number/313.Super-Ugly-Number_dp_pq.cpp
@@ -1,30 +1,33 @@
-typedef pair<int,int> PII;
+using LL = long long;
+typedef pair<LL,int> PII;
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) 
     {
-        vector<int>p(primes.size(),0);
-
-        vector<int>rets({1});
-        priority_queue<PII, vector<PII>, greater<>>pq;                
-        for (int i=0; i<primes.size(); i++)
-            pq.push({primes[i]*rets[0], i});
-
+        int k = primes.size();
+        
+        vector<LL>rets({1});
+        vector<LL>p(k, 0);
+        
+        priority_queue<PII, vector<PII>, greater<>>pq;
+        for (int i=0; i<k; i++)
+            pq.push({rets[p[i]]*primes[i], i});
+        
         for (int t=0; t<n-1; t++)
-        {            
-            int cur = pq.top().first;
+        {
+            LL cur = pq.top().first;
             rets.push_back(cur);
-
-            while (!pq.empty() && pq.top().first==cur)                
+            
+            while (!pq.empty() && cur == pq.top().first)
             {
                 int i = pq.top().second;
                 pq.pop();
-
-                p[i]++;                
-                pq.push({primes[i]*rets[p[i]], i});
-            }            
+                
+                p[i]++;
+                pq.push({rets[p[i]]*primes[i], i});
+            }
         }
-
+        
         return rets.back();
     }
 };
