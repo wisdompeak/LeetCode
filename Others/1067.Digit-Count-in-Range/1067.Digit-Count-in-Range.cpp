2a7ff0c67acb1615803397ef07954e12159191ff
wisdompeak <guan.huifeng@gmail.com>
2022-08-15 22:39:24 -0700

    Update 1067.Digit-Count-in-Range.cpp

--- Others/1067.Digit-Count-in-Range/1067.Digit-Count-in-Range.cpp
+++ Others/1067.Digit-Count-in-Range/1067.Digit-Count-in-Range.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     int digitsCount(int d, int low, int high) 
-    {       
+    {
+       
         return helper(d, high)-helper(d,low-1);
     }
     
-    int helper(int digit, int x)
+    int helper(int d, int n)
     {       
-        int len = to_string(x).size();
+        string s = to_string(n);
+        int len = s.size();
         int count = 0;
         
-        if (digit!=0)            
+        if (d!=0)            
         {
             for (int i=1; i<=len; i++)
             {
                 int divisor = pow(10,i);
-                count += x/divisor * pow(10,i-1);
+                count += n/divisor * pow(10,i-1);
                 
-                int y = (x - x/divisor*divisor)/pow(10,i-1);
-                if (y > digit)
+                int y = s[len-i]-'0';
+                if (y > d)
                     count += pow(10,i-1);
-                else if (y==digit)
-                    count += (x%(int)(pow(10,i-1)))+1;          
+                else if (y==d)
+                    count += n%(int)(pow(10,i-1)) + 1;          
             }
         }
         else
@@ -29,13 +31,13 @@ public:
             for (int i=1; i<len; i++)
             {
                 int divisor = pow(10,i);                                
-                count += (x/divisor-1) * pow(10,i-1);
+                count += (n/divisor-1) * pow(10,i-1);
                 
-                int y = (x - x/divisor*divisor)/pow(10,i-1);
-                if (y > digit)
+                int y = s[len-i]-'0';
+                if (y > d)
                     count += pow(10,i-1);
-                else if (y==digit)
-                    count += (x%(int)(pow(10,i-1)))+1;                    
+                else if (y==d)
+                    count += n%(int)(pow(10,i-1)) + 1;                    
             }
         }
         
