98744fbc4313f0c90f3ebd3bcd7c91d094de26a8
wisdompeak <guan.huifeng@gmail.com>
2022-08-25 22:07:14 -0700

    Update 834.Sum-of-Distances-in-Tree.cpp

--- Tree/834.Sum-of-Distances-in-Tree/834.Sum-of-Distances-in-Tree.cpp
+++ Tree/834.Sum-of-Distances-in-Tree/834.Sum-of-Distances-in-Tree.cpp
@@ -1,75 +1,68 @@
 class Solution {
-    unordered_map<int,unordered_set<int>>Children;
-    vector<int>SubLeaves;
-    vector<int>results;
-    
+    int visited[30005];
+    int count[30005];
+    vector<int> next[30005];
+    vector<int> rets;    
+    int n;
 public:
-    vector<int> sumOfDistancesInTree(int N, vector<vector<int>>& edges) 
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) 
     {
-        SubLeaves.assign(N,0);
-        results.assign(N,0);
-        
-        unordered_map<int,unordered_set<int>>Map;        
-        for (int i=0; i<edges.size(); i++)
+        this->n = n;
+        rets.resize(n);
+        for (auto& edge: edges)
         {
-            Map[edges[i][0]].insert(edges[i][1]);
-            Map[edges[i][1]].insert(edges[i][0]);
+            next[edge[0]].push_back(edge[1]);
+            next[edge[1]].push_back(edge[0]);
         }
-                        
-        queue<int>q;
-        q.push(0);
-        while (!q.empty())
-        {
-            int root = q.front();            
-            q.pop();
-            for (auto child:Map[root])
-            {
-                Children[root].insert(child);    
-                Map[child].erase(root);
-                q.push(child);
-            }                        
-        }
-                
-        int root = 0;
-        int temp = DFS1(root);        
-        int AllSum = DFS2(root);        
-        
-        results[0] = AllSum;
-        DFS3(root);
-        
-        return results;
-    }
-    
-    int DFS1(int x)
-    {
-        if (Children[x].size()==0)
-        {
-            SubLeaves[x]=1;
-            return 1;
-        }
-        int sum = 1;
-        for (auto a: Children[x])        
-            sum+=DFS1(a);                    
-        SubLeaves[x]=sum;   
-        return sum;
-    }
-        
-    int DFS2(int x)
-    {
-        if (Children[x].size()==0) return 0;        
-        int sum = 0;
-        for (auto a: Children[x])        
-            sum+=DFS2(a)+SubLeaves[a];        
-        return sum;
-    }
-    
-    void DFS3(int x)
-    {        
-        for (auto a: Children[x])
-        {
-            results[a] = results[x] - SubLeaves[a] + results.size()-SubLeaves[a];
-            DFS3(a);
-        }
+
+        visited[0] = 1;
+        dfs(0);     // Compute the substree sizes
+
+        for (int i=1; i<n; i++) visited[i] = 0;
+        rets[0] = dfs2(0);   // Get total distances towards root
+
+        for (int i=1; i<n; i++) visited[i] = 0;        
+        dfs3(0, rets[0]);
+
+        return rets;
     }
 
+    int dfs(int cur)
+    {
+        int sum = 1;
+        for (int x: next[cur])
+        {
+            if (visited[x]) continue;
+            visited[x] = 1;
+            sum += dfs(x);
+        }        
+        count[cur] = sum;
+        return sum;
+    }
+
+    int dfs2(int cur)
+    {
+        int sum = 0;
+        for (int x: next[cur])
+        {
+            if (visited[x]) continue;
+            visited[x] = 1;
+            sum += dfs2(x);
+        }
+        sum += count[cur]-1;
+        return sum;
+    }
+
+    void dfs3(int cur, int total)
+    {
+        for (int x: next[cur])
+        {
+            if (visited[x]) continue;
+            visited[x] = 1;
+            int a = count[x];
+            int b = n - count[x];
+            rets[x] = total + b - a;
+            dfs3(x, rets[x]);
+        }
+    }
 };
