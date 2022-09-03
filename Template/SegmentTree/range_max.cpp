15214e18202104c483601df15d2fe5c9db2f3122
wisdompeak <guan.huifeng@gmail.com>
2022-09-03 13:33:24 -0700

    Update range_max.cpp

--- Template/SegmentTree/range_max.cpp
+++ Template/SegmentTree/range_max.cpp
@@ -83,7 +83,7 @@ class SegTreeNode
 
 int main()
 {
-    SegTreeNode* root = new SegTreeNode(0, length-1);
+    SegTreeNode* root = new SegTreeNode(0, length-1, 0);
   
     for (auto& update: updates)
     {
