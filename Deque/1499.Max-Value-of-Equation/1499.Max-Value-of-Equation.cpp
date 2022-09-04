ee09d3ff84f40667f6029e672b8e86c83852077c
wisdompeak <guan.huifeng@gmail.com>
2022-09-04 11:38:38 -0700

    Update 1499.Max-Value-of-Equation.cpp

--- Deque/1499.Max-Value-of-Equation/1499.Max-Value-of-Equation.cpp
+++ Deque/1499.Max-Value-of-Equation/1499.Max-Value-of-Equation.cpp
@@ -9,12 +9,14 @@ public:
             while (q.size()>0 && points[q.front()][0] < points[i][0]-k)
                 q.pop_front();
             
-        	if (q.size() > 0)
-        	    ret = max(ret, -points[q.front()][0]+points[q.front()][1] + points[i][0]+points[i][1]);
-
-            while (q.size()>0 && -points[q.back()][0]+points[q.back()][1] < -points[i][0]+points[i][1])
+        	while (q.size()>0 && -points[q.back()][0]+points[q.back()][1] < -points[i][0]+points[i][1])
                 q.pop_back();
             q.push_back(i);
+            
+            if (q.size() > 0)
+        	    ret = max(ret, -points[q.front()][0]+points[q.front()][1] + points[i][0]+points[i][1]);
+
+            
             
         }
         return ret;
