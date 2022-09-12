e508d144f8e89416683cbd35940543b7616245bc
wisdompeak <guan.huifeng@gmail.com>
2022-09-11 23:36:43 -0700

    Update 2272.Substring-With-Largest-Variance_v1.cpp

--- Dynamic_Programming/2272.Substring-With-Largest-Variance/2272.Substring-With-Largest-Variance_v1.cpp
+++ Dynamic_Programming/2272.Substring-With-Largest-Variance/2272.Substring-With-Largest-Variance_v1.cpp
@@ -2,54 +2,34 @@ class Solution {
 public:
     int largestVariance(string s) 
     {
-        vector<int>count(26,0);
-        for (auto x: s)
-            count[x-'a']++;
-                
-        int ret = 0;
         int n = s.size();
-        for (int i=0; i<26; i++)
-            for (int j=0; j<26; j++)
-            {
-                if (i==j) continue;
-                if (count[i]==0 || count[j]==0) continue;
-                vector<int>arr(n, 0);
-                for (int k=0; k<n; k++)
-                {
-                    if (s[k]=='a'+i)
-                        arr[k] = 1;
-                    else if (s[k]=='a'+j)
-                        arr[k] = -1;
-                }                
-                ret = max(ret, helper(arr));                    
-            }
-        
-        return ret;
-    }
-    
-    int helper(vector<int>&nums)
-    {                
-        int n = nums.size();                
-        int curSum0 = 0;
-        int curSum1 = INT_MIN/2;
+        unordered_set<char>Set(s.begin(), s.end());
+
         int ret = 0;
-        
-        for (int i = 0; i < n; i++) 
-        {
-            if (nums[i]==1)
+
+        for (auto a: Set)
+            for (auto b: Set)
             {
-                curSum1 = curSum1+nums[i];
-                curSum0 = max(curSum0+nums[i], nums[i]);                
+                if (a==b) continue;                
+                int curSum0 = 0,  curSum1 = INT_MIN/2;
+
+                for (int i=0; i<n; i++)
+                {
+                    if (s[i] == a)
+                    {
+                        curSum0 = curSum0 + 1;
+                        curSum1 = curSum1 + 1;
+                    }
+                    else if (s[i] == b)
+                    {
+                        curSum1 = max(curSum0-1, curSum1-1);
+                        curSum0 = 0;                        
+                    }
+
+                    ret = max(ret, curSum1);
+                }
             }
-            else if (nums[i] == -1)
-            {
-                curSum1 = max(nums[i], max(curSum0, curSum1)+nums[i]);
-                curSum0 = 0;
-            }
-            
-            ret = max(ret, curSum1);
-        }
-        
+
         return ret;
     }
 };
