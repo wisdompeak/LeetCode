782657c2ef83640bcb4289658c25e1a1be3bb532
wisdompeak <guan.huifeng@gmail.com>
2022-09-11 23:42:23 -0700

    Update 2272.Substring-With-Largest-Variance_v2.cpp

--- Dynamic_Programming/2272.Substring-With-Largest-Variance/2272.Substring-With-Largest-Variance_v2.cpp
+++ Dynamic_Programming/2272.Substring-With-Largest-Variance/2272.Substring-With-Largest-Variance_v2.cpp
@@ -3,25 +3,26 @@ public:
     int largestVariance(string s) 
     {
         int n = s.size();
-        map<char, vector<int>>pos;
+        unordered_map<char, vector<int>>Map;
         for (int i=0; i<n; i++)
-            pos[s[i]].push_back(i);
+            Map[s[i]].push_back(i);
 
         int ret = 0;
 
-        for (auto& [a, pos0]: pos)
-            for (auto& [b, pos1]: pos)
+        for (auto& [a, pos0]: Map)
+            for (auto& [b, pos1]: Map)
             {
-                if (a==b) continue;
-                int i = 0, j = 0;
+                if (a==b) continue;                
                 int curSum0 = 0,  curSum1 = INT_MIN/2;
+
+                int i = 0, j = 0;
 
                 while (i<pos0.size() || j<pos1.size())
                 {
                     if (j==pos1.size() || (i<pos0.size() && pos0[i] < pos1[j]))
-                    {                        
-                        curSum1 +=1;
-                        curSum0 +=1;
+                    {
+                        curSum0 = curSum0 + 1;
+                        curSum1 = curSum1 + 1;
                         i++;
                     }
                     else if (i==pos0.size() || (j<pos1.size() && pos1[j] < pos0[i]))
@@ -29,10 +30,10 @@ public:
                         curSum1 = max(curSum0-1, curSum1-1);
                         curSum0 = 0;                        
                         j++;
-                    }                                            
+                    }
+
                     ret = max(ret, curSum1);
                 }
-
             }
 
         return ret;
