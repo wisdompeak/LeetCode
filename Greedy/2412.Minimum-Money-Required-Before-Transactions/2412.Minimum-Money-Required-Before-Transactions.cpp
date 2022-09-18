d82c53e2ea19523983eb03f642eeb44073fe1c1b
wisdompeak <guan.huifeng@gmail.com>
2022-09-17 20:15:55 -0700

    Create 2412.Minimum-Money-Required-Before-Transactions.cpp

---
+++ Greedy/2412.Minimum-Money-Required-Before-Transactions/2412.Minimum-Money-Required-Before-Transactions.cpp
@@ -0,0 +1,32 @@
+using LL = long long;
+class Solution {
+public:
+    long long minimumMoney(vector<vector<int>>& transactions) 
+    {
+        
+        sort(transactions.begin(), transactions.end(), [](vector<int>&a, vector<int>&b){return a[1]<b[1];});
+        LL maxCostForNetGain = 0;
+        
+        LL cur = 0;
+        LL histLow = 0;
+        
+        for (auto& trans: transactions)
+        {
+            LL cost = trans[0], back = trans[1];
+            if (cost <= back)
+            {
+                maxCostForNetGain = max(maxCostForNetGain, cost);                
+            }
+            else
+            {
+                cur -= cost;
+                histLow = min(histLow, cur);
+                cur += back;
+            }
+        }
+        
+        cur += abs(histLow);
+        
+        return abs(histLow) + max(0LL, maxCostForNetGain - cur);
+    }
+};
