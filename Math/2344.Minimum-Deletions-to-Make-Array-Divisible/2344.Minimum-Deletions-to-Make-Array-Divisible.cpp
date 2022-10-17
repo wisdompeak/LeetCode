fa039aa3223bf73b31494a35add544bc9aad552f
wisdompeak <guan.huifeng@gmail.com>
2022-10-17 01:34:35 -0700

    Create 2344.Minimum-Deletions-to-Make-Array-Divisible.cpp

---
+++ Math/2344.Minimum-Deletions-to-Make-Array-Divisible/2344.Minimum-Deletions-to-Make-Array-Divisible.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& numsDivide) 
+    {
+        int x = numsDivide[0];
+        
+        for (int i=1; i<numsDivide.size(); i++)
+            x = gcd(x, numsDivide[i]);
+        
+        sort(nums.begin(), nums.end());
+        
+        for (int i=0; i<nums.size(); i++)
+        {
+            if (x%nums[i]==0)
+                return i;
+        }
+        
+        return -1;
+        
+    }
+};
