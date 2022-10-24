6789f2d1d9ff111c38061619437e08b1f4a9535b
wisdompeak <guan.huifeng@gmail.com>
2022-10-24 00:47:11 -0700

    Update 204.Count-Primes.cpp

--- Math/204.Count-Primes/204.Count-Primes.cpp
+++ Math/204.Count-Primes/204.Count-Primes.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int countPrimes(int n) 
-    {        
-        vector<int> isPrime(n, 1);
+    {
+        vector<int>isPrime(n, true);
         int count = 0;
-        
-        for (int i = 2; i < n; i++) 
+        for (int i=2; i<n; i++)
         {
-            if (isPrime[i]==false) continue;            
+            if (isPrime[i]==false) continue;
             count++;
-            for (int j=2*i; j < n; j+=i) 
-                isPrime[j] = false;
+            
+            if (i < sqrt(n))
+                for (int j=2*i; j<n; j+=i)
+                    isPrime[j] = false;
         }
+        
         return count;
     }
 };
