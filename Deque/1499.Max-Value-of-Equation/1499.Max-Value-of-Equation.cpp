8ab0b20548e8898d2de7b79bba0f443a19987e09
wisdompeak <guan.huifeng@gmail.com>
2022-10-26 17:38:52 -0700

    Update 1499.Max-Value-of-Equation.cpp

--- Deque/1499.Max-Value-of-Equation/1499.Max-Value-of-Equation.cpp
+++ Deque/1499.Max-Value-of-Equation/1499.Max-Value-of-Equation.cpp
@@ -10,15 +10,17 @@ public:
                 q.pop_front();
             }
             
+            if (q.size() > 0) {
+        	    ret = max(ret, -points[q.front()][0]+points[q.front()][1] + points[i][0]+points[i][1]);
+            }
+            
             while (q.size()>0 && -points[q.back()][0]+points[q.back()][1] < -points[i][0]+points[i][1]) {
                 q.pop_back();
             }
             
             q.push_back(i);
             
-            if (q.size() > 0) {
-        	    ret = max(ret, -points[q.front()][0]+points[q.front()][1] + points[i][0]+points[i][1]);
-            }
+
         }
         return ret;
     }
