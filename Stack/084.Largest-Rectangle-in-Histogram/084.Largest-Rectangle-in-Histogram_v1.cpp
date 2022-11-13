97425006270b387e0e750ef25cc27213c45e84e9
wisdompeak <guan.huifeng@gmail.com>
2022-11-12 19:46:44 -0800

    Update 084.Largest-Rectangle-in-Histogram_v1.cpp

--- Stack/084.Largest-Rectangle-in-Histogram/084.Largest-Rectangle-in-Histogram_v1.cpp
+++ Stack/084.Largest-Rectangle-in-Histogram/084.Largest-Rectangle-in-Histogram_v1.cpp
@@ -17,14 +17,13 @@ public:
         
         while (!stk.empty()) stk.pop();
         vector<int>prevSmaller(n, -1);
-        for (int i=0; i<heights.size(); i++)
+        for (int i=heights.size()-1; i>=0; i--)
         {
             while (!stk.empty() && heights[stk.top()] > heights[i])
-            {                                
+            {   
+                prevSmaller[stk.top()] = i;
                 stk.pop();
             }
-            if (!stk.empty())
-                prevSmaller[i] = stk.top();
             stk.push(i);              
         }
                 
