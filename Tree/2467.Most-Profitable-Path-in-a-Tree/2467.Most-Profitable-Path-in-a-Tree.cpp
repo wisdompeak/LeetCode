22164d95c6a82921c8b307ba4db0aa1852874245
wisdompeak <guan.huifeng@gmail.com>
2022-11-13 16:36:25 -0800

    Update 2467.Most-Profitable-Path-in-a-Tree.cpp

--- Tree/2467.Most-Profitable-Path-in-a-Tree/2467.Most-Profitable-Path-in-a-Tree.cpp
+++ Tree/2467.Most-Profitable-Path-in-a-Tree/2467.Most-Profitable-Path-in-a-Tree.cpp
@@ -1,18 +1,18 @@
 class Solution {
-    vector<int> next[100005];
     int b[100005];
+    vector<int>next[100005];
+    int ret = INT_MIN/2;
     int bob;
-    int n;
-    int ret = INT_MIN;
     vector<int>amount;
 public:
     int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) 
     {
-        this->n = amount.size();
         this->bob = bob;
         this->amount = amount;
         
-        for (int i=0; i<n; i++) b[i] = INT_MAX/2;
+        int n = amount.size();
+        for (int i=0; i<n; i++)
+            b[i] = INT_MAX/2;
         
         for (auto& edge: edges)
         {
@@ -24,16 +24,15 @@ public:
         dfs(0, -1, 0);
         
         dfs2(0, -1, 0, 0);
-        
         return ret;
     }
     
     void dfs(int cur, int parent, int step)
-    {        
+    {
         if (cur==bob)
         {
             b[cur] = 0;
-            return;            
+            return;
         }
         
         int toBob = INT_MAX/2;
@@ -43,26 +42,27 @@ public:
             dfs(nxt, cur, step+1);
             toBob = min(toBob, b[nxt]+1);
         }
-        
         b[cur] = toBob;
-        
         return;
     }
     
     void dfs2(int cur, int parent, int step, int score)
-    {                
+    {
         if (step == b[cur])
             score += amount[cur]/2;
         else if (step < b[cur])
-            score += amount[cur];        
+            score += amount[cur];
         
         if (next[cur].size()==1 && next[cur][0]==parent)
+        {
             ret = max(ret, score);
-        
+            return;
+        }
+                    
         for (int nxt: next[cur])
         {
             if (nxt==parent) continue;
-            dfs2(nxt, cur, step+1, score);
+            dfs2(nxt, cur, step+1, score);          
         }
     }
 };
