a82f7501466866fe9c46f72b5e8007e4e323d93c
wisdompeak <guan.huifeng@gmail.com>
2022-12-26 18:24:54 -0800

    Update 2516.Take-K-of-Each-Character-From-Left-and-Right_v2.cpp

--- Two_Pointers/2516.Take-K-of-Each-Character-From-Left-and-Right/2516.Take-K-of-Each-Character-From-Left-and-Right_v2.cpp
+++ Two_Pointers/2516.Take-K-of-Each-Character-From-Left-and-Right/2516.Take-K-of-Each-Character-From-Left-and-Right_v2.cpp
@@ -2,37 +2,41 @@ class Solution {
 public:
     int takeCharacters(string s, int k) 
     {
-        if (k==0) return 0;
 
-        int a = 0, b = 0, c = 0;
+        int A = 0, B = 0, C = 0;
         for (auto ch: s)
         {
-            if (ch=='a') a++;
-            else if (ch=='b') b++;
-            else if (ch=='c') c++;
+            if (ch=='a') A++;
+            else if (ch=='b') B++;
+            else if (ch=='c') C++;
         }
-        if (a<k || b<k || c<k) return -1;
-                
+        if (A<k || B<k || C<k) return -1;
+
+        A = A-k;
+        B = B-k;
+        C = C-k;
+
         int n = s.size();
-        int ret = n;
         int j = 0;
+        int a = 0, b = 0, c = 0;
+        int ret = 0;
         for (int i=0; i<n; i++)
         {
-            if (s[i]=='a') a--;
-            else if (s[i]=='b') b--;
-            else if (s[i]=='c') c--;
-
-            while (j<n && (a<k || b<k || c<k))
-            {                
+            while (a<=A && b<=B && c<=C)
+            {
+                ret = max(ret, j-i);  // [i,j)
+                if (j==n) break;
                 if (s[j]=='a') a++;
                 else if (s[j]=='b') b++;
                 else if (s[j]=='c') c++;
                 j++;
             }
-            if (a>=k && b>=k && c>=k)
-                ret = min(ret, n-1-i+j);
+
+            if (s[i]=='a') a--;
+            else if (s[i]=='b') b--;
+            else if (s[i]=='c') c--;
         }
 
-        return ret;
+        return n - ret;
     }   
 };
