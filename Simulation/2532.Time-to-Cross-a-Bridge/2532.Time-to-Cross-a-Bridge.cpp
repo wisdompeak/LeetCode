6535e5b5da14ab3aa698047e4426eebf76d1f0ca
wisdompeak <guan.huifeng@gmail.com>
2023-01-09 01:50:26 -0800

    Create 2532.Time-to-Cross-a-Bridge.cpp

---
+++ Simulation/2532.Time-to-Cross-a-Bridge/2532.Time-to-Cross-a-Bridge.cpp
@@ -0,0 +1,71 @@
+using PII = pair<int,int>;
+class Solution {
+public:
+    int findCrossingTime(int n, int k, vector<vector<int>>& time) 
+    {        
+        priority_queue<PII, vector<PII>, greater<>>leftArrive;
+        priority_queue<PII, vector<PII>, greater<>>rightArrive;
+        for (int i=0; i<k; i++)        
+            leftArrive.push({0, i});        
+
+        int nextFree = 0;
+        priority_queue<PII>leftWait;
+        priority_queue<PII>rightWait;
+        
+        int ret = 0;
+        int count = 0;
+        int taken = 0;
+
+        while (count < n)
+        {
+            while (!leftArrive.empty() && leftArrive.top().first <= nextFree)
+            {
+                auto [arriveTime, id] = leftArrive.top();
+                leftArrive.pop();                
+                leftWait.push({time[id][0]+time[id][2], id});
+            }
+            while (!rightArrive.empty() && rightArrive.top().first <= nextFree)
+            {
+                auto [arriveTime, id] = rightArrive.top();
+                rightArrive.pop();
+                rightWait.push({time[id][0]+time[id][2], id});
+            }            
+
+            if (leftWait.empty() && rightWait.empty())
+            {
+                int t1 = leftArrive.empty() ? INT_MAX : leftArrive.top().first;
+                int t2 = rightArrive.empty() ? INT_MAX : rightArrive.top().first;
+                nextFree = min(t1, t2);
+                continue;
+            }
+
+            if (!rightWait.empty())   // R -> L
+            {
+                auto [_, id] = rightWait.top();
+                rightWait.pop();
+                nextFree += time[id][2];
+                leftArrive.push({nextFree+time[id][3], id});                
+
+                count++;
+                ret = max(ret, nextFree);
+            }
+            else if (!leftWait.empty())   // L -> R
+            {
+                if (taken == n) 
+                {
+                    while (!leftWait.empty()) 
+                        leftWait.pop();
+                } else
+                {
+                    auto [_, id] = leftWait.top();
+                    leftWait.pop();
+                    nextFree += time[id][0];
+                    rightArrive.push({nextFree+time[id][1], id});
+                    taken++;
+              }
+            }
+        }
+        
+        return nextFree;
+    }
+};
