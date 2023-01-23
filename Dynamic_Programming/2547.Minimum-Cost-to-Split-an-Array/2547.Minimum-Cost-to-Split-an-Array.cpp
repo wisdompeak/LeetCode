79763973545a695a2ac003950906789bfcc8f027
wisdompeak <guan.huifeng@gmail.com>
2023-01-22 19:05:48 -0800

    Update 2547.Minimum-Cost-to-Split-an-Array.cpp

--- Dynamic_Programming/2547.Minimum-Cost-to-Split-an-Array/2547.Minimum-Cost-to-Split-an-Array.cpp
+++ Dynamic_Programming/2547.Minimum-Cost-to-Split-an-Array/2547.Minimum-Cost-to-Split-an-Array.cpp
@@ -1,31 +1,29 @@
-using LL = long long;
 class Solution {
-    LL dp[1005];
+    int dp[1005];
 public:
     int minCost(vector<int>& nums, int k) 
     {
-        int n = nums.size();
-        unordered_map<int,int>Map;
+        int n = nums.size();        
+
         for (int i=0; i<n; i++)
         {
-            dp[i] = LLONG_MAX;
-            Map.clear();            
-            LL v = 0;
+            unordered_map<int,int>Map;
+            int score = 0;
+            dp[i] = INT_MAX;
             for (int j=i; j>=0; j--)
             {
                 Map[nums[j]]++;
                 if (Map[nums[j]]==2)
-                    v+=2;
+                    score += 2;
                 else if (Map[nums[j]]>2)
-                    v+=1;                
-                
+                    score += 1;
+
                 if (j>=1)
-                    dp[i] = min(dp[i], dp[j-1] + v+k);
+                    dp[i] = min(dp[i], dp[j-1] + score + k);
                 else
-                    dp[i] = min(dp[i], v+k);
-            }            
+                    dp[i] = min(dp[i], score + k);
+            }
         }
         return dp[n-1];
-        
     }
 };
