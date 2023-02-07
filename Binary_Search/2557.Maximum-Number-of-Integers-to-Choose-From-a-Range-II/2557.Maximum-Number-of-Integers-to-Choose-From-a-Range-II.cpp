2fb854cd573578125d43b9a302e0b59c265faa92
wisdompeak <guan.huifeng@gmail.com>
2023-02-06 20:41:35 -0800

    Create 2557.Maximum-Number-of-Integers-to-Choose-From-a-Range-II.cpp

---
+++ Binary_Search/2557.Maximum-Number-of-Integers-to-Choose-From-a-Range-II/2557.Maximum-Number-of-Integers-to-Choose-From-a-Range-II.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    vector<long long>presum;
+public:
+    int maxCount(vector<int>& banned, int n, long long maxSum) 
+    {
+        sort(banned.begin(), banned.end());
+
+        presum.resize(banned.size());
+        for (int i=0; i<banned.size(); i++)
+            presum[i] = (i==0?0:presum[i-1]) + banned[i];
+
+        int left = 0, right = n;
+        while (left < right)
+        {
+            int mid = right - (right-left)/2;
+            if (More(mid, banned, n, maxSum))
+                right = mid-1;
+            else
+                left = mid;
+        }
+        int m = left;
+        int t = upper_bound(banned.begin(), banned.end(), m) - banned.begin();
+        return m - t;
+    }
+
+    bool More(long long m, vector<int>& banned, int n, long long maxSum)
+    {
+        int t = upper_bound(banned.begin(), banned.end(), m) - banned.begin();
+        long long sum = (1+m)*m/2 - (t==0?0:presum[t-1]);
+        return sum > maxSum;
+    }
+};
