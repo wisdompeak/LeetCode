c79d82753ce67688c6e62f4c7531b34b20974304
wisdompeak <guan.huifeng@gmail.com>
2023-02-19 11:01:21 -0800

    Create  2568.Minimum-Impossible-OR_v1.cpp

---
+++ Greedy/2568.Minimum-Impossible-OR/2568.Minimum-Impossible-OR_v1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minImpossibleOR(vector<int>& nums) 
+    {
+        sort(nums.begin(), nums.end());
+        int mx = 0;        
+        for (int i = 0; i < nums.size(); i++)        
+        {
+            if (nums[i] > mx+1)
+                return mx+1;
+            else
+                mx = (mx | nums[i]);
+        }
+  
+        return -1;        
+    }
+};
