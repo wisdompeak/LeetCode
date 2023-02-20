5acc9e045635f297eed83a9377a54c382b97bd6e
wisdompeak <guan.huifeng@gmail.com>
2023-02-19 17:58:40 -0800

    Update 2568.Minimum-Impossible-OR_v1.cpp

--- Greedy/2568.Minimum-Impossible-OR/2568.Minimum-Impossible-OR_v1.cpp
+++ Greedy/2568.Minimum-Impossible-OR/2568.Minimum-Impossible-OR_v1.cpp
@@ -3,15 +3,15 @@ public:
     int minImpossibleOR(vector<int>& nums) 
     {
         sort(nums.begin(), nums.end());
-        int mx = 0;        
-        for (int i = 0; i < nums.size(); i++)        
+        int mx = 0;
+        for (int i=0; i<nums.size(); i++)
         {
             if (nums[i] > mx+1)
                 return mx+1;
             else
                 mx = (mx | nums[i]);
         }
-  
-        return -1;        
+
+        return mx+1;        
     }
 };
