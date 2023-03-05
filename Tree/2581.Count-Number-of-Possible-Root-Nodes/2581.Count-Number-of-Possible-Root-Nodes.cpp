86c5d98876671ac3919954eb4b25e0fd0308f17a
wisdompeak <guan.huifeng@gmail.com>
2023-03-05 09:18:18 -0800

    Update 2581.Count-Number-of-Possible-Root-Nodes.cpp

--- Tree/2581.Count-Number-of-Possible-Root-Nodes/2581.Count-Number-of-Possible-Root-Nodes.cpp
+++ Tree/2581.Count-Number-of-Possible-Root-Nodes/2581.Count-Number-of-Possible-Root-Nodes.cpp
@@ -1,51 +1,46 @@
 class Solution {
-    vector<int>next[100005];
-    unordered_set<int>guess[100005];
-    int ret = 0;
+    vector<int> next[100005];
+    unordered_set<int> guess[100005];
     int k;
-    int count[100005];
+    int ret = 0;
 public:
-    int rootCount(vector<vector<int>>& edges, vector<vector<int>>& guesses, int k) 
-    {
+    int rootCount(vector<vector<int>>& edges, vector<vector<int>>& guesses, int k) {
+        this->k = k;
         int n = edges.size()+1;
-        this-> k = k;
-        for (auto e: edges)
+        for (auto& e: edges)
         {
             next[e[0]].push_back(e[1]);
             next[e[1]].push_back(e[0]);
         }
-        
-        for (auto g: guesses)        
-            guess[g[0]].insert(g[1]);        
-        
-        dfs(0, -1);
-        
-        dfs2(0, -1, count[0]);
-        
+        for (auto& g: guesses)
+            guess[g[0]].insert(g[1]);
+
+        int count = dfs(0, -1);
+
+        dfs2(0, -1, count);
+
         return ret;
     }
-    
-    void dfs(int cur, int parent)
+
+    int dfs(int cur, int parent)
     {
+        int count = 0;
         for (int nxt: next[cur])
         {
             if (nxt==parent) continue;
-            
+            count += dfs(nxt, cur);
             if (guess[cur].find(nxt)!=guess[cur].end())
-                count[cur] += 1;
-            
-            dfs(nxt, cur);
-            count[cur] += count[nxt];
+                count +=1;
         }
+        return count;
     }
-    
+
     void dfs2(int cur, int parent, int count)
     {
         if (count >= k) ret++;
-        
         for (int nxt: next[cur])
         {
-            if (nxt==parent) continue;            
+            if (nxt==parent) continue;
             int temp = count;
             if (guess[cur].find(nxt)!=guess[cur].end())
                 temp -= 1;
