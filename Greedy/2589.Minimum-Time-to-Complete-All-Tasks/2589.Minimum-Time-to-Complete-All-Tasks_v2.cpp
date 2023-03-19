c84f8343a67f61e09234bc7bafc23c8f1655e696
wisdompeak <guan.huifeng@gmail.com>
2023-03-19 12:52:20 -0700

    Update 2589.Minimum-Time-to-Complete-All-Tasks_v2.cpp

--- Greedy/2589.Minimum-Time-to-Complete-All-Tasks/2589.Minimum-Time-to-Complete-All-Tasks_v2.cpp
+++ Greedy/2589.Minimum-Time-to-Complete-All-Tasks/2589.Minimum-Time-to-Complete-All-Tasks_v2.cpp
@@ -3,29 +3,30 @@ class Solution {
 public:
     int findMinimumTime(vector<vector<int>>& tasks) 
     {
-        sort(tasks.begin(), tasks.end(), 
-             [](vector<int>&a, vector<int>&b){return a[1]<b[1];}
-            );
-        
-        vector<AI3>arr;
+        sort(tasks.begin(), tasks.end(), [](vector<int>&a, vector<int>&b){
+            return a[1] < b[1];
+        });
+
+        vector<AI3>arr;   
         arr.push_back({-2,-1,0});
-                
+
         for (int i=0; i<tasks.size(); i++)
         {
             int start = tasks[i][0], end = tasks[i][1], duration = tasks[i][2];
 
             auto iter = lower_bound(arr.begin(), arr.end(), AI3({start,0,0}));
-            iter = iter-1;
-            int overlap = 0;
+            iter = prev(iter);
+
+            int overlap;
             if ((*iter)[1] < start)
                 overlap = arr.back()[2] - (*iter)[2];
             else
-                overlap = arr.back()[2] - (*iter)[2] + abs((*iter)[1] - start + 1) ;
-                
+                overlap = arr.back()[2] - (*iter)[2] + abs((*iter)[1] - start + 1);
+
             int diff = duration - overlap;
             int cur = end;
             while (diff > 0)
-            {                
+            {
                 if (abs(arr.back()[1] - cur) < diff)
                 {
                     diff -= abs(arr.back()[1] - cur);
@@ -34,12 +35,12 @@ public:
                 }
                 else
                 {
-                    arr.push_back({cur-diff+1, end, arr.back()[2]+end-(cur-diff)});
+                    arr.push_back({cur-diff+1, end, arr.back()[2] + end-(cur-diff)});
                     diff = 0;
-                }                
-            }            
+                }
+            }
         }
-                
+
         return arr.back()[2];
     }
 };
