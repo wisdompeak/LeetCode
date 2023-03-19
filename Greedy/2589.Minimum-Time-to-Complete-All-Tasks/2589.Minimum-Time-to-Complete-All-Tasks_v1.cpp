399d233afbc56b1d094b523769ec16f8b979bbb3
wisdompeak <guan.huifeng@gmail.com>
2023-03-19 12:52:41 -0700

    Update 2589.Minimum-Time-to-Complete-All-Tasks_v1.cpp

--- Greedy/2589.Minimum-Time-to-Complete-All-Tasks/2589.Minimum-Time-to-Complete-All-Tasks_v1.cpp
+++ Greedy/2589.Minimum-Time-to-Complete-All-Tasks/2589.Minimum-Time-to-Complete-All-Tasks_v1.cpp
@@ -3,29 +3,31 @@ public:
     int findMinimumTime(vector<vector<int>>& tasks) 
     {
         sort(tasks.begin(), tasks.end(), [](vector<int>&a, vector<int>&b){
-             return a[1]<b[1];}
-            );
-        
-        vector<int>time(4005);
+            return a[1] < b[1];
+        });
+
+        vector<int>time(2005);
         for (int i=0; i<tasks.size(); i++)
         {
-            int a = tasks[i][0], b = tasks[i][1], d = tasks[i][2];
-            int count = 0;
-            for (int t=a; t<=b; t++)
-                count += (time[t]==1);
-            if (count >= d) continue;
-            int diff = d - count;
-            for (int j=b; j>=a; j--)
+            int start = tasks[i][0], end = tasks[i][1], duration = tasks[i][2];
+            int overlap = 0;
+            for (int t=start; t<=end; t++)
+                overlap += (time[t]==1);
+
+            if (overlap >= duration) continue;
+            int diff = duration - overlap;
+            for (int t=end; t>=start; t--)
             {
-                if (time[j]==0)
+                if (time[t]==0)
                 {
-                    time[j] = 1;
+                    time[t] = 1;
                     diff--;
                 }
-                if (diff == 0) break;
-            }            
+                if (diff == 0)
+                    break;
+            }
         }
-        
+
         int ret = 0;
         for (int t=0; t<=2000; t++)
             ret += (time[t]==1);
