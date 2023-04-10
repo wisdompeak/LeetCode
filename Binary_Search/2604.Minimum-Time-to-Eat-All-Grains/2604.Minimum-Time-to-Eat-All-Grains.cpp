e52f4665221f458c80b0a393ea7f4bee38e8d7e4
wisdompeak <guan.huifeng@gmail.com>
2023-04-09 21:34:57 -0700

    Create 2604.Minimum-Time-to-Eat-All-Grains.cpp

---
+++ Binary_Search/2604.Minimum-Time-to-Eat-All-Grains/2604.Minimum-Time-to-Eat-All-Grains.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int minimumTime(vector<int>& hens, vector<int>& grains) 
+    {
+        sort(hens.begin(), hens.end());
+        sort(grains.begin(), grains.end());
+
+        int left = 0, right = INT_MAX/2;
+        while (left < right)
+        {
+            int mid = left + (right-left)/2;
+            if (isOK(mid, hens, grains))
+                right = mid;
+            else
+                left = mid+1;
+        }
+        return left;        
+    }
+
+    bool isOK(int time, vector<int>& hens, vector<int>& grains)
+    {
+        int j = 0;
+        for (int i=0; i<hens.size(); i++)
+        {
+            int t = 0;
+            if (grains[j]<hens[i])
+            {
+                if (grains[j]<hens[i]-time) return false;
+                t = (hens[i]-grains[j]);
+            }
+
+            while (j<grains.size() && grains[j]<=hens[i])
+                j++;
+
+            if (time < 3*t)
+            {                
+                while (j<grains.size() && (grains[j]-hens[i])*2 + t <= time)
+                    j++;
+            }
+            else
+            {
+                while (j<grains.size() && (grains[j]-hens[i]) + t*2 <= time)
+                    j++;
+            }
+
+            if (j==grains.size()) return true;
+        }
+
+        return false;
+    }
+};
