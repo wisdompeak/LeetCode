af912e13f1b041ebb4405986e448c9ad580fc34f
wisdompeak <guan.huifeng@gmail.com>
2023-04-18 20:26:45 -0700

    Create 2607.Make-K-Subarray-Sums-Equal_v1.cpp

---
+++ Math/2607.Make-K-Subarray-Sums-Equal/2607.Make-K-Subarray-Sums-Equal_v1.cpp
@@ -0,0 +1,35 @@
+using LL = long long;
+class Solution {
+public:
+    long long makeSubKSumEqual(vector<int>& arr, int k) 
+    {        
+        int n = arr.size();
+        LL ret = 0;
+        vector<int>visited(n);
+        
+        for (int i=0; i<k; i++)
+        {
+            if (visited[i]==1) continue;
+            vector<int>nums;
+            int j = i;
+            while (visited[j]==0)
+            {                              
+                visited[j] = 1;
+                nums.push_back(arr[j]);
+                j = (j+k)%n;                                
+            }
+            ret += helper(nums);            
+        }        
+        return ret;
+    }
+    
+    LL helper(vector<int>&nums)
+    {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        LL ret = 0;
+        for (int i=0; i<n; i++)
+            ret += (LL)abs(nums[i] - nums[n/2]);
+        return ret;
+    }
+};
