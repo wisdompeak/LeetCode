41dd845b1b06565916d74a806e45870fa902eb58
wisdompeak <guan.huifeng@gmail.com>
2023-04-23 17:50:22 -0700

    Create 2653.Sliding-Subarray-Beauty.cpp

---
+++ Heap/2653.Sliding-Subarray-Beauty/2653.Sliding-Subarray-Beauty.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) 
+    {
+        multiset<int>Set1;
+        multiset<int>Set2;
+        vector<int>rets;
+        int count_neg = 0;
+        for (int i=0; i<nums.size(); i++)
+        {
+            if (Set1.size() < x)
+                Set1.insert(nums[i]);
+            else
+            {
+                int v = *Set1.rbegin();
+                if (v > nums[i])
+                {
+                    Set1.erase(Set1.find(v));
+                    Set2.insert(v);
+                    Set1.insert(nums[i]);
+                } 
+                else 
+                {
+                    Set2.insert(nums[i]);
+                }
+            }
+            
+            if (i>=k-1)
+            {
+                int v = *Set1.rbegin();
+                rets.push_back(min(v, 0));
+            }            
+            
+            if (i>=k-1)
+            {
+                int v = nums[i-k+1];
+                auto iter = Set2.find(v);
+                if (iter!=Set2.end()) 
+                    Set2.erase(iter);
+                else
+                {
+                    Set1.erase(Set1.find(v));
+                    if (!Set2.empty())
+                    {
+                        Set1.insert(*Set2.begin());
+                        Set2.erase(Set2.begin());
+                    }
+                }
+            }            
+        }
+        
+        return rets;
+        
+    }
+};
