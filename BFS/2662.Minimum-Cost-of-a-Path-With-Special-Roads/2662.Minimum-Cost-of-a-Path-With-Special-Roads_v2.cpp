db51e1d4f4907f37209b96556da11bbbb79de149
wisdompeak <guan.huifeng@gmail.com>
2023-05-02 00:17:17 -0700

    Create 2662.Minimum-Cost-of-a-Path-With-Special-Roads_v2.cpp

--- BFS/2662.Minimum-Cost-of-a-Path-With-Special-Roads/2662.Minimum-Cost-of-a-Path-With-Special-Roads_v2.cpp
+++ BFS/2662.Minimum-Cost-of-a-Path-With-Special-Roads/2662.Minimum-Cost-of-a-Path-With-Special-Roads_v2.cpp
@@ -4,44 +4,42 @@ class Solution {
 public:
     int minimumCost(vector<int>& start, vector<int>& target, vector<vector<int>>& specialRoads) 
     {
-        map<LL, LL>dist;
-
-        priority_queue<PLL, vector<PLL>, greater<>>pq;
+        priority_queue<PLL, vector<PLL>, greater<>>pq;   // {dist to node, node id}
         pq.push({0, encode(start[0], start[1])});
         for (auto& road: specialRoads)
-        {            
-            int x2 = road[2], y2 = road[3];                        
-            LL pos2 = encode(x2,y2);
-            pq.push({abs(start[0]-x2)+abs(start[1]-y2), pos2});
+        {
+            int x = road[2], y = road[3];
+            pq.push({abs(start[0]-x)+abs(start[1]-y), encode(x,y)});
         }
 
+        map<LL, LL>dist;
         LL ret = INT_MAX;
         while (!pq.empty())
         {
-            auto [len, pos] = pq.top();
+            auto [len, id] = pq.top();
             pq.pop();
-            if (dist.find(pos)!=dist.end()) continue;
-            dist[pos] = len;
-            auto [x,y] = decode(pos);
+            if (dist.find(id)!=dist.end()) continue;
+            dist[id] = len;
+            auto [x,y] = decode(id);
 
-            ret = min(ret, len+abs(x-target[0])+abs(y-target[1]));
+            ret = min(ret, len + abs(x-target[0])+abs(y-target[1]));
 
             for (auto& road: specialRoads)
             {
                 int x1 = road[0], y1 = road[1];
                 int x2 = road[2], y2 = road[3];
                 int cost = road[4];
-                LL pos1 = encode(x1,y1);
-                LL pos2 = encode(x2,y2);
+                LL id2 = encode(x2,y2);
 
-                if (dist.find(pos2)==dist.end())
-                    pq.push({len + abs(x-x1)+abs(y-y1) + cost, pos2});
+                if (dist.find(id2)==dist.end())                
+                    pq.push({len + abs(x-x1)+abs(y-y1) + cost, id2});
             }
+
         }
 
         return ret;
     }
 
     LL encode(LL x, LL y) {return (x<<32) + y;}
-    PLL decode(LL p) {return {p>>32, p%(1LL<<32)};}
+    PLL decode(LL id) {return {id>>32, id%(1LL<<32)};}
 };
