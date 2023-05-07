f2333c9f700345192ec1111bb1359b9a90236644
wisdompeak <guan.huifeng@gmail.com>
2023-05-07 13:30:57 -0700

    Update 440.K-th-Smallest-in-Lexicographical-Order.cpp

--- Others/440.K-th-Smallest-in-Lexicographical-Order/440.K-th-Smallest-in-Lexicographical-Order.cpp
+++ Others/440.K-th-Smallest-in-Lexicographical-Order/440.K-th-Smallest-in-Lexicographical-Order.cpp
@@ -7,35 +7,36 @@ public:
     }
     
     // return the Lexicographically Kth element that begin with the prefix
-    // excluding the prefix itself
+    // if k==0, return the prefix itself.
     int FindKthNumberBeginWith(int prefix, int n, int k)
     {       
-        if (k==0) return prefix;        
+        if (k==0) return prefix;
         
         for (int i=(prefix==0?1:0); i<=9; i++)
         {
-            int count = TotalNumbersBeginWith(prefix*10+i,n);
+            int count = 1 + TotalNumbersBeginWith(prefix*10+i,n);
             if (count<k)
                 k-=count;
             else
                 return FindKthNumberBeginWith(prefix*10+i,n,k-1);
         }
+        return 0;
     }
     
     // return how many numbers that begin with the prefix and within the range of n 
-    // including the prefix itself
+    // excluding the prefix itself
     int TotalNumbersBeginWith(long long prefix, int n)
     {
-        long long fac = 1;
+        long long fac = 10;
         int count = 0;
         
-        for(int digit = 1; digit <= 9; digit++) 
+        while (1)
         {
             long long min = prefix*fac;
             long long max = prefix*fac+fac-1;
-            if(n < min)
-                break;
-            else if(min <= n && n <= max) 
+            if (min > n) break;
+                
+            if(min <= n && n <= max) 
             {
                 count += (n-min+1);
                 break;
