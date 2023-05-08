81a55a9864de9b34cfefd047df8bd7ea5698fded
wisdompeak <guan.huifeng@gmail.com>
2023-05-07 21:47:19 -0700

    Update 440.K-th-Smallest-in-Lexicographical-Order.cpp

--- Others/440.K-th-Smallest-in-Lexicographical-Order/440.K-th-Smallest-in-Lexicographical-Order.cpp
+++ Others/440.K-th-Smallest-in-Lexicographical-Order/440.K-th-Smallest-in-Lexicographical-Order.cpp
@@ -1,50 +1,54 @@
 class Solution {
-    
 public:
     int findKthNumber(int n, int k) 
-    {        
-        return FindKthNumberBeginWith(0,n,k);
-    }
-    
-    // return the Lexicographically Kth element that begin with the prefix
-    // if k==0, return the prefix itself.
-    int FindKthNumberBeginWith(int prefix, int n, int k)
-    {       
-        if (k==0) return prefix;
-        
-        for (int i=(prefix==0?1:0); i<=9; i++)
-        {
-            int count = 1 + TotalNumbersBeginWith(prefix*10+i,n);
-            if (count<k)
-                k-=count;
-            else
-                return FindKthNumberBeginWith(prefix*10+i,n,k-1);
-        }
-        return 0;
-    }
-    
-    // return how many numbers that begin with the prefix and within the range of n 
-    // excluding the prefix itself
-    int TotalNumbersBeginWith(long long prefix, int n)
     {
-        long long fac = 10;
-        int count = 0;
-        
+        return FindKthNUmberBeginWith(0, n, k);
+    }
+
+    // return the Lexicographically k-th element that begins with prefix
+    // if k==0, return prefix itself
+    int FindKthNUmberBeginWith(int prefix, int n, int k)
+    {
+        if (k==0) return prefix;
+
+        int start = (prefix == 0) ? 1 : 0;
+        for (int i=start; i<=9; i++)
+        {
+            int count = 1 + TotalNumbersBeginWith(prefix*10+i, n);
+            if (count < k)
+                k = k-count;
+            else
+                return FindKthNUmberBeginWith(prefix*10+i, n, k-1);
+        }
+
+        return -1;
+    }
+
+    // return how many numbers that begin with prefix (and <= n)
+    // excluding prefix itself
+    int TotalNumbersBeginWith(long prefix, int n)
+    {
+        long count = 0;
+        long exp = 10;
         while (1)
         {
-            long long min = prefix*fac;
-            long long max = prefix*fac+fac-1;
-            if (min > n) break;
-                
-            if(min <= n && n <= max) 
+            long lower = prefix * exp;
+            long upper = prefix * exp + exp + 1;
+            if (lower > n) break;
+            if (lower <= n && upper >= n)
             {
-                count += (n-min+1);
+                count += (n - lower +1);
                 break;
             }
-            else  
-                count += fac;
-            fac *= 10;
+            else
+            {
+                count += exp;
+            }
+
+            exp *= 10;
         }
+
         return count;
-    }    
+    }
 };
+
