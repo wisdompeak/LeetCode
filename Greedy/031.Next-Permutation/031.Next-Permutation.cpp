9f2503764066a65e289b6b0c9c19b788140fc146
wisdompeak <guan.huifeng@gmail.com>
2023-05-08 23:17:54 -0700

    Create 031.Next-Permutation.cpp

---
+++ Greedy/031.Next-Permutation/031.Next-Permutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) 
+    {
+        int i = nums.size()-1;
+        while (i>=1 && nums[i]<=nums[i-1])
+            i--;
+        
+        if (i==0)
+        {
+            sort(nums.begin(), nums.end());
+            return;
+        }
+        
+        i--;
+        
+        int j = nums.size()-1;
+        while (nums[j]<=nums[i])
+            j--;
+        swap(nums[i], nums[j]);
+        sort(nums.begin()+i+1, nums.end());
+        return;
+    }
+};
