897586ef42edff7536306e100a72307f27921c63
wisdompeak <guan.huifeng@gmail.com>
2023-05-09 00:10:44 -0700

    Update 1358.Number-of-Substrings-Containing-All-Three-Characters.cpp

--- Two_Pointers/1358.Number-of-Substrings-Containing-All-Three-Characters/1358.Number-of-Substrings-Containing-All-Three-Characters.cpp
+++ Two_Pointers/1358.Number-of-Substrings-Containing-All-Three-Characters/1358.Number-of-Substrings-Containing-All-Three-Characters.cpp
@@ -7,14 +7,14 @@ public:
         int ret = 0;
         for (int i=0; i<s.size(); i++)
         {
-            while (!(count1&&count2&&count3) && j<s.size())
+            while (j<s.size() && (count1 * count2 * count3 == 0))
             {
                 if (s[j]=='a') count1++;
                 else if (s[j]=='b') count2++;
                 else if (s[j]=='c') count3++;
                 j++;
             }
-            if (count1&&count2&&count3)
+            if (count1 * count2 * count3 > 0)
                 ret += s.size()-j+1;
             
             if (s[i]=='a') count1--;
