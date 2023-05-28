7924679ad69b49601675dcd9c68cac75b15ad56b
wisdompeak <guan.huifeng@gmail.com>
2023-05-28 10:50:02 -0700

    Update 2713.Maximum-Strictly-Inreasing-Cells-in-a-Matrix.cpp

--- Dynamic_Programming/2713.Maximum-Strictly-Inreasing-Cells-in-a-Matrix/2713.Maximum-Strictly-Inreasing-Cells-in-a-Matrix.cpp
+++ Dynamic_Programming/2713.Maximum-Strictly-Inreasing-Cells-in-a-Matrix/2713.Maximum-Strictly-Inreasing-Cells-in-a-Matrix.cpp
@@ -1,42 +1,42 @@
 using AI3 = array<int,3>;
-using PII = pair<int,int>;
 class Solution {
 public:
     int maxIncreasingCells(vector<vector<int>>& mat) 
     {
         int m = mat.size(), n = mat[0].size();
-        vector<map<int,int>>rows(m);
-        vector<map<int,int>>cols(n);
         vector<AI3>nums;
         for (int i=0; i<m; i++)
-            for (int j=0; j<n; j++)            
-                nums.push_back({mat[i][j], i, j});                
-        
+            for (int j=0; j<n; j++)
+                nums.push_back({mat[i][j], i, j});
+        sort(nums.begin(), nums.end());
+
+        vector<map<int,int>> rows(m);        
+        vector<map<int,int>> cols(n);
+
         for (int i=0; i<m; i++)
             rows[i].insert({INT_MIN, 0});
         for (int j=0; j<n; j++)
             cols[j].insert({INT_MIN, 0});
-                
-        sort(nums.begin(), nums.end());
-        
-        int ans = 0;
-        
+
+        int ret = 1;
         for (auto [val, i, j]: nums)
         {
-            int ret = 1;
+            int len = 1;
+
             auto iter = rows[i].lower_bound(val);
-            ret = max(ret, prev(iter)->second + 1);
-            
+            iter = prev(iter);
+            len = max(len, iter->second + 1);
+
             iter = cols[j].lower_bound(val);
-            ret = max(ret, prev(iter)->second + 1);
-                            
-            rows[i][val] = max(rows[i][val], ret);
-            cols[j][val] = max(cols[j][val], ret);
-            
-            ans = max(ans, ret);                 
+            iter = prev(iter);
+            len = max(len, iter->second + 1);
+
+            rows[i][val] = max(len, rows[i][val]);
+            cols[j][val] = max(len, cols[j][val]);
+
+            ret = max(ret, len);
         }
-        
-        return ans;
-        
+
+        return ret;        
     }
 };
