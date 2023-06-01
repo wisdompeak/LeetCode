05fcfdfde5b0579ce94963a6c03301cb9ae31491
wisdompeak <guan.huifeng@gmail.com>
2023-05-31 19:28:18 -0700

    Create 2702.Minimum-Operations-to-Make-Numbers-Non-positive.cpp

---
+++ Binary_Search/2702.Minimum-Operations-to-Make-Numbers-Non-positive/2702.Minimum-Operations-to-Make-Numbers-Non-positive.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x, int y) 
+    {
+        sort(nums.rbegin(), nums.rend());
+        int left = 0, right = INT_MAX/2;
+        while (left < right)
+        {
+            int mid = left+(right-left)/2;
+            if (isOK(mid, nums, x, y))
+                right = mid;
+            else
+                left = mid+1;
+        }
+        return left;        
+    }
+
+    bool isOK(int k, vector<int>& nums, int x, int y) 
+    {
+        int count = 0;
+        for (int i=0; i<nums.size(); i++)
+        {
+            count += ceil(fmax(0.0, nums[i] - k*1.0*y)*1.0/(x-y));
+            if (count > k) return false;
+        }
+        return true;
+    }
+};
