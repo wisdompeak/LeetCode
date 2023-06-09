e23355fd167e496ad33e7efeab23957be638d346
wisdompeak <guan.huifeng@gmail.com>
2023-06-08 22:41:15 -0700

    Update 307.Range-Sum-Query-Mutable_BIT.cpp

--- Segment_Tree/307.Range-Sum-Query-Mutable/307.Range-Sum-Query-Mutable_BIT.cpp
+++ Segment_Tree/307.Range-Sum-Query-Mutable/307.Range-Sum-Query-Mutable_BIT.cpp
@@ -1,41 +1,76 @@
-class NumArray {
-public:
-    vector<int>bitArr;
-    vector<int>nums;
+class BIT{
+    public:
+    int N;    
+    vector<long long>bitArr; // Note: all arrays are 1-index
+    vector<long long>nums;
+    long long M = 1e9+7;
 
-    NumArray(vector<int>& nums) {
-        this->nums = nums;
-        bitArr.resize(nums.size()+1);
-        for (int i=0; i<nums.size(); i++)
-            my_update(i, nums[i]);
-        cout<<"init done"<<endl;
+    void init(int N)
+    {
+        this->N = N;
+        bitArr.resize(N+1);
+        nums.resize(N+1);        
     }
 
-    void update(int i, int val){
-        my_update(i, val-nums[i]);
-        nums[i] = val;
-    }
-
-    void my_update(int i, int delta) {
-        int idx = i+1;
-        while (idx<bitArr.size())
+    // increase nums[i] by delta
+    void updateDelta(int i, long long delta) {
+        int idx = i;
+        while (idx <= N)
         {
             bitArr[idx]+=delta;
+            // bitArr[idx] %= M;
             idx+=idx&(-idx);
         }
     }
 
-    int my_query(int idx){
-        idx+=1;
-        int result = 0;
+    // sum of a range nums[1:j] inclusively
+    long long queryPreSum(int idx){
+        long long result = 0;
         while (idx){
             result += bitArr[idx];
+            // result %= M;
             idx-=idx&(-idx);
         }
         return result;
     }
 
-    int sumRange(int i, int j) {
-        return my_query(j)-my_query(i-1);
+    // sum of a range nums[i:j] inclusively
+    long long sumRange(int i, int j) {    
+        return queryPreSum(j)-queryPreSum(i-1);
+    } 
+};
+
+class NumArray {
+    BIT bit;
+    vector<int>nums;
+public:
+    NumArray(vector<int>& nums) 
+    {
+        this->nums = nums;
+        int n = nums.size();        
+        bit.init(n+10);        
+
+        for (int i=0; i<n; i++) {
+            bit.updateDelta(i+1, nums[i]);            
+        }
+        
+    }
+    
+    void update(int index, int val) 
+    {
+        bit.updateDelta(index+1, val-nums[index]);     
+        nums[index] = val;   
+    }
+    
+    int sumRange(int left, int right) 
+    {
+        return bit.sumRange(left+1, right+1);        
     }
 };
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
