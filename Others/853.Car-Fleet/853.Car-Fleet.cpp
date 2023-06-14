ed65a73287608321fb4d4744677c9b18ce37f584
wisdompeak <guan.huifeng@gmail.com>
2023-06-13 23:15:01 -0700

    Update 853.Car-Fleet.cpp

--- Others/853.Car-Fleet/853.Car-Fleet.cpp
+++ Others/853.Car-Fleet/853.Car-Fleet.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) 
-    {        
-        vector<pair<int,int>>q;
+    {                
         int N= position.size();
         if (N==0) return 0;
         
+        vector<pair<int,int>>q;
         for (int i=0; i<N; i++)
-            q.push_back({target-position[i],speed[i]});
-        sort(q.begin(),q.end());
-        
-        int i = 0;                
-        int count = 0;
-        while (i<N)
+            q.push_back({position[i],speed[i]});
+        sort(q.begin(),q.end());        
+
+        int count=0;
+        for (int i=N-1; i>=0; i--)
         {
-            double time = q[i].first*1.0/q[i].second;
-            int j = i+1;
-            while (j<N && q[j].first*1.0/q[j].second<=time)
-                j++;
+            double T = (target-q[i].first)*1.0/q[i].second;
+            int j = i-1;
+            while (j>=0 && (target-q[j].first)*1.0/q[j].second <= T)            
+                j--;
             count++;
-            i = j;
-        }
+            i = j+1;
+        }        
         return count;
     }
 };
