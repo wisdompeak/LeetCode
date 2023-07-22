644d3c0e11d85cff0932587da82744cdfcd2fbc1
wisdompeak <guan.huifeng@gmail.com>
2023-07-22 11:58:34 -0700

    Update 2787.Ways-to-Express-an-Integer-as-Sum-of-Powers.cpp

--- Dynamic_Programming/2787.Ways-to-Express-an-Integer-as-Sum-of-Powers/2787.Ways-to-Express-an-Integer-as-Sum-of-Powers.cpp
+++ Dynamic_Programming/2787.Ways-to-Express-an-Integer-as-Sum-of-Powers/2787.Ways-to-Express-an-Integer-as-Sum-of-Powers.cpp
@@ -1,50 +1,28 @@
 using LL = long long;
 LL M = 1e9+7;
-class Solution {
-    int n;
+class Solution {    
     LL dp[305][305];
-public:
-    LL getXPower(int a, int x)
-    {
-        LL ret = 1;
-        for (int i=0; i<x; i++)
-        {
-            ret *= a;
-            if (ret > n)
-                return INT_MAX;
-        }
-        return ret;
-    }
-    
+public:    
     int numberOfWays(int n, int x) 
-    {
-        this->n = n;
-                        
+    {                        
         dp[0][0] = 1;
         
-        for (int i=1; i<=n; i++)
-        {
+        for (int i=0; i<=n; i++)        
             for (int j=1; j<=n; j++)
             {
-                LL cur = getXPower(j,x);
-                if (cur>i) break;
-                
-                for (int jj=0; jj<j; jj++)
+                dp[i][j] = dp[i][j-1];
+
+                LL num = 1;
+                for (int t=0; t<x; t++)
+                    num *= j;
+
+                if (num <= i)
                 {
-                    dp[i][j] += dp[i-cur][jj];
+                    dp[i][j] += dp[i-num][j-1];
                     dp[i][j] %= M;
-                }
+                }                
             }            
-        }
-        
-        LL ret = 0;
-        for (int j=1; j<=n; j++)
-        {
-            LL cur = getXPower(j,x);
-            if (cur>n) break;
-            ret += dp[n][j];
-            ret %= M;
-        }
-        return ret;
+                        
+        return dp[n][n];
     }
 };
