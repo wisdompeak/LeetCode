088e0a634ff5abd72addcfeff1a42eb3ec1523f0
wisdompeak <guan.huifeng@gmail.com>
2023-08-06 02:18:43 -0700

    Update 2813.Maximum-Elegance-of-a-K-Length-Subsequence.cpp

--- Greedy/2813.Maximum-Elegance-of-a-K-Length-Subsequence/2813.Maximum-Elegance-of-a-K-Length-Subsequence.cpp
+++ Greedy/2813.Maximum-Elegance-of-a-K-Length-Subsequence/2813.Maximum-Elegance-of-a-K-Length-Subsequence.cpp
@@ -1,49 +1,49 @@
 using LL = long long;
-using PII = pair<LL,LL>;
+using PII = pair<LL, LL>;
 class Solution {
 public:
     long long findMaximumElegance(vector<vector<int>>& items, int k) 
     {
         sort(items.rbegin(), items.rend());
-        
-        unordered_map<int,vector<int>>Map;
+
         LL sum = 0;
+        unordered_map<int, int>Map;
         for (int i=0; i<k; i++)
         {
-            Map[items[i][1]].push_back(i);
-            sum += items[i][0];            
+            sum += items[i][0];
+            Map[items[i][1]]++;
         }
-        
-        LL t = (int)Map.size();                
+
+        LL t = Map.size();
         LL ret = sum + t*t;
-        
+
         priority_queue<PII, vector<PII>, greater<>>pq;
-        for (int i=0; i<k; i++)        
+        for (int i=0; i<k; i++)
             pq.push({items[i][0], items[i][1]});
-        
+
         for (int i=k; i<items.size(); i++)
         {
             if (Map.find(items[i][1])!=Map.end()) continue;
 
             while (!pq.empty())
             {
-                auto [val, cate] = pq.top();
+                auto [profit, cate] = pq.top();
                 pq.pop();
-                
-                if (Map[cate].size()>1) 
-                {                    
-                    sum -= val;
+
+                if (Map[cate] > 1)
+                {
+                    sum -= profit;
                     sum += items[i][0];
                     t++;
-                    Map[cate].pop_back();
-                    Map[items[i][1]].push_back(i);
-                    ret = max(ret, sum + t*t);                    
-                    break;                    
+                    Map[cate]--;
+                    Map[items[i][1]]++;
+
+                    ret = max(ret, sum + t*t);
+                    break;
                 }
-            }            
+            }
         }
-        
+
         return ret;
-        
     }
 };
