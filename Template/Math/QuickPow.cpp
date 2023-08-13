9e7a146a7b654735c555dc6eacbf271fb56e5d9c
wisdompeak <guan.huifeng@gmail.com>
2023-08-13 15:50:22 -0700

    Update QuickPow.cpp

--- Template/Math/QuickPow.cpp
+++ Template/Math/QuickPow.cpp
@@ -1,5 +1,14 @@
 class Solution {
+long long M = 1e9+7;
 public:
+    long long quickMul(long long x, long long N) {
+        if (N == 0) {
+            return 1;
+        }
+        LL y = quickMul(x, N / 2) % M;
+        return N % 2 == 0 ? (y * y % M) : (y * y % M * x % M);
+    }
+
     double quickMul(double x, long long N) {
         if (N == 0) {
             return 1.0;
