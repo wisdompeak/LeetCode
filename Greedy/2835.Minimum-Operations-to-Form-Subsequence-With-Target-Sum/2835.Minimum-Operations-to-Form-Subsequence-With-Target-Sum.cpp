d7b02bcd3f55186d7a870549f4518919fa37d7e7
wisdompeak <guan.huifeng@gmail.com>
2023-08-27 12:23:00 -0700

    Create 2835.Minimum-Operations-to-Form-Subsequence-With-Target-Sum.cpp

---
+++ Greedy/2835.Minimum-Operations-to-Form-Subsequence-With-Target-Sum/2835.Minimum-Operations-to-Form-Subsequence-With-Target-Sum.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int target) 
+    {
+        vector<int>count(32,0);
+        
+        for (auto x: nums)
+        {
+            int i = 0;
+            while (x>0)
+            {
+                i++;
+                x/=2;
+            }
+            count[i-1] += 1;
+        }
+        
+        vector<int>t;
+        for (int i=0; i<31; i++)
+        {
+            if ((target>>i)&1)
+                t.push_back(i);
+        }
+                
+        int ret = 0;
+        
+        for (int i: t)
+        {
+            int j = 0;
+            while (j < i)
+            {
+                count[j+1] += count[j] / 2;
+                count[j] %= 2;
+                j++;
+            }
+            if (j == i && count[j] > 0)
+            {
+                count[j] -= 1;
+                continue;
+            }            
+            
+            while (j<31 && count[j] == 0)
+                j++;
+            if (j==31) return -1;
+            
+            count[j] -= 1;
+            for (int t=j-1; t>=i; t--)
+                count[t]+=1;
+            ret += j-i;
+            count[i] -= 1;            
+        }
+        
+        return ret;
+    }
+};
