da12168078d6ed660f0e02a08e9acf7d05970306
wisdompeak <guan.huifeng@gmail.com>
2023-09-17 11:37:21 -0700

    Update 2862.Maximum-Element-Sum-of-a-Complete-Subset-of-Indices.cpp

--- Thinking/2862.Maximum-Element-Sum-of-a-Complete-Subset-of-Indices/2862.Maximum-Element-Sum-of-a-Complete-Subset-of-Indices.cpp
+++ Thinking/2862.Maximum-Element-Sum-of-a-Complete-Subset-of-Indices/2862.Maximum-Element-Sum-of-a-Complete-Subset-of-Indices.cpp
@@ -4,17 +4,18 @@ public:
     long long maximumSum(vector<int>& nums) 
     {
         int n = nums.size();
+
         int k = 1;
-        LL ret = *max_element(nums.begin(), nums.end());
-        while (k<n)
+        LL ret = 0;
+        while (k<=n)
         {
             LL sum = 0;
-            for (int i=1; i*i*k<=n; i++)
-                sum += nums[i*i*k-1];            
-            ret = max(sum, ret);
+            for (int i=1; k*i*i<=n; i++)
+                sum += nums[k*i*i-1];
+            ret = max(ret, sum);
             k++;
         }
-        
+
         return ret;
     }
 };
