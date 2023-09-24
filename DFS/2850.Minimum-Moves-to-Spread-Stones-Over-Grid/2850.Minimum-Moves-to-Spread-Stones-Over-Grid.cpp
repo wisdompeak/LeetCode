8588b3290a8efac97494d772e902af6a3b5dab58
wisdompeak <guan.huifeng@gmail.com>
2023-09-24 14:47:34 -0700

    Update 2850.Minimum-Moves-to-Spread-Stones-Over-Grid.cpp

--- DFS/2850.Minimum-Moves-to-Spread-Stones-Over-Grid/2850.Minimum-Moves-to-Spread-Stones-Over-Grid.cpp
+++ DFS/2850.Minimum-Moves-to-Spread-Stones-Over-Grid/2850.Minimum-Moves-to-Spread-Stones-Over-Grid.cpp
@@ -3,31 +3,37 @@ class Solution {
 public:
     int minimumMoves(vector<vector<int>>& grid) 
     {
-        dfs(0, 0, grid);
-        return global;        
+        dfs(0, 0, grid);    
+        return global;    
     }
-    
-    void dfs(int cur, int total, vector<vector<int>>& grid)
+
+    void dfs(int cur, int moves, vector<vector<int>>& grid)
     {
-        if (total >= global) return;
+        if (moves >= global) return;
+
+        if (cur==9)
+        {
+            global = min(global, moves);
+            return;
+        }            
 
         int i = cur/3;
         int j = cur%3;
         if (grid[i][j]!=0)
         {
-            dfs(cur+1, total, grid);
+            dfs(cur+1, moves, grid);
             return;
-        }      
+        }
 
         for (int x=0; x<3; x++)
             for (int y=0; y<3; y++)
             {
-                if (grid[x][y]<=1) continue;                                
-                grid[x][y]-=1;
-                grid[i][j]+=1;
-                dfs(cur+1, total+abs(x-i)+abs(y-j), grid);
-                grid[x][y]+=1;
-                grid[i][j]-=1;
-            }        
+                if (grid[x][y]<=1) continue;
+                grid[x][y] -= 1;
+                grid[i][j] += 1;
+                dfs(cur+1, moves+abs(x-i)+abs(y-j), grid);
+                grid[x][y] += 1;
+                grid[i][j] -= 1;
+            }
     }
 };
