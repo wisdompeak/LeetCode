8b6a81aa24d792bb82fae91a9c3cfcf3ddb68768
wisdompeak <guan.huifeng@gmail.com>
2023-09-30 11:19:20 -0700

    Update 1504.Count-Submatrices-With-All-Ones.cpp

--- Greedy/1504.Count-Submatrices-With-All-Ones/1504.Count-Submatrices-With-All-Ones.cpp
+++ Greedy/1504.Count-Submatrices-With-All-Ones/1504.Count-Submatrices-With-All-Ones.cpp
@@ -3,29 +3,33 @@ public:
     int numSubmat(vector<vector<int>>& mat) 
     {
         int m = mat.size(), n = mat[0].size();
-        vector<int>nums(n+1, 0);    
-        int ret = 0;    
+        vector<int>h(n+1, 0);
+
+        int ret = 0;
+
         for (int i=0; i<m; i++)
         {
             for (int j=0; j<n; j++)
-                nums[j+1] = (mat[i][j]==1 ? (nums[j+1]+1) : 0);
-            stack<int>stk;       
-            stk.push(0);     
-            int sum = 0;
+                h[j+1] = (mat[i][j] == 1? (h[j+1]+1):0);
+
+            stack<int>stk;
+            stk.push(0);
+            int c = 0;
             for (int j=1; j<=n; j++)
             {
-                while (!stk.empty() && nums[stk.top()] > nums[j])
+                while (!stk.empty() && h[stk.top()] > h[j])
                 {
                     int p1 = stk.top();
                     stk.pop();
                     int p2 = stk.top();
-                    sum -= (p1-p2) * (nums[p1] - nums[j]);
+                    c =  c - (p1-p2)*(h[p1]-h[j]);
                 }
+                c += h[j];
+                ret += c;
                 stk.push(j);
-                sum += nums[j];
-                ret += sum;
-            }            
+            }
         }
+
         return ret;        
     }
 };
