9881bab509b35615d09963a256b3eb2b3d21d304
wisdompeak <guan.huifeng@gmail.com>
2023-10-01 10:56:02 -0700

    Update 2875.Minimum-Size-Subarray-in-Infinite-Array.cpp

--- Hash/2875.Minimum-Size-Subarray-in-Infinite-Array/2875.Minimum-Size-Subarray-in-Infinite-Array.cpp
+++ Hash/2875.Minimum-Size-Subarray-in-Infinite-Array/2875.Minimum-Size-Subarray-in-Infinite-Array.cpp
@@ -3,37 +3,36 @@ class Solution {
 public:
     int minSizeSubarray(vector<int>& nums, int target) 
     {
-        LL total = accumulate(nums.begin(), nums.end(), 0LL);
+        LL total  = accumulate(nums.begin(), nums.end(), 0LL);
+
         int n = nums.size();
-        
         for (int i=0; i<n; i++)
             nums.push_back(nums[i]);
-        
+
         vector<LL>presum(2*n, 0);
         presum[0] = nums[0];
         for (int i=1; i<2*n; i++)
-            presum[i] = presum[i-1] + nums[i];        
-        
+            presum[i] = presum[i-1] +  nums[i];
+
         LL ret = INT_MAX/2;
-                
-        unordered_map<LL,int>Map;
+
+        unordered_map<LL, int>Map; // presum module, index
         Map[0] = -1;
-        
         for (int i=0; i<2*n; i++)
-        {                                         
-            LL r = ((presum[i] - target)%total+total)%total;            
-            
+        {
+            LL r = ((presum[i] - target) % total + total) % total;
+
             if (Map.find(r)!=Map.end())
             {
-                int p = Map[r];
-                LL k = ((p==-1?0:presum[p]) - presum[i] + target) / total;
-                ret = min(ret, i-p+k*n);
+                int j = Map[r];
+                LL k = ((j==-1? 0: presum[j]) - presum[i] + target) / total;
+                ret = min(ret, i-j+k*n);
             }
-            
-            Map[presum[i]] = i;
+
+            Map[presum[i]%total] = i;
         }
-        
+
         if (ret == INT_MAX/2) return -1;
-        else return ret;
+        else return ret;        
     }
 };
