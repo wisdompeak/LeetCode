a2cd400441cb045950703109160ba767d5ba7260
wisdompeak <guan.huifeng@gmail.com>
2023-11-05 14:48:46 -0800

    Create 2926.Maximum-Balanced-Subsequence-Sum.cpp

---
+++ Heap/2926.Maximum-Balanced-Subsequence-Sum/2926.Maximum-Balanced-Subsequence-Sum.cpp
@@ -0,0 +1,42 @@
+using LL =  long long;
+class Solution {
+public:
+    long long maxBalancedSubsequenceSum(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<int>arr(n);
+        for (int i=0; i<n; i++)
+            arr[i] = nums[i] - i;
+        
+        map<int,LL>dp;
+        LL ret = LLONG_MIN;
+        
+        for (int i=0; i<n; i++)
+        {
+            int x = arr[i];
+            auto iter = dp.upper_bound(x);
+            if (iter!=dp.begin())
+            {
+                iter = prev(iter);
+                dp[x] = max((LL)nums[i], iter->second + nums[i]);                
+            }
+            else
+            {
+                dp[x] = nums[i];                
+            }
+            
+            ret = max(ret, dp[x]);
+            
+            iter = dp.find(x);
+            iter = next(iter);
+            while (iter!=dp.end() && iter->second <= dp[x])
+            {
+                int y = iter->first;
+                iter = next(iter);
+                dp.erase(y);
+            }            
+        }
+        
+        return ret;        
+    }
+};
