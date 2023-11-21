ffd0a2cca7e6557d10124e83f5e7f46a1336cf5e
wisdompeak <guan.huifeng@gmail.com>
2023-11-20 23:05:26 -0800

    Create 2939.Maximum-Xor-Product.cpp

---
+++ Thinking/2939.Maximum-Xor-Product/2939.Maximum-Xor-Product.cpp
@@ -0,0 +1,63 @@
+using LL = long long;
+LL M = 1e9+7;
+class Solution {
+public:
+    int maximumXorProduct(long long a, long long b, int n) 
+    {
+        LL A = ((a>>n)<<n);
+        LL B = ((b>>n)<<n);
+        
+        if (A<B) return maximumXorProduct(b,a,n);
+        
+        if (A==B)
+        {
+            int flag = 0;
+            for (int k=n-1; k>=0; k--)
+            {
+                LL bit1 = ((a>>k)&1LL);
+                LL bit2 = ((b>>k)&1LL);
+                if (bit1==bit2)
+                {
+                    a = a - (bit1<<k) + (1LL<<k);
+                    b = b - (bit2<<k) + (1LL<<k);
+                }
+                else
+                {
+                    if (flag==0)
+                    {
+                        a = a - (bit1<<k) + (1LL<<k);
+                        b = b - (bit2<<k);
+                        flag = 1-flag;
+                    }
+                    else
+                    {
+                        a = a - (bit1<<k);
+                        b = b - (bit2<<k) + (1LL<<k);
+                    }
+                }
+            }            
+        }
+        
+        if (A>B)
+        {
+            for (int k=n-1; k>=0; k--)
+            {
+                LL bit1 = ((a>>k)&1LL);
+                LL bit2 = ((b>>k)&1LL);
+                if (bit1==bit2)
+                {
+                    a = a - (bit1<<k) + (1LL<<k);
+                    b = b - (bit2<<k) + (1LL<<k);
+                }
+                else
+                {
+                    a = a - (bit1<<k);
+                    b = b - (bit2<<k) + (1LL<<k);
+                }
+            }            
+        }
+    
+        return a%M*(b%M)%M;
+        
+    }
+};
