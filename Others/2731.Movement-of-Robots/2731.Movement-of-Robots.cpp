ab739eaf9eabf38ee3e969c095ecc16c357ccd21
wisdompeak <guan.huifeng@gmail.com>
2023-11-24 05:02:00 -0800

    Update 2731.Movement-of-Robots.cpp

--- Others/2731.Movement-of-Robots/2731.Movement-of-Robots.cpp
+++ Others/2731.Movement-of-Robots/2731.Movement-of-Robots.cpp
@@ -5,26 +5,25 @@ public:
     int sumDistance(vector<int>& nums, string s, int d) 
     {
         int n = nums.size();
-        vector<LL>pos;               
+        vector<LL>pos;
         for (int i=0; i<n; i++)
         {
             LL p;
             if (s[i]=='L')
-                p = nums[i] - d;
+                p = (LL)nums[i] - d;
             else
-                p = nums[i] + d;
+                p = (LL)nums[i] + d;
             pos.push_back(p);
         }
         sort(pos.begin(), pos.end());
-        
+
         LL ret = 0;
         for (int i=0; i+1<n; i++)
         {
-            LL s = (pos[i+1]-pos[i]) % M;
-            ret += s * (i+1) % M * (n-1-i) % M;
+            LL s = (pos[i+1]-pos[i]);
+            ret += s * (i+1) %M * (n-1-i) %M;
             ret %= M;
         }
-      
         return ret;
     }
 };
