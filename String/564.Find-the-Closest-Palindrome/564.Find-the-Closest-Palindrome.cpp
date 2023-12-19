428e0931f786e95ad73e328f4c330e3de364aabc
wisdompeak <guan.huifeng@gmail.com>
2023-12-18 22:07:19 -0800

    Update 564.Find-the-Closest-Palindrome.cpp

--- String/564.Find-the-Closest-Palindrome/564.Find-the-Closest-Palindrome.cpp
+++ String/564.Find-the-Closest-Palindrome/564.Find-the-Closest-Palindrome.cpp
@@ -2,115 +2,79 @@ class Solution {
 public:
     string nearestPalindromic(string n) 
     {
-        int N=n.size();
-        string s1,s2,s3;
-        
-        if (N%2==1)
-        {
-            string t=n.substr(0,N/2+1);
-            long long num=convert(t);
-            string t1,t2;
-            
-            // candidate 1
-            t1 = to_string(num);
-            t2 = t1;
-            reverse(t2.begin(),t2.end());
-            s1 = t1.substr(0,N/2)+t2;
-            
-            // candidate 2
-            t1 = to_string(num-1);
-            t2=t1;
-            reverse(t2.begin(),t2.end());
-            s2 = t1.substr(0,N/2)+t2;
-            
-            // candidate 3
-            t1 = to_string(num+1);
-            t2=t1;
-            reverse(t2.begin(),t2.end());
-            s3 = t1.substr(0,N/2)+t2;
-
-            cout<<s1<<" "<<s2<<" "<<s3<<endl;
-            
-            return pick(n,s1,s2,s3);
-        }
-        
-        if (N%2==0)
-        {
-            string t=n.substr(0,N/2);
-            long long num=convert(t);
-            string t1,t2;
-            
-            //candidate 1
-            t1 = n.substr(0,N/2);
-            reverse(t1.begin(),t1.end());
-            s1 = to_string(num)+t1;
-            
-            //candidate 2
-            t1 = to_string(num-1);
-            if (t1=="0")
-                s2="9";
-            else if (t1.size()==t.size())
-            {
-                t2=t1;
-                reverse(t2.begin(),t2.end());
-                s2=t1+t2;
-            }
-            else if (t1.size()!=t.size())
-            {
-                t2=t1;
-                reverse(t2.begin(),t2.end());
-                s2=t1+'9'+t2;
-            }
-            
-            //candidate 3
-            t1 = to_string(num+1);
-            if (t1.size()==t.size())
-            {
-                t2=t1;
-                reverse(t2.begin(),t2.end());
-                s3=t1+t2;
-            }
-            else if (t1.size()!=t.size())
-            {
-                t2=t1;
-                reverse(t2.begin(),t2.end());
-                t1.pop_back();
-                s3=t1+t2;
-            }
-            
-            cout<<s1<<" "<<s2<<" "<<s3<<endl;
-            
-            return pick(n,s1,s2,s3);
-        }        
-    }
-    
-    string pick(string n, string s1, string s2, string s3)
-    {
-        long long m= convert(n);
-        long long n1= convert(s1);
-        long long n2= convert(s2);
-        long long n3= convert(s3);
-        
-        if (n1==m)
-        {
-            return abs(n2-m)<=abs(n3-m)?s2:s3;
-        }
+        string a = makeSmaller(n);
+        string b = makeGreater(n);
+        if (stoll(b)-stoll(n) >= stoll(n)-stoll(a))
+            return a;
         else
-        {
-            long long num_temp = abs(n1-m)<=abs(n3-m)?n1:n3;
-            string string_temp = abs(n1-m)<=abs(n3-m)?s1:s3;
-            
-            return abs(n2-m)<=abs(num_temp-m)?s2:string_temp;
-        }        
-    }    
-    
-    long long convert(string s)
+            return b;
+    }
+
+    string makeSmaller(const string &n) 
     {
-        long long result=0;
-        for (int i=0; i<s.size(); i++)
-        {
-            result = result*10 + s[i]-'0';
+        const int m = n.length();
+        string s = n;
+        for (int i = 0, j = m - 1; i <= j;)
+            s[j--] = s[i++];                    
+        if (s < n) {
+            return s;
         }
-        return result;
-    }    
+
+        int carry = 1;
+        for (int i = (m - 1)/2; i >= 0; i--) 
+        {
+            int d = s[i]-'0'-carry;
+            if (d>=0) 
+            {
+                s[i] = '0'+d;
+                carry = 0;
+            }
+            else
+            {
+                s[i] = '9';
+                carry = 1;
+            }
+            s[m-1-i] = s[i];
+        }
+        if (s[0]=='0' && m>1)
+            return string(m - 1, '9');
+        else
+            return s;
+    }
+
+    string makeGreater(const string &n) 
+    {
+        const int m = n.length();
+        string s = n;
+        for (int i = 0, j = m - 1; i <= j;)
+            s[j--] = s[i++];                    
+        if (s > n) {
+            return s;
+        }
+
+        int carry = 1;
+        for (int i = (m - 1)/2; i >= 0; i--) 
+        {
+            int d = s[i]-'0'+carry;
+            if (d<=9) 
+            {
+                s[i] = '0'+d;
+                carry = 0;
+            }
+            else
+            {
+                s[i] = '0';
+                carry = 1;
+            }
+            s[m-1-i] = s[i];
+        }
+        if (carry == 1)
+        {
+            s = string(m + 1, '0');
+            s[0] = s.back() = '1';
+            return s;
+        }            
+        else
+            return s;
+    }
 };
