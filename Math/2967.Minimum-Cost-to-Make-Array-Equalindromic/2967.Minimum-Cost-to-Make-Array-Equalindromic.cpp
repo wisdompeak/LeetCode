5eee049491b3fb85a97b95064b0d3a1c6498d25d
wisdompeak <guan.huifeng@gmail.com>
2023-12-19 00:08:39 -0800

    Update 2967.Minimum-Cost-to-Make-Array-Equalindromic.cpp

--- Math/2967.Minimum-Cost-to-Make-Array-Equalindromic/2967.Minimum-Cost-to-Make-Array-Equalindromic.cpp
+++ Math/2967.Minimum-Cost-to-Make-Array-Equalindromic/2967.Minimum-Cost-to-Make-Array-Equalindromic.cpp
@@ -1,43 +1,47 @@
-using LL = long long;
 class Solution {
 public:
     long long minimumCost(vector<int>& nums) 
-    {        
-        int n = nums.size();        
-        sort(nums.begin(), nums.end());        
-        int m;        
-        if (n%2==1)        
-            m = nums[n/2];        
-        else        
-            m = (nums[n/2-1]+nums[n/2])/2;        
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int m;
+        if (n%2==1)
+            m = nums[n/2];
+        else
+            m = (nums[n/2] + nums[n/2-1])/2;
         
-        string a = nextSmaller(to_string(m));
-        string b = nextGreater(to_string(m));
-        
-        LL ret = LLONG_MAX;
+        string a = nextSmallerOrEqual(to_string(m));
+        string b = nextGreaterOrEqual(to_string(m));
+
+        long long ret = LLONG_MAX;
         ret = min(ret, check(nums, stoll(a)));
         ret = min(ret, check(nums, stoll(b)));
-        
         return ret;        
     }
-    
-    string nextSmaller(const string &n) 
+
+    long long check(vector<int>& nums, long long p)
     {
-        const int m = n.length();
+        long long sum = 0;
+        for (int i=0; i<nums.size(); i++)
+            sum += abs(nums[i] - p);
+        return sum;
+    }
+
+    string nextSmallerOrEqual(string n)
+    {
+        int m = n.size();
         string s = n;
-        for (int i = 0, j = m - 1; i <= j;)
-            s[j--] = s[i++];                    
-        if (s <= n) {
-            return s;
-        }
+        for (int i=0, j=m-1; i<=j; )
+            s[j--] = s[i++];
+        if (s <= n) return s;
 
         int carry = 1;
-        for (int i = (m - 1)/2; i >= 0; i--) 
+        for (int i=(m-1)/2; i>=0; i--)
         {
-            int d = s[i]-'0'-carry;
-            if (d>=0) 
+            int d = s[i] - '0' - carry;
+            if (d>=0)
             {
-                s[i] = '0'+d;
+                s[i] = '0' + d;
                 carry = 0;
             }
             else
@@ -47,29 +51,28 @@ public:
             }
             s[m-1-i] = s[i];
         }
-        if (s[0]=='0' && m>1)
-            return string(m - 1, '9');
+
+        if (s[0] == '0' && m>1)
+            return string(m-1, '9');
         else
             return s;
     }
 
-    string nextGreater(const string &n) 
+    string nextGreaterOrEqual(string n)
     {
-        const int m = n.length();
+        int m = n.size();
         string s = n;
-        for (int i = 0, j = m - 1; i <= j;)
-            s[j--] = s[i++];                    
-        if (s >= n) {
-            return s;
-        }
+        for (int i=0, j=m-1; i<=j; )
+            s[j--] = s[i++];
+        if (s >= n) return s;
 
         int carry = 1;
-        for (int i = (m - 1)/2; i >= 0; i--) 
+        for (int i=(m-1)/2; i>=0; i--)
         {
-            int d = s[i]-'0'+carry;
-            if (d<=9) 
+            int d = s[i] - '0' + carry;
+            if (d<=9)
             {
-                s[i] = '0'+d;
+                s[i] = '0' + d;
                 carry = 0;
             }
             else
@@ -79,21 +82,14 @@ public:
             }
             s[m-1-i] = s[i];
         }
+
         if (carry == 1)
         {
-            s = string(m + 1, '0');
+            s = string(m+1, '0');
             s[0] = s.back() = '1';
             return s;
         }            
         else
             return s;
-    }
-
-    LL check(vector<int>&nums, LL x)
-    {
-        LL sum = 0;
-        for (int i=0; i<nums.size(); i++)
-            sum += abs(x - nums[i]);
-        return sum;
     }
 };
