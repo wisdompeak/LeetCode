b0548a7d746d3971366a9ff2cb4b7758b8822193
wisdompeak <guan.huifeng@gmail.com>
2023-12-20 21:06:51 -0800

    Create 2954.Count-the-Number-of-Infection-Sequences.cpp

---
+++ Math/2954.Count-the-Number-of-Infection-Sequences/2954.Count-the-Number-of-Infection-Sequences.cpp
@@ -0,0 +1,80 @@
+using LL = long long;
+class Solution {
+    LL power[100005];
+    LL fact[100005];
+    LL M = 1e9+7;
+    LL mod = 1e9+7;
+public:
+    int numberOfSequence(int n, vector<int>& sick) 
+    {
+        power[0] = 1;
+        for (int i=1; i<=n; i++)
+            power[i] = power[i-1]*2 % M;
+        
+        fact[0] = 1;
+        for (int i=1; i<=n; i++)
+            fact[i] = fact[i-1]*i % M;
+        
+        vector<int>groups;
+        for (int i=0; i<sick.size(); i++)
+        {
+            if (i==0)
+            {
+                if (sick[0]!=0)
+                    groups.push_back(sick[i]);
+            }                
+            else 
+            {
+                if (sick[i]-sick[i-1]-1>0)
+                    groups.push_back(sick[i]-sick[i-1]-1);                
+            }
+                
+        }
+        if (sick.back()!=n-1)
+            groups.push_back(n-1-sick.back());
+        
+        // for (int x: groups) cout<<x<<" "; cout<<endl;
+        
+        int total = accumulate(groups.begin(), groups.end(), 0);
+        
+        LL ret = fact[total];
+        for (int x: groups)
+        {
+            // cout<<fact[x]<<endl;
+            ret = ret * inv(fact[x]) % M;
+        }
+        
+        // cout<<ret<<endl;
+            
+        
+        for (int i=1; i<sick.size(); i++)
+        {
+            int x = sick[i]-sick[i-1]-1;
+            if (x>0)
+                ret = ret * power[x-1] % M;
+        }
+        
+        return ret;
+    }
+    
+    LL quickPow(LL x, LL y) 
+    {
+        LL ret = 1;
+        LL cur = x;
+        while (y) 
+        {
+            if (y & 1) 
+            {
+                ret = (LL)ret * cur % mod;
+            }
+            cur = (LL)cur * cur % mod;
+            y >>= 1;
+        }
+        return ret;
+    }
+
+    LL inv(LL x) 
+    {
+        return quickPow(x, mod - 2);
+    }
+};
