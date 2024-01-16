704b23c50031605ed5d503eafc94e69f097cdc0b
wisdompeak <guan.huifeng@gmail.com>
2024-01-15 16:36:28 -0800

    Create 2983.Palindrome-Rearrangement-Queries.cpp

---
+++ Greedy/2983.Palindrome-Rearrangement-Queries/2983.Palindrome-Rearrangement-Queries.cpp
@@ -0,0 +1,95 @@
+using PII = pair<int,int>;
+class Solution {
+    int diff[100005];
+    int presum1[100005][26];
+    int presum2[100005][26];
+public:
+    vector<bool> canMakePalindromeQueries(string s, vector<vector<int>>& queries) 
+    {
+        int n = s.size();        
+        string t = s.substr(n/2, n/2);
+        reverse(t.begin(), t.end());
+        int m = t.size();
+        s = s.substr(0, n/2);
+        t = "#"+t;
+        s = "#"+s;
+
+        for (int i=1; i<=m; i++)
+            diff[i] = diff[i-1] + (s[i]!=t[i]);
+
+        for (int i=1; i<=m; i++)        
+            for (int j=0; j<26; j++)
+            {
+                presum1[i][j] = presum1[i-1][j] + (s[i]=='a'+j);                
+                presum2[i][j] = presum2[i-1][j] + (t[i]=='a'+j);                
+            }        
+        
+        vector<bool>rets;
+        for (auto& query: queries)
+        {
+            int a = query[0]+1, b = query[1]+1;
+            int c = m-1-(query[3]-n/2)+1;
+            int d = m-1-(query[2]-n/2)+1;
+
+            rets.push_back(process(a,b,c,d,m));
+        }
+        return rets;
+    }
+
+    bool process(int a, int b, int c, int d, int m)
+    {
+        vector<PII>cross;
+        if (max(a,c) <= min(b,d)) cross.push_back({max(a,c), min(b,d)});
+        vector<PII>part1;
+        vector<PII>part2;
+        if (cross.size() == 0)
+        {
+            part1.push_back({a,b});
+            part2.push_back({c,d});
+        }
+        else
+        {
+            if (a<=c-1) part1.push_back({a,c-1});
+            if (d+1<=b) part1.push_back({d+1,b});
+            if (c<=a-1) part2.push_back({c,a-1});
+            if (b+1<=d) part2.push_back({b+1,d});
+        }
+
+
+        int count_diff = 0;            
+        vector<PII>Union;
+        for (auto x: cross) Union.push_back(x);            
+        for (auto x: part1) Union.push_back(x);
+        for (auto x: part2) Union.push_back(x);
+        for (auto [s, e]: Union)
+            count_diff += diff[e]-diff[s-1];
+        if (count_diff != diff[m]) return false;
+
+        vector<int>count1(26);
+        vector<int>count2(26);
+        for (int ch=0; ch<26; ch++)
+        {
+            count1[ch] = presum1[b][ch]-presum1[a-1][ch];
+            count2[ch] = presum2[d][ch]-presum2[c-1][ch];
+        }
+
+        for (int ch=0; ch<26; ch++)
+        {
+            for (auto [s,e]: part1)
+                count1[ch] -= presum2[e][ch]-presum2[s-1][ch];
+            for (auto [s,e]: part2)
+                count2[ch] -= presum1[e][ch]-presum1[s-1][ch];                
+            if (count1[ch]<0 || count2[ch]<0)
+                return false;
+            
+        }
+
+        for (int ch=0; ch<26; ch++)
+        {
+            if (count1[ch]!=count2[ch]) return false;
+        }
+
+        return true;
+    }
+    
+};
