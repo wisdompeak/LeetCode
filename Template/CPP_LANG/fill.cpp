50afd51e40f62de6284dbf490b3c9b22f87f623d
wisdompeak <guan.huifeng@gmail.com>
2024-02-19 22:48:18 -0800

    Update fill.cpp

--- Template/CPP_LANG/fill.cpp
+++ Template/CPP_LANG/fill.cpp
@@ -1,10 +1,10 @@
 main() {
       int arr[10];
       fill(arr, arr+10, 3);
-
+      
       int arr2[2][5];    
       fill(&arr2[0][0], &arr2[0][0]+10, 3);
-
+      
       vector<int>arr3(10);
       fill(arr3.begin(), arr3.end(), 3);
 }
