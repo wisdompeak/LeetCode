cbcf34d084307c1c50469184fc7bd106eb13284c
wisdompeak <guan.huifeng@gmail.com>
2024-02-19 22:48:06 -0800

    Update fill.cpp

--- Template/CPP_LANG/fill.cpp
+++ Template/CPP_LANG/fill.cpp
@@ -1,17 +1,10 @@
 main() {
-  int arr[10];
-  fill(arr, arr+10, 3);
-  for (int i=0; i<10; i++) cout<<arr[i]<<" "; cout<<endl;
+      int arr[10];
+      fill(arr, arr+10, 3);
 
-  // Underfined Behavior on LeetCode
-  int arr2[5][2];    
-  fill(arr2[0], arr2[0]+10, 3);
-  for (int i=0; i<2; i++)
-      for (int j=0; j<5; j++) 
-          cout<<arr2[i][j]<<" "; 
-  cout<<endl;
+      int arr2[2][5];    
+      fill(&arr2[0][0], &arr2[0][0]+10, 3);
 
-  vector<int>arr3(10);
-  fill(arr3.begin(), arr3.end(), 3);
-  for (int i=0; i<10; i++) cout<<arr3[i]<<" "; cout<<endl;
+      vector<int>arr3(10);
+      fill(arr3.begin(), arr3.end(), 3);
 }
