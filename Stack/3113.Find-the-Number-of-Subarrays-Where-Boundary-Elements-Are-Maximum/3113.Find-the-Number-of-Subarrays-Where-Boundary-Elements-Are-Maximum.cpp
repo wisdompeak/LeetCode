3a1ea523981225159dc8bb7c7de0021d2d09a750
wisdompeak <guan.huifeng@gmail.com>
2024-04-20 10:31:39 -0700

    Create 3113.Find-the-Number-of-Subarrays-Where-Boundary-Elements-Are-Maximum.cpp

---
+++ Stack/3113.Find-the-Number-of-Subarrays-Where-Boundary-Elements-Are-Maximum/3113.Find-the-Number-of-Subarrays-Where-Boundary-Elements-Are-Maximum.cpp
@@ -0,0 +1,29 @@
+using LL = long long;
+class Solution {
+public:
+    long long numberOfSubarrays(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<int>prevGreater(n, -1);
+        
+        stack<int>stk;
+        for (int i=0; i<n; i++)
+        {
+            while (!stk.empty() && nums[stk.top()]<=nums[i])
+                stk.pop();
+            if (!stk.empty()) prevGreater[i] = stk.top();
+            stk.push(i);
+        }
+        
+        unordered_map<int,vector<int>>Map;
+        LL ret = 0;
+        for (int i=0; i<n; i++)
+        {
+            Map[nums[i]].push_back(i);
+            auto iter = lower_bound(Map[nums[i]].begin(), Map[nums[i]].end(), prevGreater[i]);                        
+            ret += Map[nums[i]].end() - iter;
+        }
+        
+        return ret;        
+    }
+};
