157c95cdc3aec941041cfb9c8271031a54e8f57f
wisdompeak <guan.huifeng@gmail.com>
2024-04-21 10:01:18 -0700

    Update 3123.Find-Edges-in-Shortest-Paths.cpp

--- Graph/3123.Find-Edges-in-Shortest-Paths/3123.Find-Edges-in-Shortest-Paths.cpp
+++ Graph/3123.Find-Edges-in-Shortest-Paths/3123.Find-Edges-in-Shortest-Paths.cpp
@@ -1,60 +1,63 @@
 using PII = pair<int,int>;
 
 class Solution {
-    vector<array<int,3>>next[50005];
+    vector<PII>next[50005];
 public:
     vector<bool> findAnswer(int n, vector<vector<int>>& edges) 
     {
         int m = edges.size();
         vector<bool>rets(m, false);
         
-        for (int i=0; i<m; i++)
+        for (auto& edge : edges)
         {
-            auto& edge = edges[i];
             int a = edge[0], b = edge[1], w = edge[2];
-            next[a].push_back({b,w, i});
-            next[b].push_back({a,w, i});
+            next[a].push_back({b,w});
+            next[b].push_back({a,w});
         }
         
         priority_queue<PII, vector<PII>, greater<>>pq;
         
         pq.push({0, 0});        
-        vector<int>d1(n, INT_MAX/2);
+        vector<int>d1(n, INT_MAX/3);
         while (!pq.empty()) 
         {
             auto [dist, cur] = pq.top();
             pq.pop();
-            if (d1[cur]!= INT_MAX/2) continue;
+            if (d1[cur]!= INT_MAX/3) continue;
             d1[cur] = dist;                
 
-            for (auto [nxt, len, idx]: next[cur])
+            for (auto [nxt, len]: next[cur])
             {
-                if (d1[nxt]!= INT_MAX/2) continue;                
+                if (d1[nxt]!= INT_MAX/3) continue;                
                 pq.push({dist + len, nxt});
             }
         }
         
         while (!pq.empty()) pq.pop();        
         pq.push({0, n-1});        
-        vector<int>d2(n, INT_MAX/2);
+        vector<int>d2(n, INT_MAX/3);
         while (!pq.empty()) 
         {
             auto [dist, cur] = pq.top();
             pq.pop();
-            if (d2[cur]!= INT_MAX/2) continue;
+            if (d2[cur]!= INT_MAX/3) continue;
             d2[cur] = dist;
 
-            for (auto [nxt, len, idx]: next[cur])
+            for (auto [nxt, len]: next[cur])
             {
-                if (d2[nxt]!= INT_MAX/2) continue;                
+                if (d2[nxt]!= INT_MAX/3) continue;                
                 pq.push({dist + len, nxt});
-                
-                if (dist+len + d1[nxt] == d1[n-1])
-                    rets[idx] = true;
             }
         }
+
+        for (int i=0; i<m; i++)
+        {
+            auto& edge = edges[i];
+            int a = edge[0], b = edge[1], w = edge[2];
+            if (d1[a]+w+d2[b]==d1[n-1] || d1[b]+w+d2[a]==d1[n-1])
+                rets[i] = true;
+        }
                 
-        return rets;
-        
+        return rets;        
     }
 };
