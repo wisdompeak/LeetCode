cbb096314d8b9f6747f01c9b119aacd7d8c4229f
wisdompeak <guan.huifeng@gmail.com>
2024-05-04 16:55:01 -0700

    Update 3116.Kth-Smallest-Amount-With-Single-Denomination-Combination.cpp

--- Bit_Manipulation/3116.Kth-Smallest-Amount-With-Single-Denomination-Combination/3116.Kth-Smallest-Amount-With-Single-Denomination-Combination.cpp
+++ Bit_Manipulation/3116.Kth-Smallest-Amount-With-Single-Denomination-Combination/3116.Kth-Smallest-Amount-With-Single-Denomination-Combination.cpp
@@ -7,45 +7,44 @@ public:
         while (left < right)
         {
             LL mid = left+(right-left)/2;
-            if (isOK(coins, mid, k))
+            if (countNumber(mid, coins) >= k)
                 right = mid;
             else
                 left = mid+1;
         }
         return left;
     }
-    
-    bool isOK(vector<int>& coins, LL A, int t)
+
+    LL countNumber(LL M, vector<int>& coins)
     {
         int m = coins.size();
-        
-        int sign = 1;
+
         LL ret = 0;
-        
+        int sign = 1;
+
         for (int k=1; k<=m; k++)
         {
             LL sum = 0;
             int state = (1 << k) - 1;            
             while (state < (1 << m))
             {
-                LL product = 1;
+                LL LCM = 1;
                 for (int i=0; i<m; i++)
                 {
                     if ((state>>i)&1)
-                        product = product * coins[i] / gcd(product, coins[i]);
+                        LCM = lcm(LCM, coins[i]);
                 }
-                sum += A / product;                
+                sum += M / LCM;
 
                 int c = state & - state;
                 int r = state + c;
                 state = (((r ^ state) >> 2) / c) | r;
             }
-            
+
             ret += sum * sign;
             sign *= -1;
         }
-        
-        return ret >= t;
-        
+
+        return ret;
     }
 };
