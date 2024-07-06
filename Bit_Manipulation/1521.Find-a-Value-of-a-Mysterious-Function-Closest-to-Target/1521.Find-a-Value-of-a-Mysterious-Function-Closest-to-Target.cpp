d0a44d0f243f0b5292f43a7b563fe34dccd104c9
wisdompeak <guan.huifeng@gmail.com>
2024-07-06 15:45:22 -0700

    Update 1521.Find-a-Value-of-a-Mysterious-Function-Closest-to-Target.cpp

--- Bit_Manipulation/1521.Find-a-Value-of-a-Mysterious-Function-Closest-to-Target/1521.Find-a-Value-of-a-Mysterious-Function-Closest-to-Target.cpp
+++ Bit_Manipulation/1521.Find-a-Value-of-a-Mysterious-Function-Closest-to-Target/1521.Find-a-Value-of-a-Mysterious-Function-Closest-to-Target.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int closestToTarget(vector<int>& arr, int target) {
-        unordered_set<int>s;
+    int closestToTarget(vector<int>& arr, int target) 
+    {
+        set<int>Set, temp;
         int ret = INT_MAX;
-        for (int i=0; i<arr.size(); i++)
+        for (int x: arr)
         {
-            unordered_set<int>s2;
-            for (auto x: s)
-                s2.insert(x&arr[i]);
-            s2.insert(arr[i]);
-            for (auto x: s2)
-                ret = min(ret, abs(x-target));
-            s = s2;
+            for (auto y: Set)
+                temp.insert(y&x);
+            temp.insert(x);
+
+            for (int y: temp)
+                ret = min(ret, abs(y-target));
+            
+            Set = temp;
+            temp.clear();
         }
+
         return ret;
     }
 };
