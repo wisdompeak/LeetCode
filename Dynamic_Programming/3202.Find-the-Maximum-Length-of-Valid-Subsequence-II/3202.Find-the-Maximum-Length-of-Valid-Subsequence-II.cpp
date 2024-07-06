a28fd1fd29f3fce7d18f6e7cbfefebcfbfc3a81e
wisdompeak <guan.huifeng@gmail.com>
2024-07-05 17:07:19 -0700

    Update 3202.Find-the-Maximum-Length-of-Valid-Subsequence-II.cpp

--- Dynamic_Programming/3202.Find-the-Maximum-Length-of-Valid-Subsequence-II/3202.Find-the-Maximum-Length-of-Valid-Subsequence-II.cpp
+++ Dynamic_Programming/3202.Find-the-Maximum-Length-of-Valid-Subsequence-II/3202.Find-the-Maximum-Length-of-Valid-Subsequence-II.cpp
@@ -4,24 +4,28 @@ public:
     int maximumLength(vector<int>& nums, int k) 
     {
         vector<int>last(k, -1);
-        
         int ret = 0;
+
         int n = nums.size();
         for (int i=0; i<n; i++)
         {
             for (int r=0; r<k; r++)
-            {                
-                int d = (r - nums[i]%k + k) % k;                      
-                if (last[d]!=-1)
+            {
+                int d = (r - nums[i]%k + k) % k;
+                if (last[d] != -1) 
                 {
                     int j = last[d];
-                    dp[i][r] = (dp[j][r]==0)?2:(dp[j][r]+1);
-                }                
+                    dp[i][r] = dp[j][r] + 1;
+                } 
+                else
+                {
+                    dp[i][r] = 1;
+                }
                 ret = max(ret, dp[i][r]);
             }
-            
             last[nums[i]%k] = i;
         }
+
         return ret;
     }
 };
