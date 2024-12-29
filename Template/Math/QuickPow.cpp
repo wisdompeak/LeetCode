24826e3954d3eec1229e8edcd9276544fe369aad
wisdompeak <guan.huifeng@gmail.com>
2024-12-28 20:03:03 -0800

    Update QuickPow.cpp

--- Template/Math/QuickPow.cpp
+++ Template/Math/QuickPow.cpp
@@ -1,12 +1,12 @@
 class Solution {
-long long M = 1e9+7;
+long long MOD = 1e9+7;
 public:
     long long quickMul(long long x, long long N) {
         if (N == 0) {
             return 1;
         }
-        LL y = quickMul(x, N / 2) % M;
-        return N % 2 == 0 ? (y * y % M) : (y * y % M * x % M);
+        LL y = quickMul(x, N / 2) % MOD;
+        return N % 2 == 0 ? (y * y % MOD) : (y * y % MOD * x % MOD);
     }
 
     double quickMul(double x, long long N) {
@@ -17,8 +17,8 @@ public:
         return N % 2 == 0 ? y * y : y * y * x;
     }
 
-    double myPow(double x, int n) {
-        long long N = n;
-        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    // n can be negative.
+    double myPow(double x, int n) {        
+        return n >= 0 ? quickMul(x, n) : 1.0 / quickMul(x, -n);
     }
 };
