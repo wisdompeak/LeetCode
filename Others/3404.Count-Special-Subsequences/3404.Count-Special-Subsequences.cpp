57522d8a5c2f307ee5976ac42526bc74dffe526c
wisdompeak <guan.huifeng@gmail.com>
2025-01-01 22:15:07 -0800

    Update 3404.Count-Special-Subsequences.cpp

--- Others/3404.Count-Special-Subsequences/3404.Count-Special-Subsequences.cpp
+++ Others/3404.Count-Special-Subsequences/3404.Count-Special-Subsequences.cpp
@@ -1,31 +1,32 @@
-using LL = long long;
 class Solution {
 public:
-    LL getKey(int x, int y)
+    int getKey(int x, int y)
     {
         int g = gcd(x,y);
         x = x/g;
         y = y/g;
-        return (LL)x*1000+y;
+        return x*1000+y;
     }
+
     long long numberOfSubsequences(vector<int>& nums) 
     {
-        unordered_map<LL, vector<int>>Map;
+        unordered_map<int, vector<int>>Map;
         int n = nums.size();
-        for (int i=0; i<n; i++)
-            for (int j=i+2; j<n; j++)            
-                Map[getKey(nums[i], nums[j])].push_back(j);        
-        
-        for (auto& [k,v]:Map)
+        for (int i=0; i<n; i++) 
+            for (int j=i+2; j<n; j++)
+                Map[getKey(nums[i], nums[j])].push_back(i);
+
+        for (auto& [k,v]: Map)
             sort(v.begin(), v.end());
-        
-        LL ret = 0;
-        for (int r=2; r<n; r++)
-            for (int s=r+2; s<n; s++)
+
+        long long ret = 0;
+        for (int p=0; p<n; p++)
+            for (int q=p+2; q<n; q++)
             {
-                int key = getKey(nums[s], nums[r]);                
-                auto iter = lower_bound(Map[key].begin(), Map[key].end(), r-1);
-                ret += iter - Map[key].begin();
+                int key = getKey(nums[q], nums[p]);
+                if (Map.find(key)==Map.end()) continue;
+                auto iter = lower_bound(Map[key].begin(), Map[key].end(), q+2);
+                ret += Map[key].end()- iter;
             }
         return ret;
     }
