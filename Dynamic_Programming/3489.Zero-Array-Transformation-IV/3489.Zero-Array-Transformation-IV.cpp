f938d2966291a612e078a28716c81102afc5d5b0
wisdompeak <guan.huifeng@gmail.com>
2025-03-17 01:28:19 -0700

    Update 3489.Zero-Array-Transformation-IV.cpp

--- Dynamic_Programming/3489.Zero-Array-Transformation-IV/3489.Zero-Array-Transformation-IV.cpp
+++ Dynamic_Programming/3489.Zero-Array-Transformation-IV/3489.Zero-Array-Transformation-IV.cpp
@@ -1,38 +1,34 @@
 class Solution {
-    int dp[10][1005];
+    int dp[10][1001];
 public:
     bool isOK(vector<int>& nums)
     {
-        int flag = 1;
         for (int i=0; i<nums.size(); i++)
         {
-            if (dp[i][nums[i]]==false)
-            {
-                flag = 0;
-                break;
-            }
+            if (dp[i][nums[i]] == false)
+                return false;
         }
-        return flag;
+        return true;
     }
-    
+
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) 
     {
         for (int i=0; i<nums.size(); i++)
             dp[i][0] = true;
         
         if (isOK(nums)) return 0;
-        
+
         for (int k=0; k<queries.size(); k++)
         {
             int a = queries[k][0], b = queries[k][1], d = queries[k][2];
             for (int i=a; i<=b; i++)
             {
                 for (int v=1000; v>=0; v--)
-                    if (v>=d) dp[i][v] = (dp[i][v] || dp[i][v-d]);                
-            }            
-            
+                    if (v>=d && dp[i][v-d] == true)
+                        dp[i][v] = true;
+            }
             if (isOK(nums)) return k+1;
         }
-        return -1;
+        return -1;        
     }
 };
