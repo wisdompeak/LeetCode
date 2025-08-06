d985bd606f4cc91c1dade9b257514bd3f04a4af1
wisdompeak <guan.huifeng@gmail.com>
2025-08-06 01:57:04 -0700

    Create 3641.Longest-Semi-Repeating-Subarray.cpp

---
+++ Two_Pointers/3641.Longest-Semi-Repeating-Subarray/3641.Longest-Semi-Repeating-Subarray.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int k) {
+        unordered_map<int,int>Map;
+        int count = 0;
+        int n = nums.size();
+        int j = -1;
+        int ret = 0;
+        for (int i=0; i<n; i++) {
+            while (j+1<n && count<=k) {                
+                j++;
+                Map[nums[j]]++;
+                if (Map[nums[j]]==2)
+                    count++;                
+            }       
+            if (count>k)     
+                ret = max(ret, j - i);
+            else
+                ret = max(ret, j - i + 1);
+            Map[nums[i]]--;
+            if (Map[nums[i]]==1) count--;
+        }
+        return ret;
+        
+    }
+};
