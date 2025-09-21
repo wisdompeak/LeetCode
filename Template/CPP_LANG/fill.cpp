a05319ecbd994eaf5af2eb53de84fa57bdf700c3
wisdompeak <guan.huifeng@gmail.com>
2025-09-21 11:58:54 -0700

    Update fill.cpp

--- Template/CPP_LANG/fill.cpp
+++ Template/CPP_LANG/fill.cpp
@@ -7,4 +7,7 @@ main() {
       
       vector<int>arr3(10);
       fill(arr3.begin(), arr3.end(), 3);
+
+      int low[n+10][K+10];    memset(low, 0x3f, sizeof(low));  // set 0x3f3f3f3f for all. This is a big positive number
+      int upp[n+10][K+10];    memset(upp, 0xcf, sizeof(upp));  // set 0xcfcfcfcf for all. This is a big negative number.
 }
