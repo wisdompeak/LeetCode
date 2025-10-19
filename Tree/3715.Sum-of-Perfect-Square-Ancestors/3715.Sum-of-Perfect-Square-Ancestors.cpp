5e34b384142abe2a0afd4b75c4b5eb48b39beeaa
wisdompeak <guan.huifeng@gmail.com>
2025-10-19 11:21:57 -0700

    Update 3715.Sum-of-Perfect-Square-Ancestors.cpp

--- Tree/3715.Sum-of-Perfect-Square-Ancestors/3715.Sum-of-Perfect-Square-Ancestors.cpp
+++ Tree/3715.Sum-of-Perfect-Square-Ancestors/3715.Sum-of-Perfect-Square-Ancestors.cpp
@@ -17,7 +17,7 @@ public:
     vector<int>ComputeSpf(int N) {
         vector<int>spf(N+1);
         for (int i = 0; i <= N; ++i) spf[i] = i;
-        for (int i = 2; i * i <= maxA; ++i) {
+        for (int i = 2; i * i <= N; ++i) {
             if (spf[i] == i) {
                 for (int j = i * i; j <= N; j += i) {
                     if (spf[j] == j) spf[j] = i;
