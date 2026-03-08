83b943f5d1cb2402a94a77acb0d44134211adca1
wisdompeak <guan.huifeng@gmail.com>
2026-03-08 04:07:23 -0700

    Update 3864.Minimum-Cost-to-Partition-a-Binary-String.cpp

--- DFS/3864.Minimum-Cost-to-Partition-a-Binary-String/3864.Minimum-Cost-to-Partition-a-Binary-String.cpp
+++ DFS/3864.Minimum-Cost-to-Partition-a-Binary-String/3864.Minimum-Cost-to-Partition-a-Binary-String.cpp
@@ -1,6 +1,5 @@
 using ll = long long;
 class Solution {
-    ll dp[100005][18];    
 public:    
     long long minCost(string s, int encCost, int flatCost) {
         int n = s.size();
@@ -14,28 +13,13 @@ public:
             if (X==0) return (long long)flatCost;
             else return 1LL*L*X*encCost;
         };
-
-        for (int i=0; i<n; i++)
-            dp[i][0] = cost(i,i);
-
-        for (int t=1; (1<<t)<=n; t++) 
-            for (int i=0; i+(1<<t)-1<n; i++) {                
-                int len = (1<<t);
-                int j = i+len-1;                
-                dp[i][t] = min(cost(i,j), dp[i][t-1]+dp[i+len/2][t-1]);
-            }
         
         std::function<ll(int,int)> dfs = [&](int l, int r) -> ll {
             int len = r-l+1;
             if (len<=0) return 0;
             if (len%2==1) return cost(l,r);
 
-            if (len > 0 && (len & (len - 1)) == 0) {
-                int t = log(len)/log(2);
-                return dp[l][t];
-            }
-            else 
-                return min(cost(l,r), dfs(l, l+len/2-1) + dfs(l+len/2, r));
+            return min(cost(l,r), dfs(l, l+len/2-1) + dfs(l+len/2, r));
         };
                 
         return dfs(0, n-1);
